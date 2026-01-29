b5933d1a4cde68fdecd05410c33520646a6210fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:33 +0000

    Add package types-aiobotocore-mwaa/3.1.1/types_aiobotocore_mwaa-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-mwaa/types_aiobotocore_mwaa-3.1.1-py3-none-any.whl/types_aiobotocore_mwaa/client.py
@@ -0,0 +1,254 @@
+"""
+Type annotations for mwaa service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_mwaa.client import MWAAClient
+
+    session = get_session()
+    async with session.create_client("mwaa") as client:
+        client: MWAAClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import ListEnvironmentsPaginator
+from .type_defs import (
+    CreateCliTokenRequestTypeDef,
+    CreateCliTokenResponseTypeDef,
+    CreateEnvironmentInputTypeDef,
+    CreateEnvironmentOutputTypeDef,
+    CreateWebLoginTokenRequestTypeDef,
+    CreateWebLoginTokenResponseTypeDef,
+    DeleteEnvironmentInputTypeDef,
+    GetEnvironmentInputTypeDef,
+    GetEnvironmentOutputTypeDef,
+    InvokeRestApiRequestTypeDef,
+    InvokeRestApiResponseTypeDef,
+    ListEnvironmentsInputTypeDef,
+    ListEnvironmentsOutputTypeDef,
+    ListTagsForResourceInputTypeDef,
+    ListTagsForResourceOutputTypeDef,
+    PublishMetricsInputTypeDef,
+    TagResourceInputTypeDef,
+    UntagResourceInputTypeDef,
+    UpdateEnvironmentInputTypeDef,
+    UpdateEnvironmentOutputTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Self, Unpack
+else:
+    from typing_extensions import Literal, Self, Unpack
+
+
+__all__ = ("MWAAClient",)
+
+
+class Exceptions(BaseClientExceptions):
+    AccessDeniedException: type[BotocoreClientError]
+    ClientError: type[BotocoreClientError]
+    InternalServerException: type[BotocoreClientError]
+    ResourceNotFoundException: type[BotocoreClientError]
+    RestApiClientException: type[BotocoreClientError]
+    RestApiServerException: type[BotocoreClientError]
+    ValidationException: type[BotocoreClientError]
+
+
+class MWAAClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa.html#MWAA.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        MWAAClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa.html#MWAA.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#generate_presigned_url)
+        """
+
+    async def create_cli_token(
+        self, **kwargs: Unpack[CreateCliTokenRequestTypeDef]
+    ) -> CreateCliTokenResponseTypeDef:
+        """
+        Creates a CLI token for the Airflow CLI.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/create_cli_token.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#create_cli_token)
+        """
+
+    async def create_environment(
+        self, **kwargs: Unpack[CreateEnvironmentInputTypeDef]
+    ) -> CreateEnvironmentOutputTypeDef:
+        """
+        Creates an Amazon Managed Workflows for Apache Airflow (Amazon MWAA)
+        environment.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/create_environment.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#create_environment)
+        """
+
+    async def create_web_login_token(
+        self, **kwargs: Unpack[CreateWebLoginTokenRequestTypeDef]
+    ) -> CreateWebLoginTokenResponseTypeDef:
+        """
+        Creates a web login token for the Airflow Web UI.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/create_web_login_token.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#create_web_login_token)
+        """
+
+    async def delete_environment(
+        self, **kwargs: Unpack[DeleteEnvironmentInputTypeDef]
+    ) -> dict[str, Any]:
+        """
+        Deletes an Amazon Managed Workflows for Apache Airflow (Amazon MWAA)
+        environment.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/delete_environment.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#delete_environment)
+        """
+
+    async def get_environment(
+        self, **kwargs: Unpack[GetEnvironmentInputTypeDef]
+    ) -> GetEnvironmentOutputTypeDef:
+        """
+        Describes an Amazon Managed Workflows for Apache Airflow (MWAA) environment.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/get_environment.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#get_environment)
+        """
+
+    async def invoke_rest_api(
+        self, **kwargs: Unpack[InvokeRestApiRequestTypeDef]
+    ) -> InvokeRestApiResponseTypeDef:
+        """
+        Invokes the Apache Airflow REST API on the webserver with the specified inputs.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/invoke_rest_api.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#invoke_rest_api)
+        """
+
+    async def list_environments(
+        self, **kwargs: Unpack[ListEnvironmentsInputTypeDef]
+    ) -> ListEnvironmentsOutputTypeDef:
+        """
+        Lists the Amazon Managed Workflows for Apache Airflow (MWAA) environments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/list_environments.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#list_environments)
+        """
+
+    async def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceInputTypeDef]
+    ) -> ListTagsForResourceOutputTypeDef:
+        """
+        Lists the key-value tag pairs associated to the Amazon Managed Workflows for
+        Apache Airflow (MWAA) environment.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/list_tags_for_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#list_tags_for_resource)
+        """
+
+    async def publish_metrics(self, **kwargs: Unpack[PublishMetricsInputTypeDef]) -> dict[str, Any]:
+        """
+        <b>Internal only</b>.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/publish_metrics.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#publish_metrics)
+        """
+
+    async def tag_resource(self, **kwargs: Unpack[TagResourceInputTypeDef]) -> dict[str, Any]:
+        """
+        Associates key-value tag pairs to your Amazon Managed Workflows for Apache
+        Airflow (MWAA) environment.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/tag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#tag_resource)
+        """
+
+    async def untag_resource(self, **kwargs: Unpack[UntagResourceInputTypeDef]) -> dict[str, Any]:
+        """
+        Removes key-value tag pairs associated to your Amazon Managed Workflows for
+        Apache Airflow (MWAA) environment.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/untag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#untag_resource)
+        """
+
+    async def update_environment(
+        self, **kwargs: Unpack[UpdateEnvironmentInputTypeDef]
+    ) -> UpdateEnvironmentOutputTypeDef:
+        """
+        Updates an Amazon Managed Workflows for Apache Airflow (MWAA) environment.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/update_environment.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#update_environment)
+        """
+
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_environments"]
+    ) -> ListEnvironmentsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa.html#MWAA.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mwaa.html#MWAA.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/client/)
+        """
