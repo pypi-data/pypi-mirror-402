aee5c1dbf201773a70f3dba7d01fc46b27a8fe59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 12:08:15 +0000

    Add package philoch-bib-sdk/0.4.0/philoch_bib_sdk-0.4.0-cp313-cp313-macosx_10_12_x86_64.whl

---
+++ packages/philoch-bib-sdk/philoch_bib_sdk-0.4.0-cp313-cp313-macosx_10_12_x86_64.whl/philoch_bib_sdk/converters/latex.py
@@ -0,0 +1,6 @@
+def unicode_to_latex(unicode_str: str) -> str:
+    raise NotImplementedError("This function is not implemented yet.")
+
+
+def latex_to_unicode(latex_str: str) -> str:
+    raise NotImplementedError("This function is not implemented yet.")
