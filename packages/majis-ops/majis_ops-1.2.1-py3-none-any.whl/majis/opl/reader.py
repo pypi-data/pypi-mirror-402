7c8030ea506845e965761ab65b09c4f850ea060c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:12:39 +0000

    Add package majis-ops/1.2.1/majis_ops-1.2.1-py3-none-any.whl

---
+++ packages/majis-ops/majis_ops-1.2.1-py3-none-any.whl/majis/opl/reader.py
@@ -0,0 +1,78 @@
+"""OPL generic reader module."""
+
+import csv
+from pathlib import Path
+
+from planetary_coverage.events import EventsDict, EventsList, EventWindow
+
+from ..itl.reader import read_itl_json
+from ..misc.events import flatten, group
+
+
+def read_opl(fname: str | Path, flat: bool = False) -> EventsDict | EventsList:
+    """Read OPL file.
+
+    Both, CSV (``.csv``) and JSON (``.json``) format are supported.
+
+    In the case of
+
+    The result can be return as a nested dict of events (default) or
+    as a flat list of events.
+
+    """
+    f = Path(fname)
+
+    match f.suffix.lower():
+        case '.csv':
+            events = read_opl_csv(f)
+        case '.json':
+            events = read_opl_json(f)
+        case _:
+            raise ValueError(f'Invalid OPL file input: {fname}')
+
+    return flatten(events) if flat else group(events)
+
+
+def read_opl_csv(fname: str | Path) -> list[EventWindow]:
+    """Read OPL file in CSV format."""
+    with Path(fname).open(encoding='utf-8') as content:
+        return [EventWindow(**parse_opl_csv(*params)) for params in csv.reader(content)]
+
+
+def parse_opl_csv(key, start, end, obs_key, instrument) -> dict:
+    """OPL CSV line parser.
+
+    Warning
+    -------
+    Currently the OPL CSV don't have an explicit header.
+    Here we assume that the header is as follow"
+
+        # key, start, end, obs_key, instrument
+
+    The initial ``key`` is composed of the following parameters:
+
+        <instrument>_<observation_type>_<type>
+
+    """
+    _, observation_type, _type = key.split('_')
+
+    return {
+        'key': key,
+        't_start': start,
+        't_end': end,
+        'OBS_KEY': obs_key,
+        'INSTRUMENT': instrument,
+        'OBSERVATION_TYPE': observation_type,
+        'TYPE': _type,
+    }
+
+
+def read_opl_json(fname: str | Path) -> list[EventWindow]:
+    """Read OPL file in JSON format.
+
+    Note
+    ----
+    Re-use ITL JSON reader with OPL schema instead.
+
+    """
+    return read_itl_json(fname, fmt='OPL')
