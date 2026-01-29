5b29b65128e9a7ee083c924bbfc640a79c10132e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:15:09 +0000

    Add package file2txt/1.0.10/file2txt-1.0.10-py3-none-any.whl

---
+++ packages/file2txt/file2txt-1.0.10-py3-none-any.whl/file2txt/parsers/csv_file_parser.py
@@ -0,0 +1,19 @@
+from .core import BaseParser, custom_parser
+import pandas as pd
+
+
+@custom_parser("csv", ["csv"])
+class CsvFileParser(BaseParser):
+    """
+    Subclass of FileParser specifically for parsing CSV files.
+    Inherits all methods from the parent class without modifications.
+    """
+
+    def extract_text(self) -> list[str]:
+        """
+        Extracts and returns the text content from the file.
+        """
+
+        with open(self.temp_dir / "output.md", "w") as f:
+            pd.read_csv(self.file_path).to_markdown(f)
+        return [(self.temp_dir / "output.md").read_text()]
---
+++ packages/file2txt/file2txt-1.0.10-py3-none-any.whl/file2txt/parsers/excel_file_parser.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from .core import BaseParser, custom_parser
+
+
+@custom_parser("excel", ["xls", "xlsx"])
+class ExcelFileParser(BaseParser):
+    """
+    Subclass of FileParser specifically for parsing Excel files.
+    Overrides load_file to load the workbook and extract_text to extract workbook's sheets.
+    """
+
+    def extract_text(self) -> list[str]:
+        """
+        Extracts and returns the text content from the file.
+        """
+
+        with open(self.temp_dir / "output.md", "w") as f:
+            pd.read_excel(self.file_path).to_markdown(f)
+        return [(self.temp_dir / "output.md").read_text()]
