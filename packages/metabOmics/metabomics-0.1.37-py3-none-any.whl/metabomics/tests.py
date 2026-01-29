8fef1067982afb7414b14378646e62c1d7ee939b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:10:02 +0000

    Add package metabOmics/0.1.37/metabomics-0.1.37-py3-none-any.whl

---
+++ packages/metabOmics/metabomics-0.1.37-py3-none-any.whl/metabomics/tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+from analysis.tests import *
+from extensions.tests import *
+from preprocessing.tests import *
+from utils.tests import *
+
+if __name__ == '__main__':
+    unittest.main()
