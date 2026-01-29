409f05ec57bb59267e435b24d44228379f3a6969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:29 +0000

    Add package wagtail-sitemap-seo/1.4.10/wagtail_sitemap_seo-1.4.10-py3-none-any.whl

---
+++ packages/wagtail-sitemap-seo/wagtail_sitemap_seo-1.4.10-py3-none-any.whl/wagtail_sitemap_seo/sub_map_builder.py
@@ -0,0 +1,60 @@
+from io import BytesIO
+from .root_builder import RootBuilder
+from wagtail.models import Locale
+from datetime import datetime
+
+import xml.etree.cElementTree as ET
+
+from django.conf import settings
+
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+
+def save_xml(name: str, content: bytes) -> str:
+    """
+    Save an XML file using Django's default storage.
+
+    - In dev (SITEMAP_WRITE_S3): saves to MEDIA_ROOT
+    - In ECS/UAT (SITEMAP_WRITE_S3=True): saves to S3 bucket root (because your default storage is S3Boto3Storage)
+
+    Returns the final storage path.
+    """
+    return default_storage.save(name, ContentFile(content))
+
+
+class MapBuilder(RootBuilder):
+
+    def __init__(self, root_file):
+        super().__init__(root_file)
+
+    def build_map(self, page):
+        locale = Locale.objects.get(language_code='en')
+        pages = page.get_descendants(inclusive=True).live().filter(locale=locale)
+
+        new_map = self.site_map_init()
+        for p in pages:
+            elem = self.build_url_elem(p)
+            new_map.append(elem)
+        title = page.title.replace(' ', '').lower()
+        tree = ET.ElementTree(new_map)
+        if settings.SITEMAP_WRITE_S3:
+            buffer = BytesIO()
+            tree.write(buffer, encoding='utf-8', xml_declaration=True)
+            content = buffer.getvalue()
+            save_xml('map_{}.xml'.format(title), content)
+        else:
+            tree.write('map_{}.xml'.format(title), encoding='utf-8', xml_declaration=True)
+
+    def build_root_elem(self, url):
+        sitemap_elem = ET.Element('sitemap')
+        loc_elem = ET.Element('loc')
+        published_elem = ET.Element('lastmod')
+        print(url)
+
+        # TODO: perhaps load path from settings?
+        loc_elem.text = 'https://' + self.get_site() + '/sitemap/map_{}/'.format(url.title.replace(' ', '').lower())
+        published_elem.text = self._format_date(datetime.today())
+
+        sitemap_elem.append(loc_elem)
+        sitemap_elem.append(published_elem)
+        return sitemap_elem
