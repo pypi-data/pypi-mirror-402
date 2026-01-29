10e920923b3e8f2ff6c28a21bd21353e9c7cf8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:11:06 +0000

    Add package ocf-data-sampler/1.0.9/ocf_data_sampler-1.0.9-py3-none-any.whl

---
+++ packages/ocf-data-sampler/ocf_data_sampler-1.0.9-py3-none-any.whl/ocf_data_sampler/load/satellite.py
@@ -0,0 +1,57 @@
+"""Satellite loader."""
+import numpy as np
+import xarray as xr
+
+from ocf_data_sampler.load.open_xarray_tensorstore import open_zarr, open_zarrs
+from ocf_data_sampler.load.utils import (
+    check_time_unique_increasing,
+    get_xr_data_array_from_xr_dataset,
+    make_spatial_coords_increasing,
+)
+
+
+def open_sat_data(zarr_path: str | list[str]) -> xr.DataArray:
+    """Lazily opens the zarr store and validates data types.
+
+    Args:
+      zarr_path: Cloud URL or local path pattern, or list of these. If GCS URL,
+                 it must start with 'gs://'
+    """
+    # Open the data
+    if isinstance(zarr_path, list | tuple):
+        ds = open_zarrs(zarr_path, concat_dim="time", data_source="satellite")
+    else:
+        ds = open_zarr(zarr_path)
+
+    check_time_unique_increasing(ds.time)
+
+    ds = ds.rename(
+        {
+            "variable": "channel",
+            "time": "time_utc",
+        },
+    )
+
+    check_time_unique_increasing(ds.time_utc)
+    ds = make_spatial_coords_increasing(ds, x_coord="x_geostationary", y_coord="y_geostationary")
+    ds = ds.transpose("time_utc", "channel", "x_geostationary", "y_geostationary")
+
+    data_array = get_xr_data_array_from_xr_dataset(ds)
+
+    # Validate data types directly loading function
+    if not np.issubdtype(data_array.dtype, np.number):
+        raise TypeError(f"Satellite data should be numeric, not {data_array.dtype}")
+
+    coord_dtypes = {
+        "time_utc": np.datetime64,
+        "channel": np.str_,
+        "x_geostationary": np.floating,
+        "y_geostationary": np.floating,
+    }
+
+    for coord, expected_dtype in coord_dtypes.items():
+        if not np.issubdtype(data_array.coords[coord].dtype, expected_dtype):
+            dtype = data_array.coords[coord].dtype
+            raise TypeError(f"{coord} should be {expected_dtype.__name__}, not {dtype}")
+
+    return data_array
---
+++ packages/ocf-data-sampler/ocf_data_sampler-1.0.9-py3-none-any.whl/ocf_data_sampler/torch_datasets/utils/merge_and_fill_utils.py
@@ -0,0 +1,47 @@
+"""Utility functions for merging dictionaries and filling NaNs in arrays."""
+
+import numpy as np
+
+from ocf_data_sampler.config.model import Configuration
+from ocf_data_sampler.numpy_sample.generation import GenerationSampleKey
+from ocf_data_sampler.numpy_sample.nwp import NWPSampleKey
+from ocf_data_sampler.numpy_sample.satellite import SatelliteSampleKey
+
+
+def merge_dicts(list_of_dicts: list[dict]) -> dict:
+    """Merge a list of dictionaries into a single dictionary."""
+    # TODO: This doesn't account for duplicate keys, which will be overwritten
+    combined_dict = {}
+    for d in list_of_dicts:
+        combined_dict.update(d)
+    return combined_dict
+
+
+def fill_nans_in_arrays(
+    sample: dict, config: Configuration | None = None, nwp_provider: str | None = None,
+) -> dict:
+    """Fills all NaN values in each np.ndarray in the sample dictionary.
+
+    Operation is performed in-place on the sample.
+    By default a fill value of 0.0 are used, but if a config is provided,
+    it can use the configured dropout values.
+    """
+    for k, v in sample.items():
+        if isinstance(v, np.ndarray) and np.issubdtype(v.dtype, np.number):
+            if np.isnan(v).any():
+                fill_value = 0.0
+                if config is not None:
+                    if k == GenerationSampleKey.generation:
+                        fill_value = config.input_data.generation.dropout_value
+                    elif k == SatelliteSampleKey.satellite_actual:
+                        fill_value = config.input_data.satellite.dropout_value
+                    elif k == NWPSampleKey.nwp and nwp_provider in config.input_data.nwp:
+                        fill_value = config.input_data.nwp[nwp_provider].dropout_value
+
+                sample[k] = np.nan_to_num(v, copy=False, nan=fill_value)
+
+        # Recursion is included to reach NWP arrays in subdict
+        elif isinstance(v, dict):
+            fill_nans_in_arrays(v, config=config, nwp_provider=k)
+
+    return sample
