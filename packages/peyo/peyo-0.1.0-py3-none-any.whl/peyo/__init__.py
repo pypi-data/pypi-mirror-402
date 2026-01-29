2121ca49cb85c2b222d2f480fd2720abcf75835c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:10:57 +0000

    Add package peyo/0.1.0/peyo-0.1.0-py3-none-any.whl

---
+++ packages/peyo/peyo-0.1.0-py3-none-any.whl/peyo/__init__.py
@@ -0,0 +1,12 @@
+from .dictionary import Dictionary
+from .eyo import Eyo
+from .builtins import safe_dictionary, not_safe_dictionary, safe_eyo, not_safe_eyo
+
+__all__ = [
+    "Dictionary",
+    "Eyo",
+    "safe_dictionary",
+    "not_safe_dictionary",
+    "safe_eyo",
+    "not_safe_eyo",
+]
