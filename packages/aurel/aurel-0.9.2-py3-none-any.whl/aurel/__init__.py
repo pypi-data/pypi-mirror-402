5bc10da3e0038063057fc77f23613d44a52005a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:08:45 +0000

    Add package aurel/0.9.2/aurel-0.9.2-py3-none-any.whl

---
+++ packages/aurel/aurel-0.9.2-py3-none-any.whl/aurel/__init__.py
@@ -0,0 +1,15 @@
+"""Aurel: A Python package for automatic relativistic calculations."""
+
+__version__ = "0.9.2"
+from .core import *
+from .coresymbolic import *
+from .finitedifference import *
+from .maths import *
+from .numerical import *
+from .reading import *
+from .time import *
+
+
+def helloworld():
+    """Print a welcome message."""
+    print("Hello! Welcome to Aurel!", flush=True)
