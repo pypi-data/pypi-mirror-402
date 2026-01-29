f8040f1026d520013464cadd150832755f4a31b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:09:05 +0000

    Add package routio/1.0.0/routio-1.0.0-cp311-cp311-manylinux2014_aarch64.manylinux_2_17_aarch64.whl

---
+++ packages/routio/routio-1.0.0-cp311-cp311-manylinux2014_aarch64.manylinux_2_17_aarch64.whl/routio/source/loop.cpp
@@ -0,0 +1,200 @@
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <chrono>
+#include <algorithm>
+
+#include "debug.h"
+#include <routio/loop.h>
+
+using namespace std;
+
+#define MAXEVENTS 16
+
+namespace routio {
+	
+IOLoop::IOLoop() {
+
+    efd = epoll_create1 (0);
+    if (efd == -1) {
+        throw runtime_error("Unable to use epoll");
+    }
+
+}
+
+IOLoop::~IOLoop() {
+
+
+}
+
+bool IOBase::observe(SharedIOBaseObserver observer) {
+
+    SYNCHRONIZED(mutex);
+
+    if (std::find(observers.begin(), observers.end(), observer) != observers.end())
+        return false;
+
+    observers.push_back(observer);
+
+    return true;
+
+}
+
+bool IOBase::unobserve(SharedIOBaseObserver observer) {
+
+    SYNCHRONIZED(mutex);
+
+    auto position = std::find(observers.begin(), observers.end(), observer);
+    if (position == observers.end())
+        return false;
+    else observers.erase(position);
+
+    return true;
+}
+
+void IOBase::notify_output() {
+
+    if (observers.size() > 0) {
+
+            SharedIOBase ref = std::dynamic_pointer_cast<IOBase>(shared_from_this());
+
+            for (auto it : observers) {
+                (*it).on_output(ref);
+            }
+        }
+}
+
+void IOLoop::add_handler(SharedIOBase base) {
+
+	int fd = base->get_file_descriptor();
+
+	//if (handlers.find(fd) != handlers.end()) {
+	//	throw runtime_error("Listener for file descriptor already registered");
+	//}
+
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN;
+    if (epoll_ctl (efd, EPOLL_CTL_ADD, fd, &event) == -1) {
+        throw runtime_error(_format_string("Unable to use epoll (%d)", errno));
+    }
+
+	handlers[fd] = base;
+
+}
+
+void IOLoop::remove_handler(SharedIOBase base) {
+
+	int fd = base->get_file_descriptor();
+
+    DEBUGMSG("Removing client handler FID=%d\n", fd);
+
+	if (handlers.find(fd) == handlers.end()) {
+		DEBUGMSG("Listener for file descriptor not registered\n");
+        return;
+	}
+
+    if (epoll_ctl (efd, EPOLL_CTL_DEL, fd, NULL) == -1) {
+        DEBUGMSG("Removing unknown file descriptor from epoll FID=%d\n", fd);
+    }
+
+	handlers.erase(handlers.find(fd));
+
+}
+
+bool IOLoop::wait(int64_t timeout) {
+
+    //SYNCHRONIZED(mutex);
+
+    struct epoll_event *events;
+    events = (epoll_event *) calloc (MAXEVENTS, sizeof(epoll_event));
+
+    bool write_done = true;
+
+    auto start = std::chrono::system_clock::now();
+    while (handlers.size() > 0) {
+        auto current = std::chrono::system_clock::now();
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(current - start);
+
+        int64_t remaining = write_done ? -1 : 1;
+
+        if (timeout > 0) {
+            remaining = timeout - (duration.count());
+            if (remaining < 1)
+                break;
+            if (!write_done) remaining = 1;
+        }
+        int n = epoll_wait (efd, events, MAXEVENTS, remaining);
+        for (int i = 0; i < n; i++) {
+        	int fd = events[i].data.fd;
+        	if (handlers.find(fd) == handlers.end()) continue;
+
+			SharedIOBase base = handlers[fd];
+
+            if ((events[i].events & EPOLLERR) || (events[i].events & EPOLLHUP)) {
+                base->disconnect();
+                remove_handler(base);
+            } else if (events[i].events & EPOLLIN) {
+                // TODO: handle timeout
+                if (!base->handle_input()) {
+                    base->disconnect();
+                    remove_handler(base);
+                }
+            } else if (events[i].events & EPOLLOUT) {
+                struct epoll_event event;
+                event.data.fd = fd;
+                event.events = EPOLLOUT;
+                if (epoll_ctl (efd, EPOLL_CTL_DEL, event.data.fd, &event) == -1) {
+                    throw runtime_error(_format_string("Error when removing an epoll FD %d (%d)", event.data.fd, errno));
+                }
+            }
+        }
+
+        write_done = true;
+        for (std::map<int, SharedIOBase>::iterator it = handlers.begin(); it != handlers.end(); it++) {
+            bool done = it->second->handle_output();
+            if (!done) {
+                struct epoll_event event;
+                event.data.fd = it->second->get_file_descriptor();
+                event.events = EPOLLOUT;
+                if (epoll_ctl (efd, EPOLL_CTL_ADD, event.data.fd, &event) == -1) {
+                    if (errno == EBADF) {
+                        DEBUGMSG("Bad file descriptor, ignoring");
+                    } else if (errno != EEXIST) {
+                        throw runtime_error(_format_string("Error when adding an epoll FD %d (%d)", event.data.fd, errno));
+                    }
+                }
+            }
+            write_done &= done;
+        }
+     
+    }
+
+    free(events);
+    return handlers.size() > 0;
+
+}
+
+SharedIOLoop loop;
+
+SharedIOLoop default_loop() {
+
+    if (!loop) {
+        loop = make_shared<IOLoop>();
+    }
+
+    return loop;
+
+}
+
+bool wait(int64_t timeout) {
+
+    return default_loop()->wait(timeout);
+
+}
+
+}
---
+++ packages/routio/routio-1.0.0-cp311-cp311-manylinux2014_aarch64.manylinux_2_17_aarch64.whl/routio/source/routio/datatypes.h
@@ -0,0 +1,127 @@
+/* -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4 -*- */
+
+#ifndef ECHO_DATATYPES_HPP_
+#define ECHO_DATATYPES_HPP_
+
+#include <chrono>
+
+#include <routio/client.h>
+#include <routio/message.h>
+
+namespace routio {
+
+template<typename T> string get_type_identifier();
+
+template <typename T>
+class TypedSubscriber : Subscriber {
+  public:
+    TypedSubscriber(SharedClient client, const string &alias, function<void(shared_ptr<T>)> callback) : Subscriber(client, alias, get_type_identifier<T>()), callback(callback) {
+
+    }
+
+    virtual ~TypedSubscriber() {
+
+    }
+
+    virtual void on_message(SharedMessage message) {
+
+        try {
+
+            shared_ptr<T> data = Message::unpack<T>(message);
+
+            callback(data);
+
+        } catch (routio::ParseException &e) {
+            Subscriber::on_error(e);
+        }
+
+    };
+
+  private:
+    function<void(shared_ptr<T>)> callback;
+
+};
+
+template <typename T>
+class TypedPublisher : Publisher {
+  public:
+    TypedPublisher(SharedClient client, const string &alias, int queue = -1) : Publisher(client, alias, get_type_identifier<T>(), queue) {
+
+    }
+
+    virtual ~TypedPublisher() {
+
+    }
+
+    bool send(const T &data) {
+
+        return Publisher::send_message<T>(data);
+
+    }
+
+};
+
+template<typename T> using SharedTypedSubscriber = shared_ptr<TypedSubscriber<T> >;
+template<typename T> using SharedTypedPublisher = shared_ptr<TypedPublisher<T> >;
+
+typedef std::chrono::system_clock::time_point time_point;
+
+template <> 
+inline void read(MessageReader& reader, time_point& timepoint) {
+    
+    std::chrono::microseconds raw((uint64_t) reader.read_long());
+
+    timepoint = time_point(raw);
+
+}
+
+template <> 
+inline void write(MessageWriter& writer, const time_point& timepoint) {
+
+    uint64_t microseconds = std::chrono::time_point_cast<std::chrono::microseconds>(timepoint).time_since_epoch().count();
+
+    writer.write_long((int64_t) microseconds);
+
+}
+
+class Header {
+public:
+    Header(std::string source = "", time_point timestamp = std::chrono::system_clock::now());
+    virtual ~Header();
+
+    std::string source;
+    std::chrono::system_clock::time_point timestamp;
+};
+
+template <> 
+inline void read(MessageReader& reader, Header& header) {
+    
+    read(reader, header.source);
+    read(reader, header.timestamp);
+
+}
+
+template <> 
+inline void write(MessageWriter& writer, const Header& header) {
+
+    write(writer, header.source);
+    write(writer, header.timestamp);
+
+}
+
+
+
+template <> inline string get_type_identifier<Dictionary>();
+
+template<> shared_ptr<Message> Message::pack(const Dictionary&);
+template<> shared_ptr<Message> Message::pack(const Header&);
+
+template<> shared_ptr<Header> Message::unpack(SharedMessage);
+template<> shared_ptr<Dictionary> Message::unpack(SharedMessage);
+
+
+
+}
+
+
+#endif
