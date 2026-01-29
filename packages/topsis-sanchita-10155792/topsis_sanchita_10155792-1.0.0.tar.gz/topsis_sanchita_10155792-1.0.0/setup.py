37b7cf091352054d6bfc8308474e1aa287b2c7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:12:26 +0000

    Add package topsis-sanchita-10155792/1.0.0/topsis_sanchita_10155792-1.0.0.tar.gz

---
+++ packages/topsis-sanchita-10155792/topsis_sanchita_10155792-1.0.0.tar.gz/topsis_sanchita_10155792-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topsis-sanchita-10155792",
+    version="1.0.0",
+    author="Sanchita Sharma",
+    description="TOPSIS implementation as a Python package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas"],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.topsis:main"
+        ]
+    },
+)
