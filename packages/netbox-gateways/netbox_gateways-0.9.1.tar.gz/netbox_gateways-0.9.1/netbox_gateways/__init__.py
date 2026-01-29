6518a276d055cb7d1c9d0fb55572ca13767668b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:13:03 +0000

    Add package netbox-gateways/0.9.1/netbox_gateways-0.9.1.tar.gz

---
+++ packages/netbox-gateways/netbox_gateways-0.9.1.tar.gz/netbox_gateways-0.9.1/netbox_gateways/__init__.py
@@ -0,0 +1,12 @@
+from netbox.plugins import PluginConfig
+
+
+class NetBoxGatewayConfig(PluginConfig):
+    name = "netbox_gateways"
+    verbose_name = " Netbox Gateway"
+    description = "Manage simple prefix default gateways"
+    version = "0.9.1"
+    base_url = "nb-gateways"
+
+
+config = NetBoxGatewayConfig
