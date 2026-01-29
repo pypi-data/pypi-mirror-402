ee5f9404287bb5f7d6be7d5fb6ef31f059c4fc44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:13:40 +0000

    Add package Topsis-Naman-102303352/1.0.1/topsis_naman_102303352-1.0.1.tar.gz

---
+++ packages/Topsis-Naman-102303352/topsis_naman_102303352-1.0.1.tar.gz/topsis_naman_102303352-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Topsis-Naman-102303352',
+    version='1.0.1',
+    author='Naman',
+    description='A Python package for TOPSIS multi-criteria decision making.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy'],
+    entry_points={
+        'console_scripts': [
+            'topsis=topsis_Naman_102303352.topsis:topsis',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
