8c62a563745e116a10972b34787a493bdd0f057f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:50 +0000

    Add package types-aiobotocore-sso/3.1.1/types_aiobotocore_sso-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-sso/types_aiobotocore_sso-3.1.1.tar.gz/types_aiobotocore_sso-3.1.1/types_aiobotocore_sso/client.pyi
@@ -0,0 +1,171 @@
+"""
+Type annotations for sso service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_sso.client import SSOClient
+
+    session = get_session()
+    async with session.create_client("sso") as client:
+        client: SSOClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import ListAccountRolesPaginator, ListAccountsPaginator
+from .type_defs import (
+    EmptyResponseMetadataTypeDef,
+    GetRoleCredentialsRequestTypeDef,
+    GetRoleCredentialsResponseTypeDef,
+    ListAccountRolesRequestTypeDef,
+    ListAccountRolesResponseTypeDef,
+    ListAccountsRequestTypeDef,
+    ListAccountsResponseTypeDef,
+    LogoutRequestTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Self, Unpack
+else:
+    from typing_extensions import Literal, Self, Unpack
+
+__all__ = ("SSOClient",)
+
+class Exceptions(BaseClientExceptions):
+    ClientError: type[BotocoreClientError]
+    InvalidRequestException: type[BotocoreClientError]
+    ResourceNotFoundException: type[BotocoreClientError]
+    TooManyRequestsException: type[BotocoreClientError]
+    UnauthorizedException: type[BotocoreClientError]
+
+class SSOClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso.html#SSO.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        SSOClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso.html#SSO.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/#generate_presigned_url)
+        """
+
+    async def get_role_credentials(
+        self, **kwargs: Unpack[GetRoleCredentialsRequestTypeDef]
+    ) -> GetRoleCredentialsResponseTypeDef:
+        """
+        Returns the STS short-term credentials for a given role name that is assigned
+        to the user.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/client/get_role_credentials.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/#get_role_credentials)
+        """
+
+    async def list_account_roles(
+        self, **kwargs: Unpack[ListAccountRolesRequestTypeDef]
+    ) -> ListAccountRolesResponseTypeDef:
+        """
+        Lists all roles that are assigned to the user for a given AWS account.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/client/list_account_roles.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/#list_account_roles)
+        """
+
+    async def list_accounts(
+        self, **kwargs: Unpack[ListAccountsRequestTypeDef]
+    ) -> ListAccountsResponseTypeDef:
+        """
+        Lists all AWS accounts assigned to the user.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/client/list_accounts.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/#list_accounts)
+        """
+
+    async def logout(self, **kwargs: Unpack[LogoutRequestTypeDef]) -> EmptyResponseMetadataTypeDef:
+        """
+        Removes the locally stored SSO tokens from the client-side cache and sends an
+        API call to the IAM Identity Center service to invalidate the corresponding
+        server-side IAM Identity Center sign in session.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/client/logout.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/#logout)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_account_roles"]
+    ) -> ListAccountRolesPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_accounts"]
+    ) -> ListAccountsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso.html#SSO.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso.html#SSO.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/client/)
+        """
---
+++ packages/types-aiobotocore-sso/types_aiobotocore_sso-3.1.1.tar.gz/types_aiobotocore_sso-3.1.1/types_aiobotocore_sso/paginator.pyi
@@ -0,0 +1,83 @@
+"""
+Type annotations for sso service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_sso.client import SSOClient
+    from types_aiobotocore_sso.paginator import (
+        ListAccountRolesPaginator,
+        ListAccountsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("sso") as client:
+        client: SSOClient
+
+        list_account_roles_paginator: ListAccountRolesPaginator = client.get_paginator("list_account_roles")
+        list_accounts_paginator: ListAccountsPaginator = client.get_paginator("list_accounts")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    ListAccountRolesRequestPaginateTypeDef,
+    ListAccountRolesResponseTypeDef,
+    ListAccountsRequestPaginateTypeDef,
+    ListAccountsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("ListAccountRolesPaginator", "ListAccountsPaginator")
+
+if TYPE_CHECKING:
+    _ListAccountRolesPaginatorBase = AioPaginator[ListAccountRolesResponseTypeDef]
+else:
+    _ListAccountRolesPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListAccountRolesPaginator(_ListAccountRolesPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/paginator/ListAccountRoles.html#SSO.Paginator.ListAccountRoles)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/paginators/#listaccountrolespaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListAccountRolesRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListAccountRolesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/paginator/ListAccountRoles.html#SSO.Paginator.ListAccountRoles.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/paginators/#listaccountrolespaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListAccountsPaginatorBase = AioPaginator[ListAccountsResponseTypeDef]
+else:
+    _ListAccountsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListAccountsPaginator(_ListAccountsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/paginator/ListAccounts.html#SSO.Paginator.ListAccounts)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/paginators/#listaccountspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListAccountsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListAccountsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso/paginator/ListAccounts.html#SSO.Paginator.ListAccounts.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sso/paginators/#listaccountspaginator)
+        """
