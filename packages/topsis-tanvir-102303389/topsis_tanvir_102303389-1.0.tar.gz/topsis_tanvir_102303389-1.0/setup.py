658a5ad78a72ea4d17d706ab5176546bdade38ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:10:49 +0000

    Add package topsis-tanvir-102303389/1.0/topsis_tanvir_102303389-1.0.tar.gz

---
+++ packages/topsis-tanvir-102303389/topsis_tanvir_102303389-1.0.tar.gz/topsis_tanvir_102303389-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topsis-tanvir-102303389",
+    version="1.0",
+    author="Tanvir",
+    author_email="tanvir102303389@gmail.com",
+    description="TOPSIS implementation as a Python package",
+    packages=find_packages(),
+    install_requires=["pandas", "numpy"],
+)
