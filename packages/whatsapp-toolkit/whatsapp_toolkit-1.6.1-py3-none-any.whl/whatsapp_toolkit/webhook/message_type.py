69d8d9a3f40edf8f94d42fe59660edfa01f711a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:11 +0000

    Add package whatsapp-toolkit/1.6.1/whatsapp_toolkit-1.6.1-py3-none-any.whl

---
+++ packages/whatsapp-toolkit/whatsapp_toolkit-1.6.1-py3-none-any.whl/whatsapp_toolkit/webhook/message_type.py
@@ -0,0 +1,11 @@
+
+
+class MessageType:
+    CONVERSATION = "conversation"
+    EXTENDED_TEXT_MESSAGE = "extendedTextMessage"
+    IMAGE_MESSAGE = "imageMessage"
+    VIDEO_MESSAGE = "videoMessage"
+    DOCUMENT_MESSAGE = "documentMessage"
+    AUDIO_MESSAGE = "audioMessage"
+    STIKER_MESSAGE = "stickerMessage"
+    
