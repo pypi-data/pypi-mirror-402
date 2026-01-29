93d92900ba15c62f3ef65e3142b6420fd46efea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:16 +0000

    Add package types-aiobotocore-drs/3.1.1/types_aiobotocore_drs-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-drs/types_aiobotocore_drs-3.1.1-py3-none-any.whl/types_aiobotocore_drs/paginator.pyi
@@ -0,0 +1,303 @@
+"""
+Type annotations for drs service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_drs.client import DrsClient
+    from types_aiobotocore_drs.paginator import (
+        DescribeJobLogItemsPaginator,
+        DescribeJobsPaginator,
+        DescribeLaunchConfigurationTemplatesPaginator,
+        DescribeRecoveryInstancesPaginator,
+        DescribeRecoverySnapshotsPaginator,
+        DescribeReplicationConfigurationTemplatesPaginator,
+        DescribeSourceNetworksPaginator,
+        DescribeSourceServersPaginator,
+        ListExtensibleSourceServersPaginator,
+        ListLaunchActionsPaginator,
+        ListStagingAccountsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("drs") as client:
+        client: DrsClient
+
+        describe_job_log_items_paginator: DescribeJobLogItemsPaginator = client.get_paginator("describe_job_log_items")
+        describe_jobs_paginator: DescribeJobsPaginator = client.get_paginator("describe_jobs")
+        describe_launch_configuration_templates_paginator: DescribeLaunchConfigurationTemplatesPaginator = client.get_paginator("describe_launch_configuration_templates")
+        describe_recovery_instances_paginator: DescribeRecoveryInstancesPaginator = client.get_paginator("describe_recovery_instances")
+        describe_recovery_snapshots_paginator: DescribeRecoverySnapshotsPaginator = client.get_paginator("describe_recovery_snapshots")
+        describe_replication_configuration_templates_paginator: DescribeReplicationConfigurationTemplatesPaginator = client.get_paginator("describe_replication_configuration_templates")
+        describe_source_networks_paginator: DescribeSourceNetworksPaginator = client.get_paginator("describe_source_networks")
+        describe_source_servers_paginator: DescribeSourceServersPaginator = client.get_paginator("describe_source_servers")
+        list_extensible_source_servers_paginator: ListExtensibleSourceServersPaginator = client.get_paginator("list_extensible_source_servers")
+        list_launch_actions_paginator: ListLaunchActionsPaginator = client.get_paginator("list_launch_actions")
+        list_staging_accounts_paginator: ListStagingAccountsPaginator = client.get_paginator("list_staging_accounts")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    DescribeJobLogItemsRequestPaginateTypeDef,
+    DescribeJobLogItemsResponseTypeDef,
+    DescribeJobsRequestPaginateTypeDef,
+    DescribeJobsResponseTypeDef,
+    DescribeLaunchConfigurationTemplatesRequestPaginateTypeDef,
+    DescribeLaunchConfigurationTemplatesResponseTypeDef,
+    DescribeRecoveryInstancesRequestPaginateTypeDef,
+    DescribeRecoveryInstancesResponseTypeDef,
+    DescribeRecoverySnapshotsRequestPaginateTypeDef,
+    DescribeRecoverySnapshotsResponseTypeDef,
+    DescribeReplicationConfigurationTemplatesRequestPaginateTypeDef,
+    DescribeReplicationConfigurationTemplatesResponseTypeDef,
+    DescribeSourceNetworksRequestPaginateTypeDef,
+    DescribeSourceNetworksResponseTypeDef,
+    DescribeSourceServersRequestPaginateTypeDef,
+    DescribeSourceServersResponseTypeDef,
+    ListExtensibleSourceServersRequestPaginateTypeDef,
+    ListExtensibleSourceServersResponseTypeDef,
+    ListLaunchActionsRequestPaginateTypeDef,
+    ListLaunchActionsResponseTypeDef,
+    ListStagingAccountsRequestPaginateTypeDef,
+    ListStagingAccountsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = (
+    "DescribeJobLogItemsPaginator",
+    "DescribeJobsPaginator",
+    "DescribeLaunchConfigurationTemplatesPaginator",
+    "DescribeRecoveryInstancesPaginator",
+    "DescribeRecoverySnapshotsPaginator",
+    "DescribeReplicationConfigurationTemplatesPaginator",
+    "DescribeSourceNetworksPaginator",
+    "DescribeSourceServersPaginator",
+    "ListExtensibleSourceServersPaginator",
+    "ListLaunchActionsPaginator",
+    "ListStagingAccountsPaginator",
+)
+
+if TYPE_CHECKING:
+    _DescribeJobLogItemsPaginatorBase = AioPaginator[DescribeJobLogItemsResponseTypeDef]
+else:
+    _DescribeJobLogItemsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class DescribeJobLogItemsPaginator(_DescribeJobLogItemsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeJobLogItems.html#Drs.Paginator.DescribeJobLogItems)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describejoblogitemspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeJobLogItemsRequestPaginateTypeDef]
+    ) -> AioPageIterator[DescribeJobLogItemsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeJobLogItems.html#Drs.Paginator.DescribeJobLogItems.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describejoblogitemspaginator)
+        """
+
+if TYPE_CHECKING:
+    _DescribeJobsPaginatorBase = AioPaginator[DescribeJobsResponseTypeDef]
+else:
+    _DescribeJobsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class DescribeJobsPaginator(_DescribeJobsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeJobs.html#Drs.Paginator.DescribeJobs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describejobspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeJobsRequestPaginateTypeDef]
+    ) -> AioPageIterator[DescribeJobsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeJobs.html#Drs.Paginator.DescribeJobs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describejobspaginator)
+        """
+
+if TYPE_CHECKING:
+    _DescribeLaunchConfigurationTemplatesPaginatorBase = AioPaginator[
+        DescribeLaunchConfigurationTemplatesResponseTypeDef
+    ]
+else:
+    _DescribeLaunchConfigurationTemplatesPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class DescribeLaunchConfigurationTemplatesPaginator(
+    _DescribeLaunchConfigurationTemplatesPaginatorBase
+):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeLaunchConfigurationTemplates.html#Drs.Paginator.DescribeLaunchConfigurationTemplates)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describelaunchconfigurationtemplatespaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeLaunchConfigurationTemplatesRequestPaginateTypeDef]
+    ) -> AioPageIterator[DescribeLaunchConfigurationTemplatesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeLaunchConfigurationTemplates.html#Drs.Paginator.DescribeLaunchConfigurationTemplates.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describelaunchconfigurationtemplatespaginator)
+        """
+
+if TYPE_CHECKING:
+    _DescribeRecoveryInstancesPaginatorBase = AioPaginator[DescribeRecoveryInstancesResponseTypeDef]
+else:
+    _DescribeRecoveryInstancesPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class DescribeRecoveryInstancesPaginator(_DescribeRecoveryInstancesPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeRecoveryInstances.html#Drs.Paginator.DescribeRecoveryInstances)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describerecoveryinstancespaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeRecoveryInstancesRequestPaginateTypeDef]
+    ) -> AioPageIterator[DescribeRecoveryInstancesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeRecoveryInstances.html#Drs.Paginator.DescribeRecoveryInstances.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describerecoveryinstancespaginator)
+        """
+
+if TYPE_CHECKING:
+    _DescribeRecoverySnapshotsPaginatorBase = AioPaginator[DescribeRecoverySnapshotsResponseTypeDef]
+else:
+    _DescribeRecoverySnapshotsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class DescribeRecoverySnapshotsPaginator(_DescribeRecoverySnapshotsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeRecoverySnapshots.html#Drs.Paginator.DescribeRecoverySnapshots)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describerecoverysnapshotspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeRecoverySnapshotsRequestPaginateTypeDef]
+    ) -> AioPageIterator[DescribeRecoverySnapshotsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeRecoverySnapshots.html#Drs.Paginator.DescribeRecoverySnapshots.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describerecoverysnapshotspaginator)
+        """
+
+if TYPE_CHECKING:
+    _DescribeReplicationConfigurationTemplatesPaginatorBase = AioPaginator[
+        DescribeReplicationConfigurationTemplatesResponseTypeDef
+    ]
+else:
+    _DescribeReplicationConfigurationTemplatesPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class DescribeReplicationConfigurationTemplatesPaginator(
+    _DescribeReplicationConfigurationTemplatesPaginatorBase
+):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeReplicationConfigurationTemplates.html#Drs.Paginator.DescribeReplicationConfigurationTemplates)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describereplicationconfigurationtemplatespaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeReplicationConfigurationTemplatesRequestPaginateTypeDef]
+    ) -> AioPageIterator[DescribeReplicationConfigurationTemplatesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeReplicationConfigurationTemplates.html#Drs.Paginator.DescribeReplicationConfigurationTemplates.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describereplicationconfigurationtemplatespaginator)
+        """
+
+if TYPE_CHECKING:
+    _DescribeSourceNetworksPaginatorBase = AioPaginator[DescribeSourceNetworksResponseTypeDef]
+else:
+    _DescribeSourceNetworksPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class DescribeSourceNetworksPaginator(_DescribeSourceNetworksPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeSourceNetworks.html#Drs.Paginator.DescribeSourceNetworks)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describesourcenetworkspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeSourceNetworksRequestPaginateTypeDef]
+    ) -> AioPageIterator[DescribeSourceNetworksResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeSourceNetworks.html#Drs.Paginator.DescribeSourceNetworks.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describesourcenetworkspaginator)
+        """
+
+if TYPE_CHECKING:
+    _DescribeSourceServersPaginatorBase = AioPaginator[DescribeSourceServersResponseTypeDef]
+else:
+    _DescribeSourceServersPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class DescribeSourceServersPaginator(_DescribeSourceServersPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeSourceServers.html#Drs.Paginator.DescribeSourceServers)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describesourceserverspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeSourceServersRequestPaginateTypeDef]
+    ) -> AioPageIterator[DescribeSourceServersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/DescribeSourceServers.html#Drs.Paginator.DescribeSourceServers.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#describesourceserverspaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListExtensibleSourceServersPaginatorBase = AioPaginator[
+        ListExtensibleSourceServersResponseTypeDef
+    ]
+else:
+    _ListExtensibleSourceServersPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListExtensibleSourceServersPaginator(_ListExtensibleSourceServersPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/ListExtensibleSourceServers.html#Drs.Paginator.ListExtensibleSourceServers)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#listextensiblesourceserverspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListExtensibleSourceServersRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListExtensibleSourceServersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/ListExtensibleSourceServers.html#Drs.Paginator.ListExtensibleSourceServers.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#listextensiblesourceserverspaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListLaunchActionsPaginatorBase = AioPaginator[ListLaunchActionsResponseTypeDef]
+else:
+    _ListLaunchActionsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListLaunchActionsPaginator(_ListLaunchActionsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/ListLaunchActions.html#Drs.Paginator.ListLaunchActions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#listlaunchactionspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListLaunchActionsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListLaunchActionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/ListLaunchActions.html#Drs.Paginator.ListLaunchActions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#listlaunchactionspaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListStagingAccountsPaginatorBase = AioPaginator[ListStagingAccountsResponseTypeDef]
+else:
+    _ListStagingAccountsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListStagingAccountsPaginator(_ListStagingAccountsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/ListStagingAccounts.html#Drs.Paginator.ListStagingAccounts)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#liststagingaccountspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListStagingAccountsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListStagingAccountsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/drs/paginator/ListStagingAccounts.html#Drs.Paginator.ListStagingAccounts.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_drs/paginators/#liststagingaccountspaginator)
+        """
