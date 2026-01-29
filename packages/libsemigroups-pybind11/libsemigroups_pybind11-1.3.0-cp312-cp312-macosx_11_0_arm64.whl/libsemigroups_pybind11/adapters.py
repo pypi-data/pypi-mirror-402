61cd441aed2ece9a7928070a67c2ec29f5b9076c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:44 +0000

    Add package libsemigroups-pybind11/1.3.0/libsemigroups_pybind11-1.3.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.3.0-cp312-cp312-macosx_11_0_arm64.whl/libsemigroups_pybind11/adapters.py
@@ -0,0 +1,143 @@
+# Copyright (c) 2024, J. D. Mitchell
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+"""This package provides the user-facing python part of libsemigroups_pybind11
+for various adapters from libsemigroups.
+"""
+
+from typing import TypeVar as _TypeVar
+
+from typing_extensions import Self as _Self
+
+from _libsemigroups_pybind11 import (
+    # TODO(1) Transf
+    # TODO(1) other pperms
+    BMat8 as _BMat8,
+    ImageLeftActionBMat8BMat8 as _ImageLeftActionBMat8BMat8,
+    ImageLeftActionPPerm1PPerm1 as _ImageLeftActionPPerm1PPerm1,
+    ImageRightActionBMat8BMat8 as _ImageRightActionBMat8BMat8,
+    ImageRightActionPPerm1List as _ImageRightActionPPerm1List,
+    ImageRightActionPPerm1PPerm1 as _ImageRightActionPPerm1PPerm1,
+    PPerm1 as _PPerm1,
+)
+
+from .detail.cxx_wrapper import (
+    CxxWrapper as _CxxWrapper,
+    copy_cxx_mem_fns as _copy_cxx_mem_fns,
+    register_cxx_wrapped_type as _register_cxx_wrapped_type,
+    to_cxx as _to_cxx,
+    to_py as _to_py,
+)
+from .detail.decorators import copydoc as _copydoc
+
+########################################################################
+# The ImageAction protected class
+########################################################################
+
+
+class _ImageAction(_CxxWrapper):
+    """This is a protected base class for ImageRightAction and ImageLeftAction.
+    See the documentation for more details.
+    """
+
+    Element = _TypeVar("Element")
+    Point = _TypeVar("Point")
+
+    def __init__(self: _Self, *args, point: Point = None, element: Element = None) -> None:
+        """Construct from sample element and sample point.
+
+        :Keyword Arguments:
+          * **element** (*Element*) -- a sample element.
+          * **point** (*Point*) -- a sample point.
+
+        :raises KeyError:
+            if the action defined by the arguments is not defined.
+        """
+        super().__init__(*args, required_kwargs=("element", "point"), point=point, element=element)
+        if _to_cxx(self) is not None:
+            return
+        if len(args) != 0:
+            raise TypeError(f"expected 0 positional arguments, but found {len(args)}")
+        self.py_template_params = (type(_to_cxx(element)), type(_to_cxx(point)))
+        self.init_cxx_obj()
+
+    def __call__(self: _Self, *args):
+        return _to_py(_to_cxx(self)(*(_to_cxx(x) for x in args)))
+
+
+########################################################################
+# The ImageRightAction class
+########################################################################
+
+
+class ImageRightAction(_ImageAction):
+    Element = _TypeVar("Element")
+    Point = _TypeVar("Point")
+
+    __doc__ = _ImageRightActionPPerm1PPerm1.__doc__
+
+    _py_template_params_to_cxx_type = {
+        (_BMat8, _BMat8): _ImageRightActionBMat8BMat8,
+        (_PPerm1, _PPerm1): _ImageRightActionPPerm1PPerm1,
+        (_PPerm1, list): _ImageRightActionPPerm1List,
+    }
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {*_py_template_params_to_cxx_type.values()}
+
+    @_copydoc(_ImageRightActionPPerm1PPerm1.__call__)
+    def __call__(self: _Self, pt: Point, x: Element) -> Point:
+        return _to_py(_to_cxx(self)(_to_cxx(pt), _to_cxx(x)))
+
+
+########################################################################
+# Copy mem fns from sample C++ type and register types
+########################################################################
+
+_copy_cxx_mem_fns(_ImageRightActionPPerm1PPerm1, ImageRightAction)
+
+for _type in (
+    ImageRightAction._py_template_params_to_cxx_type.values()  # pylint:disable=protected-access
+):
+    _register_cxx_wrapped_type(_type, ImageRightAction)
+
+
+########################################################################
+# The ImageLeftAction class
+########################################################################
+
+
+class ImageLeftAction(_ImageAction):
+    Element = _TypeVar("Element")
+    Point = _TypeVar("Point")
+
+    __doc__ = _ImageLeftActionPPerm1PPerm1.__doc__
+
+    _py_template_params_to_cxx_type = {
+        (_BMat8, _BMat8): _ImageLeftActionBMat8BMat8,
+        (_PPerm1, _PPerm1): _ImageLeftActionPPerm1PPerm1,
+    }
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {*_py_template_params_to_cxx_type.values()}
+
+    @_copydoc(_ImageLeftActionPPerm1PPerm1.__call__)
+    def __call__(self: _Self, pt: Point, x: Element) -> Point:
+        return _to_py(_to_cxx(self)(_to_cxx(pt), _to_cxx(x)))
---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.3.0-cp312-cp312-macosx_11_0_arm64.whl/libsemigroups_pybind11/knuth_bendix.py
@@ -0,0 +1,112 @@
+# Copyright (c) 2024, J. D. Mitchell, Joseph Edwards
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+"""This page contains the documentation for various helper functions for
+manipulating :any:`KnuthBendix` objects. All such functions
+are contained in the submodule ``knuth_bendix``.
+"""
+
+from _libsemigroups_pybind11 import (
+    KnuthBendixStringRewriteFromLeft as _KnuthBendixStringRewriteFromLeft,
+    KnuthBendixStringRewriteTrie as _KnuthBendixStringRewriteTrie,
+    KnuthBendixWordRewriteFromLeft as _KnuthBendixWordRewriteFromLeft,
+    KnuthBendixWordRewriteTrie as _KnuthBendixWordRewriteTrie,
+    congruence_kind as _congruence_kind,
+    knuth_bendix_by_overlap_length as _knuth_bendix_by_overlap_length,
+    knuth_bendix_is_reduced as _knuth_bendix_is_reduced,
+    knuth_bendix_non_trivial_classes as _knuth_bendix_non_trivial_classes,
+    knuth_bendix_normal_forms as _knuth_bendix_normal_forms,
+    knuth_bendix_partition as _knuth_bendix_partition,
+    knuth_bendix_redundant_rule as _knuth_bendix_redundant_rule,
+)
+
+from .detail.congruence_common import CongruenceCommon as _CongruenceCommon
+from .detail.cxx_wrapper import (
+    copy_cxx_mem_fns as _copy_cxx_mem_fns,
+    register_cxx_wrapped_type as _register_cxx_wrapped_type,
+    to_cxx as _to_cxx,
+    wrap_cxx_free_fn as _wrap_cxx_free_fn,
+)
+from .detail.decorators import copydoc as _copydoc
+from .presentation import Presentation as _Presentation
+
+########################################################################
+# The KnuthBendix class
+########################################################################
+
+
+class KnuthBendix(_CongruenceCommon):
+    __doc__ = _KnuthBendixStringRewriteTrie.__doc__
+
+    _py_template_params_to_cxx_type = {
+        (list[int],): _KnuthBendixWordRewriteTrie,
+        (str,): _KnuthBendixStringRewriteTrie,
+        (list[int], "RewriteTrie"): _KnuthBendixWordRewriteTrie,
+        (str, "RewriteTrie"): _KnuthBendixStringRewriteTrie,
+        (list[int], "RewriteFromLeft"): _KnuthBendixWordRewriteFromLeft,
+        (str, "RewriteFromLeft"): _KnuthBendixStringRewriteFromLeft,
+    }
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {*_py_template_params_to_cxx_type.values()}
+
+    options = _KnuthBendixStringRewriteTrie.options
+
+    @_copydoc(_KnuthBendixStringRewriteTrie.__init__)
+    def __init__(self, *args, rewriter="RewriteTrie", **kwargs) -> None:
+        if rewriter not in ("RewriteFromLeft", "RewriteTrie"):
+            raise TypeError(
+                f'expected the keyword argument "rewriter" to be '
+                f'"RewriteFromLeft" or "RewriteTrie", but found "{rewriter}"'
+            )
+
+        msg = f"""expected either:
+1) 2 positional arguments of types congruence_kind and Presentation; or
+2) 0 positional arguments and the keyword argument "word"
+   (and possibly the keyword argument "rewriter").
+Found {len(args)} positional arguments and keyword arguments
+{list(kwargs.keys())}!"""
+
+        super().__init__(*args, wrong_num_args_msg=msg, **kwargs)
+        if _to_cxx(self) is not None:
+            return
+        if len(args) == 2:
+            if isinstance(args[1], _Presentation):
+                self.py_template_params = args[1].py_template_params + (rewriter,)
+            else:
+                raise TypeError(
+                    f"expected the 2nd argument to be a Presentation, but found {type(args[1])}"
+                )
+        self.init_cxx_obj(*args)
+
+
+########################################################################
+# Copy mem fns from sample C++ type and register types
+########################################################################
+
+_copy_cxx_mem_fns(_KnuthBendixStringRewriteTrie, KnuthBendix)
+_register_cxx_wrapped_type(_KnuthBendixStringRewriteTrie, KnuthBendix)
+_register_cxx_wrapped_type(_KnuthBendixWordRewriteTrie, KnuthBendix)
+_register_cxx_wrapped_type(_KnuthBendixStringRewriteFromLeft, KnuthBendix)
+_register_cxx_wrapped_type(_KnuthBendixWordRewriteFromLeft, KnuthBendix)
+
+########################################################################
+# Helpers
+########################################################################
+
+by_overlap_length = _wrap_cxx_free_fn(_knuth_bendix_by_overlap_length)
+is_reduced = _wrap_cxx_free_fn(_knuth_bendix_is_reduced)
+non_trivial_classes = _wrap_cxx_free_fn(_knuth_bendix_non_trivial_classes)
+normal_forms = _wrap_cxx_free_fn(_knuth_bendix_normal_forms)
+partition = _wrap_cxx_free_fn(_knuth_bendix_partition)
+redundant_rule = _wrap_cxx_free_fn(_knuth_bendix_redundant_rule)
