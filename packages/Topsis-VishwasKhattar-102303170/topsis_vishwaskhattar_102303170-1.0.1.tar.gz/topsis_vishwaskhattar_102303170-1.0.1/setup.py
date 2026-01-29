94c0064e05a95d6a14b2f7b8108ab17386481ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:28 +0000

    Add package Topsis-VishwasKhattar-102303170/1.0.1/topsis_vishwaskhattar_102303170-1.0.1.tar.gz

---
+++ packages/Topsis-VishwasKhattar-102303170/topsis_vishwaskhattar_102303170-1.0.1.tar.gz/topsis_vishwaskhattar_102303170-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-VishwasKhattar-102303170",
+    version="1.0.1",
+    author="Vishwas Khattar",
+    author_email="your_email@gmail.com",
+    description="A command-line implementation of TOPSIS",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["pandas", "numpy"],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis_vishwas_102303170.topsis:run"
+        ]
+    },
+)
