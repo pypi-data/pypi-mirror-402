13e7b4bf054c991fbdcf156dda8cf5dbee30d920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:52 +0000

    Add package types-aiobotocore-trustedadvisor/3.1.1/types_aiobotocore_trustedadvisor-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-trustedadvisor/types_aiobotocore_trustedadvisor-3.1.1-py3-none-any.whl/types_aiobotocore_trustedadvisor/client.pyi
@@ -0,0 +1,307 @@
+"""
+Type annotations for trustedadvisor service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_trustedadvisor.client import TrustedAdvisorPublicAPIClient
+
+    session = get_session()
+    async with session.create_client("trustedadvisor") as client:
+        client: TrustedAdvisorPublicAPIClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import (
+    ListChecksPaginator,
+    ListOrganizationRecommendationAccountsPaginator,
+    ListOrganizationRecommendationResourcesPaginator,
+    ListOrganizationRecommendationsPaginator,
+    ListRecommendationResourcesPaginator,
+    ListRecommendationsPaginator,
+)
+from .type_defs import (
+    BatchUpdateRecommendationResourceExclusionRequestTypeDef,
+    BatchUpdateRecommendationResourceExclusionResponseTypeDef,
+    EmptyResponseMetadataTypeDef,
+    GetOrganizationRecommendationRequestTypeDef,
+    GetOrganizationRecommendationResponseTypeDef,
+    GetRecommendationRequestTypeDef,
+    GetRecommendationResponseTypeDef,
+    ListChecksRequestTypeDef,
+    ListChecksResponseTypeDef,
+    ListOrganizationRecommendationAccountsRequestTypeDef,
+    ListOrganizationRecommendationAccountsResponseTypeDef,
+    ListOrganizationRecommendationResourcesRequestTypeDef,
+    ListOrganizationRecommendationResourcesResponseTypeDef,
+    ListOrganizationRecommendationsRequestTypeDef,
+    ListOrganizationRecommendationsResponseTypeDef,
+    ListRecommendationResourcesRequestTypeDef,
+    ListRecommendationResourcesResponseTypeDef,
+    ListRecommendationsRequestTypeDef,
+    ListRecommendationsResponseTypeDef,
+    UpdateOrganizationRecommendationLifecycleRequestTypeDef,
+    UpdateRecommendationLifecycleRequestTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Self, Unpack
+else:
+    from typing_extensions import Literal, Self, Unpack
+
+__all__ = ("TrustedAdvisorPublicAPIClient",)
+
+class Exceptions(BaseClientExceptions):
+    AccessDeniedException: type[BotocoreClientError]
+    ClientError: type[BotocoreClientError]
+    ConflictException: type[BotocoreClientError]
+    InternalServerException: type[BotocoreClientError]
+    ResourceNotFoundException: type[BotocoreClientError]
+    ThrottlingException: type[BotocoreClientError]
+    ValidationException: type[BotocoreClientError]
+
+class TrustedAdvisorPublicAPIClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        TrustedAdvisorPublicAPIClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#generate_presigned_url)
+        """
+
+    async def batch_update_recommendation_resource_exclusion(
+        self, **kwargs: Unpack[BatchUpdateRecommendationResourceExclusionRequestTypeDef]
+    ) -> BatchUpdateRecommendationResourceExclusionResponseTypeDef:
+        """
+        Update one or more exclusion status for a list of recommendation resources.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/batch_update_recommendation_resource_exclusion.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#batch_update_recommendation_resource_exclusion)
+        """
+
+    async def get_organization_recommendation(
+        self, **kwargs: Unpack[GetOrganizationRecommendationRequestTypeDef]
+    ) -> GetOrganizationRecommendationResponseTypeDef:
+        """
+        Get a specific recommendation within an AWS Organizations organization.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/get_organization_recommendation.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#get_organization_recommendation)
+        """
+
+    async def get_recommendation(
+        self, **kwargs: Unpack[GetRecommendationRequestTypeDef]
+    ) -> GetRecommendationResponseTypeDef:
+        """
+        Get a specific Recommendation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/get_recommendation.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#get_recommendation)
+        """
+
+    async def list_checks(
+        self, **kwargs: Unpack[ListChecksRequestTypeDef]
+    ) -> ListChecksResponseTypeDef:
+        """
+        List a filterable set of Checks.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/list_checks.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#list_checks)
+        """
+
+    async def list_organization_recommendation_accounts(
+        self, **kwargs: Unpack[ListOrganizationRecommendationAccountsRequestTypeDef]
+    ) -> ListOrganizationRecommendationAccountsResponseTypeDef:
+        """
+        Lists the accounts that own the resources for an organization aggregate
+        recommendation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/list_organization_recommendation_accounts.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#list_organization_recommendation_accounts)
+        """
+
+    async def list_organization_recommendation_resources(
+        self, **kwargs: Unpack[ListOrganizationRecommendationResourcesRequestTypeDef]
+    ) -> ListOrganizationRecommendationResourcesResponseTypeDef:
+        """
+        List Resources of a Recommendation within an Organization.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/list_organization_recommendation_resources.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#list_organization_recommendation_resources)
+        """
+
+    async def list_organization_recommendations(
+        self, **kwargs: Unpack[ListOrganizationRecommendationsRequestTypeDef]
+    ) -> ListOrganizationRecommendationsResponseTypeDef:
+        """
+        List a filterable set of Recommendations within an Organization.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/list_organization_recommendations.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#list_organization_recommendations)
+        """
+
+    async def list_recommendation_resources(
+        self, **kwargs: Unpack[ListRecommendationResourcesRequestTypeDef]
+    ) -> ListRecommendationResourcesResponseTypeDef:
+        """
+        List Resources of a Recommendation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/list_recommendation_resources.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#list_recommendation_resources)
+        """
+
+    async def list_recommendations(
+        self, **kwargs: Unpack[ListRecommendationsRequestTypeDef]
+    ) -> ListRecommendationsResponseTypeDef:
+        """
+        List a filterable set of Recommendations.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/list_recommendations.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#list_recommendations)
+        """
+
+    async def update_organization_recommendation_lifecycle(
+        self, **kwargs: Unpack[UpdateOrganizationRecommendationLifecycleRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Update the lifecycle of a Recommendation within an Organization.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/update_organization_recommendation_lifecycle.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#update_organization_recommendation_lifecycle)
+        """
+
+    async def update_recommendation_lifecycle(
+        self, **kwargs: Unpack[UpdateRecommendationLifecycleRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Update the lifecyle of a Recommendation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/update_recommendation_lifecycle.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#update_recommendation_lifecycle)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_checks"]
+    ) -> ListChecksPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_organization_recommendation_accounts"]
+    ) -> ListOrganizationRecommendationAccountsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_organization_recommendation_resources"]
+    ) -> ListOrganizationRecommendationResourcesPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_organization_recommendations"]
+    ) -> ListOrganizationRecommendationsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_recommendation_resources"]
+    ) -> ListRecommendationResourcesPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_recommendations"]
+    ) -> ListRecommendationsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/trustedadvisor.html#TrustedAdvisorPublicAPI.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_trustedadvisor/client/)
+        """
