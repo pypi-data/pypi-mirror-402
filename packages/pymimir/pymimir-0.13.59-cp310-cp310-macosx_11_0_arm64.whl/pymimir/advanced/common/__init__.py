84f6cbdccbddb0fd1ca8b72c0f8f19a3fe5b90cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:17:47 +0000

    Add package pymimir/0.13.59/pymimir-0.13.59-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/pymimir/pymimir-0.13.59-cp310-cp310-macosx_11_0_arm64.whl/pymimir/advanced/common/__init__.py
@@ -0,0 +1,9 @@
+# Import all classes for better IDE support
+
+from pymimir.pymimir.advanced.common import (
+    FlatBitset,
+    FlatDoubleList,
+    FlatIndexList,
+)
+
+__all__ = ["FlatBitset", "FlatDoubleList", "FlatIndexList"]
