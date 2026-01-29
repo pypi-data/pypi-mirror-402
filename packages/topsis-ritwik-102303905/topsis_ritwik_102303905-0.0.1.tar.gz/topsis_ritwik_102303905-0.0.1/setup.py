7f5637ebbce894e4a72ea0e0d1d16cd2a9563bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:15:01 +0000

    Add package topsis-ritwik-102303905/0.0.1/topsis_ritwik_102303905-0.0.1.tar.gz

---
+++ packages/topsis-ritwik-102303905/topsis_ritwik_102303905-0.0.1.tar.gz/topsis_ritwik_102303905-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Python project on TOPSIS'
+
+setup(
+  name = "topsis-ritwik-102303905",
+  version = VERSION,
+  author = "Ritwik Bhandari",
+  author_email = "rbhandari_be23@thapar.edu",
+  description = DESCRIPTION,
+  packages = find_packages(),
+  install_requires = [],
+  keywords = ['python', 'topsis', 'data ranking', 'numpy']
+)
