a002fa149e904060435cdb94734cade4de06c251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:14:30 +0000

    Add package number-guessing-game-bhimesh/1.0.1/number_guessing_game_bhimesh-1.0.1-py3-none-any.whl

---
+++ packages/number-guessing-game-bhimesh/number_guessing_game_bhimesh-1.0.1-py3-none-any.whl/number_guessing_game/__init__.py
@@ -0,0 +1,9 @@
+"""Number Guessing Game - A fun interactive guessing game."""
+
+__version__ = "1.0.0"
+__author__ = "bhimesh"
+__description__ = "An interactive number guessing game where players try to guess a random number between 1 and 100."
+
+from .game import main
+
+__all__ = ["main"]
