56053ead8d1fce0af53ef6d88bec1d666c27fe75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:08:15 +0000

    Add package simple-static/0.0.4/simple_static-0.0.4-py3-none-any.whl

---
+++ packages/simple-static/simple_static-0.0.4-py3-none-any.whl/simple_static/plugins.py
@@ -0,0 +1,43 @@
+import os
+from glob import glob
+
+# import these so we can expose them to plugins
+from .utils import config, ctx, logging
+from .utils import generate_output_path, trim_output_dir, generate_output_url
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+def build_page(template_path, output_path, can_prettify=True, **user_context):
+    """
+    Useful for plugins that want to generate extra pages. Args:
+        - template_path = file path to jinja template inside INPUT_DIR
+        - output_path = desired place where file will live in OUTPUT_DIR
+        - user_context = any other key/values to add to site context
+    """
+
+    for k, v in user_context.items():
+        ctx[k] = v
+
+    env = Environment(
+        loader=FileSystemLoader(config.INPUT_DIR),
+        autoescape=select_autoescape()
+    )
+
+    template = env.get_template(template_path)
+
+    output_path = generate_output_path(output_path, can_prettify=can_prettify)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)  # https://stackoverflow.com/a/12517490/625840
+
+    with open(output_path, "w+") as f:
+        html = template.render(**ctx)
+        f.write(html)
+
+
+def get_pages():
+    rendered_pages = glob(f"{config.OUTPUT_DIR}/**/*.html", recursive=True)
+    return [{
+        "file_path": page,
+        "url_path": trim_output_dir(page),
+        "pretty_url": generate_output_url(page),
+    } for page in sorted(rendered_pages)]
---
+++ packages/simple-static/simple_static-0.0.4-py3-none-any.whl/simple_static/serve.py
@@ -0,0 +1,59 @@
+import os
+import time
+import threading
+import http.server
+
+from .build import build
+from .utils import get_paths, config, logging
+
+# override SimpleHTTPRequestHandler to serve files from config.OUTPUT_DIR
+# via https://stackoverflow.com/a/52531444/625840
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=config.OUTPUT_DIR, **kwargs)
+
+def start_server():
+    logging.info(f"starting server at: http://{config.LOCAL_HOST}:{config.LOCAL_PORT}")
+    httpd = http.server.HTTPServer((config.LOCAL_HOST, int(config.LOCAL_PORT)), Handler)
+    httpd.serve_forever()
+
+def serve():
+
+    # start server in the background so we can watch files for changes
+    server_thread = threading.Thread(target=start_server, name="local_server", daemon=True)
+    server_thread.start()
+
+    build()  # initial build
+    file_mtimes = {}
+    prev_paths = get_paths()
+
+    while True:
+
+        # check if source files were added, removed or renamed
+        curr_paths = get_paths()
+        if prev_paths != curr_paths:
+            if len(prev_paths) < len(curr_paths):
+                logging.info("file added...")
+            elif len(prev_paths) > len(curr_paths):
+                logging.info("file removed...")
+            else:
+                logging.info("file renamed...")
+            build()
+
+        # check if files have been edited since we last checked
+        for path in curr_paths:
+
+            curr_mtime = os.path.getmtime(path)
+            prev_mtime = file_mtimes.get(path)
+            file_mtimes[path] = curr_mtime
+
+            if prev_mtime and prev_mtime != curr_mtime:
+                logging.info(f"{path} changed...")
+                build()
+                break
+
+        prev_paths = curr_paths
+        time.sleep(1)
+
+if __name__ == '__main__':
+    serve()
