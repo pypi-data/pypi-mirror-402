801316e1d699dcde3e4de66378a0b6601430192c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:18 +0000

    Add package types-aiobotocore-eks/3.1.1/types_aiobotocore_eks-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-eks/types_aiobotocore_eks-3.1.1.tar.gz/types_aiobotocore_eks-3.1.1/types_aiobotocore_eks/waiter.pyi
@@ -0,0 +1,175 @@
+"""
+Type annotations for eks service client waiters.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_eks.client import EKSClient
+    from types_aiobotocore_eks.waiter import (
+        AddonActiveWaiter,
+        AddonDeletedWaiter,
+        ClusterActiveWaiter,
+        ClusterDeletedWaiter,
+        FargateProfileActiveWaiter,
+        FargateProfileDeletedWaiter,
+        NodegroupActiveWaiter,
+        NodegroupDeletedWaiter,
+    )
+
+    session = get_session()
+    async with session.create_client("eks") as client:
+        client: EKSClient
+
+        addon_active_waiter: AddonActiveWaiter = client.get_waiter("addon_active")
+        addon_deleted_waiter: AddonDeletedWaiter = client.get_waiter("addon_deleted")
+        cluster_active_waiter: ClusterActiveWaiter = client.get_waiter("cluster_active")
+        cluster_deleted_waiter: ClusterDeletedWaiter = client.get_waiter("cluster_deleted")
+        fargate_profile_active_waiter: FargateProfileActiveWaiter = client.get_waiter("fargate_profile_active")
+        fargate_profile_deleted_waiter: FargateProfileDeletedWaiter = client.get_waiter("fargate_profile_deleted")
+        nodegroup_active_waiter: NodegroupActiveWaiter = client.get_waiter("nodegroup_active")
+        nodegroup_deleted_waiter: NodegroupDeletedWaiter = client.get_waiter("nodegroup_deleted")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+
+from aiobotocore.waiter import AIOWaiter
+
+from .type_defs import (
+    DescribeAddonRequestWaitExtraTypeDef,
+    DescribeAddonRequestWaitTypeDef,
+    DescribeClusterRequestWaitExtraTypeDef,
+    DescribeClusterRequestWaitTypeDef,
+    DescribeFargateProfileRequestWaitExtraTypeDef,
+    DescribeFargateProfileRequestWaitTypeDef,
+    DescribeNodegroupRequestWaitExtraTypeDef,
+    DescribeNodegroupRequestWaitTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = (
+    "AddonActiveWaiter",
+    "AddonDeletedWaiter",
+    "ClusterActiveWaiter",
+    "ClusterDeletedWaiter",
+    "FargateProfileActiveWaiter",
+    "FargateProfileDeletedWaiter",
+    "NodegroupActiveWaiter",
+    "NodegroupDeletedWaiter",
+)
+
+class AddonActiveWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/AddonActive.html#EKS.Waiter.AddonActive)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#addonactivewaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeAddonRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/AddonActive.html#EKS.Waiter.AddonActive.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#addonactivewaiter)
+        """
+
+class AddonDeletedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/AddonDeleted.html#EKS.Waiter.AddonDeleted)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#addondeletedwaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeAddonRequestWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/AddonDeleted.html#EKS.Waiter.AddonDeleted.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#addondeletedwaiter)
+        """
+
+class ClusterActiveWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/ClusterActive.html#EKS.Waiter.ClusterActive)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#clusteractivewaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeClusterRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/ClusterActive.html#EKS.Waiter.ClusterActive.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#clusteractivewaiter)
+        """
+
+class ClusterDeletedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/ClusterDeleted.html#EKS.Waiter.ClusterDeleted)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#clusterdeletedwaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeClusterRequestWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/ClusterDeleted.html#EKS.Waiter.ClusterDeleted.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#clusterdeletedwaiter)
+        """
+
+class FargateProfileActiveWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/FargateProfileActive.html#EKS.Waiter.FargateProfileActive)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#fargateprofileactivewaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeFargateProfileRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/FargateProfileActive.html#EKS.Waiter.FargateProfileActive.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#fargateprofileactivewaiter)
+        """
+
+class FargateProfileDeletedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/FargateProfileDeleted.html#EKS.Waiter.FargateProfileDeleted)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#fargateprofiledeletedwaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeFargateProfileRequestWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/FargateProfileDeleted.html#EKS.Waiter.FargateProfileDeleted.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#fargateprofiledeletedwaiter)
+        """
+
+class NodegroupActiveWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/NodegroupActive.html#EKS.Waiter.NodegroupActive)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#nodegroupactivewaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeNodegroupRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/NodegroupActive.html#EKS.Waiter.NodegroupActive.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#nodegroupactivewaiter)
+        """
+
+class NodegroupDeletedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/NodegroupDeleted.html#EKS.Waiter.NodegroupDeleted)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#nodegroupdeletedwaiter)
+    """
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeNodegroupRequestWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/eks/waiter/NodegroupDeleted.html#EKS.Waiter.NodegroupDeleted.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_eks/waiters/#nodegroupdeletedwaiter)
+        """
