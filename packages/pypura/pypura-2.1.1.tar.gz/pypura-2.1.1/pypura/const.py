bac772336e8565a85502f34732aa3a9bd95111e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:11:14 +0000

    Add package pypura/2.1.1/pypura-2.1.1.tar.gz

---
+++ packages/pypura/pypura-2.1.1.tar.gz/pypura-2.1.1/pypura/const.py
@@ -0,0 +1,8 @@
+"""Constants."""
+
+from __future__ import annotations
+
+from typing import Final
+
+USER_POOL_ID: Final = "dXMtZWFzdC0xX0xhQjcxOGhZdg=="
+CLIENT_ID: Final = "NGlla3ViYXQwamI1aWxqZmJhYWxzaXFmOWo="
