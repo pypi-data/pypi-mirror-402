2ccde5a3d6f564b73b5aa84f58924f82ec05a3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:08:17 +0000

    Add package Topsis-Charu-102303113/1.0.0/topsis_charu_102303113-1.0.0.tar.gz

---
+++ packages/Topsis-Charu-102303113/topsis_charu_102303113-1.0.0.tar.gz/topsis_charu_102303113-1.0.0/README.md
@@ -0,0 +1,7 @@
+# TOPSIS Package
+
+## Install
+pip install Topsis-Charu-102303113
+
+## Usage
+python topsis.py input.csv "1,1,1,2" "+,+,-,+" output.csv
---
+++ packages/Topsis-Charu-102303113/topsis_charu_102303113-1.0.0.tar.gz/topsis_charu_102303113-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-Charu-102303113",
+    version="1.0.0",
+    author="Charu",
+    author_email="charugoyal195@gmail.com",
+    description="TOPSIS implementation as a Python package",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas"],
+)
