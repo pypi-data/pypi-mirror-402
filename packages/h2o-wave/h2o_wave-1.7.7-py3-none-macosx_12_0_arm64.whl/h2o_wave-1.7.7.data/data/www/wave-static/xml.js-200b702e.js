f539fd31c1ad820a0b7818b48e58c4b9ad09a720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:15:13 +0000

    Add package h2o-wave/1.7.7/h2o_wave-1.7.7-py3-none-macosx_12_0_arm64.whl

---
+++ packages/h2o-wave/h2o_wave-1.7.7-py3-none-macosx_12_0_arm64.whl/h2o_wave-1.7.7.data/data/www/wave-static/xml.js-200b702e.js
@@ -0,0 +1,1 @@
+import n from"./xml-7b0fd495.js";function e(){e.warned||(e.warned=!0,console.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/xml" instead of "highlight.js/lib/languages/xml.js"'))}e();export{n as default};
