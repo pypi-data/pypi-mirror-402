758f92a94c36c94c042243e0cf26e7dcf0cee905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:34:52 +0000

    Add package pyaiagent/0.1.2/pyaiagent-0.1.2.tar.gz

---
+++ packages/pyaiagent/pyaiagent-0.1.2.tar.gz/pyaiagent-0.1.2/src/pyaiagent/__init__.py
@@ -0,0 +1,36 @@
+"""
+PyAiAgent is a modern, fast (high-performance), async framework for building AI agents with pythonic code.
+"""
+
+from pyaiagent.openai.agent import OpenAIAgent
+from pyaiagent.openai.shutdown import shutdown
+from pyaiagent.openai.exceptions.process import (OpenAIAgentProcessError,
+                                                 OpenAIAgentClosedError,
+                                                 InvalidInputError,
+                                                 InvalidSessionError,
+                                                 InvalidMetadataError,
+                                                 InvalidLlmMessagesError,
+                                                 InvalidInstructionParamsError,
+                                                 InstructionKeyError,
+                                                 ClientError,
+                                                 MaxStepsExceededError)
+
+__all__ = [
+    # AI Agent
+    "OpenAIAgent",
+    # Shutdown function
+    "shutdown",
+    # Exceptions (runtime errors)
+    "OpenAIAgentProcessError",
+    "OpenAIAgentClosedError",
+    "InvalidInputError",
+    "InvalidSessionError",
+    "InvalidMetadataError",
+    "InvalidLlmMessagesError",
+    "InvalidInstructionParamsError",
+    "InstructionKeyError",
+    "ClientError",
+    "MaxStepsExceededError",
+]
+
+__version__ = "0.1.2"
