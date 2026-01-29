d3f32dd7f133b403e33c0e93432b463effdf8272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:21 +0000

    Add package types-aiobotocore-grafana/3.1.1/types_aiobotocore_grafana-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-grafana/types_aiobotocore_grafana-3.1.1.tar.gz/types_aiobotocore_grafana-3.1.1/types_aiobotocore_grafana/paginator.py
@@ -0,0 +1,175 @@
+"""
+Type annotations for grafana service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_grafana.client import ManagedGrafanaClient
+    from types_aiobotocore_grafana.paginator import (
+        ListPermissionsPaginator,
+        ListVersionsPaginator,
+        ListWorkspaceServiceAccountTokensPaginator,
+        ListWorkspaceServiceAccountsPaginator,
+        ListWorkspacesPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("grafana") as client:
+        client: ManagedGrafanaClient
+
+        list_permissions_paginator: ListPermissionsPaginator = client.get_paginator("list_permissions")
+        list_versions_paginator: ListVersionsPaginator = client.get_paginator("list_versions")
+        list_workspace_service_account_tokens_paginator: ListWorkspaceServiceAccountTokensPaginator = client.get_paginator("list_workspace_service_account_tokens")
+        list_workspace_service_accounts_paginator: ListWorkspaceServiceAccountsPaginator = client.get_paginator("list_workspace_service_accounts")
+        list_workspaces_paginator: ListWorkspacesPaginator = client.get_paginator("list_workspaces")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    ListPermissionsRequestPaginateTypeDef,
+    ListPermissionsResponseTypeDef,
+    ListVersionsRequestPaginateTypeDef,
+    ListVersionsResponseTypeDef,
+    ListWorkspaceServiceAccountsRequestPaginateTypeDef,
+    ListWorkspaceServiceAccountsResponseTypeDef,
+    ListWorkspaceServiceAccountTokensRequestPaginateTypeDef,
+    ListWorkspaceServiceAccountTokensResponseTypeDef,
+    ListWorkspacesRequestPaginateTypeDef,
+    ListWorkspacesResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = (
+    "ListPermissionsPaginator",
+    "ListVersionsPaginator",
+    "ListWorkspaceServiceAccountTokensPaginator",
+    "ListWorkspaceServiceAccountsPaginator",
+    "ListWorkspacesPaginator",
+)
+
+
+if TYPE_CHECKING:
+    _ListPermissionsPaginatorBase = AioPaginator[ListPermissionsResponseTypeDef]
+else:
+    _ListPermissionsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListPermissionsPaginator(_ListPermissionsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/grafana/paginator/ListPermissions.html#ManagedGrafana.Paginator.ListPermissions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/#listpermissionspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListPermissionsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListPermissionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/grafana/paginator/ListPermissions.html#ManagedGrafana.Paginator.ListPermissions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/#listpermissionspaginator)
+        """
+
+
+if TYPE_CHECKING:
+    _ListVersionsPaginatorBase = AioPaginator[ListVersionsResponseTypeDef]
+else:
+    _ListVersionsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListVersionsPaginator(_ListVersionsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/grafana/paginator/ListVersions.html#ManagedGrafana.Paginator.ListVersions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/#listversionspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListVersionsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListVersionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/grafana/paginator/ListVersions.html#ManagedGrafana.Paginator.ListVersions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/#listversionspaginator)
+        """
+
+
+if TYPE_CHECKING:
+    _ListWorkspaceServiceAccountTokensPaginatorBase = AioPaginator[
+        ListWorkspaceServiceAccountTokensResponseTypeDef
+    ]
+else:
+    _ListWorkspaceServiceAccountTokensPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListWorkspaceServiceAccountTokensPaginator(_ListWorkspaceServiceAccountTokensPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/grafana/paginator/ListWorkspaceServiceAccountTokens.html#ManagedGrafana.Paginator.ListWorkspaceServiceAccountTokens)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/#listworkspaceserviceaccounttokenspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListWorkspaceServiceAccountTokensRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListWorkspaceServiceAccountTokensResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/grafana/paginator/ListWorkspaceServiceAccountTokens.html#ManagedGrafana.Paginator.ListWorkspaceServiceAccountTokens.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/#listworkspaceserviceaccounttokenspaginator)
+        """
+
+
+if TYPE_CHECKING:
+    _ListWorkspaceServiceAccountsPaginatorBase = AioPaginator[
+        ListWorkspaceServiceAccountsResponseTypeDef
+    ]
+else:
+    _ListWorkspaceServiceAccountsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListWorkspaceServiceAccountsPaginator(_ListWorkspaceServiceAccountsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/grafana/paginator/ListWorkspaceServiceAccounts.html#ManagedGrafana.Paginator.ListWorkspaceServiceAccounts)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/#listworkspaceserviceaccountspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListWorkspaceServiceAccountsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListWorkspaceServiceAccountsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/grafana/paginator/ListWorkspaceServiceAccounts.html#ManagedGrafana.Paginator.ListWorkspaceServiceAccounts.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/#listworkspaceserviceaccountspaginator)
+        """
+
+
+if TYPE_CHECKING:
+    _ListWorkspacesPaginatorBase = AioPaginator[ListWorkspacesResponseTypeDef]
+else:
+    _ListWorkspacesPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListWorkspacesPaginator(_ListWorkspacesPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/grafana/paginator/ListWorkspaces.html#ManagedGrafana.Paginator.ListWorkspaces)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/#listworkspacespaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListWorkspacesRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListWorkspacesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/grafana/paginator/ListWorkspaces.html#ManagedGrafana.Paginator.ListWorkspaces.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_grafana/paginators/#listworkspacespaginator)
+        """
