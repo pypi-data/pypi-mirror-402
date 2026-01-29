c913d746a820fdcf472555e4cab0ff357597e639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:08:00 +0000

    Add package types-aiobotocore-codeguru-reviewer/3.1.1/types_aiobotocore_codeguru_reviewer-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-codeguru-reviewer/types_aiobotocore_codeguru_reviewer-3.1.1-py3-none-any.whl/types_aiobotocore_codeguru_reviewer/paginator.pyi
@@ -0,0 +1,63 @@
+"""
+Type annotations for codeguru-reviewer service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeguru_reviewer/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_codeguru_reviewer.client import CodeGuruReviewerClient
+    from types_aiobotocore_codeguru_reviewer.paginator import (
+        ListRepositoryAssociationsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("codeguru-reviewer") as client:
+        client: CodeGuruReviewerClient
+
+        list_repository_associations_paginator: ListRepositoryAssociationsPaginator = client.get_paginator("list_repository_associations")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    ListRepositoryAssociationsRequestPaginateTypeDef,
+    ListRepositoryAssociationsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("ListRepositoryAssociationsPaginator",)
+
+if TYPE_CHECKING:
+    _ListRepositoryAssociationsPaginatorBase = AioPaginator[
+        ListRepositoryAssociationsResponseTypeDef
+    ]
+else:
+    _ListRepositoryAssociationsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListRepositoryAssociationsPaginator(_ListRepositoryAssociationsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeguru-reviewer/paginator/ListRepositoryAssociations.html#CodeGuruReviewer.Paginator.ListRepositoryAssociations)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeguru_reviewer/paginators/#listrepositoryassociationspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListRepositoryAssociationsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListRepositoryAssociationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeguru-reviewer/paginator/ListRepositoryAssociations.html#CodeGuruReviewer.Paginator.ListRepositoryAssociations.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeguru_reviewer/paginators/#listrepositoryassociationspaginator)
+        """
