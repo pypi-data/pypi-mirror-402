a6c3f0d22db6eba1d21316f6e615de348e8e11e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:17:39 +0000

    Add package condorgame/0.5.3/condorgame-0.5.3-py3-none-any.whl

---
+++ packages/condorgame/condorgame-0.5.3-py3-none-any.whl/condorgame/__version__.py
@@ -0,0 +1,9 @@
+__title__ = 'condorgame'
+__description__ = 'A package for participating in the Condor Game on CrunchDAO'
+__version__ = '0.5.3'
+__author__ = [
+    "Abdennour BOUTRIG",
+    "Alexis Gassmann",
+]
+__author_email__ = 'abdennour.boutrig@crunchdao.com'
+__url__ = 'https://github.com/crunchdao/condorgame'
