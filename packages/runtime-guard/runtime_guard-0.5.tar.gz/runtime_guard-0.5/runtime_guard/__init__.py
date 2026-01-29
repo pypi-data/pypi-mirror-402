498efea116c5190660d60c07223b43bd0d8793f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:13:34 +0000

    Add package runtime-guard/0.5/runtime_guard-0.5.tar.gz

---
+++ packages/runtime-guard/runtime_guard-0.5.tar.gz/runtime_guard-0.5/runtime_guard/__init__.py
@@ -0,0 +1,10 @@
+"""
+The RuntimeGuard provides functionality to make sure your algorithm runs fine. 
+
+It got developed to counter memory exhaustion during training deep learning models. 
+
+Does not need any dependencies!
+"""
+
+from .runtime_guard import *
+
---
+++ packages/runtime-guard/runtime_guard-0.5.tar.gz/runtime_guard-0.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+# relative links to absolute
+with open("./README.md", "r") as f:
+    readme = f.read()
+# readme = readme.replace('src="./img_phy_sim/raytracing_example.png"', 'src="https://github.com/M-106/Image-Physics-Simulation/raw/main/img_phy_sim/raytracing_example.png"')  # click on your picture and click copy lin address to graphic
+
+setup(
+    name='runtime_guard',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[],
+    author="Tobia Ippolito",
+    description = 'A ressource monitor without any dependencies.',
+    long_description = readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,  # Ensures files from MANIFEST.in are included
+    download_url = 'https://github.com/M-106/Runtime-Guard/archive/v_03.tar.gz',
+    url="https://github.com/M-106/Runtime-Guard",
+    project_urls={
+        "Documentation": "https://M-106.github.io/Runtime-Guard/",
+        "Source": "https://github.com/M-106/Runtime-Guard"
+    },
+    keywords = ['Training', 'Monitoring', 'DeepLearning'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',      # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13'
+    ],
+    license="MIT",
+)
+
