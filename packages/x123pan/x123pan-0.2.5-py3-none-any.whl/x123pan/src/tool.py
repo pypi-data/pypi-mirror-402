ac873fff9655086dc1f1a7368b3b1022dd0c803d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:32:26 +0000

    Add package x123pan/0.2.5/x123pan-0.2.5-py3-none-any.whl

---
+++ packages/x123pan/x123pan-0.2.5-py3-none-any.whl/x123pan/src/tool.py
@@ -0,0 +1,40 @@
+import hashlib
+import io
+from typing import Union
+
+from x123pan.src.type import SectionFileReader, SectionDataReader, Ctx
+
+
+def size_md5(file_source:Union[str, bytes]):
+    """
+    内部方法，用于获取文件的 md5 。
+    :param file_source: 文件名或字节流对象，如果是字节流对象请手动将光标移动到开头。
+    :return: 文件 md5 。
+    """
+    try:
+        if isinstance(file_source, bytes):
+            file_info = io.BytesIO(file_source)
+        elif isinstance(file_source, str):
+            file_info = open(file_source, "rb")
+        else:
+            raise Exception("参数类型错误")
+        file_info.seek(0)
+        md5_hash = hashlib.md5()
+        for byte_block in iter(lambda: file_info.read(4096), b""):
+            md5_hash.update(byte_block)
+        size = file_info.tell()
+        return size, md5_hash.hexdigest()
+    finally:
+        if 'file_info' in locals() and hasattr(file_info, 'close'):
+            file_info.close()
+
+def read(file_info:Union[str,bytes], limit, ctx:Ctx=None):
+    if ctx is None:
+        ctx = Ctx()
+    if isinstance(file_info, str):
+        return SectionFileReader(ctx, file_info, limit)
+    elif isinstance(file_info, bytes):
+        return SectionDataReader(ctx, file_info, limit)
+    else:
+        raise Exception("参数类型错误")
+
