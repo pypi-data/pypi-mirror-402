983e3167919404257b079d10143f37ee43d1e6ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:15:16 +0000

    Add package fstdb/0.1.0/fstdb-0.1.0.tar.gz

---
+++ packages/fstdb/fstdb-0.1.0.tar.gz/fstdb-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+# Read README for long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fstdb",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="File System Database - A simple file-based database library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/fstdb",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8",
+    zip_safe=False,
+)
---
+++ packages/fstdb/fstdb-0.1.0.tar.gz/fstdb-0.1.0/src/fstdb/__init__.py
@@ -0,0 +1,11 @@
+"""
+FSTDB - File System Database
+A simple file-based database library for Python.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Your Name"
+
+from .db.tree_db import TreeDB
+
+__all__ = ["TreeDB"]
