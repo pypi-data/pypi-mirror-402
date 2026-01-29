ee6226a1473714888970f552910c82983c4a3946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:09:46 +0000

    Add package topsis-tatvam-102303484/1.0.3/topsis_tatvam_102303484-1.0.3.tar.gz

---
+++ packages/topsis-tatvam-102303484/topsis_tatvam_102303484-1.0.3.tar.gz/topsis_tatvam_102303484-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="topsis-tatvam-102303484",
+    version="1.0.3",
+    author="Tatvam Jain",
+    author_email="tjain_be23@thapar.edu",
+    description="A Python package for TOPSIS (Technique for Order of Preference by Similarity to Ideal Solution) analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tatvamjain/topsis-102303484",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pandas>=1.0.0',
+        'numpy>=1.18.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'topsis=topsis_tatvam_102303484.topsis:main',
+        ],
+    },
+)
