f93e4112d0d66242152b871d816e10c4899beda3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:12:50 +0000

    Add package topsis-hardik-102303494/0.1.0/topsis_hardik_102303494-0.1.0.tar.gz

---
+++ packages/topsis-hardik-102303494/topsis_hardik_102303494-0.1.0.tar.gz/topsis_hardik_102303494-0.1.0/README.md
@@ -0,0 +1,7 @@
+# TOPSIS-Hardik-102303494
+
+This package implements the TOPSIS (Technique for Order Preference by Similarity to Ideal Solution) method.
+
+## Installation
+```bash
+pip install topsis-hardik-102303494
---
+++ packages/topsis-hardik-102303494/topsis_hardik_102303494-0.1.0.tar.gz/topsis_hardik_102303494-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topsis-hardik-102303494",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas"],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis_hardik_102303494.cli:main"
+        ]
+    },
+    author="Hardik",
+    description="TOPSIS implementation for decision making",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+)
