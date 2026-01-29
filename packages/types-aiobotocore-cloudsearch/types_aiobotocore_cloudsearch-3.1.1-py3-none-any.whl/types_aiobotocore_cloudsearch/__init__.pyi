c1546b814a0d32410a9c017cad12a71052e18e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:58 +0000

    Add package types-aiobotocore-cloudsearch/3.1.1/types_aiobotocore_cloudsearch-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-cloudsearch/types_aiobotocore_cloudsearch-3.1.1-py3-none-any.whl/types_aiobotocore_cloudsearch/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for cloudsearch service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cloudsearch/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_cloudsearch import (
+        Client,
+        CloudSearchClient,
+    )
+
+    session = get_session()
+    async with session.create_client("cloudsearch") as client:
+        client: CloudSearchClient
+        ...
+
+    ```
+"""
+
+from .client import CloudSearchClient
+
+Client = CloudSearchClient
+
+__all__ = ("Client", "CloudSearchClient")
