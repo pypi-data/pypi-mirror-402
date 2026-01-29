1f700d1c6a096a3171d441cf14af047cc7d484bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:07:02 +0000

    Add package easyclimate-map/2026.1.0/easyclimate_map-2026.1.0-py3-none-any.whl

---
+++ packages/easyclimate-map/easyclimate_map-2026.1.0-py3-none-any.whl/easyclimate_map/__init__.py
@@ -0,0 +1,14 @@
+from .version import __version__, show_versions
+
+from .map_zh_CN import *
+from .map_tibetan_plateau import *
+from .tool import *
+
+from rich import print
+print(
+    "[bold yellow]<easyclimate-map notice>[/bold yellow]: "
+    "Maps are provided [bold]as-is[/bold]. "
+    "Users assume all risk. "
+    "No liability. "
+    "No political or territorial claims."
+)
