6ed5b9c716ce423082b401b65506389d0af4b61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 13:27:01 +0000

    Add package math-tools-v4-0/0.2.0/math_tools_v4_0-0.2.0-py3-none-any.whl

---
+++ packages/math-tools-v4-0/math_tools_v4_0-0.2.0-py3-none-any.whl/math_tools_v4_0/server.py
@@ -0,0 +1,50 @@
+"""
+我的MCP服务器
+"""
+
+from mcp.server.fastmcp import FastMCP
+import asyncio
+import sys
+from typing import Annotated
+
+# 创建MCP服务器实例
+mcp = FastMCP("DemoServer")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """
+    将两个数字相加
+    Args:
+        a: 这是参数a的描述
+        b: 这是参数b的描述
+    """
+    print(f"🔢 计算: {a} + {b} = {a + b}")
+    return a + b
+
+@mcp.tool()
+def multiply(
+    a: Annotated[float, "Annotated描述参数a"],
+    b: Annotated[float, "Annotated描述参数b"]) -> float:
+    """将两个数字相乘"""
+    result = a * b
+    print(f"🔢 计算: {a} × {b} = {result}")
+    return result
+
+@mcp.tool()
+def greet(name: str) -> str:
+    """向用户问候"""
+    greeting = f"👋 你好，{name}！欢迎使用MCP服务器。"
+    print(greeting)
+    return greeting
+
+
+def main():
+    print("🚀 MCP服务器启动中...")
+    print("📡 等待 MCP 客户端连接...")
+    #mcp.run()
+    
+    mcp.run(transport="sse")
+
+
+if __name__ == "__main__":
+    main()
