333aa20e27ab48e509ad765d65fcab655a09ac06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:40 +0000

    Add package lmfetch/0.2.0/lmfetch-0.2.0-py3-none-any.whl

---
+++ packages/lmfetch/lmfetch-0.2.0-py3-none-any.whl/lmfetch/__init__.py
@@ -0,0 +1,6 @@
+"""lmfetch - Build intelligent context from codebases and URLs for LLMs."""
+
+from .builder import ContextBuilder, ContextResult
+
+__all__ = ["ContextBuilder", "ContextResult"]
+__version__ = "0.2.0"
