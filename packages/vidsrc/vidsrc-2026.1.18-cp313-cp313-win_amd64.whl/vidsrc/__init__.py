10052ed1c7c6e96a98fb6b83140b308c21a75106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:12:47 +0000

    Add package vidsrc/2026.1.18/vidsrc-2026.1.18-cp313-cp313-win_amd64.whl

---
+++ packages/vidsrc/vidsrc-2026.1.18-cp313-cp313-win_amd64.whl/vidsrc/__init__.py
@@ -0,0 +1,10 @@
+# vidsrc/__init__.py
+
+__all__ = ['VideoSource', '__version__']
+
+from .vidsrc import VideoSource, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Vidsrc version string."""
