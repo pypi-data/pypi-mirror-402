341cdbc7f09821bac1d5bde94f47b5ef18c236de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:31 +0000

    Add package types-aiobotocore-meteringmarketplace/3.1.1/types_aiobotocore_meteringmarketplace-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-meteringmarketplace/types_aiobotocore_meteringmarketplace-3.1.1-py3-none-any.whl/types_aiobotocore_meteringmarketplace/client.pyi
@@ -0,0 +1,166 @@
+"""
+Type annotations for meteringmarketplace service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_meteringmarketplace.client import MarketplaceMeteringClient
+
+    session = get_session()
+    async with session.create_client("meteringmarketplace") as client:
+        client: MarketplaceMeteringClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .type_defs import (
+    BatchMeterUsageRequestTypeDef,
+    BatchMeterUsageResultTypeDef,
+    MeterUsageRequestTypeDef,
+    MeterUsageResultTypeDef,
+    RegisterUsageRequestTypeDef,
+    RegisterUsageResultTypeDef,
+    ResolveCustomerRequestTypeDef,
+    ResolveCustomerResultTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Self, Unpack
+else:
+    from typing_extensions import Self, Unpack
+
+__all__ = ("MarketplaceMeteringClient",)
+
+class Exceptions(BaseClientExceptions):
+    ClientError: type[BotocoreClientError]
+    CustomerNotEntitledException: type[BotocoreClientError]
+    DisabledApiException: type[BotocoreClientError]
+    DuplicateRequestException: type[BotocoreClientError]
+    ExpiredTokenException: type[BotocoreClientError]
+    IdempotencyConflictException: type[BotocoreClientError]
+    InternalServiceErrorException: type[BotocoreClientError]
+    InvalidCustomerIdentifierException: type[BotocoreClientError]
+    InvalidEndpointRegionException: type[BotocoreClientError]
+    InvalidProductCodeException: type[BotocoreClientError]
+    InvalidPublicKeyVersionException: type[BotocoreClientError]
+    InvalidRegionException: type[BotocoreClientError]
+    InvalidTagException: type[BotocoreClientError]
+    InvalidTokenException: type[BotocoreClientError]
+    InvalidUsageAllocationsException: type[BotocoreClientError]
+    InvalidUsageDimensionException: type[BotocoreClientError]
+    PlatformNotSupportedException: type[BotocoreClientError]
+    ThrottlingException: type[BotocoreClientError]
+    TimestampOutOfBoundsException: type[BotocoreClientError]
+
+class MarketplaceMeteringClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/meteringmarketplace.html#MarketplaceMetering.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        MarketplaceMeteringClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/meteringmarketplace.html#MarketplaceMetering.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/meteringmarketplace/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/meteringmarketplace/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/#generate_presigned_url)
+        """
+
+    async def batch_meter_usage(
+        self, **kwargs: Unpack[BatchMeterUsageRequestTypeDef]
+    ) -> BatchMeterUsageResultTypeDef:
+        """
+        The <code>CustomerIdentifier</code> parameter is scheduled for deprecation on
+        March 31, 2026.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/meteringmarketplace/client/batch_meter_usage.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/#batch_meter_usage)
+        """
+
+    async def meter_usage(
+        self, **kwargs: Unpack[MeterUsageRequestTypeDef]
+    ) -> MeterUsageResultTypeDef:
+        """
+        API to emit metering records.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/meteringmarketplace/client/meter_usage.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/#meter_usage)
+        """
+
+    async def register_usage(
+        self, **kwargs: Unpack[RegisterUsageRequestTypeDef]
+    ) -> RegisterUsageResultTypeDef:
+        """
+        Paid container software products sold through Amazon Web Services Marketplace
+        must integrate with the Amazon Web Services Marketplace Metering Service and
+        call the <code>RegisterUsage</code> operation for software entitlement and
+        metering.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/meteringmarketplace/client/register_usage.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/#register_usage)
+        """
+
+    async def resolve_customer(
+        self, **kwargs: Unpack[ResolveCustomerRequestTypeDef]
+    ) -> ResolveCustomerResultTypeDef:
+        """
+        <code>ResolveCustomer</code> is called by a SaaS application during the
+        registration process.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/meteringmarketplace/client/resolve_customer.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/#resolve_customer)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/meteringmarketplace.html#MarketplaceMetering.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/meteringmarketplace.html#MarketplaceMetering.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_meteringmarketplace/client/)
+        """
