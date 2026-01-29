d0c026b4a2824febb78f97959a21f6c718fbc308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:46 +0000

    Add package types-aiobotocore-savingsplans/3.1.1/types_aiobotocore_savingsplans-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-savingsplans/types_aiobotocore_savingsplans-3.1.1.tar.gz/types_aiobotocore_savingsplans-3.1.1/types_aiobotocore_savingsplans/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for savingsplans service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_savingsplans/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_savingsplans import (
+        Client,
+        SavingsPlansClient,
+    )
+
+    session = get_session()
+    async with session.create_client("savingsplans") as client:
+        client: SavingsPlansClient
+        ...
+
+    ```
+"""
+
+from .client import SavingsPlansClient
+
+Client = SavingsPlansClient
+
+__all__ = ("Client", "SavingsPlansClient")
