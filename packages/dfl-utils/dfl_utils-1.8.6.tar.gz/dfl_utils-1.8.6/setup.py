6ee3c3cd9f64bd2a5124176975589b5eb3e05ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:14 +0000

    Add package dfl-utils/1.8.6/dfl_utils-1.8.6.tar.gz

---
+++ packages/dfl-utils/dfl_utils-1.8.6.tar.gz/dfl_utils-1.8.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+setup(name='dfl_utils',
+version='1.8.6',
+description=f'A Powerful Windows-PC-Wechat automation Tool',
+long_description=open('README.md','r',encoding='utf-8').read(),
+long_description_content_type='text/markdown',
+packages=find_packages(),
+license='Apache-2.0',
+keywords=['rpa','windows','wechat','automation'],
+install_requires=[]
+)
