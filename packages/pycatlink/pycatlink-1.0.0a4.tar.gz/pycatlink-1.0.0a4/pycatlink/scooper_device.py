78f5e911828ad555c60a64b16d2966a621a7a7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:12:38 +0000

    Add package pycatlink/1.0.0a4/pycatlink-1.0.0a4.tar.gz

---
+++ packages/pycatlink/pycatlink-1.0.0a4.tar.gz/pycatlink-1.0.0a4/pycatlink/scooper_device.py
@@ -0,0 +1,30 @@
+"""Scooper device module for CatLink integration."""
+
+from .const import (
+    API_SCOOPER_INFO,
+    API_SCOOPER_LOGS,
+    DATA_KEY_SCOOPER_LOG_TOP5,
+    CatlinkAction,
+    CatlinkWorkModel,
+)
+from .device import CatlinkDevice
+from .models import CatlinkDeviceDetails
+
+
+class CatlinkScooperDevice(CatlinkDevice[CatlinkDeviceDetails]):
+    """Scooper device class for CatLink."""
+
+    DETAILS_CLASS = CatlinkDeviceDetails
+    SUPPORTED_MODES = [
+        CatlinkWorkModel.AUTO,
+        CatlinkWorkModel.MANUAL,
+        CatlinkWorkModel.TIME,
+        CatlinkWorkModel.EMPTY,
+    ]
+    SUPPORTED_ACTIONS = [
+        CatlinkAction.PAUSE,
+        CatlinkAction.START,
+    ]
+    PATH_DETAILS = API_SCOOPER_INFO
+    PATH_LOGS = API_SCOOPER_LOGS
+    RESPONSE_KEY_LOGS = DATA_KEY_SCOOPER_LOG_TOP5
