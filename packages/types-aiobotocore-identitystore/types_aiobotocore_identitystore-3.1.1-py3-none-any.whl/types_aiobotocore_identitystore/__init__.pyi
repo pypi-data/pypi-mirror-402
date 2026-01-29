07a157b6273036646ba6e9011c9e56210509ecf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:21 +0000

    Add package types-aiobotocore-identitystore/3.1.1/types_aiobotocore_identitystore-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-identitystore/types_aiobotocore_identitystore-3.1.1-py3-none-any.whl/types_aiobotocore_identitystore/__init__.pyi
@@ -0,0 +1,51 @@
+"""
+Main interface for identitystore service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_identitystore/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_identitystore import (
+        Client,
+        IdentityStoreClient,
+        ListGroupMembershipsForMemberPaginator,
+        ListGroupMembershipsPaginator,
+        ListGroupsPaginator,
+        ListUsersPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("identitystore") as client:
+        client: IdentityStoreClient
+        ...
+
+
+    list_group_memberships_for_member_paginator: ListGroupMembershipsForMemberPaginator = client.get_paginator("list_group_memberships_for_member")
+    list_group_memberships_paginator: ListGroupMembershipsPaginator = client.get_paginator("list_group_memberships")
+    list_groups_paginator: ListGroupsPaginator = client.get_paginator("list_groups")
+    list_users_paginator: ListUsersPaginator = client.get_paginator("list_users")
+    ```
+"""
+
+from .client import IdentityStoreClient
+from .paginator import (
+    ListGroupMembershipsForMemberPaginator,
+    ListGroupMembershipsPaginator,
+    ListGroupsPaginator,
+    ListUsersPaginator,
+)
+
+Client = IdentityStoreClient
+
+__all__ = (
+    "Client",
+    "IdentityStoreClient",
+    "ListGroupMembershipsForMemberPaginator",
+    "ListGroupMembershipsPaginator",
+    "ListGroupsPaginator",
+    "ListUsersPaginator",
+)
