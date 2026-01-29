af8802705b8645465ebdac4db148fda64f98f073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:11:26 +0000

    Add package licomp/0.5.20/licomp-0.5.20.tar.gz

---
+++ packages/licomp/licomp-0.5.20.tar.gz/licomp-0.5.20/licomp/config.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+licomp_version = '0.5.20'
+licomp_api_version = '.'.join(licomp_version.split('.')[0:2])
+licomp_module_name = 'licomp'
+licomp_description = 'An interface to generalise license compatibility resources'
+licomp_epilog = ''
