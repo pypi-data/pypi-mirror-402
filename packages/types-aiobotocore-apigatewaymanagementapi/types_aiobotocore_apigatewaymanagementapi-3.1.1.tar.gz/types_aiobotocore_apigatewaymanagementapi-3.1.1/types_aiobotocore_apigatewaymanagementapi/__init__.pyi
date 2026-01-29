afc89c4715e40fb3641c42df3e5eb7cd552516ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:47 +0000

    Add package types-aiobotocore-apigatewaymanagementapi/3.1.1/types_aiobotocore_apigatewaymanagementapi-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-apigatewaymanagementapi/types_aiobotocore_apigatewaymanagementapi-3.1.1.tar.gz/types_aiobotocore_apigatewaymanagementapi-3.1.1/types_aiobotocore_apigatewaymanagementapi/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for apigatewaymanagementapi service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_apigatewaymanagementapi import (
+        ApiGatewayManagementApiClient,
+        Client,
+    )
+
+    session = get_session()
+    async with session.create_client("apigatewaymanagementapi") as client:
+        client: ApiGatewayManagementApiClient
+        ...
+
+    ```
+"""
+
+from .client import ApiGatewayManagementApiClient
+
+Client = ApiGatewayManagementApiClient
+
+__all__ = ("ApiGatewayManagementApiClient", "Client")
---
+++ packages/types-aiobotocore-apigatewaymanagementapi/types_aiobotocore_apigatewaymanagementapi-3.1.1.tar.gz/types_aiobotocore_apigatewaymanagementapi-3.1.1/types_aiobotocore_apigatewaymanagementapi/client.pyi
@@ -0,0 +1,134 @@
+"""
+Type annotations for apigatewaymanagementapi service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_apigatewaymanagementapi.client import ApiGatewayManagementApiClient
+
+    session = get_session()
+    async with session.create_client("apigatewaymanagementapi") as client:
+        client: ApiGatewayManagementApiClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .type_defs import (
+    DeleteConnectionRequestTypeDef,
+    EmptyResponseMetadataTypeDef,
+    GetConnectionRequestTypeDef,
+    GetConnectionResponseTypeDef,
+    PostToConnectionRequestTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Self, Unpack
+else:
+    from typing_extensions import Self, Unpack
+
+__all__ = ("ApiGatewayManagementApiClient",)
+
+class Exceptions(BaseClientExceptions):
+    ClientError: type[BotocoreClientError]
+    ForbiddenException: type[BotocoreClientError]
+    GoneException: type[BotocoreClientError]
+    LimitExceededException: type[BotocoreClientError]
+    PayloadTooLargeException: type[BotocoreClientError]
+
+class ApiGatewayManagementApiClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/apigatewaymanagementapi.html#ApiGatewayManagementApi.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        ApiGatewayManagementApiClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/apigatewaymanagementapi.html#ApiGatewayManagementApi.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/apigatewaymanagementapi/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/apigatewaymanagementapi/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/client/#generate_presigned_url)
+        """
+
+    async def delete_connection(
+        self, **kwargs: Unpack[DeleteConnectionRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Delete the connection with the provided id.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/apigatewaymanagementapi/client/delete_connection.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/client/#delete_connection)
+        """
+
+    async def get_connection(
+        self, **kwargs: Unpack[GetConnectionRequestTypeDef]
+    ) -> GetConnectionResponseTypeDef:
+        """
+        Get information about the connection with the provided id.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/apigatewaymanagementapi/client/get_connection.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/client/#get_connection)
+        """
+
+    async def post_to_connection(
+        self, **kwargs: Unpack[PostToConnectionRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Sends the provided data to the specified connection.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/apigatewaymanagementapi/client/post_to_connection.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/client/#post_to_connection)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/apigatewaymanagementapi.html#ApiGatewayManagementApi.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/apigatewaymanagementapi.html#ApiGatewayManagementApi.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apigatewaymanagementapi/client/)
+        """
