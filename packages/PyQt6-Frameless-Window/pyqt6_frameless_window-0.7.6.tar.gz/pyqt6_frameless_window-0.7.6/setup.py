e88e7db66fe1f0e0f76d760de2a07eabb9fde435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:18 +0000

    Add package PyQt6-Frameless-Window/0.7.6/pyqt6_frameless_window-0.7.6.tar.gz

---
+++ packages/PyQt6-Frameless-Window/pyqt6_frameless_window-0.7.6.tar.gz/pyqt6_frameless_window-0.7.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="PyQt6-Frameless-Window",
+    version="0.7.6",
+    keywords="pyqt6 frameless",
+    author="zhiyiYo",
+    author_email="shokokawaii@outlook.com",
+    description="A cross-platform frameless window based on pyqt6, support Win32, Linux and macOS.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="GPLv3",
+    url="https://github.com/zhiyiYo/PyQt-Frameless-Window/tree/PyQt6",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pywin32;platform_system=='Windows'",
+        "pyobjc;platform_system=='Darwin'",
+        "PyCocoa;platform_system=='Darwin'",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent'
+    ]
+)
