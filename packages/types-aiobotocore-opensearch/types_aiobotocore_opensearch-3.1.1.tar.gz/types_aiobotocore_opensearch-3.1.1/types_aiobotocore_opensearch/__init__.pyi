e417a9a87d5e40f06308562c66fb1ce74ab053f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:36 +0000

    Add package types-aiobotocore-opensearch/3.1.1/types_aiobotocore_opensearch-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-opensearch/types_aiobotocore_opensearch-3.1.1.tar.gz/types_aiobotocore_opensearch-3.1.1/types_aiobotocore_opensearch/__init__.pyi
@@ -0,0 +1,33 @@
+"""
+Main interface for opensearch service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_opensearch/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_opensearch import (
+        Client,
+        ListApplicationsPaginator,
+        OpenSearchServiceClient,
+    )
+
+    session = get_session()
+    async with session.create_client("opensearch") as client:
+        client: OpenSearchServiceClient
+        ...
+
+
+    list_applications_paginator: ListApplicationsPaginator = client.get_paginator("list_applications")
+    ```
+"""
+
+from .client import OpenSearchServiceClient
+from .paginator import ListApplicationsPaginator
+
+Client = OpenSearchServiceClient
+
+__all__ = ("Client", "ListApplicationsPaginator", "OpenSearchServiceClient")
