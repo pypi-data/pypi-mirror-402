8fbbb0cbc8db6125d3a102b52edd3a06ea7a288c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:13:58 +0000

    Add package dragonfly-schema/1.15.3/dragonfly_schema-1.15.3.tar.gz

---
+++ packages/dragonfly-schema/dragonfly_schema-1.15.3.tar.gz/dragonfly_schema-1.15.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="dragonfly-schema",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="Dragonfly Data-Model Objects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/dragonfly-schema",
+    packages=setuptools.find_packages(exclude=["tests", "scripts", "samples"]),
+    install_requires=requirements,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Operating System :: OS Independent"
+    ],
+    license="MIT"
+)
