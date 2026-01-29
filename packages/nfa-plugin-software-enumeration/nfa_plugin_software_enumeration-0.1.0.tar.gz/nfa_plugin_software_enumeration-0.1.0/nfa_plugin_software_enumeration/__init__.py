3e7d27b0e4fcaf646c1e00e37a3a96a3f0632648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:12 +0000

    Add package nfa-plugin-software-enumeration/0.1.0/nfa_plugin_software_enumeration-0.1.0.tar.gz

---
+++ packages/nfa-plugin-software-enumeration/nfa_plugin_software_enumeration-0.1.0.tar.gz/nfa_plugin_software_enumeration-0.1.0/nfa_plugin_software_enumeration/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Software Enumeration Plugin for NFA.
+"""
+
+__version__ = "0.1.0"
---
+++ packages/nfa-plugin-software-enumeration/nfa_plugin_software_enumeration-0.1.0.tar.gz/nfa_plugin_software_enumeration-0.1.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nfa-plugin-software-enumeration",
+    version="0.1.0",
+    author="Damian Krawczyk",
+    author_email="damian.krawczyk@limberduck.org",
+    description="Software Enumeration report plugin for LimberDuck NFA (nessus file analyzer)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://limberduck.org/en/latest/tools/nessus-file-analyzer/advanced-reports/software-enumeration/",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "nessus-file-analyzer>=0.11.0",
+    ],
+    entry_points={
+        "nfa.plugins": [
+            "software_enumeration = nfa_plugin_software_enumeration.software_enumeration:SoftwareEnumerationPlugin",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires=">=3.10",
+)
