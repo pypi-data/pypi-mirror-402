2600274785539d62b73fdf28cc9b9ecd5cc84df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:33 +0000

    Add package types-aiobotocore-mwaa/3.1.1/types_aiobotocore_mwaa-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-mwaa/types_aiobotocore_mwaa-3.1.1.tar.gz/types_aiobotocore_mwaa-3.1.1/types_aiobotocore_mwaa/__init__.pyi
@@ -0,0 +1,33 @@
+"""
+Main interface for mwaa service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mwaa/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_mwaa import (
+        Client,
+        ListEnvironmentsPaginator,
+        MWAAClient,
+    )
+
+    session = get_session()
+    async with session.create_client("mwaa") as client:
+        client: MWAAClient
+        ...
+
+
+    list_environments_paginator: ListEnvironmentsPaginator = client.get_paginator("list_environments")
+    ```
+"""
+
+from .client import MWAAClient
+from .paginator import ListEnvironmentsPaginator
+
+Client = MWAAClient
+
+__all__ = ("Client", "ListEnvironmentsPaginator", "MWAAClient")
