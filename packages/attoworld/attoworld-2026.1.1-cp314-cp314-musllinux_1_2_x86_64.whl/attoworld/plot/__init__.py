40aa41ea671422c50bec10bbea1a5e4741f64b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:09:24 +0000

    Add package attoworld/2026.1.1/attoworld-2026.1.1-cp314-cp314-musllinux_1_2_x86_64.whl

---
+++ packages/attoworld/attoworld-2026.1.1-cp314-cp314-musllinux_1_2_x86_64.whl/attoworld/plot/__init__.py
@@ -0,0 +1,5 @@
+"""Contains functions for plotting, and for formatting plots, with the goal of more easily obtaining a consistent style across plots made by different co-authors."""
+
+from .plot import Char, label_letter, set_style, showmo
+
+__all__ = ["showmo", "set_style", "label_letter", "Char"]
