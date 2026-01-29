604e072e72e944b72c6e965f58b9cb368eb16ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:14:47 +0000

    Add package terminalbars/1.0/terminalbars-1.0.tar.gz

---
+++ packages/terminalbars/terminalbars-1.0.tar.gz/terminalbars-1.0/terminalprobar/__init__.py
@@ -0,0 +1,6 @@
+from terminalprobar import pasek
+from terminalprobar import superpasek
+from terminalprobar import propasek
+from terminalprobar import ultrapasek
+from terminalprobar import ultrapropasek
+import terminalprobar
