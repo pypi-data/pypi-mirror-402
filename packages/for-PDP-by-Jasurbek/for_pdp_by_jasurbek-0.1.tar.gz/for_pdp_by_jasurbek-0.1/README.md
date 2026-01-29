5f28ae17d047f0dca0f9e9cd681961d77041c117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:19:35 +0000

    Add package for-PDP-by-Jasurbek/0.1/for_pdp_by_jasurbek-0.1.tar.gz

---
+++ packages/for-PDP-by-Jasurbek/for_pdp_by_jasurbek-0.1.tar.gz/for_pdp_by_jasurbek-0.1/README.md
@@ -0,0 +1,5 @@
+for online lesson
+
+```jsunicoderegexp
+always returns "Hello, PyPI!"
+```
---
+++ packages/for-PDP-by-Jasurbek/for_pdp_by_jasurbek-0.1.tar.gz/for_pdp_by_jasurbek-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='for_PDP_by_Jasurbek',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+    author='Jasurbek',
+    author_email='olimjonovzilxa@gmail.com',
+    description='For PDP lesson',
+    long_description=open('README.md').read(),
+    long_descriptoin_content_type='text/markdown',
+    licence='MIT'
+)
