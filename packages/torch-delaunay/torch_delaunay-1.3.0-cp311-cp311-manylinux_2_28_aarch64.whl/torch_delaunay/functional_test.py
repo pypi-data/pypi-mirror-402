b4eefa603c2610f11d996388a9f52b4b3f6780fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:39 +0000

    Add package torch-delaunay/1.3.0/torch_delaunay-1.3.0-cp311-cp311-manylinux_2_28_aarch64.whl

---
+++ packages/torch-delaunay/torch_delaunay-1.3.0-cp311-cp311-manylinux_2_28_aarch64.whl/torch_delaunay/functional_test.py
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2025 Yakau Bubnou
+# SPDX-FileType: SOURCE
+
+import torch
+from shapely import Point
+from geopandas import GeoDataFrame
+from geopandas import points_from_xy
+
+from torch_delaunay.functional import shull2d, circumcenter2d, circumradius2d
+
+
+def test_shull2d() -> None:
+    points = torch.randn((1000, 2), dtype=torch.float64)
+    simplices = shull2d(points)
+
+    assert (simplices >= 1000).sum() == 0, "simplices are outside of points array limit"
+
+    assert simplices.shape[1] == 3
+    assert simplices.shape[0] > 0
+
+    vertices = GeoDataFrame(geometry=points_from_xy(points[:, 0], points[:, 1]))
+
+    # Ensure that triangles comply with Delaunay definition.
+    tri = points[simplices]
+
+    centers = circumcenter2d(tri[:, 0], tri[:, 1], tri[:, 2]).detach().numpy()
+    radii = circumradius2d(tri[:, 0], tri[:, 1], tri[:, 2]).detach().numpy()
+
+    for center, radius in zip(centers, radii):
+        circle = Point(*center).buffer(radius)
+        incircle = vertices.intersection(circle)
+        assert (~incircle.is_empty).sum() <= 3
