5c73483c301a41440a0351db5bb14c7875afae67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:10:51 +0000

    Add package discordbotframework/0.1.0/discordbotframework-0.1.0-py3-none-any.whl

---
+++ packages/discordbotframework/discordbotframework-0.1.0-py3-none-any.whl/botframework/cli.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import argparse
+from .builder import rebuild_project
+
+def main():
+    parser = argparse.ArgumentParser(prog="myframework")
+    parser.add_argument("command", choices=["init"])
+    parser.add_argument("target", nargs="?", help="Target folder for project")
+    args = parser.parse_args()
+
+    if args.command == "init":
+        target_dir = args.target or "my_project"
+        dump_file = os.path.join(os.path.dirname(__file__), "dump.json")
+        print(f"Building project in {target_dir}...")
+        rebuild_project(dump_file, target_dir)
+        print("Project created! 🎉")
