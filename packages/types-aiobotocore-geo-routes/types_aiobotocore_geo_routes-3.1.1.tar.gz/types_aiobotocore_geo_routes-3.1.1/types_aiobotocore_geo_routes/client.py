e8c31d247337047fd62b3e5697aef3985d885624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:20 +0000

    Add package types-aiobotocore-geo-routes/3.1.1/types_aiobotocore_geo_routes-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-geo-routes/types_aiobotocore_geo_routes-3.1.1.tar.gz/types_aiobotocore_geo_routes-3.1.1/types_aiobotocore_geo_routes/client.py
@@ -0,0 +1,168 @@
+"""
+Type annotations for geo-routes service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_geo_routes.client import LocationServiceRoutesV2Client
+
+    session = get_session()
+    async with session.create_client("geo-routes") as client:
+        client: LocationServiceRoutesV2Client
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .type_defs import (
+    CalculateIsolinesRequestTypeDef,
+    CalculateIsolinesResponseTypeDef,
+    CalculateRouteMatrixRequestTypeDef,
+    CalculateRouteMatrixResponseTypeDef,
+    CalculateRoutesRequestTypeDef,
+    CalculateRoutesResponseTypeDef,
+    OptimizeWaypointsRequestTypeDef,
+    OptimizeWaypointsResponseTypeDef,
+    SnapToRoadsRequestTypeDef,
+    SnapToRoadsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Self, Unpack
+else:
+    from typing_extensions import Self, Unpack
+
+
+__all__ = ("LocationServiceRoutesV2Client",)
+
+
+class Exceptions(BaseClientExceptions):
+    AccessDeniedException: type[BotocoreClientError]
+    ClientError: type[BotocoreClientError]
+    InternalServerException: type[BotocoreClientError]
+    ThrottlingException: type[BotocoreClientError]
+    ValidationException: type[BotocoreClientError]
+
+
+class LocationServiceRoutesV2Client(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes.html#LocationServiceRoutesV2.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        LocationServiceRoutesV2Client exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes.html#LocationServiceRoutesV2.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/#generate_presigned_url)
+        """
+
+    async def calculate_isolines(
+        self, **kwargs: Unpack[CalculateIsolinesRequestTypeDef]
+    ) -> CalculateIsolinesResponseTypeDef:
+        """
+        Use the <code>CalculateIsolines</code> action to find service areas that can be
+        reached in a given threshold of time, distance.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes/client/calculate_isolines.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/#calculate_isolines)
+        """
+
+    async def calculate_route_matrix(
+        self, **kwargs: Unpack[CalculateRouteMatrixRequestTypeDef]
+    ) -> CalculateRouteMatrixResponseTypeDef:
+        """
+        Use <code>CalculateRouteMatrix</code> to compute results for all pairs of
+        Origins to Destinations.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes/client/calculate_route_matrix.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/#calculate_route_matrix)
+        """
+
+    async def calculate_routes(
+        self, **kwargs: Unpack[CalculateRoutesRequestTypeDef]
+    ) -> CalculateRoutesResponseTypeDef:
+        """
+        <code>CalculateRoutes</code> computes routes given the following required
+        parameters: <code>Origin</code> and <code>Destination</code>.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes/client/calculate_routes.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/#calculate_routes)
+        """
+
+    async def optimize_waypoints(
+        self, **kwargs: Unpack[OptimizeWaypointsRequestTypeDef]
+    ) -> OptimizeWaypointsResponseTypeDef:
+        """
+        <code>OptimizeWaypoints</code> calculates the optimal order to travel between a
+        set of waypoints to minimize either the travel time or the distance travelled
+        during the journey, based on road network restrictions and the traffic pattern
+        data.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes/client/optimize_waypoints.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/#optimize_waypoints)
+        """
+
+    async def snap_to_roads(
+        self, **kwargs: Unpack[SnapToRoadsRequestTypeDef]
+    ) -> SnapToRoadsResponseTypeDef:
+        """
+        <code>SnapToRoads</code> matches GPS trace to roads most likely traveled on.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes/client/snap_to_roads.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/#snap_to_roads)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes.html#LocationServiceRoutesV2.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/geo-routes.html#LocationServiceRoutesV2.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_geo_routes/client/)
+        """
