010f07707f37dcb4b8f16902850b28b9b5455657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:11:10 +0000

    Add package topsis-purnika-102303412/1.0.2/topsis_purnika_102303412-1.0.2.tar.gz

---
+++ packages/topsis-purnika-102303412/topsis_purnika_102303412-1.0.2.tar.gz/topsis_purnika_102303412-1.0.2/README.md
@@ -0,0 +1,27 @@
+# TOPSIS – Multi-Criteria Decision Making Tool
+
+This Python package implements the **TOPSIS (Technique for Order Preference by Similarity to Ideal Solution)** method.  
+It is developed to rank alternatives based on multiple criteria and is useful in decision-making problems such as product selection, college ranking, supplier evaluation, and more.
+
+---
+
+##  Author
+
+This package has been **created and developed by Purnika Malhotra** 
+---
+
+##  Features
+- Command-line based TOPSIS implementation  
+- Accepts CSV input file  
+- Supports user-defined weights and impacts  
+- Generates ranked output CSV file  
+- Proper input validation and error handling  
+
+---
+
+##  Installation
+
+Install the package using pip:
+
+```bash
+pip install Topsis-Purnika-102303412
---
+++ packages/topsis-purnika-102303412/topsis_purnika_102303412-1.0.2.tar.gz/topsis_purnika_102303412-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="topsis-purnika-102303412",
+    version="1.0.2",   # 🔴 VERSION BUMP IS MUST
+    author="Purnika Malhotra",
+    author_email="purnikamalhotra53@gmail.com",
+    description="This package implements the TOPSIS method for multi-criteria decision making using a command-line interface. It allows users to rank alternatives from a CSV file by specifying custom weights and impacts. The package includes input validation and generates a ranked output file efficiently. It is designed for academic and practical decision-analysis use.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy"
+    ],
+    entry_points={
+        "console_scripts": [
+            "topsis=TOPSIS_MAIN.topsis:main"
+        ]
+    },
+    python_requires=">=3.8",
+)
+
