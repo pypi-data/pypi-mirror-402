1a70697a5db8f47a2c51988dbd0362def798c5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:45 +0000

    Add package libsemigroups-pybind11/1.3.0/libsemigroups_pybind11-1.3.0-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.3.0-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/libsemigroups_pybind11/words.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2024 Joseph Edwards
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+#
+
+"""This page contains the documentation for the ``words`` subpackage, that
+contains helper functions related to words.
+"""
+
+from _libsemigroups_pybind11 import (  # pylint: disable=unused-import,redefined-builtin
+    words_human_readable_index as human_readable_index,
+    words_human_readable_letter as human_readable_letter,
+    words_parse_relations as parse_relations,
+    words_pow as pow,
+    words_prod as prod,
+)
