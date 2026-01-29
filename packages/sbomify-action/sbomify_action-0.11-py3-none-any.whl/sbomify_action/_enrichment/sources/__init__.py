047a7480ff5a038b4f342946ff01a40207f7c249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:13:47 +0000

    Add package sbomify-action/0.11/sbomify_action-0.11-py3-none-any.whl

---
+++ packages/sbomify-action/sbomify_action-0.11-py3-none-any.whl/sbomify_action/_enrichment/sources/__init__.py
@@ -0,0 +1,27 @@
+"""Data source implementations for SBOM enrichment."""
+
+from .clearlydefined import ClearlyDefinedSource
+from .cratesio import CratesIOSource
+from .debian import DebianSource
+from .depsdev import DepsDevSource
+from .ecosystems import EcosystemsSource
+from .license_db import LicenseDBSource
+from .lifecycle import LifecycleSource
+from .pubdev import PubDevSource
+from .purl import PURLSource
+from .pypi import PyPISource
+from .repology import RepologySource
+
+__all__ = [
+    "PyPISource",
+    "PubDevSource",
+    "CratesIOSource",
+    "DebianSource",
+    "DepsDevSource",
+    "EcosystemsSource",
+    "PURLSource",
+    "ClearlyDefinedSource",
+    "RepologySource",
+    "LicenseDBSource",
+    "LifecycleSource",
+]
