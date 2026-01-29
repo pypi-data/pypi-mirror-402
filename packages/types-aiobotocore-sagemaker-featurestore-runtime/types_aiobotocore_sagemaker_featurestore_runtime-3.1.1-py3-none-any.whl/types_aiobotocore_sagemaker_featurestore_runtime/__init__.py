da0cb8b3eaa4ae8bf194f81c19f066996a1a62d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:45 +0000

    Add package types-aiobotocore-sagemaker-featurestore-runtime/3.1.1/types_aiobotocore_sagemaker_featurestore_runtime-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-sagemaker-featurestore-runtime/types_aiobotocore_sagemaker_featurestore_runtime-3.1.1-py3-none-any.whl/types_aiobotocore_sagemaker_featurestore_runtime/__init__.py
@@ -0,0 +1,30 @@
+"""
+Main interface for sagemaker-featurestore-runtime service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sagemaker_featurestore_runtime/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_sagemaker_featurestore_runtime import (
+        Client,
+        SageMakerFeatureStoreRuntimeClient,
+    )
+
+    session = get_session()
+    async with session.create_client("sagemaker-featurestore-runtime") as client:
+        client: SageMakerFeatureStoreRuntimeClient
+        ...
+
+    ```
+"""
+
+from .client import SageMakerFeatureStoreRuntimeClient
+
+Client = SageMakerFeatureStoreRuntimeClient
+
+
+__all__ = ("Client", "SageMakerFeatureStoreRuntimeClient")
