75708dd1126c57f7ff9d4cd394ebb2ef1c64ad28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:31 +0000

    Add package types-aiobotocore-mediatailor/3.1.1/types_aiobotocore_mediatailor-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-mediatailor/types_aiobotocore_mediatailor-3.1.1.tar.gz/types_aiobotocore_mediatailor-3.1.1/types_aiobotocore_mediatailor/__init__.py
@@ -0,0 +1,68 @@
+"""
+Main interface for mediatailor service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediatailor/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_mediatailor import (
+        Client,
+        GetChannelSchedulePaginator,
+        ListAlertsPaginator,
+        ListChannelsPaginator,
+        ListLiveSourcesPaginator,
+        ListPlaybackConfigurationsPaginator,
+        ListPrefetchSchedulesPaginator,
+        ListSourceLocationsPaginator,
+        ListVodSourcesPaginator,
+        MediaTailorClient,
+    )
+
+    session = get_session()
+    async with session.create_client("mediatailor") as client:
+        client: MediaTailorClient
+        ...
+
+
+    get_channel_schedule_paginator: GetChannelSchedulePaginator = client.get_paginator("get_channel_schedule")
+    list_alerts_paginator: ListAlertsPaginator = client.get_paginator("list_alerts")
+    list_channels_paginator: ListChannelsPaginator = client.get_paginator("list_channels")
+    list_live_sources_paginator: ListLiveSourcesPaginator = client.get_paginator("list_live_sources")
+    list_playback_configurations_paginator: ListPlaybackConfigurationsPaginator = client.get_paginator("list_playback_configurations")
+    list_prefetch_schedules_paginator: ListPrefetchSchedulesPaginator = client.get_paginator("list_prefetch_schedules")
+    list_source_locations_paginator: ListSourceLocationsPaginator = client.get_paginator("list_source_locations")
+    list_vod_sources_paginator: ListVodSourcesPaginator = client.get_paginator("list_vod_sources")
+    ```
+"""
+
+from .client import MediaTailorClient
+from .paginator import (
+    GetChannelSchedulePaginator,
+    ListAlertsPaginator,
+    ListChannelsPaginator,
+    ListLiveSourcesPaginator,
+    ListPlaybackConfigurationsPaginator,
+    ListPrefetchSchedulesPaginator,
+    ListSourceLocationsPaginator,
+    ListVodSourcesPaginator,
+)
+
+Client = MediaTailorClient
+
+
+__all__ = (
+    "Client",
+    "GetChannelSchedulePaginator",
+    "ListAlertsPaginator",
+    "ListChannelsPaginator",
+    "ListLiveSourcesPaginator",
+    "ListPlaybackConfigurationsPaginator",
+    "ListPrefetchSchedulesPaginator",
+    "ListSourceLocationsPaginator",
+    "ListVodSourcesPaginator",
+    "MediaTailorClient",
+)
