c4274be0b0824cd924705483c898d6a06d0d77ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:34:30 +0000

    Add package razc-nester/1.0.0/razc_nester-1.0.0.tar.gz

---
+++ packages/razc-nester/razc_nester-1.0.0.tar.gz/razc_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+ name = 'razc-nester',
+ version = '1.0.0',
+ py_modules = ['razc-nester'],
+ author = 'hfpython',
+ author_email = 'hfpython@headfirstlabs.com',
+ url = 'http://www.headfirstlabs.com',
+ description = 'A simple printer of nested lists',
+ )
