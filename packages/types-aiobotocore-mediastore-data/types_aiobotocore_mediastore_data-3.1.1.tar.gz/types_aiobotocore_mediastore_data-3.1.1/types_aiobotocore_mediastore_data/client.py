622d2109630f784a2db798e8bc7b17c1efed80c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:31 +0000

    Add package types-aiobotocore-mediastore-data/3.1.1/types_aiobotocore_mediastore_data-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-mediastore-data/types_aiobotocore_mediastore_data-3.1.1.tar.gz/types_aiobotocore_mediastore_data-3.1.1/types_aiobotocore_mediastore_data/client.py
@@ -0,0 +1,171 @@
+"""
+Type annotations for mediastore-data service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_mediastore_data.client import MediaStoreDataClient
+
+    session = get_session()
+    async with session.create_client("mediastore-data") as client:
+        client: MediaStoreDataClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import ListItemsPaginator
+from .type_defs import (
+    DeleteObjectRequestTypeDef,
+    DescribeObjectRequestTypeDef,
+    DescribeObjectResponseTypeDef,
+    GetObjectRequestTypeDef,
+    GetObjectResponseTypeDef,
+    ListItemsRequestTypeDef,
+    ListItemsResponseTypeDef,
+    PutObjectRequestTypeDef,
+    PutObjectResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Self, Unpack
+else:
+    from typing_extensions import Literal, Self, Unpack
+
+
+__all__ = ("MediaStoreDataClient",)
+
+
+class Exceptions(BaseClientExceptions):
+    ClientError: type[BotocoreClientError]
+    ContainerNotFoundException: type[BotocoreClientError]
+    InternalServerError: type[BotocoreClientError]
+    ObjectNotFoundException: type[BotocoreClientError]
+    RequestedRangeNotSatisfiableException: type[BotocoreClientError]
+
+
+class MediaStoreDataClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data.html#MediaStoreData.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        MediaStoreDataClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data.html#MediaStoreData.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/#generate_presigned_url)
+        """
+
+    async def delete_object(self, **kwargs: Unpack[DeleteObjectRequestTypeDef]) -> dict[str, Any]:
+        """
+        Deletes an object at the specified path.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data/client/delete_object.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/#delete_object)
+        """
+
+    async def describe_object(
+        self, **kwargs: Unpack[DescribeObjectRequestTypeDef]
+    ) -> DescribeObjectResponseTypeDef:
+        """
+        Gets the headers for an object at the specified path.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data/client/describe_object.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/#describe_object)
+        """
+
+    async def get_object(
+        self, **kwargs: Unpack[GetObjectRequestTypeDef]
+    ) -> GetObjectResponseTypeDef:
+        """
+        Downloads the object at the specified path.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data/client/get_object.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/#get_object)
+        """
+
+    async def list_items(
+        self, **kwargs: Unpack[ListItemsRequestTypeDef]
+    ) -> ListItemsResponseTypeDef:
+        """
+        Provides a list of metadata entries about folders and objects in the specified
+        folder.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data/client/list_items.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/#list_items)
+        """
+
+    async def put_object(
+        self, **kwargs: Unpack[PutObjectRequestTypeDef]
+    ) -> PutObjectResponseTypeDef:
+        """
+        Uploads an object to the specified path.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data/client/put_object.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/#put_object)
+        """
+
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_items"]
+    ) -> ListItemsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data.html#MediaStoreData.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data.html#MediaStoreData.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/client/)
+        """
---
+++ packages/types-aiobotocore-mediastore-data/types_aiobotocore_mediastore_data-3.1.1.tar.gz/types_aiobotocore_mediastore_data-3.1.1/types_aiobotocore_mediastore_data/paginator.pyi
@@ -0,0 +1,58 @@
+"""
+Type annotations for mediastore-data service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_mediastore_data.client import MediaStoreDataClient
+    from types_aiobotocore_mediastore_data.paginator import (
+        ListItemsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("mediastore-data") as client:
+        client: MediaStoreDataClient
+
+        list_items_paginator: ListItemsPaginator = client.get_paginator("list_items")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import ListItemsRequestPaginateTypeDef, ListItemsResponseTypeDef
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("ListItemsPaginator",)
+
+if TYPE_CHECKING:
+    _ListItemsPaginatorBase = AioPaginator[ListItemsResponseTypeDef]
+else:
+    _ListItemsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListItemsPaginator(_ListItemsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data/paginator/ListItems.html#MediaStoreData.Paginator.ListItems)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/paginators/#listitemspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListItemsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListItemsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore-data/paginator/ListItems.html#MediaStoreData.Paginator.ListItems.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore_data/paginators/#listitemspaginator)
+        """
