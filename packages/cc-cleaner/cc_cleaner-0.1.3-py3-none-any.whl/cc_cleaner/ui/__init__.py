2aff0bcd45ac81cd8d9616ab32622cdb8ae7c15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:07:29 +0000

    Add package cc-cleaner/0.1.3/cc_cleaner-0.1.3-py3-none-any.whl

---
+++ packages/cc-cleaner/cc_cleaner-0.1.3-py3-none-any.whl/cc_cleaner/ui/__init__.py
@@ -0,0 +1,29 @@
+"""UI module for dev-cleaner."""
+
+from .console import (
+    confirm_clean,
+    console,
+    create_progress,
+    create_scan_progress,
+    print_clean_results,
+    print_cleaner_detail,
+    print_cleaner_list,
+    print_error,
+    print_status_table,
+    print_success,
+    print_warning,
+)
+
+__all__ = [
+    "confirm_clean",
+    "console",
+    "create_progress",
+    "create_scan_progress",
+    "print_clean_results",
+    "print_cleaner_detail",
+    "print_cleaner_list",
+    "print_error",
+    "print_status_table",
+    "print_success",
+    "print_warning",
+]
