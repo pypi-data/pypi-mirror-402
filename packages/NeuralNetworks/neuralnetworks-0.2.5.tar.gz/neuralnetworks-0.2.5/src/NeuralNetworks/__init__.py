d78160b2ddc8bdcefa61d09e966365c18c92e182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:13:35 +0000

    Add package NeuralNetworks/0.2.5/neuralnetworks-0.2.5.tar.gz

---
+++ packages/NeuralNetworks/neuralnetworks-0.2.5.tar.gz/neuralnetworks-0.2.5/src/NeuralNetworks/__init__.py
@@ -0,0 +1,18 @@
+# NeuralNetworks - Multi-Layer Perceptrons avec encodage Fourier
+# Copyright (C) 2026 Alexandre Brun
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Import des dépendances et utilitaires globaux (device, settings, tensorise, etc.)
+from .Dependances import norms, crits, optims, rglen, device, pi, e, tensorise
+
+# Modèle MLP principal + fonction d'entraînement associée
+from .MLP import MLP
+
+from .Trainer import Trainer
+
+from .UI import *
+
+__version__ = "0.2.5"
