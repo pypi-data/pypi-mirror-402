ce134b3bbffe416b20b5800105520da7648d2dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:12:27 +0000

    Add package dorkbot/1.7.2/dorkbot-1.7.2-py3-none-any.whl

---
+++ packages/dorkbot/dorkbot-1.7.2-py3-none-any.whl/dorkbot/indexers/stdin.py
@@ -0,0 +1,21 @@
+import io
+import logging
+import sys
+from urllib.parse import urlparse
+
+
+def populate_parser(args, parser):
+    module_group = parser.add_argument_group(
+        __name__, "Accepts urls from stdin, one per line"
+    )  # noqa:F841
+
+
+def run(args):
+    source = __name__.split(".")[-1]
+    with io.open(sys.stdin.fileno(), encoding="utf-8") as stdin:
+        results = [urlparse(item.strip()).geturl() for item in stdin]
+
+        for result in results:
+            logging.debug(result)
+        logging.info("Fetched %d results", len(results))
+        return results, source
