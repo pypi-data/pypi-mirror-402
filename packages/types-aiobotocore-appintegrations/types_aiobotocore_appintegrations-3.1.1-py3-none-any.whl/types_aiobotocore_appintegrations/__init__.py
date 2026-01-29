306baa345cdb34f1b4e7b0e7f22968a97e24d952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:47 +0000

    Add package types-aiobotocore-appintegrations/3.1.1/types_aiobotocore_appintegrations-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-appintegrations/types_aiobotocore_appintegrations-3.1.1-py3-none-any.whl/types_aiobotocore_appintegrations/__init__.py
@@ -0,0 +1,60 @@
+"""
+Main interface for appintegrations service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_appintegrations/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_appintegrations import (
+        AppIntegrationsServiceClient,
+        Client,
+        ListApplicationAssociationsPaginator,
+        ListApplicationsPaginator,
+        ListDataIntegrationAssociationsPaginator,
+        ListDataIntegrationsPaginator,
+        ListEventIntegrationAssociationsPaginator,
+        ListEventIntegrationsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("appintegrations") as client:
+        client: AppIntegrationsServiceClient
+        ...
+
+
+    list_application_associations_paginator: ListApplicationAssociationsPaginator = client.get_paginator("list_application_associations")
+    list_applications_paginator: ListApplicationsPaginator = client.get_paginator("list_applications")
+    list_data_integration_associations_paginator: ListDataIntegrationAssociationsPaginator = client.get_paginator("list_data_integration_associations")
+    list_data_integrations_paginator: ListDataIntegrationsPaginator = client.get_paginator("list_data_integrations")
+    list_event_integration_associations_paginator: ListEventIntegrationAssociationsPaginator = client.get_paginator("list_event_integration_associations")
+    list_event_integrations_paginator: ListEventIntegrationsPaginator = client.get_paginator("list_event_integrations")
+    ```
+"""
+
+from .client import AppIntegrationsServiceClient
+from .paginator import (
+    ListApplicationAssociationsPaginator,
+    ListApplicationsPaginator,
+    ListDataIntegrationAssociationsPaginator,
+    ListDataIntegrationsPaginator,
+    ListEventIntegrationAssociationsPaginator,
+    ListEventIntegrationsPaginator,
+)
+
+Client = AppIntegrationsServiceClient
+
+
+__all__ = (
+    "AppIntegrationsServiceClient",
+    "Client",
+    "ListApplicationAssociationsPaginator",
+    "ListApplicationsPaginator",
+    "ListDataIntegrationAssociationsPaginator",
+    "ListDataIntegrationsPaginator",
+    "ListEventIntegrationAssociationsPaginator",
+    "ListEventIntegrationsPaginator",
+)
