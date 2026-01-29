e6fad0ce9fbc867748e77eac4e6a898baf7c46f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:12:07 +0000

    Add package rocketcea/1.2.2/rocketcea-1.2.2-cp310-cp310-win_amd64.whl

---
+++ packages/rocketcea/rocketcea-1.2.2-cp310-cp310-win_amd64.whl/rocketcea/find_mingw_lib.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+"""
+For users that compile FORTRAN with MinGW, python 3.8 and above need to 
+add the import path of the DLL libraries.
+"""
+
+here = os.path.abspath(os.path.dirname(__file__))
+if hasattr(os, 'add_dll_directory'):
+    #os.add_dll_directory( here )
+    if os.path.isdir( os.path.join(here, '.libs') ):
+        os.add_dll_directory( os.path.join( here, '.libs') )
+
+def add_mingw_lib():
+    # python >=3.8 needs to be given permission to import DLL files.
+    if hasattr(os, 'add_dll_directory'):
+        is_64_bit = sys.maxsize > 2**32 
+
+        path_str = os.environ.get('PATH')
+
+        pathL = path_str.split(';')
+        for path in pathL:
+            slower = path.lower()
+            if slower.endswith('bin'):
+                if is_64_bit:
+                    if slower.find('mingw64') >= 0:
+                        #print( '-->Adding: "%s"'%path,'to os.add_dll_directory' )
+                        os.add_dll_directory( path )
+                else:
+                    if slower.find('mingw32') >= 0:
+                        #print( '-->Adding: "%s"'%path,'to os.add_dll_directory' )
+                        os.add_dll_directory( path )
+    
+    
+    
