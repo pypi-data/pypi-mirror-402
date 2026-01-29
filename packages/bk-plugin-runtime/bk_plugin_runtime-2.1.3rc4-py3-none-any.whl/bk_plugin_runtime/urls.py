145b1a8ee649d04df10409d05877c66ae721ce9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:12:53 +0000

    Add package bk-plugin-runtime/2.1.3rc4/bk_plugin_runtime-2.1.3rc4-py3-none-any.whl

---
+++ packages/bk-plugin-runtime/bk_plugin_runtime-2.1.3rc4-py3-none-any.whl/bk_plugin_runtime/urls.py
@@ -0,0 +1,51 @@
+"""
+Tencent is pleased to support the open source community by making 蓝鲸智云 - PaaS平台 (BlueKing - PaaS System) available.
+Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="PluginService API",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+    re_path(r"^admin/", admin.site.urls),
+    re_path(r"^account/", include("blueapps.account.urls")),
+    re_path(r"^i18n/", include("django.conf.urls.i18n")),
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    re_path(r"^bk_plugin/", include("bk_plugin_framework.services.bpf_service.urls")),
+]
+
+
+if settings.ENVIRONMENT == "dev":
+    from bk_plugin_framework.services.debug_panel.views import debug_panel
+
+    urlpatterns.extend(
+        [
+            re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+            re_path(r"^$", debug_panel, name="debug-panel"),
+        ]
+    )
+else:
+    urlpatterns.append(
+        re_path(r"^$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    )
