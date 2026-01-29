0374b8a78639ee1808c339a62cdec71d01437985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:16:05 +0000

    Add package python-qlv-helper/0.9.5/python_qlv_helper-0.9.5.tar.gz

---
+++ packages/python-qlv-helper/python_qlv_helper-0.9.5.tar.gz/python_qlv_helper-0.9.5/qlv_helper/config/url_const.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+# ---------------------------------------------------------------------------------------------------------
+# ProjectName:  qlv-helper
+# FileName:     url_const.py
+# Description:  url配置常量
+# Author:       ASUS
+# CreateDate:   2025/12/16
+# Copyright ©2011-2025. Hunan xxxxxxx Company limited. All rights reserved.
+# ---------------------------------------------------------------------------------------------------------
+"""
+
+order_detail_url = "/OrderProcessing/NewTicket_show/{}?&r={}"
+domestic_activity_order_url = "/OrderList/GuoNei_ActivityOrders"
+login_url = "/Home/Login"
