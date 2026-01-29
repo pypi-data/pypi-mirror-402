7131b337d47c92b11163bb300e9e8cd423f587c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:47:17 +0000

    Add package trustifi/29/trustifi-29-py3-none-any.whl

---
+++ packages/trustifi/trustifi-29-py3-none-any.whl/trustifi/__main__.py
@@ -0,0 +1,8 @@
+import argparse
+from trustifi import contents, where
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--contents", action="store_true")
+args = parser.parse_args()
+
+print(contents() if args.contents else where())
