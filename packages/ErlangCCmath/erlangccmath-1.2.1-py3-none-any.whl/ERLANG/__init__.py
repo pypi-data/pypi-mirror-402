2ae15dde7f11cd3b32bbc80d6b101c85e4e12cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:17:58 +0000

    Add package ErlangCCmath/1.2.1/erlangccmath-1.2.1-py3-none-any.whl

---
+++ packages/ErlangCCmath/erlangccmath-1.2.1-py3-none-any.whl/ERLANG/__init__.py
@@ -0,0 +1,9 @@
+# =============================================================================
+# IMPORT SUB-PACKAGES
+# =============================================================================
+from ERLANG.X import *
+from ERLANG.BL import *
+from ERLANG.CHAT import *
+from ERLANG.settings import *
+from ERLANG.settings import setUID, getUID
+
