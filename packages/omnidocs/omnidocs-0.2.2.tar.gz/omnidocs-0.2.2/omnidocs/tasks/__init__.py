dac442e51dc196d8e5e3eaa8b47973cffdb998b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:12:19 +0000

    Add package omnidocs/0.2.2/omnidocs-0.2.2.tar.gz

---
+++ packages/omnidocs/omnidocs-0.2.2.tar.gz/omnidocs-0.2.2/omnidocs/tasks/__init__.py
@@ -0,0 +1,11 @@
+"""
+OmniDocs Task Modules.
+
+Each task module provides extractors for specific document processing tasks.
+"""
+
+from omnidocs.tasks import layout_extraction
+
+__all__ = [
+    "layout_extraction",
+]
