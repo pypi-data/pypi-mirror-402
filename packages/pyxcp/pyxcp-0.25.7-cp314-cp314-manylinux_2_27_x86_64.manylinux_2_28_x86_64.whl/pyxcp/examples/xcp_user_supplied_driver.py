3d0944e74439eb768f1cb4016a972b522a740ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:34:14 +0000

    Add package pyxcp/0.25.7/pyxcp-0.25.7-cp314-cp314-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pyxcp/pyxcp-0.25.7-cp314-cp314-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/pyxcp/examples/xcp_user_supplied_driver.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from typing import Optional
+
+import can
+
+from pyxcp.cmdline import ArgumentParser
+from pyxcp.transport.can import CanInterfaceBase
+
+
+class CustomCANInterface(CanInterfaceBase):
+    def init(self):
+        """Initialize the CAN interface here."""
+
+    def set_filters(self, filters):
+        print(f"set_filters({filters})")
+        self._filters = filters
+
+    def recv(self, timeout: Optional[float] = None) -> Optional[can.message.Message]:
+        """Receive CAN frames."""
+        return can.message.Message()
+
+    def send(self, msg: can.message.Message):
+        """Send CAN frames."""
+        print(f"send({msg})")
+
+    @property
+    def filters(self):
+        """Return the current CAN filters."""
+        return self._filters
+
+    @property
+    def state(self):
+        """Return the current state of the CAN interface."""
+        return can.BusState.ACTIVE
+
+
+custom_interface = CustomCANInterface()
+
+ap = ArgumentParser(description="User supplied CAN driver.")
+with ap.run(transport_layer_interface=custom_interface) as x:
+    x.connect()
+    x.disconnect()
