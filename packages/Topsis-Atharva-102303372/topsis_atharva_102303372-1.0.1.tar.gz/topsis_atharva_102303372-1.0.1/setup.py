4aece275d042b6085b285776d568460421bab71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:13:35 +0000

    Add package Topsis-Atharva-102303372/1.0.1/topsis_atharva_102303372-1.0.1.tar.gz

---
+++ packages/Topsis-Atharva-102303372/topsis_atharva_102303372-1.0.1.tar.gz/topsis_atharva_102303372-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-atharva-102303372",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=["pandas", "numpy"],
+    entry_points={
+        'console_scripts': [
+            'topsis=topsis.topsis:main'
+        ]
+    },
+    author="Atharva",
+    description="TOPSIS implementation using Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
