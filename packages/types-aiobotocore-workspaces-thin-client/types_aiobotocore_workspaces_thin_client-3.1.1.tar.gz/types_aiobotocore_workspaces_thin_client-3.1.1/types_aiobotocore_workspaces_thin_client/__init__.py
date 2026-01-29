e6a52e952109979050cfc87471f01d38a5609d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:50 +0000

    Add package types-aiobotocore-workspaces-thin-client/3.1.1/types_aiobotocore_workspaces_thin_client-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-workspaces-thin-client/types_aiobotocore_workspaces_thin_client-3.1.1.tar.gz/types_aiobotocore_workspaces_thin_client-3.1.1/types_aiobotocore_workspaces_thin_client/__init__.py
@@ -0,0 +1,44 @@
+"""
+Main interface for workspaces-thin-client service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_workspaces_thin_client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_workspaces_thin_client import (
+        Client,
+        ListDevicesPaginator,
+        ListEnvironmentsPaginator,
+        ListSoftwareSetsPaginator,
+        WorkSpacesThinClientClient,
+    )
+
+    session = get_session()
+    async with session.create_client("workspaces-thin-client") as client:
+        client: WorkSpacesThinClientClient
+        ...
+
+
+    list_devices_paginator: ListDevicesPaginator = client.get_paginator("list_devices")
+    list_environments_paginator: ListEnvironmentsPaginator = client.get_paginator("list_environments")
+    list_software_sets_paginator: ListSoftwareSetsPaginator = client.get_paginator("list_software_sets")
+    ```
+"""
+
+from .client import WorkSpacesThinClientClient
+from .paginator import ListDevicesPaginator, ListEnvironmentsPaginator, ListSoftwareSetsPaginator
+
+Client = WorkSpacesThinClientClient
+
+
+__all__ = (
+    "Client",
+    "ListDevicesPaginator",
+    "ListEnvironmentsPaginator",
+    "ListSoftwareSetsPaginator",
+    "WorkSpacesThinClientClient",
+)
