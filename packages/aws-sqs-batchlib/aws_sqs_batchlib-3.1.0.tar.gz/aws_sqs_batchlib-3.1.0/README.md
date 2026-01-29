c5281c1732100cded746d3387591f0063d1cbeaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:53 +0000

    Add package aws-sqs-batchlib/3.1.0/aws_sqs_batchlib-3.1.0.tar.gz

---
+++ packages/aws-sqs-batchlib/aws_sqs_batchlib-3.1.0.tar.gz/aws_sqs_batchlib-3.1.0/README.md
@@ -0,0 +1,176 @@
+# aws-sqs-batchlib for Python
+
+Consume and process Amazon SQS queues in large batches.
+
+## Features
+
+* Consume arbitrary number of messages from an Amazon SQS queue.
+
+  * Define maximum batch size and batching window in seconds to receive a batch
+    of messages from Amazon SQS queue similar to Lambda Event Source Mapping.
+
+* Send arbitrary number of messages to an Amazon SQS queue.
+
+* Delete arbitrary number of messages from an Amazon SQS queue.
+
+
+## Installation
+
+Install from PyPI with pip
+
+```
+pip install aws-sqs-batchlib
+```
+
+or with the package manager of choice.
+
+## Usage
+
+`aws-sqs-batchlib` provides the following methods:
+
+* `delete_message_batch()` - Delete arbitrary number of messages from an Amazon SQS queue.
+* `receive_message()` - Receive arbitrary number of messages from an Amazon SQS queue.
+* `send_message_batch()` - Send arbitrary number of messages to an Amazon SQS queue.
+
+These methods invoke the corresponding boto3 [SQS.Client](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs.html#client)
+methods multiple times to send, receive or delete an arbitrary number of messages from an Amazon SQS queue. They accept the same arguments and have
+the same response structure as their boto3 counterparts. See boto3 documentation for more details:
+
+* [delete_message_batch()](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs.html#SQS.Client.delete_message_batch)
+* [receive_message()](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs.html#SQS.Client.receive_message)
+* [send_message_batch()](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs.html#SQS.Client.send_message_batch)
+
+### Receive
+
+```python
+import aws_sqs_batchlib
+
+# Receive up-to 100 messages from the given queue, polling the queue for
+# up-to 15 seconds to fill the batch.
+res = aws_sqs_batchlib.receive_message(
+    QueueUrl = "https://sqs.eu-north-1.amazonaws.com/123456789012/MyQueue",
+    MaxNumberOfMessages=100,
+    WaitTimeSeconds=15,
+)
+
+# Returns messages in the same format as boto3 / botocore SQS Client
+# receive_message() method.
+assert res == {
+    'Messages': [
+        {'MessageId': '[.]', 'ReceiptHandle': 'AQ[.]JA==', 'MD5OfBody': '[.]', 'Body': '[.]'},
+        {'MessageId': '[.]', 'ReceiptHandle': 'AQ[.]wA==', 'MD5OfBody': '[.]', 'Body': '[.]'}
+        # ... up-to 100 messages
+    ]
+}
+```
+
+### Send
+
+```python
+import aws_sqs_batchlib
+
+# Send an arbitrary number of messages to a queue
+res = aws_sqs_batchlib.send_message_batch(
+    QueueUrl="https://sqs.eu-north-1.amazonaws.com/123456789012/MyQueue",
+    Entries=[
+        {"Id": "1", "MessageBody": "<...>"},
+        {"Id": "2", "MessageBody": "<...>", "DelaySeconds": 1000000},
+        # ...
+        {"Id": "175", "MessageBody": "<...>"},
+        # ...
+    ],
+)
+
+# Returns result in the same format as boto3 / botocore SQS Client
+# send_message_batch() method.
+assert res == {
+    "Successful": [
+        {"Id": "1", "MessageId": "<...>", "MD5OfMessageBody": "<...>"},
+        # ...
+    ],
+    "Failed": [
+        {
+            "Id": "2",
+            "SenderFault": True,
+            "Code": "InvalidParameterValue",
+            "Message": "Value 1000000 for parameter DelaySeconds is invalid. Reason: DelaySeconds must be >= 0 and <= 900.",
+        }
+    ],
+}
+```
+
+### Delete
+
+```python
+import aws_sqs_batchlib
+
+# Delete an arbitrary number of messages from a queue
+res = aws_sqs_batchlib.delete_message_batch(
+    QueueUrl="https://sqs.eu-north-1.amazonaws.com/123456789012/MyQueue",
+    Entries=[
+        {"Id": "1", "ReceiptHandle": "<...>"},
+        {"Id": "2", "ReceiptHandle": "<...>"},
+        # ...
+        {"Id": "175", "ReceiptHandle": "<...>"},
+        # ...
+    ],
+)
+
+# Returns result in the same format as boto3 / botocore SQS Client
+# delete_message_batch() method.
+assert res == {
+    "Successful": [
+        {"Id": "1"},
+        # ...
+    ],
+    "Failed": [
+        {
+            "Id": "2",
+            "SenderFault": True,
+            "Code": "ReceiptHandleIsInvalid",
+            "Message": "The input receipt handle is invalid.",
+        }
+    ],
+}
+```
+
+
+## Development
+
+Requires Python 3 and uv. Useful commands:
+
+```bash
+# Run tests (integration test requires rights to create, delete and use DynamoDB tables)
+make test
+
+# Run linters
+make -k lint
+
+# Format code
+make format
+```
+
+## Benchmarks & Manual Testing
+
+Use `benchmark/end2end.py` to benchmark and test the library functionality and performance. Execute following commands in project virtualenv:
+
+```bash
+# Setup
+export PYTHONPATH=$(pwd)
+export AWS_DEFAULT_REGION=eu-north-1
+
+# Send, receive and delete 512 messages, run test 5 times
+uv run benchmark/end2end.py \
+  --queue-url https://sqs.eu-north-1.amazonaws.com/123456789012/MyQueue --num-messages 512 --iterations 5
+```
+
+Benchmarks against an Amazon SQS queue on the same AWS region (eu-north-1, c5.large instance) show following
+throughput:
+
+* Send - ~500 to ~800 messages / second
+* Receive - ~800 to ~1400 messages / second
+* Delete - ~900 to ~1600 messages / second
+
+## License
+
+MIT.
---
+++ packages/aws-sqs-batchlib/aws_sqs_batchlib-3.1.0.tar.gz/aws_sqs_batchlib-3.1.0/aws_sqs_batchlib/aws_sqs_batchlib.py
@@ -0,0 +1,233 @@
+"""Amazon SQS Batchlib"""
+
+import time
+import uuid
+from typing import TYPE_CHECKING, List, Optional, Sequence, Tuple, overload
+
+import boto3
+import boto3.session
+
+if TYPE_CHECKING:  # pragma: no cover
+    from mypy_boto3_sqs import SQSClient
+    from mypy_boto3_sqs.type_defs import (
+        BatchResultErrorEntryTypeDef,
+        DeleteMessageBatchRequestEntryTypeDef,
+        DeleteMessageBatchResultEntryTypeDef,
+        MessageTypeDef,
+        SendMessageBatchRequestEntryTypeDef,
+        SendMessageBatchResultEntryTypeDef,
+    )
+    from typing_extensions import TypedDict
+
+    ReceiveMessageResultTypeDef = TypedDict(
+        "ReceiveMessageResultTypeDef",
+        {"Messages": List["MessageTypeDef"]},
+    )
+
+    DeleteMessageBatchResultTypeDef = TypedDict(
+        "DeleteMessageBatchResultTypeDef",
+        {
+            "Successful": List["DeleteMessageBatchResultEntryTypeDef"],
+            "Failed": List["BatchResultErrorEntryTypeDef"],
+        },
+    )
+
+    SendMessageBatchResultTypeDef = TypedDict(
+        "SendMessageBatchResultTypeDef",
+        {
+            "Successful": List["SendMessageBatchResultEntryTypeDef"],
+            "Failed": List["BatchResultErrorEntryTypeDef"],
+        },
+    )
+
+
+def create_sqs_client(session: Optional[boto3.session.Session] = None) -> "SQSClient":
+    """Create default SQS client.
+
+    Args:
+        session: boto3 Session to use for creating SQS client. Optional.
+                 Default: boto3 default session.
+    """
+    session = session or boto3.session.Session()
+    region = session.region_name
+    return boto3.client("sqs", endpoint_url=f"https://sqs.{region}.amazonaws.com")
+
+
+def receive_message(
+    sqs_client: Optional["SQSClient"] = None,
+    session: Optional[boto3.session.Session] = None,
+    **kwargs,
+) -> "ReceiveMessageResultTypeDef":
+    """Receive an arbitrary number of messages from an Amazon SQS queue.
+
+    This method performs multiple boto3 SQS receive_message() calls to
+    consume an arbitrary number of messages from an Amazon SQS queue. It
+    polls the queue for messages until `MaxNumberOfMessages` messages is
+    received or `WaitTimeSeconds` has elapsed.
+
+    receive_message() accepts the same arguments and has the same response
+    structure as boto3 SQS receive_message() method.
+
+    If you provide a ReceiveRequestAttemptId, receive_message() generates
+    a unique ReceiveRequestAttemptId for each SQS request. If you do not
+    provide a ReceiveRequestAttemptId, receive_message() sends each SQS
+    request without ReceiveRequestAttemptId argument.
+
+    Args:
+        sqs_client: boto3 SQS client to use. Optional. Default: client created
+                    with default session and configuration.
+        session: boto3 Session to use for creating SQS client if sqs_client is
+                 not provided. Optional. Default: boto3 default session.
+        **kwargs: keyword arguments to pass to boto3 SQS receive_message()
+                  method
+
+    Returns:
+        SQS messages similar to boto3 SQS receive_message() method.
+    """
+    sqs_client = sqs_client or create_sqs_client(session)
+
+    batch_size = kwargs.get("MaxNumberOfMessages", 1)
+    batching_window = kwargs.get("WaitTimeSeconds", 1)
+
+    batch: List["MessageTypeDef"] = []
+    start = time.time()
+    while time.time() - start < batching_window and len(batch) < batch_size:
+        kwargs["WaitTimeSeconds"] = 1
+        kwargs["MaxNumberOfMessages"] = min(batch_size - len(batch), 10)
+        if "ReceiveRequestAttemptId" in kwargs:
+            kwargs["ReceiveRequestAttemptId"] = str(uuid.uuid4())
+        batch.extend(sqs_client.receive_message(**kwargs).get("Messages", []))
+
+    return {"Messages": batch}
+
+
+def delete_message_batch(
+    QueueUrl: str,  # pylint: disable=invalid-name
+    Entries: List[  # pylint: disable=invalid-name
+        "DeleteMessageBatchRequestEntryTypeDef"
+    ],
+    sqs_client: Optional["SQSClient"] = None,
+    session: Optional[boto3.session.Session] = None,
+) -> "DeleteMessageBatchResultTypeDef":
+    """Delete an arbitrary number of messages from an Amazon SQS queue.
+
+    This method performs multiple boto3 SQS delete_message_batch() calls to
+    delete an arbitrary number of messages from an Amazon SQS queue.
+
+    delete_message_batch() accepts the same arguments and has the same response
+    structure as boto3 SQS delete_message_batch() method.
+
+    Args:
+        QueueUrl: The URL of the Amazon SQS queue from which messages are deleted.
+        Entries: A list of receipt handles for the messages to be deleted.
+        sqs_client: boto3 SQS client to use. Optional. Default: client created
+                    with default session and configuration.
+        session: boto3 Session to use for creating SQS client if sqs_client is
+                 not provided. Optional. Default: boto3 default session.
+    Returns:
+        Results similar to boto3 SQS delete_message_batch() method.
+    """
+    sqs_client = sqs_client or create_sqs_client(session)
+    result: "DeleteMessageBatchResultTypeDef" = {"Successful": [], "Failed": []}
+
+    while Entries:
+        chunk, Entries = Entries[:10], Entries[10:]
+        res = sqs_client.delete_message_batch(QueueUrl=QueueUrl, Entries=chunk)
+
+        failed, retryable = _divide_failures(res.get("Failed", []), chunk)
+        result["Failed"].extend(failed)
+        result["Successful"].extend(res.get("Successful", []))
+        Entries = retryable + Entries
+
+    return result
+
+
+def send_message_batch(
+    QueueUrl: str,  # pylint: disable=invalid-name
+    Entries: List[  # pylint: disable=invalid-name
+        "SendMessageBatchRequestEntryTypeDef"
+    ],
+    sqs_client: Optional["SQSClient"] = None,
+    session: Optional[boto3.session.Session] = None,
+) -> "SendMessageBatchResultTypeDef":
+    """Send an arbitrary number of messages to an Amazon SQS queue.
+
+    This method performs multiple boto3 SQS send_message_batch() calls to
+    delete an arbitrary number of messages from an Amazon SQS queue.
+
+    send_message_batch() accepts the same arguments and has the same response
+    structure as boto3 SQS send_message_batch() method.
+
+    Args:
+        QueueUrl: The URL of the Amazon SQS queue to which batched messages
+                  are sent.
+        Entries: A list of send message entries for the messages to send to
+                 SQS.
+        sqs_client: boto3 SQS client to use. Optional. Default: client created
+                    with default session and configuration.
+        session: boto3 Session to use for creating SQS client if sqs_client is
+                 not provided. Optional. Default: boto3 default session.
+
+    Returns:
+        Results similar to boto3 SQS send_message_batch() method.
+    """
+    sqs_client = sqs_client or create_sqs_client(session)
+    result: "SendMessageBatchResultTypeDef" = {"Successful": [], "Failed": []}
+
+    while Entries:
+        chunk, Entries = Entries[:10], Entries[10:]
+        res = sqs_client.send_message_batch(QueueUrl=QueueUrl, Entries=chunk)
+
+        failed, retryable = _divide_failures(res.get("Failed", []), chunk)
+        result["Failed"].extend(failed)
+        result["Successful"].extend(res.get("Successful", []))
+        Entries = retryable + Entries
+
+    return result
+
+
+@overload
+def _divide_failures(
+    failed: List["BatchResultErrorEntryTypeDef"],
+    entries: Sequence["SendMessageBatchRequestEntryTypeDef"],
+) -> Tuple[
+    List["BatchResultErrorEntryTypeDef"],
+    List["SendMessageBatchRequestEntryTypeDef"],
+]: ...  # pragma: no cover
+
+
+@overload
+def _divide_failures(
+    failed: List["BatchResultErrorEntryTypeDef"],
+    entries: Sequence["DeleteMessageBatchRequestEntryTypeDef"],
+) -> Tuple[
+    List["BatchResultErrorEntryTypeDef"],
+    List["DeleteMessageBatchRequestEntryTypeDef"],
+]: ...  # pragma: no cover
+
+
+def _divide_failures(failed, entries):
+    """Helper to divide errors into retryable and non-retryable errors.
+
+    Args;
+        failed: list of failed batch operations
+        entries: list of entries passed to batch operation
+
+    Returns: tuple with (errors, retryable_entries) where errors contains
+        non-retryable batch result entries and retryable_entries contains
+        entries that can be retried.
+    """
+    if not failed:
+        return [], []
+
+    not_retryable: List["BatchResultErrorEntryTypeDef"] = []
+    retryable: List["SendMessageBatchRequestEntryTypeDef"] = []
+
+    entry_map = {entry["Id"]: entry for entry in entries}
+    for msg in failed:
+        if msg["SenderFault"]:
+            not_retryable.append(msg)
+        else:
+            retryable.append(entry_map[msg["Id"]])
+
+    return not_retryable, retryable
