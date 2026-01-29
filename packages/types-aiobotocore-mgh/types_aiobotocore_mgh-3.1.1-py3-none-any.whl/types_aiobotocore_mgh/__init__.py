72d0528b4cb504b4e4217b72977ee9812b9e8026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:31 +0000

    Add package types-aiobotocore-mgh/3.1.1/types_aiobotocore_mgh-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-mgh/types_aiobotocore_mgh-3.1.1-py3-none-any.whl/types_aiobotocore_mgh/__init__.py
@@ -0,0 +1,64 @@
+"""
+Main interface for mgh service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mgh/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_mgh import (
+        Client,
+        ListApplicationStatesPaginator,
+        ListCreatedArtifactsPaginator,
+        ListDiscoveredResourcesPaginator,
+        ListMigrationTaskUpdatesPaginator,
+        ListMigrationTasksPaginator,
+        ListProgressUpdateStreamsPaginator,
+        ListSourceResourcesPaginator,
+        MigrationHubClient,
+    )
+
+    session = get_session()
+    async with session.create_client("mgh") as client:
+        client: MigrationHubClient
+        ...
+
+
+    list_application_states_paginator: ListApplicationStatesPaginator = client.get_paginator("list_application_states")
+    list_created_artifacts_paginator: ListCreatedArtifactsPaginator = client.get_paginator("list_created_artifacts")
+    list_discovered_resources_paginator: ListDiscoveredResourcesPaginator = client.get_paginator("list_discovered_resources")
+    list_migration_task_updates_paginator: ListMigrationTaskUpdatesPaginator = client.get_paginator("list_migration_task_updates")
+    list_migration_tasks_paginator: ListMigrationTasksPaginator = client.get_paginator("list_migration_tasks")
+    list_progress_update_streams_paginator: ListProgressUpdateStreamsPaginator = client.get_paginator("list_progress_update_streams")
+    list_source_resources_paginator: ListSourceResourcesPaginator = client.get_paginator("list_source_resources")
+    ```
+"""
+
+from .client import MigrationHubClient
+from .paginator import (
+    ListApplicationStatesPaginator,
+    ListCreatedArtifactsPaginator,
+    ListDiscoveredResourcesPaginator,
+    ListMigrationTasksPaginator,
+    ListMigrationTaskUpdatesPaginator,
+    ListProgressUpdateStreamsPaginator,
+    ListSourceResourcesPaginator,
+)
+
+Client = MigrationHubClient
+
+
+__all__ = (
+    "Client",
+    "ListApplicationStatesPaginator",
+    "ListCreatedArtifactsPaginator",
+    "ListDiscoveredResourcesPaginator",
+    "ListMigrationTaskUpdatesPaginator",
+    "ListMigrationTasksPaginator",
+    "ListProgressUpdateStreamsPaginator",
+    "ListSourceResourcesPaginator",
+    "MigrationHubClient",
+)
