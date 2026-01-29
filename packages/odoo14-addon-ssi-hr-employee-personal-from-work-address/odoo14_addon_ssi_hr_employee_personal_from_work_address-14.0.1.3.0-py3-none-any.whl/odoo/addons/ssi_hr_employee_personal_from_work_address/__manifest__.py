e1e3e59e1a983616b95ed4265558359aef6f78ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:17 +0000

    Add package odoo14-addon-ssi-hr-employee-personal-from-work-address/14.0.1.3.0/odoo14_addon_ssi_hr_employee_personal_from_work_address-14.0.1.3.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-hr-employee-personal-from-work-address/odoo14_addon_ssi_hr_employee_personal_from_work_address-14.0.1.3.0-py3-none-any.whl/odoo/addons/ssi_hr_employee_personal_from_work_address/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=C8101
+{
+    "name": "Employee Personal Information From Home Address",
+    "version": "14.0.1.3.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "ssi_partner",
+        "ssi_hr",
+    ],
+    "data": [
+        "views/hr_employee_views.xml",
+    ],
+    "demo": [],
+}
---
+++ packages/odoo14-addon-ssi-hr-employee-personal-from-work-address/odoo14_addon_ssi_hr_employee_personal_from_work_address-14.0.1.3.0-py3-none-any.whl/odoo/addons/ssi_hr_employee_personal_from_work_address/models/hr_employee.py
@@ -0,0 +1,101 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    country_id = fields.Many2one(
+        comodel_name="res.country",
+        related="address_home_id.nationality_id",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    gender = fields.Selection(
+        related="address_home_id.gender",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    birthday = fields.Date(
+        related="address_home_id.birthdate_date",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    place_of_birth = fields.Char(
+        related="address_home_id.birth_city",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    country_of_birth = fields.Many2one(
+        comodel_name="res.country",
+        related="address_home_id.birth_country_id",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    birth_state_id = fields.Many2one(
+        string="Birth state",
+        comodel_name="res.country.state",
+        related="address_home_id.birth_state_id",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    blood_type = fields.Selection(
+        string="Blood Type (ABO)",
+        related="address_home_id.blood_type",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    blood_type_rhesus = fields.Selection(
+        string="Blood Type (Rh)",
+        related="address_home_id.blood_type_rhesus",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    religion_id = fields.Many2one(
+        string="Religion",
+        comodel_name="res_partner_religion",
+        related="address_home_id.religion_id",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    ethnicity_id = fields.Many2one(
+        string="Ethnicity",
+        comodel_name="res_partner_ethnicity",
+        related="address_home_id.ethnicity_id",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    marital = fields.Selection(
+        string="Marital Status",
+        related="address_home_id.marital",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    spouse_complete_name = fields.Char(
+        string="Spouse Complete Name",
+        related="address_home_id.spouse_complete_name",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
+    spouse_birthdate = fields.Date(
+        string="Spouse Birthdate",
+        related="address_home_id.spouse_birthdate",
+        readonly=False,
+        store=True,
+        compute_sudo=True,
+    )
