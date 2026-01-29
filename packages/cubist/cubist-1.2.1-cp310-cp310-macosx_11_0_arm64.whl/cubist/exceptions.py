c741dd3c13de7588931dcc3b77a244e362e41376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:12:23 +0000

    Add package cubist/1.2.1/cubist-1.2.1-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/cubist/cubist-1.2.1-cp310-cp310-macosx_11_0_arm64.whl/cubist/exceptions.py
@@ -0,0 +1,9 @@
+"""Exceptions for Cubist"""
+
+
+class _Error(Exception):
+    """Base class for exceptions in this module."""
+
+
+class CubistError(_Error):
+    """Raised when the C Cubist library raises errors."""
