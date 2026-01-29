34cc429d1530bb53706833d84d4b313914ffa50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:18 +0000

    Add package types-aiobotocore-es/3.1.1/types_aiobotocore_es-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-es/types_aiobotocore_es-3.1.1-py3-none-any.whl/types_aiobotocore_es/paginator.pyi
@@ -0,0 +1,165 @@
+"""
+Type annotations for es service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_es.client import ElasticsearchServiceClient
+    from types_aiobotocore_es.paginator import (
+        DescribeReservedElasticsearchInstanceOfferingsPaginator,
+        DescribeReservedElasticsearchInstancesPaginator,
+        GetUpgradeHistoryPaginator,
+        ListElasticsearchInstanceTypesPaginator,
+        ListElasticsearchVersionsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("es") as client:
+        client: ElasticsearchServiceClient
+
+        describe_reserved_elasticsearch_instance_offerings_paginator: DescribeReservedElasticsearchInstanceOfferingsPaginator = client.get_paginator("describe_reserved_elasticsearch_instance_offerings")
+        describe_reserved_elasticsearch_instances_paginator: DescribeReservedElasticsearchInstancesPaginator = client.get_paginator("describe_reserved_elasticsearch_instances")
+        get_upgrade_history_paginator: GetUpgradeHistoryPaginator = client.get_paginator("get_upgrade_history")
+        list_elasticsearch_instance_types_paginator: ListElasticsearchInstanceTypesPaginator = client.get_paginator("list_elasticsearch_instance_types")
+        list_elasticsearch_versions_paginator: ListElasticsearchVersionsPaginator = client.get_paginator("list_elasticsearch_versions")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    DescribeReservedElasticsearchInstanceOfferingsRequestPaginateTypeDef,
+    DescribeReservedElasticsearchInstanceOfferingsResponseTypeDef,
+    DescribeReservedElasticsearchInstancesRequestPaginateTypeDef,
+    DescribeReservedElasticsearchInstancesResponseTypeDef,
+    GetUpgradeHistoryRequestPaginateTypeDef,
+    GetUpgradeHistoryResponseTypeDef,
+    ListElasticsearchInstanceTypesRequestPaginateTypeDef,
+    ListElasticsearchInstanceTypesResponseTypeDef,
+    ListElasticsearchVersionsRequestPaginateTypeDef,
+    ListElasticsearchVersionsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = (
+    "DescribeReservedElasticsearchInstanceOfferingsPaginator",
+    "DescribeReservedElasticsearchInstancesPaginator",
+    "GetUpgradeHistoryPaginator",
+    "ListElasticsearchInstanceTypesPaginator",
+    "ListElasticsearchVersionsPaginator",
+)
+
+if TYPE_CHECKING:
+    _DescribeReservedElasticsearchInstanceOfferingsPaginatorBase = AioPaginator[
+        DescribeReservedElasticsearchInstanceOfferingsResponseTypeDef
+    ]
+else:
+    _DescribeReservedElasticsearchInstanceOfferingsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class DescribeReservedElasticsearchInstanceOfferingsPaginator(
+    _DescribeReservedElasticsearchInstanceOfferingsPaginatorBase
+):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/es/paginator/DescribeReservedElasticsearchInstanceOfferings.html#ElasticsearchService.Paginator.DescribeReservedElasticsearchInstanceOfferings)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/#describereservedelasticsearchinstanceofferingspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeReservedElasticsearchInstanceOfferingsRequestPaginateTypeDef]
+    ) -> AioPageIterator[DescribeReservedElasticsearchInstanceOfferingsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/es/paginator/DescribeReservedElasticsearchInstanceOfferings.html#ElasticsearchService.Paginator.DescribeReservedElasticsearchInstanceOfferings.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/#describereservedelasticsearchinstanceofferingspaginator)
+        """
+
+if TYPE_CHECKING:
+    _DescribeReservedElasticsearchInstancesPaginatorBase = AioPaginator[
+        DescribeReservedElasticsearchInstancesResponseTypeDef
+    ]
+else:
+    _DescribeReservedElasticsearchInstancesPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class DescribeReservedElasticsearchInstancesPaginator(
+    _DescribeReservedElasticsearchInstancesPaginatorBase
+):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/es/paginator/DescribeReservedElasticsearchInstances.html#ElasticsearchService.Paginator.DescribeReservedElasticsearchInstances)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/#describereservedelasticsearchinstancespaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeReservedElasticsearchInstancesRequestPaginateTypeDef]
+    ) -> AioPageIterator[DescribeReservedElasticsearchInstancesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/es/paginator/DescribeReservedElasticsearchInstances.html#ElasticsearchService.Paginator.DescribeReservedElasticsearchInstances.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/#describereservedelasticsearchinstancespaginator)
+        """
+
+if TYPE_CHECKING:
+    _GetUpgradeHistoryPaginatorBase = AioPaginator[GetUpgradeHistoryResponseTypeDef]
+else:
+    _GetUpgradeHistoryPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class GetUpgradeHistoryPaginator(_GetUpgradeHistoryPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/es/paginator/GetUpgradeHistory.html#ElasticsearchService.Paginator.GetUpgradeHistory)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/#getupgradehistorypaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[GetUpgradeHistoryRequestPaginateTypeDef]
+    ) -> AioPageIterator[GetUpgradeHistoryResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/es/paginator/GetUpgradeHistory.html#ElasticsearchService.Paginator.GetUpgradeHistory.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/#getupgradehistorypaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListElasticsearchInstanceTypesPaginatorBase = AioPaginator[
+        ListElasticsearchInstanceTypesResponseTypeDef
+    ]
+else:
+    _ListElasticsearchInstanceTypesPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListElasticsearchInstanceTypesPaginator(_ListElasticsearchInstanceTypesPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/es/paginator/ListElasticsearchInstanceTypes.html#ElasticsearchService.Paginator.ListElasticsearchInstanceTypes)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/#listelasticsearchinstancetypespaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListElasticsearchInstanceTypesRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListElasticsearchInstanceTypesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/es/paginator/ListElasticsearchInstanceTypes.html#ElasticsearchService.Paginator.ListElasticsearchInstanceTypes.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/#listelasticsearchinstancetypespaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListElasticsearchVersionsPaginatorBase = AioPaginator[ListElasticsearchVersionsResponseTypeDef]
+else:
+    _ListElasticsearchVersionsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListElasticsearchVersionsPaginator(_ListElasticsearchVersionsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/es/paginator/ListElasticsearchVersions.html#ElasticsearchService.Paginator.ListElasticsearchVersions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/#listelasticsearchversionspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListElasticsearchVersionsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListElasticsearchVersionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/es/paginator/ListElasticsearchVersions.html#ElasticsearchService.Paginator.ListElasticsearchVersions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_es/paginators/#listelasticsearchversionspaginator)
+        """
