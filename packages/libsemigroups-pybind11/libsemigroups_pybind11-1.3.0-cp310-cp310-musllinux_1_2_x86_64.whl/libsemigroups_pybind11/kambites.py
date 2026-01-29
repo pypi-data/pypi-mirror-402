f32c4754cd5c520bbbc599e85dad5ebe5fb8aaa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:42 +0000

    Add package libsemigroups-pybind11/1.3.0/libsemigroups_pybind11-1.3.0-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.3.0-cp310-cp310-musllinux_1_2_x86_64.whl/libsemigroups_pybind11/kambites.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2024 J. D. Mitchell
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+"""This page contains the documentation for the ``kambites`` subpackage, that
+contains helper functions for the :any:`Kambites` class.
+"""
+
+from typing_extensions import Self as _Self
+
+from _libsemigroups_pybind11 import (
+    KambitesMultiViewString as _KambitesMultiViewString,
+    KambitesString as _KambitesString,
+    KambitesWord as _KambitesWord,
+    congruence_kind as _congruence_kind,
+    kambites_non_trivial_classes as _kambites_non_trivial_classes,
+    kambites_normal_forms as _kambites_normal_forms,
+    kambites_partition as _kambites_partition,
+)
+
+from .detail.congruence_common import CongruenceCommon as _CongruenceCommon
+from .detail.cxx_wrapper import (
+    copy_cxx_mem_fns as _copy_cxx_mem_fns,
+    register_cxx_wrapped_type as _register_cxx_wrapped_type,
+    to_cxx as _to_cxx,
+    wrap_cxx_free_fn as _wrap_cxx_free_fn,
+)
+from .detail.decorators import copydoc as _copydoc
+from .presentation import Presentation as _Presentation
+
+
+class Kambites(_CongruenceCommon):
+    __doc__ = _KambitesWord.__doc__
+
+    _py_template_params_to_cxx_type = {
+        (list[int],): _KambitesWord,
+        (str,): _KambitesMultiViewString,
+    }
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    ) | {_KambitesString: (str,)}
+
+    _all_wrapped_cxx_types = {*_cxx_type_to_py_template_params.keys()}
+
+    @_copydoc(_KambitesWord.__init__)
+    def __init__(self: _Self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        if _to_cxx(self) is not None:
+            return
+        if len(args) == 2:
+            if isinstance(args[1], _Presentation):
+                self.py_template_params = args[1].py_template_params
+            else:
+                raise TypeError(
+                    f"expected the 2nd argument to be a Presentation, but found {type(args[1])}"
+                )
+        self.init_cxx_obj(*args)
+
+
+_copy_cxx_mem_fns(_KambitesWord, Kambites)
+
+_register_cxx_wrapped_type(_KambitesWord, Kambites)
+_register_cxx_wrapped_type(_KambitesMultiViewString, Kambites)
+_register_cxx_wrapped_type(_KambitesString, Kambites)
+
+partition = _wrap_cxx_free_fn(_kambites_partition)
+non_trivial_classes = _wrap_cxx_free_fn(_kambites_non_trivial_classes)
+normal_forms = _wrap_cxx_free_fn(_kambites_normal_forms)
