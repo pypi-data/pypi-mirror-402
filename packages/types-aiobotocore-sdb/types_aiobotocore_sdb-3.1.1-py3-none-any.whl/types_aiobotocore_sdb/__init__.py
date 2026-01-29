cec584261923b985a7a9db6b4287cd4904d25b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:46 +0000

    Add package types-aiobotocore-sdb/3.1.1/types_aiobotocore_sdb-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-sdb/types_aiobotocore_sdb-3.1.1-py3-none-any.whl/types_aiobotocore_sdb/__init__.py
@@ -0,0 +1,36 @@
+"""
+Main interface for sdb service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sdb/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_sdb import (
+        Client,
+        ListDomainsPaginator,
+        SelectPaginator,
+        SimpleDBClient,
+    )
+
+    session = get_session()
+    async with session.create_client("sdb") as client:
+        client: SimpleDBClient
+        ...
+
+
+    list_domains_paginator: ListDomainsPaginator = client.get_paginator("list_domains")
+    select_paginator: SelectPaginator = client.get_paginator("select")
+    ```
+"""
+
+from .client import SimpleDBClient
+from .paginator import ListDomainsPaginator, SelectPaginator
+
+Client = SimpleDBClient
+
+
+__all__ = ("Client", "ListDomainsPaginator", "SelectPaginator", "SimpleDBClient")
