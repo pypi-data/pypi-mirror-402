d94306818620a7c3b8147dfbd406db1c07665b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:11:44 +0000

    Add package minorun365/1.0.10/minorun365-1.0.10.tar.gz

---
+++ packages/minorun365/minorun365-1.0.10.tar.gz/minorun365-1.0.10/README.md
@@ -0,0 +1,7 @@
+# minorun365-card
+
+minorun365のプロフィールを表示するCLIカードパッケージです。
+
+```zsh
+uvx minorun365
+```
---
+++ packages/minorun365/minorun365-1.0.10.tar.gz/minorun365-1.0.10/src/minorun365/__init__.py
@@ -0,0 +1,8 @@
+"""
+minorun365 Profile Card
+-----------------
+
+minorun365のプロフィールを表示
+"""
+
+__version__ = "1.0.10"
