3f2423815d3801692b7118cccd15116de3a29cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:33:09 +0000

    Add package weaviate-orm/0.1.11/weaviate_orm-0.1.11-py3-none-any.whl

---
+++ packages/weaviate-orm/weaviate_orm-0.1.11-py3-none-any.whl/weaviate_orm/__init__.py
@@ -0,0 +1,11 @@
+"""
+weaviate-orm
+A Pythonic ORM-style layer for Weaviate
+
+`weaviate-orm` provides a structured, object-oriented interface to the Weaviate vector database. Inspired by traditional ORM patterns, it allows you to define collections as Python classes, automatically generate Weaviate schemas, and perform CRUD and similarity-based queries without leaving the object-oriented paradigm.
+"""
+
+__version__ = "0.1.11"
+__author__ = "Tobias Heimig-Elschner <Tobias.Heimig-Elschner@bbr.bund.de>"
+__license__ = "GPL-3.0-or-later"
+__url__ = "https://gitlab.opencode.de/bbsr_ida_public/weaviate_orm"
