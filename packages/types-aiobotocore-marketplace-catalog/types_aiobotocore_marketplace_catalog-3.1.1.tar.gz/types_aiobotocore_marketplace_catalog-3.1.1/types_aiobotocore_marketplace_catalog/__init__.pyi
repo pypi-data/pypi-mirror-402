febdbc0353cdacba43e9b002646b2ba654f9cbb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:29 +0000

    Add package types-aiobotocore-marketplace-catalog/3.1.1/types_aiobotocore_marketplace_catalog-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-marketplace-catalog/types_aiobotocore_marketplace_catalog-3.1.1.tar.gz/types_aiobotocore_marketplace_catalog-3.1.1/types_aiobotocore_marketplace_catalog/__init__.pyi
@@ -0,0 +1,35 @@
+"""
+Main interface for marketplace-catalog service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_marketplace_catalog/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_marketplace_catalog import (
+        Client,
+        ListChangeSetsPaginator,
+        ListEntitiesPaginator,
+        MarketplaceCatalogClient,
+    )
+
+    session = get_session()
+    async with session.create_client("marketplace-catalog") as client:
+        client: MarketplaceCatalogClient
+        ...
+
+
+    list_change_sets_paginator: ListChangeSetsPaginator = client.get_paginator("list_change_sets")
+    list_entities_paginator: ListEntitiesPaginator = client.get_paginator("list_entities")
+    ```
+"""
+
+from .client import MarketplaceCatalogClient
+from .paginator import ListChangeSetsPaginator, ListEntitiesPaginator
+
+Client = MarketplaceCatalogClient
+
+__all__ = ("Client", "ListChangeSetsPaginator", "ListEntitiesPaginator", "MarketplaceCatalogClient")
