f4cff2b1c69efa923c57c34c45db13da68590b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:55 +0000

    Add package types-aiobotocore-chime-sdk-meetings/3.1.1/types_aiobotocore_chime_sdk_meetings-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-chime-sdk-meetings/types_aiobotocore_chime_sdk_meetings-3.1.1-py3-none-any.whl/types_aiobotocore_chime_sdk_meetings/__init__.py
@@ -0,0 +1,30 @@
+"""
+Main interface for chime-sdk-meetings service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_chime_sdk_meetings/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_chime_sdk_meetings import (
+        ChimeSDKMeetingsClient,
+        Client,
+    )
+
+    session = get_session()
+    async with session.create_client("chime-sdk-meetings") as client:
+        client: ChimeSDKMeetingsClient
+        ...
+
+    ```
+"""
+
+from .client import ChimeSDKMeetingsClient
+
+Client = ChimeSDKMeetingsClient
+
+
+__all__ = ("ChimeSDKMeetingsClient", "Client")
