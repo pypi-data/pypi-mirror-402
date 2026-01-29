4df99c5a98a62588824710e9ed4ff1e747ec728c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:17:40 +0000

    Add package psf/2026.1.18/psf-2026.1.18-cp314-cp314t-win32.whl

---
+++ packages/psf/psf-2026.1.18-cp314-cp314t-win32.whl/psf/__init__.py
@@ -0,0 +1,9 @@
+# psf/__init__.py
+
+from .psf import *
+from .psf import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Psf version string."""
