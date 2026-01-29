c5c86e6f65e394433c56880754bd70fb02830cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:13:21 +0000

    Add package slidex/0.1.1/slidex-0.1.1-cp310-cp310-win_amd64.whl

---
+++ packages/slidex/slidex-0.1.1-cp310-cp310-win_amd64.whl/slidex/__init__.py
@@ -0,0 +1,39 @@
+"""slidex Python package."""
+
+from ._core import (
+    Chart,
+    ChartTypeUnsupportedError,
+    InvalidPresentationError,
+    OpenXmlError,
+    Picture,
+    Presentation,
+    SerializationError,
+    Shape,
+    ShapeNotFoundError,
+    Shapes,
+    Slide,
+    Slides,
+    SlidexError,
+    Table,
+    TableDimensionsError,
+    TextFrame,
+)
+
+__all__ = [
+    "Chart",
+    "ChartTypeUnsupportedError",
+    "InvalidPresentationError",
+    "OpenXmlError",
+    "Picture",
+    "Presentation",
+    "SerializationError",
+    "Shape",
+    "ShapeNotFoundError",
+    "Shapes",
+    "Slide",
+    "Slides",
+    "SlidexError",
+    "Table",
+    "TableDimensionsError",
+    "TextFrame",
+]
