51cc1556a65cd33ba82da2f57608378e709a18d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:15:02 +0000

    Add package terminalbars/2.6/terminalbars-2.6-py3-none-any.whl

---
+++ packages/terminalbars/terminalbars-2.6-py3-none-any.whl/terminalbars/__init__.py
@@ -0,0 +1,6 @@
+# terminalbars/__init__.py
+
+from .terminalbars import pasek, superpasek, propasek, ultrapasek, ultrapropasek
+
+# Opcjonalnie, możesz też dodać listę eksportowanych symboli
+__all__ = ["pasek", "superpasek", "propasek", "ultrapasek", "ultrapropasek"]
