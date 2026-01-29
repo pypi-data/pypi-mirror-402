9e67ef62763de8def3167156e04a21f92f26e990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:12:52 +0000

    Add package pyo-oracle/0.2.0/pyo_oracle-0.2.0-py3-none-any.whl

---
+++ packages/pyo-oracle/pyo_oracle-0.2.0-py3-none-any.whl/pyo_oracle/_config.py
@@ -0,0 +1,101 @@
+"""
+Module to handle configuration, such as data directory, etc.
+"""
+import configparser
+from pathlib import Path
+from typing import Any, Dict
+
+from erddapy import ERDDAP
+
+_default_config = {
+    "data_directory": str(Path(__file__).parent.joinpath("data/").absolute()),
+    "erddap_server": "https://erddap.bio-oracle.org/erddap/",
+    "skip_confirmation": False,
+}
+
+config_file = Path(__file__).absolute().parent.joinpath(("config.ini"))
+
+
+def create_config(
+    default_config: Dict[str, Any] = _default_config, path: str = None
+) -> None:
+    """
+    Creates the configuration file.
+    """
+    config = configparser.ConfigParser()
+    config["DEFAULT"] = default_config
+    target_path = Path(path) if path else config_file
+    if target_path.exists():
+        response = input(
+            f"Config file '{target_path}' already exists, overwrite it? y/N \n"
+        )
+        if (response.lower() not in "y yes") or (not response):
+            print("Operation cancelled.\n")
+            return
+    with open(target_path, "w") as f:
+        config.write(f)
+        print(f"Created configuration at '{target_path}'.")
+
+
+def _get_default_config() -> configparser.ConfigParser:
+    if config_file.exists():
+        current_config = configparser.ConfigParser()
+        _ = current_config.read(config_file)
+        return current_config
+    else:
+        try:
+            print("Config file doesn't exist, creating it.")
+            create_config()
+            return _get_default_config()
+        except Exception as e:
+            print(
+                f"Error: could not load or create configuration file. Loading default values."
+            )
+            config = configparser.ConfigParser()
+            config["DEFAULT"] = _default_config
+            return config
+
+
+def get_config_path() -> Path:
+    """
+    Returns the path to the config file.
+    """
+    if config_file.exists():
+        return config_file
+    else:
+        print("Config file doesn't exist, creating it.")
+        create_config()
+        return get_config_path()
+
+
+def print_config_values() -> None:
+    """
+    Prints configuration values.
+    """
+    if not config_file.exists():
+        print("Config file doesn't exist, creating it.")
+        create_config()
+    print(f"Configuration values in '{config_file}' are as following:\n")
+    for key, value in _get_default_config()["DEFAULT"].items():
+        print(key, "\t", value)
+    print()
+    print(
+        f"Edit the configuration file '{config_file}' or use the 'update_setting' function in this module to edit them."
+    )
+
+
+def update_setting(key: str, value: Any) -> None:
+    """
+    Modifies a setting in the configuration file.
+    """
+    config = _get_default_config()
+    config["DEFAULT"][key] = value
+    with open(config_file, "w") as f:
+        config.write(f)
+    print(f"Successfully updated config file at '{config_file}'.")
+
+
+config = dict(_get_default_config()["DEFAULT"])
+default_server = ERDDAP(
+    server=config["erddap_server"],
+)
