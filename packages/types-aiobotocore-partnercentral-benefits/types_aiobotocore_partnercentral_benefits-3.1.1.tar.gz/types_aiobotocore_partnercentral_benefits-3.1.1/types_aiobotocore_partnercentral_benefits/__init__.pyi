37e664ab80fe33153232f42538df32a465d5b11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:37 +0000

    Add package types-aiobotocore-partnercentral-benefits/3.1.1/types_aiobotocore_partnercentral_benefits-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-partnercentral-benefits/types_aiobotocore_partnercentral_benefits-3.1.1.tar.gz/types_aiobotocore_partnercentral_benefits-3.1.1/types_aiobotocore_partnercentral_benefits/__init__.pyi
@@ -0,0 +1,47 @@
+"""
+Main interface for partnercentral-benefits service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_partnercentral_benefits/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_partnercentral_benefits import (
+        Client,
+        ListBenefitAllocationsPaginator,
+        ListBenefitApplicationsPaginator,
+        ListBenefitsPaginator,
+        PartnerCentralBenefitsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("partnercentral-benefits") as client:
+        client: PartnerCentralBenefitsClient
+        ...
+
+
+    list_benefit_allocations_paginator: ListBenefitAllocationsPaginator = client.get_paginator("list_benefit_allocations")
+    list_benefit_applications_paginator: ListBenefitApplicationsPaginator = client.get_paginator("list_benefit_applications")
+    list_benefits_paginator: ListBenefitsPaginator = client.get_paginator("list_benefits")
+    ```
+"""
+
+from .client import PartnerCentralBenefitsClient
+from .paginator import (
+    ListBenefitAllocationsPaginator,
+    ListBenefitApplicationsPaginator,
+    ListBenefitsPaginator,
+)
+
+Client = PartnerCentralBenefitsClient
+
+__all__ = (
+    "Client",
+    "ListBenefitAllocationsPaginator",
+    "ListBenefitApplicationsPaginator",
+    "ListBenefitsPaginator",
+    "PartnerCentralBenefitsClient",
+)
