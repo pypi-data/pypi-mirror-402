2a8857d504e5a49c61b44857aac3f17330b95a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:51 +0000

    Add package types-aiobotocore-taxsettings/3.1.1/types_aiobotocore_taxsettings-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-taxsettings/types_aiobotocore_taxsettings-3.1.1-py3-none-any.whl/types_aiobotocore_taxsettings/__init__.py
@@ -0,0 +1,48 @@
+"""
+Main interface for taxsettings service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_taxsettings/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_taxsettings import (
+        Client,
+        ListSupplementalTaxRegistrationsPaginator,
+        ListTaxExemptionsPaginator,
+        ListTaxRegistrationsPaginator,
+        TaxSettingsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("taxsettings") as client:
+        client: TaxSettingsClient
+        ...
+
+
+    list_supplemental_tax_registrations_paginator: ListSupplementalTaxRegistrationsPaginator = client.get_paginator("list_supplemental_tax_registrations")
+    list_tax_exemptions_paginator: ListTaxExemptionsPaginator = client.get_paginator("list_tax_exemptions")
+    list_tax_registrations_paginator: ListTaxRegistrationsPaginator = client.get_paginator("list_tax_registrations")
+    ```
+"""
+
+from .client import TaxSettingsClient
+from .paginator import (
+    ListSupplementalTaxRegistrationsPaginator,
+    ListTaxExemptionsPaginator,
+    ListTaxRegistrationsPaginator,
+)
+
+Client = TaxSettingsClient
+
+
+__all__ = (
+    "Client",
+    "ListSupplementalTaxRegistrationsPaginator",
+    "ListTaxExemptionsPaginator",
+    "ListTaxRegistrationsPaginator",
+    "TaxSettingsClient",
+)
