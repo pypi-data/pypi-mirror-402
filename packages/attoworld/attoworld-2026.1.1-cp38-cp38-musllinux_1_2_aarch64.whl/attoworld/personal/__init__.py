4fc7066accd2726421ae1f8501bf23366cb09f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:09:14 +0000

    Add package attoworld/2026.1.1/attoworld-2026.1.1-cp38-cp38-musllinux_1_2_aarch64.whl

---
+++ packages/attoworld/attoworld-2026.1.1-cp38-cp38-musllinux_1_2_aarch64.whl/attoworld/personal/__init__.py
@@ -0,0 +1,7 @@
+"""This module will contain personal modules from our coworkers, allowing for easier sharing
+of data processing files that utilize functions that might not otherwise be of general use.
+"""
+
+from . import marco, nick, vlad
+
+__all__ = ["nick", "vlad", "marco"]
