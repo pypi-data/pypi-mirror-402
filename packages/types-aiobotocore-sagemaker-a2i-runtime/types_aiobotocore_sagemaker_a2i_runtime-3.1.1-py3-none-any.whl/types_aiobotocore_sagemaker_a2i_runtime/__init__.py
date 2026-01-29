943773c314fb6ca4d6fc0a041e06c4022f5c9b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:45 +0000

    Add package types-aiobotocore-sagemaker-a2i-runtime/3.1.1/types_aiobotocore_sagemaker_a2i_runtime-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-sagemaker-a2i-runtime/types_aiobotocore_sagemaker_a2i_runtime-3.1.1-py3-none-any.whl/types_aiobotocore_sagemaker_a2i_runtime/__init__.py
@@ -0,0 +1,34 @@
+"""
+Main interface for sagemaker-a2i-runtime service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sagemaker_a2i_runtime/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_sagemaker_a2i_runtime import (
+        AugmentedAIRuntimeClient,
+        Client,
+        ListHumanLoopsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("sagemaker-a2i-runtime") as client:
+        client: AugmentedAIRuntimeClient
+        ...
+
+
+    list_human_loops_paginator: ListHumanLoopsPaginator = client.get_paginator("list_human_loops")
+    ```
+"""
+
+from .client import AugmentedAIRuntimeClient
+from .paginator import ListHumanLoopsPaginator
+
+Client = AugmentedAIRuntimeClient
+
+
+__all__ = ("AugmentedAIRuntimeClient", "Client", "ListHumanLoopsPaginator")
