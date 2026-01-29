0cbed62fa92b2ecc38024a5c771a310e2de68dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:13:17 +0000

    Add package chebyfit/2026.1.18/chebyfit-2026.1.18.tar.gz

---
+++ packages/chebyfit/chebyfit-2026.1.18.tar.gz/chebyfit-2026.1.18/chebyfit/__init__.py
@@ -0,0 +1,9 @@
+# chebyfit/__init__.py
+
+from .chebyfit import *
+from .chebyfit import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Chebyfit version string."""
