a6611f6a9aa4129f1f91264a4e1d237746f867a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:46 +0000

    Add package dnd-5e-core/0.2.0/dnd_5e_core-0.2.0-py3-none-any.whl

---
+++ packages/dnd-5e-core/dnd_5e_core-0.2.0-py3-none-any.whl/dnd_5e_core/equipment/__init__.py
@@ -0,0 +1,45 @@
+"""
+D&D 5e Core - Equipment Module
+Contains all equipment classes (weapons, armor, potions, magic items, etc.)
+"""
+
+from .equipment import Cost, EquipmentCategory, Equipment, Inventory
+from .potion import PotionRarity, Potion, HealingPotion, SpeedPotion, StrengthPotion
+from .weapon import (
+    CategoryType, RangeType, DamageType,
+    WeaponProperty, WeaponRange, WeaponThrowRange, Weapon
+)
+from .armor import Armor
+from .magic_item import (
+    MagicItem, MagicItemRarity, MagicItemType,
+    MagicItemEffect, MagicItemAction, create_magic_item_from_data
+)
+from .predefined_magic_items import (
+    create_ring_of_protection, create_cloak_of_protection,
+    create_wand_of_magic_missiles, create_staff_of_healing,
+    create_belt_of_giant_strength, create_amulet_of_health,
+    create_bracers_of_defense, create_necklace_of_fireballs,
+    get_magic_item, PREDEFINED_MAGIC_ITEMS
+)
+
+__all__ = [
+    # Base equipment
+    'Cost', 'EquipmentCategory', 'Equipment', 'Inventory',
+    # Potions
+    'PotionRarity', 'Potion', 'HealingPotion', 'SpeedPotion', 'StrengthPotion',
+    # Weapon types
+    'CategoryType', 'RangeType', 'DamageType',
+    'WeaponProperty', 'WeaponRange', 'WeaponThrowRange', 'Weapon',
+    # Armor
+    'Armor',
+    # Magic Items
+    'MagicItem', 'MagicItemRarity', 'MagicItemType',
+    'MagicItemEffect', 'MagicItemAction', 'create_magic_item_from_data',
+    # Predefined Magic Items
+    'create_ring_of_protection', 'create_cloak_of_protection',
+    'create_wand_of_magic_missiles', 'create_staff_of_healing',
+    'create_belt_of_giant_strength', 'create_amulet_of_health',
+    'create_bracers_of_defense', 'create_necklace_of_fireballs',
+    'get_magic_item', 'PREDEFINED_MAGIC_ITEMS',
+]
+
