b163529c7eabeaa82fbe39788a1d6b8d7176e387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:34 +0000

    Add package types-aiobotocore-neptunedata/3.1.1/types_aiobotocore_neptunedata-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-neptunedata/types_aiobotocore_neptunedata-3.1.1-py3-none-any.whl/types_aiobotocore_neptunedata/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for neptunedata service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptunedata/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_neptunedata import (
+        Client,
+        NeptuneDataClient,
+    )
+
+    session = get_session()
+    async with session.create_client("neptunedata") as client:
+        client: NeptuneDataClient
+        ...
+
+    ```
+"""
+
+from .client import NeptuneDataClient
+
+Client = NeptuneDataClient
+
+__all__ = ("Client", "NeptuneDataClient")
