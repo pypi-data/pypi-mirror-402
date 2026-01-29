4b59569dc20e067f55edd49c38c5c2d639d4cebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:09:28 +0000

    Add package mogger/0.2.1/mogger-0.2.1.tar.gz

---
+++ packages/mogger/mogger-0.2.1.tar.gz/mogger-0.2.1/src/mogger/__init__.py
@@ -0,0 +1,8 @@
+import toml
+from .core import Mogger
+from .loki import LokiConfig, LokiLogger
+
+pyproject_conf = toml.load("pyproject.toml")
+__version__ = pyproject_conf["project"]["version"]
+
+__all__ = ["Mogger", "LokiConfig", "LokiLogger"]
