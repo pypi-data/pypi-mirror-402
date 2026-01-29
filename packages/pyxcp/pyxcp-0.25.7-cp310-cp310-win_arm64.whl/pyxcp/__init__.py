60c035553e3a550f76ac07c406d422df4c621837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:34:04 +0000

    Add package pyxcp/0.25.7/pyxcp-0.25.7-cp310-cp310-win_arm64.whl

---
+++ packages/pyxcp/pyxcp-0.25.7-cp310-cp310-win_arm64.whl/pyxcp/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""Universal Calibration Protocol for Python."""
+
+from rich import pretty
+from rich.console import Console
+from rich.traceback import install as tb_install
+
+
+pretty.install()
+
+from .master import Master  # noqa: F401, E402
+from .transport import Can, Eth, SxI, Usb  # noqa: F401, E402
+
+
+console = Console()
+tb_install(show_locals=True, max_frames=3)  # Install custom exception handler.
+
+# if you update this manually, do not forget to update
+# .bumpversion.cfg and pyproject.toml.
+__version__ = "0.25.7"
---
+++ packages/pyxcp/pyxcp-0.25.7-cp310-cp310-win_arm64.whl/pyxcp/scripts/xcp_fetch_a2l.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""Fetch A2L file from XCP slave (if supported)."""
+
+import sys
+from pathlib import Path
+
+from rich.prompt import Confirm
+
+from pyxcp.cmdline import ArgumentParser
+from pyxcp.types import XcpGetIdType
+
+
+def main():
+    ap = ArgumentParser(description="Fetch A2L file from XCP slave.")
+
+    with ap.run() as x:
+        x.connect()
+
+        # TODO: error-handling.
+        file_name = x.identifier(XcpGetIdType.FILENAME)
+        content = x.identifier(XcpGetIdType.FILE_TO_UPLOAD)
+        x.disconnect()
+        if not content:
+            sys.exit(f"Empty response from ID '{XcpGetIdType.FILE_TO_UPLOAD!r}'.")
+        if not file_name:
+            file_name = "output.a2l"
+        if not file_name.lower().endswith(".a2l"):
+            file_name += ".a2l"
+        dest = Path(file_name)
+        if dest.exists():
+            if not Confirm.ask(f"Destination file [green]{dest.name!r}[/green] already exists. Do you want to overwrite it?"):
+                print("Aborting...")
+                exit(1)
+        with dest.open("wt", encoding="utf-8") as of:
+            of.write(content)
+        print(f"A2L data written to {file_name!r}.")
+
+
+if __name__ == "__main__":
+    main()
