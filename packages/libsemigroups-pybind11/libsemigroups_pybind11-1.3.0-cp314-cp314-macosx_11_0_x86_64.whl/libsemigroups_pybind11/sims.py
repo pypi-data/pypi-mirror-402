85e50a0892e703029d2d0a28adf6a5677fa35994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:45 +0000

    Add package libsemigroups-pybind11/1.3.0/libsemigroups_pybind11-1.3.0-cp314-cp314-macosx_11_0_x86_64.whl

---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.3.0-cp314-cp314-macosx_11_0_x86_64.whl/libsemigroups_pybind11/sims.py
@@ -0,0 +1,295 @@
+# Copyright (c) 2024 R. Cirpons
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+
+"""full API for the helper functions is given below."""
+
+from typing_extensions import Self as _Self
+
+from _libsemigroups_pybind11 import (
+    MinimalRepOrc as _MinimalRepOrc,
+    RepOrc as _RepOrc,
+    Sims1 as _Sims1,
+    Sims2 as _Sims2,
+    SimsRefinerFaithful as _SimsRefinerFaithful,
+    SimsRefinerIdeals as _SimsRefinerIdeals,
+    sims_is_maximal_right_congruence as _is_maximal_right_congruence,
+    sims_is_right_congruence as _is_right_congruence,
+    sims_is_right_congruence_of_dual as _is_right_congruence_of_dual,
+    sims_is_two_sided_congruence as _is_two_sided_congruence,
+    sims_poset as _poset,
+    sims_right_generating_pairs as _right_generating_pairs,
+    sims_two_sided_generating_pairs as _two_sided_generating_pairs,
+)
+
+from .detail.cxx_wrapper import (
+    CxxWrapper as _CxxWrapper,
+    copy_cxx_mem_fns as _copy_cxx_mem_fns,
+    register_cxx_wrapped_type as _register_cxx_wrapped_type,
+    to_cxx as _to_cxx,
+    wrap_cxx_free_fn as _wrap_cxx_free_fn,
+)
+from .detail.decorators import copydoc as _copydoc
+from .presentation import Presentation as _Presentation
+
+
+class _SimsBase(_CxxWrapper):
+    def __init__(self: _Self, *args, **kwargs) -> None:
+        super().__init__(*args, optional_kwargs=("word",), **kwargs)
+        if _to_cxx(self) is not None:
+            return
+        if len(args) not in (0, 1):
+            raise TypeError(f"expected 0 or 1 positional arguments but found {len(args)}")
+        if len(args) != 0 and len(kwargs) != 0:
+            raise TypeError(
+                "expected either 1 positional argument or 1"
+                f"keyword argument (but not both), but found {len(args)} and {len(kwargs)}"
+            )
+        if len(args) == 0:
+            if "word" not in kwargs:
+                raise TypeError(
+                    f'expected the keyword argument "word", but found {tuple(kwargs.keys())}'
+                )
+            # for when Sims has a facade
+            # if kwargs["word"] not in (str, list[int]):
+            #     raise ValueError(
+            #         'the keyword argument "word" must be str or list[int], '
+            #         f"but found {kwargs['word']}"
+            #     )
+            if kwargs["word"] != list[int]:
+                raise ValueError(
+                    f'the keyword argument "word" must be list[int], but found {kwargs["word"]}'
+                )
+
+        if len(args) == 0:
+            self.py_template_params = (kwargs["word"],)
+        elif isinstance(args[0], _Presentation):
+            self.py_template_params = args[0].py_template_params
+        else:
+            raise TypeError(
+                f"expected the 1st argument to be a Presentation, but found {type(args[0])}"
+            )
+        self.init_cxx_obj(*args)
+
+
+########################################################################
+# Sims1 wrapper class
+########################################################################
+
+
+class Sims1(_SimsBase):
+    __doc__ = _Sims1.__doc__
+
+    _py_template_params_to_cxx_type = {(list[int],): _Sims1}
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {*_py_template_params_to_cxx_type.values()}
+
+    @_copydoc(_Sims1.__init__)
+    def __init__(self: _Self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+_copy_cxx_mem_fns(_Sims1, Sims1)
+_register_cxx_wrapped_type(_Sims1, Sims1)
+
+########################################################################
+# Sims2 wrapper class
+########################################################################
+
+
+class Sims2(_SimsBase):
+    __doc__ = _Sims2.__doc__
+
+    _py_template_params_to_cxx_type = {(list[int],): _Sims2}
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {*_py_template_params_to_cxx_type.values()}
+
+    @_copydoc(_Sims2.__init__)
+    def __init__(self: _Self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+_copy_cxx_mem_fns(_Sims2, Sims2)
+_register_cxx_wrapped_type(_Sims2, Sims2)
+
+########################################################################
+# RepOrc class wrapper
+########################################################################
+
+
+class RepOrc(_SimsBase):
+    __doc__ = _RepOrc.__doc__
+
+    _py_template_params_to_cxx_type = {(list[int],): _RepOrc}
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {*_py_template_params_to_cxx_type.values()}
+
+    @_copydoc(_RepOrc.__init__)
+    def __init__(self: _Self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+_copy_cxx_mem_fns(_RepOrc, RepOrc)
+_register_cxx_wrapped_type(_RepOrc, RepOrc)
+
+########################################################################
+# MinimalRepOrc class wrapper
+########################################################################
+
+
+class MinimalRepOrc(_SimsBase):
+    __doc__ = _MinimalRepOrc.__doc__
+
+    _py_template_params_to_cxx_type = {(list[int],): _MinimalRepOrc}
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {*_py_template_params_to_cxx_type.values()}
+
+    @_copydoc(_MinimalRepOrc.__init__)
+    def __init__(self: _Self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+_copy_cxx_mem_fns(_MinimalRepOrc, MinimalRepOrc)
+_register_cxx_wrapped_type(_MinimalRepOrc, MinimalRepOrc)
+
+
+########################################################################
+# SimsRefinerIdeals class wrapper
+########################################################################
+
+
+class SimsRefinerIdeals(_SimsBase):
+    __doc__ = _SimsRefinerIdeals.__doc__
+
+    _py_template_params_to_cxx_type = {(list[int],): _SimsRefinerIdeals}
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {*_py_template_params_to_cxx_type.values()}
+
+    @_copydoc(_SimsRefinerIdeals.__init__)
+    def __init__(self: _Self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    @_copydoc(_SimsRefinerIdeals.__call__)
+    def __call__(self: _Self, *args, **kwargs) -> bool:
+        return super().__call__(*args, **kwargs)
+
+
+_copy_cxx_mem_fns(_SimsRefinerIdeals, SimsRefinerIdeals)
+_register_cxx_wrapped_type(_SimsRefinerIdeals, SimsRefinerIdeals)
+
+########################################################################
+# SimsRefinerFaithful class wrapper
+########################################################################
+
+
+class SimsRefinerFaithful(_CxxWrapper):
+    __doc__ = _SimsRefinerFaithful.__doc__
+
+    _py_template_params_to_cxx_type = {(list[int],): _SimsRefinerFaithful}
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {*_py_template_params_to_cxx_type.values()}
+
+    @_copydoc(_SimsRefinerFaithful.__init__)
+    def __init__(self: _Self, *args, **kwargs) -> None:
+        super().__init__(*args, optional_kwargs=("word",), **kwargs)
+        if _to_cxx(self) is not None:
+            return
+
+        if len(args) not in (0, 1):
+            raise TypeError(f"expected 0 or 1 positional arguments but found {len(args)}")
+        if len(args) != 0 and len(kwargs) != 0:
+            raise TypeError(
+                "expected either 1 positional argument or 1"
+                f"keyword argument (but not both), but found {len(args)} and {len(kwargs)}"
+            )
+        if len(args) == 0:
+            if "word" not in kwargs:
+                raise TypeError(
+                    f'expected the keyword argument "word", but found {tuple(kwargs.keys())}'
+                )
+            if kwargs["word"] != list[int]:
+                raise ValueError(
+                    f'the keyword argument "word" must be list[int], but found {kwargs["word"]}'
+                )
+        if len(args) == 0:
+            self.py_template_params = (kwargs["word"],)
+        elif (
+            isinstance(args[0], list)
+            and all(isinstance(x, list) for x in args[0])
+            and all(isinstance(y, int) for x in args[0] for y in x)
+        ):
+            self.py_template_params = (list[int],)
+        else:
+            raise TypeError("expected the 1st argument to be a list[list[int]]")
+        self.init_cxx_obj(*args)
+
+    @_copydoc(_SimsRefinerFaithful.__call__)
+    def __call__(self: _Self, *args, **kwargs) -> bool:
+        return super().__call__(*args, **kwargs)
+
+
+_copy_cxx_mem_fns(_SimsRefinerFaithful, SimsRefinerFaithful)
+_register_cxx_wrapped_type(_SimsRefinerFaithful, SimsRefinerFaithful)
+
+########################################################################
+# Helper functions
+########################################################################
+
+right_generating_pairs = _wrap_cxx_free_fn(_right_generating_pairs)
+two_sided_generating_pairs = _wrap_cxx_free_fn(_two_sided_generating_pairs)
+is_right_congruence = _wrap_cxx_free_fn(_is_right_congruence)
+is_right_congruence_of_dual = _wrap_cxx_free_fn(_is_right_congruence_of_dual)
+is_two_sided_congruence = _wrap_cxx_free_fn(_is_two_sided_congruence)
+is_maximal_right_congruence = _wrap_cxx_free_fn(_is_maximal_right_congruence)
+poset = _wrap_cxx_free_fn(_poset)
---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.3.0-cp314-cp314-macosx_11_0_x86_64.whl/libsemigroups_pybind11/transf.py
@@ -0,0 +1,355 @@
+# Copyright (c) 2021-2024 J. D. Mitchell
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+"""This page contains the documentation for the ``transf`` subpackage, that
+contains helper functions for the :any:`Transf`, :any:`Perm`, :any:`PPerm`
+classes.
+"""
+
+import abc
+from typing import Any as _Any
+
+from typing_extensions import Self
+
+from _libsemigroups_pybind11 import (
+    Perm1 as _Perm1,
+    Perm2 as _Perm2,
+    Perm4 as _Perm4,
+    PPerm1 as _PPerm1,
+    PPerm2 as _PPerm2,
+    PPerm4 as _PPerm4,
+    Transf1 as _Transf1,
+    Transf2 as _Transf2,
+    Transf4 as _Transf4,
+    Undefined as _Undefined,
+    transf_domain as _transf_domain,
+    transf_image as _transf_image,
+    transf_inverse as _transf_inverse,
+    transf_left_one as _transf_left_one,
+    transf_one as _transf_one,
+    transf_right_one as _transf_right_one,
+)
+
+from .detail.cxx_wrapper import (
+    CxxWrapper as _CxxWrapper,
+    copy_cxx_mem_fns as _copy_cxx_mem_fns,
+    register_cxx_wrapped_type as _register_cxx_wrapped_type,
+    to_cxx as _to_cxx,
+    to_py as _to_py,
+    wrap_cxx_free_fn as _wrap_cxx_free_fn,
+)
+from .detail.decorators import copydoc as _copydoc
+
+########################################################################
+# PTransfBase protected python class
+########################################################################
+
+
+class _PTransfBase(_CxxWrapper):
+    """Subclasses must implement:
+    * __str__                   (function)
+    * __repr__                  (function)
+    * one                       (static method)
+    """
+
+    @abc.abstractmethod
+    def __str__(self: Self) -> str:
+        return ""  # pragma: no cover
+
+    @abc.abstractmethod
+    def __repr__(self: Self) -> str:
+        return ""  # pragma: no cover
+
+    @staticmethod
+    def _py_template_params_from_degree(n: int) -> tuple[int]:
+        if n < 2**8:
+            return (2**8,)
+        if n < 2**16:
+            return (2**16,)
+        assert n <= 2**32
+        return (2**32,)
+
+    def _set_py_template_params_from_degree(self: Self, n: int) -> None:
+        self.py_template_params = self._py_template_params_from_degree(n)
+
+    def _cxx_type_change_required(self: Self, n: int) -> bool:
+        assert n <= 2**32
+        return n > self.py_template_params[0]
+
+    def __getitem__(self: Self, i: int) -> int | _Undefined:
+        return _to_cxx(self)[i]
+
+    def __hash__(self: Self) -> int:
+        return _to_cxx(self).__hash__()
+
+    def __new__(cls, *_):
+        return super().__new__(cls)
+
+    def __init__(self: Self, arg) -> None:
+        super().__init__(arg)
+        if _to_cxx(self) is not None:
+            return
+
+        images = arg
+
+        if len(images) > 2**32:
+            raise ValueError(  # pragma: no cover
+                f"the argument (a list) must have length at most {2**32}, found {len(images)}"
+            )
+        if not isinstance(images, list):
+            images = list(images)
+
+        self._set_py_template_params_from_degree(len(images))
+        self.init_cxx_obj(images)
+
+    def __eq__(self: Self, other) -> bool:
+        if not isinstance(_to_cxx(other), type(_to_cxx(self))):
+            return False
+        return _to_cxx(self) == _to_cxx(other)
+
+    def __lt__(self: Self, other) -> bool:
+        return _to_cxx(self) < _to_cxx(other)
+
+    def __le__(self: Self, other) -> bool:
+        return self < other or self == other
+
+    def __gt__(self: Self, other) -> bool:
+        return other < self
+
+    def __ge__(self: Self, other) -> bool:
+        return other < self or self == other
+
+    def __mul__(self: Self, other: Self):
+        result = one(self)
+        result.product_inplace(self, other)
+        return result
+
+    @staticmethod
+    @abc.abstractmethod
+    def one(n: int) -> Self:  # pylint: disable=missing-function-docstring
+        pass  # pragma: no cover
+
+    def increase_degree_by(  # pylint: disable=missing-function-docstring
+        self: Self, n: int
+    ) -> Self:
+        new_degree = n + self.degree()
+        if new_degree > 2**32:
+            raise ValueError(
+                "the 1st argument (int) is too large, partial "
+                "transformations of degree > 2 ** 32 are not supported, "
+                f"expected at most {2**32 - self.degree()} but found {n}"
+            )
+        if self._cxx_type_change_required(new_degree):
+            images = list(self.images())
+            images.extend(range(self.degree(), new_degree))
+            self._set_py_template_params_from_degree(len(images))
+            self.init_cxx_obj(images)
+
+        else:
+            _to_cxx(self).increase_degree_by(n)
+        return self
+
+
+########################################################################
+# Transf python class
+########################################################################
+
+
+class Transf(_PTransfBase):
+    __doc__ = _Transf1.__doc__
+
+    _py_template_params_to_cxx_type = {(2**8,): _Transf1, (2**16,): _Transf2, (2**32,): _Transf4}
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {_Transf1, _Transf2, _Transf4}
+
+    # This method only exists to copy the doc. . .
+    @_copydoc(_Transf1.__init__)
+    def __init__(self: Self, imgs: list[int]) -> None:
+        super().__init__(imgs)
+
+    # We retain a separate __repr__ so that we can distinguish the cxx objects
+    # and their python counterparts.
+    def __repr__(self: Self) -> str:
+        result = str(self)
+        if len(result) < 72:
+            return result
+        return f"<transformation of degree {self.degree()} and rank {self.rank()}>"
+
+    # We retain a separate __repr__ so that we can distinguish the cxx objects
+    # and their python counterparts.
+    def __str__(self: Self) -> str:
+        return f"Transf({list(self.images())})"
+
+    # This method only exists to copy the doc. . .
+    @_copydoc(_Transf1.increase_degree_by)
+    def increase_degree_by(self: Self, n: int) -> Self:
+        _PTransfBase.increase_degree_by(self, n)
+        return self
+
+    @staticmethod
+    @_copydoc(_Transf1.one)
+    def one(n: int) -> Self:
+        result_type = Transf._py_template_params_to_cxx_type[
+            Transf._py_template_params_from_degree(n)
+        ]
+        return _to_py(result_type.one(n))
+
+
+_copy_cxx_mem_fns(_Transf1, Transf)
+_register_cxx_wrapped_type(_Transf1, Transf)
+_register_cxx_wrapped_type(_Transf2, Transf)
+_register_cxx_wrapped_type(_Transf4, Transf)
+
+########################################################################
+# PPerm python class
+########################################################################
+
+
+class PPerm(_PTransfBase):
+    __doc__ = _PPerm1.__doc__
+
+    _py_template_params_to_cxx_type = {(2**8,): _PPerm1, (2**16,): _PPerm2, (2**32,): _PPerm4}
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {_PPerm1, _PPerm2, _PPerm4}
+
+    @staticmethod
+    def _cxx_type_from_degree(n: int):
+        if n <= 2**8:
+            return _PPerm1
+        if n <= 2**16:
+            return _PPerm2
+        return _PPerm4
+
+    @_copydoc(_PPerm1.__init__)
+    def __init__(self: Self, *args) -> None:
+        if len(args) < 3:
+            super().__init__(*args)
+            return
+        if len(args) != 3:
+            raise TypeError(f"expected 1 or 3 arguments, found {len(args)}")
+        dom, im, deg = args
+        self._set_py_template_params_from_degree(deg)
+        self.init_cxx_obj(dom, im, deg)
+
+    # We retain a separate __repr__ so that we can distinguish the cxx objects
+    # and their python counterparts.
+    def __repr__(self: Self) -> str:
+        result = str(self)
+        if len(result) < 72:
+            return result
+        return f"<partial perm of degree {self.degree()} and rank {self.rank()}>"
+
+    # We retain a separate __str__ so that we can distinguish the cxx objects
+    # and their python counterparts.
+    def __str__(self: Self) -> str:
+        return f"PPerm({domain(self)}, {[self[i] for i in domain(self)]}, {self.degree()})"
+
+    # This method only exists to copy the doc. . .
+    @_copydoc(_PPerm1.increase_degree_by)
+    def increase_degree_by(self: Self, n: int) -> Self:
+        _PTransfBase.increase_degree_by(self, n)
+        return self
+
+    @staticmethod
+    @_copydoc(_PPerm1.one)
+    def one(n: int) -> Self:
+        result_type = PPerm._py_template_params_to_cxx_type[
+            PPerm._py_template_params_from_degree(n)
+        ]
+        return _to_py(result_type.one(n))
+
+
+_copy_cxx_mem_fns(_PPerm1, PPerm)
+
+_register_cxx_wrapped_type(_PPerm1, PPerm)
+_register_cxx_wrapped_type(_PPerm2, PPerm)
+_register_cxx_wrapped_type(_PPerm4, PPerm)
+
+
+########################################################################
+# Perm python class
+########################################################################
+
+
+# Deriving from Transf messes up the documentation
+class Perm(_PTransfBase):
+    __doc__ = _Perm1.__doc__
+
+    _py_template_params_to_cxx_type = {(2**8,): _Perm1, (2**16,): _Perm2, (2**32,): _Perm4}
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {_Perm1, _Perm2, _Perm4}
+
+    # This method only exists to copy the doc. . .
+    @_copydoc(_Perm1.__init__)
+    def __init__(self: Self, imgs: list[int]) -> None:
+        super().__init__(imgs)
+
+    # We retain a separate __repr__ so that we can distinguish the cxx objects
+    # and their python counterparts.
+    def __repr__(self: Self) -> str:
+        result = str(self)
+        if len(result) < 72:
+            return result
+        return f"<permutation of degree {self.degree()}>"
+
+    # We retain a separate __repr__ so that we can distinguish the cxx objects
+    # and their python counterparts.
+    def __str__(self: Self) -> str:
+        return f"Perm({list(self.images())})"
+
+    # This method only exists to copy the doc. . .
+    @_copydoc(_Perm1.increase_degree_by)
+    def increase_degree_by(self: Self, n: int) -> Self:
+        _PTransfBase.increase_degree_by(self, n)
+        return self
+
+    @staticmethod
+    @_copydoc(_Perm1.one)
+    def one(n: int) -> Self:
+        result_type = Perm._py_template_params_to_cxx_type[Perm._py_template_params_from_degree(n)]
+        return _to_py(result_type.one(n))
+
+
+_copy_cxx_mem_fns(_Perm1, Perm)
+
+_register_cxx_wrapped_type(_Perm1, Perm)
+_register_cxx_wrapped_type(_Perm2, Perm)
+_register_cxx_wrapped_type(_Perm4, Perm)
+
+########################################################################
+# Helper functions
+########################################################################
+
+one = _wrap_cxx_free_fn(_transf_one)
+inverse = _wrap_cxx_free_fn(_transf_inverse)
+right_one = _wrap_cxx_free_fn(_transf_right_one)
+left_one = _wrap_cxx_free_fn(_transf_left_one)
+image = _wrap_cxx_free_fn(_transf_image)
+domain = _wrap_cxx_free_fn(_transf_domain)
