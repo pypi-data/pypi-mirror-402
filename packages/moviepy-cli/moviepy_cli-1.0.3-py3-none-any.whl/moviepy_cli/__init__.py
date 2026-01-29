b51790a2b510cae035e42a99f0dd957fe7d8d4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:18:29 +0000

    Add package moviepy-cli/1.0.3/moviepy_cli-1.0.3-py3-none-any.whl

---
+++ packages/moviepy-cli/moviepy_cli-1.0.3-py3-none-any.whl/moviepy_cli/__init__.py
@@ -0,0 +1,62 @@
+import argparse
+import importlib.metadata
+import tempfile
+
+import moviepy.editor
+import path
+
+__version__: str = importlib.metadata.version("moviepy-cli")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"moviepy-cli {__version__}"
+    )
+    parser.add_argument("input_file", type=path.Path, help="input file")
+    parser.add_argument("--start", help="start time")
+    parser.add_argument("--end", help="end time")
+    parser.add_argument("--retime", type=float, help="retime")
+    parser.add_argument("--inplace", "-i", action="store_true", help="inplace")
+    parser.add_argument("--scale", type=float, help="scale")
+    parser.add_argument("--height", type=int, help="height")
+    parser.add_argument("--width", type=int, help="width")
+    parser.add_argument("--fps", type=int, help="fps")
+    parser.add_argument("--output-file", "-o", type=path.Path, help="output file")
+    args = parser.parse_args()
+
+    args.input_file = args.input_file.abspath()
+
+    clip = moviepy.editor.VideoFileClip(args.input_file)
+
+    if args.start:
+        clip = clip.subclip(args.start, args.end)
+    if args.retime:
+        clip = clip.fx(moviepy.editor.vfx.speedx, args.retime)  # type: ignore
+    if args.scale:
+        clip = clip.resize(args.scale)  # type: ignore
+    if args.height or args.width:
+        clip = clip.resize(height=args.height, width=args.width)  # type: ignore
+    if args.fps:
+        clip = clip.set_fps(args.fps)
+
+    if args.output_file is None:
+        args.output_file = path.Path(
+            tempfile.mktemp(  # type: ignore
+                prefix=args.input_file.stem + ".",
+                suffix=".mp4",
+                dir=args.input_file.parent,
+            )
+        )
+    clip.write_videofile(
+        args.output_file,
+        # following parameters are mainly for QuickTime Player on macOS
+        ffmpeg_params=["-pix_fmt", "yuv420p"],
+        audio_codec="aac",
+    )
+
+    if args.inplace:
+        print(f"Moviepy-cli - Replacing {args.input_file} by {args.output_file}")
+        args.output_file.move(args.input_file)
