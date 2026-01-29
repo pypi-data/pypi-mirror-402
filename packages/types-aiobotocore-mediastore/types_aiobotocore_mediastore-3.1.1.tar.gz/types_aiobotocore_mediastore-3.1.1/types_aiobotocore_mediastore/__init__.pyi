d2b4e2c7ca8ea4a7050f51b494c56daf43625147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:31 +0000

    Add package types-aiobotocore-mediastore/3.1.1/types_aiobotocore_mediastore-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-mediastore/types_aiobotocore_mediastore-3.1.1.tar.gz/types_aiobotocore_mediastore-3.1.1/types_aiobotocore_mediastore/__init__.pyi
@@ -0,0 +1,33 @@
+"""
+Main interface for mediastore service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_mediastore import (
+        Client,
+        ListContainersPaginator,
+        MediaStoreClient,
+    )
+
+    session = get_session()
+    async with session.create_client("mediastore") as client:
+        client: MediaStoreClient
+        ...
+
+
+    list_containers_paginator: ListContainersPaginator = client.get_paginator("list_containers")
+    ```
+"""
+
+from .client import MediaStoreClient
+from .paginator import ListContainersPaginator
+
+Client = MediaStoreClient
+
+__all__ = ("Client", "ListContainersPaginator", "MediaStoreClient")
---
+++ packages/types-aiobotocore-mediastore/types_aiobotocore_mediastore-3.1.1.tar.gz/types_aiobotocore_mediastore-3.1.1/types_aiobotocore_mediastore/paginator.pyi
@@ -0,0 +1,58 @@
+"""
+Type annotations for mediastore service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_mediastore.client import MediaStoreClient
+    from types_aiobotocore_mediastore.paginator import (
+        ListContainersPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("mediastore") as client:
+        client: MediaStoreClient
+
+        list_containers_paginator: ListContainersPaginator = client.get_paginator("list_containers")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import ListContainersInputPaginateTypeDef, ListContainersOutputTypeDef
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = ("ListContainersPaginator",)
+
+if TYPE_CHECKING:
+    _ListContainersPaginatorBase = AioPaginator[ListContainersOutputTypeDef]
+else:
+    _ListContainersPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListContainersPaginator(_ListContainersPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore/paginator/ListContainers.html#MediaStore.Paginator.ListContainers)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore/paginators/#listcontainerspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListContainersInputPaginateTypeDef]
+    ) -> AioPageIterator[ListContainersOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore/paginator/ListContainers.html#MediaStore.Paginator.ListContainers.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore/paginators/#listcontainerspaginator)
+        """
