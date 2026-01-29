e018c464ac370fc54a587cc8338106407c9d23ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:13:40 +0000

    Add package PySideSix-Frameless-Window/0.7.7/pysidesix_frameless_window-0.7.7.tar.gz

---
+++ packages/PySideSix-Frameless-Window/pysidesix_frameless_window-0.7.7.tar.gz/pysidesix_frameless_window-0.7.7/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="PySideSix-Frameless-Window",
+    version="0.7.7",
+    keywords="pyside6 frameless",
+    author="zhiyiYo",
+    author_email="shokokawaii@outlook.com",
+    description="A cross-platform frameless window based on pyside6, support Win32, Linux and macOS.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="LGPLv3",
+    url="https://github.com/zhiyiYo/PyQt-Frameless-Window/tree/PySide6",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pywin32;platform_system=='Windows'",
+        "pyobjc;platform_system=='Darwin'",
+        "PyCocoa;platform_system=='Darwin'",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent'
+    ]
+)
