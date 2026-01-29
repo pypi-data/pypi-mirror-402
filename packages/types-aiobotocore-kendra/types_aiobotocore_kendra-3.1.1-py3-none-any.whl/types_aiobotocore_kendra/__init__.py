b3b6e62950f5722cfcab168f6ebd61373f718ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:25 +0000

    Add package types-aiobotocore-kendra/3.1.1/types_aiobotocore_kendra-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-kendra/types_aiobotocore_kendra-3.1.1-py3-none-any.whl/types_aiobotocore_kendra/__init__.py
@@ -0,0 +1,30 @@
+"""
+Main interface for kendra service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kendra/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_kendra import (
+        Client,
+        KendraClient,
+    )
+
+    session = get_session()
+    async with session.create_client("kendra") as client:
+        client: KendraClient
+        ...
+
+    ```
+"""
+
+from .client import KendraClient
+
+Client = KendraClient
+
+
+__all__ = ("Client", "KendraClient")
