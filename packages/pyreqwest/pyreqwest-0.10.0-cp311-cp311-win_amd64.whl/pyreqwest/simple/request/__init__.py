57e7790bbe5bb8cfbf7135b6231f49f7dba3e0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:12:18 +0000

    Add package pyreqwest/0.10.0/pyreqwest-0.10.0-cp311-cp311-win_amd64.whl

---
+++ packages/pyreqwest/pyreqwest-0.10.0-cp311-cp311-win_amd64.whl/pyreqwest/simple/request/__init__.py
@@ -0,0 +1,21 @@
+"""Simple async interfaces for doing one-off requests."""
+
+from pyreqwest._pyreqwest.simple.request import (
+    pyreqwest_delete,
+    pyreqwest_get,
+    pyreqwest_head,
+    pyreqwest_patch,
+    pyreqwest_post,
+    pyreqwest_put,
+    pyreqwest_request,
+)
+
+__all__ = [
+    "pyreqwest_request",
+    "pyreqwest_get",
+    "pyreqwest_post",
+    "pyreqwest_put",
+    "pyreqwest_patch",
+    "pyreqwest_delete",
+    "pyreqwest_head",
+]
