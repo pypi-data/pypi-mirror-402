965c52eb1ce860475b21df49fabce815be08edae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:13:35 +0000

    Add package conferencemapper/1.0.1/conferencemapper-1.0.1.tar.gz

---
+++ packages/conferencemapper/conferencemapper-1.0.1.tar.gz/conferencemapper-1.0.1/README.md
@@ -0,0 +1,41 @@
+Simple Jigasi ConferenceMapper with Redis, Flask and Gunicorn
+=============================================================
+
+This implements the /conferenceMapper part of https://github.com/jitsi/jitsi-meet/blob/master/resources/cloud-api.swagger
+
+Prerequisites: Redis, redis-py, flask and a wsgi server like gunicorn
+
+Packages for Ubuntu Server 24.04
+
+    apt install python3-flask gunicorn redis-server, python3-redis
+
+Copy conferencemapper.service and conferencemapper.socket to /etc/systemd/system
+
+Adapt /etc/systemd/system/conferencemapper.service to match WorkingDirectory to conferencemapper module directory.
+
+Add reverse proxy in front of gunicorn (nginx)
+
+    location = /conferenceMapper {
+        proxy_pass http://127.0.0.1:8001/conferenceMapper?$query_string;
+    }
+
+Start it and activate nginx config
+
+    systemctl daemon-reload
+    systemctl enable conferencemapper.socket
+    systemctl start conferencemapper.socket
+    systemctl reload nginx
+
+## Development and Testing
+
+To install the package with test dependencies:
+
+    pip install -e ".[test]"
+
+To run the tests:
+
+    pytest tests/
+
+Or with verbose output:
+
+    pytest tests/ -v
---
+++ packages/conferencemapper/conferencemapper-1.0.1.tar.gz/conferencemapper-1.0.1/conferencemapper/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+import redis
+import hashlib
+import urllib
+from datetime import timedelta
+
+expire = timedelta(days=365)
+digits = 6
+
+app = Flask(__name__)
+r = redis.Redis()
+
+
+@app.route("/conferenceMapper")
+def mapper():
+    conference = request.args.get('conference')
+    confid = request.args.get('id')
+
+    if confid:
+        try:
+            confid = int(confid)
+        except ValueError:
+            confid = None
+    
+    if confid:
+        conference = r.get(confid)
+        
+        if conference:
+            return jsonify({
+                "message": "Successfully retrieved conference mapping",
+                "id": confid,
+                "conference": conference.decode("utf-8")
+            })
+        else:
+            return jsonify({
+                "message": "No conference mapping was found",
+                "id": confid,
+                "conference": False
+            })
+    
+    if conference:
+        # jitsi lowercases conference names internally, do this as well, before calculating the PIN
+        conference = conference.lower()
+        confid =int(hashlib.sha1(conference.encode("utf-8")).hexdigest(), 16) % 10**digits
+        # jitsi internally uses urlencoded room names, so we need to store the conference name urlencoded as well
+        # @ sign is stripped from room names, so we can add that to the list of safe characters, so that room@conference
+        # stays intact
+        encoded_conference = urllib.parse.quote(conference, safe='/@')
+        if r.set(confid, encoded_conference) and r.expire(confid, expire):
+            return jsonify({
+                "message": "Successfully retrieved conference mapping",
+                "id": confid,
+                "conference": conference
+            })
+    
+    return jsonify({
+        "message": "No conference or id provided",
+        "conference": False,
+        "id": False
+    })
+
+if __name__ == '__main__':
+    app.run(port=8001)
