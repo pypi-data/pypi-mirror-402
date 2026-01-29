0bf8e14de8bb8e0a522fb30315bd6fb05b46ea8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:12:44 +0000

    Add package ReverseBox/0.83.3/reversebox-0.83.3.tar.gz

---
+++ packages/ReverseBox/reversebox-0.83.3.tar.gz/reversebox-0.83.3/reversebox/io_files/bytes_handler.py
@@ -0,0 +1,34 @@
+"""
+Copyright © 2023  Bartłomiej Duda
+License: GPL-3.0 License
+"""
+
+
+class BytesHandler:
+    def __init__(self, input_bytes: bytes):
+        self.input_bytes = input_bytes
+        self.data_size = len(input_bytes)
+
+    def get_bytes(self, offset: int, size: int) -> bytes:
+        # fmt: off
+        output_bytes: bytes = self.input_bytes[offset: offset + size]
+        # fmt: on
+        return output_bytes
+
+    def fill_to_length(self, expected_length: int, fill_byte: bytes = b"\x00") -> bytes:
+        if len(fill_byte) > 1:
+            raise Exception("Fill byte is too long!")
+
+        if len(self.input_bytes) >= expected_length:
+            raise Exception("Bytes sequence is longer or equal expected length!")
+
+        length_to_fill: int = expected_length - len(self.input_bytes)
+        fill_value: bytes = b""
+        for _ in range(length_to_fill):
+            fill_value += fill_byte
+        return self.input_bytes + fill_value
+
+    # TODO - test if this works
+    def bytes_to_bitstring(self):
+        binary_string = "".join(format(byte, "08b") for byte in self.input_bytes)
+        return binary_string
