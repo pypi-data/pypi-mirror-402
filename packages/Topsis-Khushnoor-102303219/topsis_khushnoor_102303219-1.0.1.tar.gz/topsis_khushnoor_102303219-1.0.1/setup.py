c0687dc085a713e5df20adc3a2beadbb6f2eb5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:12:53 +0000

    Add package Topsis-Khushnoor-102303219/1.0.1/topsis_khushnoor_102303219-1.0.1.tar.gz

---
+++ packages/Topsis-Khushnoor-102303219/topsis_khushnoor_102303219-1.0.1.tar.gz/topsis_khushnoor_102303219-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-Khushnoor-102303219",
+    version="1.0.1",
+    author="Khushnoor Kaur",
+    author_email="kkaur1_be23@thapar.edu",
+    description="A Python package for TOPSIS MCDM problems",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/KKaur170/Topsis-Khushnoor-102303219",
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy'],
+    entry_points={
+        'console_scripts': [
+            'topsis = topsis_bin.topsis:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
