4f060d1f8c5573c1ff781511a89ec77b5214904e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 12:07:13 +0000

    Add package spherogram/2.4/spherogram-2.4-cp38-cp38-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/spherogram/spherogram-2.4-cp38-cp38-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/spherogram/__init__.py
@@ -0,0 +1,26 @@
+from .presentations import *
+from .links import *
+from .codecs import *
+
+# Make the module version number easily accessible.
+from . import version as _version
+
+
+def version():
+    return _version.version
+
+
+__version__ = version()
+
+
+__all__ = ['ABC', 'ClosedBraid', 'CyclicList', 'CyclicWord',
+           'DTcodec', 'Digraph',
+           'DirectedEdge', 'DirectedMultiEdge', 'Edge', 'FatEdge',
+           'FatGraph', 'Graph',
+           'Link', 'MultiEdge', 'Presentation',
+           'Crossing', 'Strand', 'WhiteheadMove',
+           'Word', 'random_link',
+           # from spherogram.links.tangles:
+           'Tangle', 'CapTangle', 'CupTangle', 'RationalTangle',
+           'ZeroTangle', 'InfinityTangle', 'MinusOneTangle', 'OneTangle', 'IntegerTangle',
+           'IdentityBraid', 'ComponentTangle', 'join_strands']
