776e54f33ce1244d2af0699a55e70734a18fdc18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:10:53 +0000

    Add package topsispackage1.1.1/0.0.7/topsispackage1_1_1-0.0.7.tar.gz

---
+++ packages/topsispackage1.1.1/topsispackage1_1_1-0.0.7.tar.gz/topsispackage1_1_1-0.0.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = "0.0.7"
+DESCRIPTION = "topsis"
+
+setup(
+    name="topsispackage1.1.1",
+    version=VERSION,
+    author="Karanveer Singh",
+    author_email="karanveer135790@gmail.com",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["pandas", "numpy"],
+    keywords=["python", "topsis", "ranking", "decision making", "mcdm"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
