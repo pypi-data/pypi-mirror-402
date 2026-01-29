85de8b098d5d4d9e4b4929e9511744ef530cd349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:17 +0000

    Add package odoo14-addon-ssi-hr/14.0.3.2.0/odoo14_addon_ssi_hr-14.0.3.2.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-hr/odoo14_addon_ssi_hr-14.0.3.2.0-py3-none-any.whl/odoo/addons/ssi_hr/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=C8101
+{
+    "name": "Human Resource",
+    "version": "14.0.3.2.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": True,
+    "auto_install": True,
+    "depends": [
+        "hr",
+        "configuration_helper",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "views/res_config_settings_views.xml",
+        "views/hr_employee_views.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+    "demo": [],
+}
---
+++ packages/odoo14-addon-ssi-hr/odoo14_addon_ssi_hr-14.0.3.2.0-py3-none-any.whl/odoo/addons/ssi_hr/models/res_company.py
@@ -0,0 +1,10 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class ResCompany(models.Model):
+    _name = "res.company"
+    _inherit = "res.company"
