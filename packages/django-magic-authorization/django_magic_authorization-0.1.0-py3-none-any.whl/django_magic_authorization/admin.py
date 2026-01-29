a58fa49690042030b6ea04abb9e84c4be0ba6e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:19:28 +0000

    Add package django-magic-authorization/0.1.0/django_magic_authorization-0.1.0-py3-none-any.whl

---
+++ packages/django-magic-authorization/django_magic_authorization-0.1.0-py3-none-any.whl/django_magic_authorization/admin.py
@@ -0,0 +1,64 @@
+from django.contrib import admin
+from django import forms
+
+from django_magic_authorization.models import AccessToken
+from django_magic_authorization.middleware import MagicAuthorizationRouter
+
+
+class AccessTokenForm(forms.ModelForm):
+    def get_routes():
+        router = MagicAuthorizationRouter()
+        return ((p, p) for p in router.get_protected_paths())
+
+    path_choice = forms.ChoiceField(choices=get_routes)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.path = self.cleaned_data.get("path_choice")
+        if commit:
+            instance.save()
+        return instance
+
+    class Meta:
+        model = AccessToken
+        exclude = ["path"]
+
+
+class AccessTokenAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_at"
+    list_display = (
+        "description",
+        "display_path",
+        "is_valid",
+        "access_link",
+        "created_at",
+        "last_accessed",
+        "times_accessed",
+    )
+    readonly_fields = (
+        "created_at",
+        "last_accessed",
+        "times_accessed",
+        "token",
+        "access_link",
+    )
+    form = AccessTokenForm
+    list_filter = ["is_valid", "created_at"]
+    search_fields = ["description", "path"]
+
+    def display_path(self, obj):
+        router = MagicAuthorizationRouter()
+        if obj.path not in router.get_protected_paths():
+            return f"❗ {obj.path}"
+        else:
+            return obj.path
+
+    display_path.short_description = "Path"
+
+    def access_link(self, obj):
+        return f"{obj.path}?token={obj.token}"
+
+    access_link.short_description = "Access Link"
+
+
+admin.site.register(AccessToken, AccessTokenAdmin)
