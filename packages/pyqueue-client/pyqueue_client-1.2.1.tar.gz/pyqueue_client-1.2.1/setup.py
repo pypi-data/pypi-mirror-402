2e286af13f5067b2ddaa2dbe07da349216891536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:19:41 +0000

    Add package pyqueue-client/1.2.1/pyqueue_client-1.2.1.tar.gz

---
+++ packages/pyqueue-client/pyqueue_client-1.2.1.tar.gz/pyqueue_client-1.2.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Read README file for long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyqueue-client",
+    version="1.2.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0",
+        "urllib3>=1.26.0"
+    ],
+    description="Python client library for adding messages to PyQueue with local and remote support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Mauro Giulivi",
+    url="https://github.com/downlevel/pyqueue-client",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
