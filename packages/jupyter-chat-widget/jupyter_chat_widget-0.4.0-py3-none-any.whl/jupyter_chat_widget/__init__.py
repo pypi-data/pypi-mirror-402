e3e4111b35ec2bd9f4510cd039f12cc819105067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:12:53 +0000

    Add package jupyter-chat-widget/0.4.0/jupyter_chat_widget-0.4.0-py3-none-any.whl

---
+++ packages/jupyter-chat-widget/jupyter_chat_widget-0.4.0-py3-none-any.whl/jupyter_chat_widget/__init__.py
@@ -0,0 +1,6 @@
+"""jupyter-chat-widget: A simple chat UI widget for Jupyter notebooks."""
+
+from jupyter_chat_widget._version import __version__
+from jupyter_chat_widget.chat import ChatUI
+
+__all__ = ["ChatUI", "__version__"]
