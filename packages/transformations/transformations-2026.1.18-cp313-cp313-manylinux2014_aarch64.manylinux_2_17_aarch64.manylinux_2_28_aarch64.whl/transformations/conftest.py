b6d753cd7ba62c750c3a261725d0cc4cbced147a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:17:30 +0000

    Add package transformations/2026.1.18/transformations-2026.1.18-cp313-cp313-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/transformations/transformations-2026.1.18-cp313-cp313-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/transformations/conftest.py
@@ -0,0 +1,18 @@
+"""Pytest configuration."""
+
+import math
+import random
+
+import numpy
+import pytest
+
+numpy.set_printoptions(legacy='1.21')
+
+
+@pytest.fixture(autouse=True)
+def doctest_config(doctest_namespace) -> None:
+    """Add random and numpy to doctest namespace."""
+    numpy.set_printoptions(suppress=True, precision=5)
+    doctest_namespace['math'] = math
+    doctest_namespace['numpy'] = numpy
+    doctest_namespace['random'] = random
