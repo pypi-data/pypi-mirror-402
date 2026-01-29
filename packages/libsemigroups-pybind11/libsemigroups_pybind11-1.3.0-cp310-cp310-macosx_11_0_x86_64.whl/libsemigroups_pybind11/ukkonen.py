3a597e9e4ace2caf707868c0fd3041ccc7261569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:42 +0000

    Add package libsemigroups-pybind11/1.3.0/libsemigroups_pybind11-1.3.0-cp310-cp310-macosx_11_0_x86_64.whl

---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.3.0-cp310-cp310-macosx_11_0_x86_64.whl/libsemigroups_pybind11/ukkonen.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2022-2024 J. D. Mitchell
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+"""This page contains the documentation for the ``ukkonen`` subpackage, that
+contains helper functions for the :any:`Ukkonen` class.
+"""
+
+from _libsemigroups_pybind11 import (  # pylint: disable=unused-import
+    ukkonen_add_word as add_word,
+    ukkonen_add_words as add_words,
+    ukkonen_dot as dot,
+    ukkonen_is_piece as is_piece,
+    ukkonen_is_subword as is_subword,
+    ukkonen_is_suffix as is_suffix,
+    ukkonen_length_maximal_piece_prefix as length_maximal_piece_prefix,
+    ukkonen_length_maximal_piece_suffix as length_maximal_piece_suffix,
+    ukkonen_maximal_piece_prefix as maximal_piece_prefix,
+    ukkonen_maximal_piece_suffix as maximal_piece_suffix,
+    ukkonen_number_of_distinct_subwords as number_of_distinct_subwords,
+    ukkonen_number_of_pieces as number_of_pieces,
+    ukkonen_pieces as pieces,
+    ukkonen_traverse as traverse,
+)
