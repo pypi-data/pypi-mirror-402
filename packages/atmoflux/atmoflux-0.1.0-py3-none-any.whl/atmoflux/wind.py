b60122af3716fabe5fa6ed2d26e84c744676eaba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:11:50 +0000

    Add package atmoflux/0.1.0/atmoflux-0.1.0-py3-none-any.whl

---
+++ packages/atmoflux/atmoflux-0.1.0-py3-none-any.whl/atmoflux/wind.py
@@ -0,0 +1,7 @@
+"""
+atmoflux.wind
+=================
+Contains functions and profiles related to atmospheric wind. 
+Includes wind speed and direction, height adjustments, shear, and roughness calculations.
+
+"""
