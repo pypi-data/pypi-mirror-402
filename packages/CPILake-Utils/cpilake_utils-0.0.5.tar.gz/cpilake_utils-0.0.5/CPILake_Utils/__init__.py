20056544e55c49ac9ea49e39ec0d7cd608167f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:13:08 +0000

    Add package CPILake-Utils/0.0.5/cpilake_utils-0.0.5.tar.gz

---
+++ packages/CPILake-Utils/cpilake_utils-0.0.5.tar.gz/cpilake_utils-0.0.5/CPILake_Utils/__init__.py
@@ -0,0 +1,15 @@
+from .CPILake_Utils import  hash_function, \
+                            send_email_via_http, \
+                            _df_to_html_table, \
+                            send_email_no_attachment, \
+                            QA_CheckUtil, \
+                            create_lakehouse_shortcuts, \
+                            create_adls_shortcuts, \
+                            lakehouse_metadata_sync, \
+                            create_adls_shortcuts_spn
+
+__all__ = [ "hash_function", "send_email_via_http", "_df_to_html_table", "send_email_no_attachment", 
+             "QA_CheckUtil", "create_lakehouse_shortcuts", "create_adls_shortcuts", "lakehouse_metadata_sync",
+             "create_adls_shortcuts_spn"
+            ]
+
