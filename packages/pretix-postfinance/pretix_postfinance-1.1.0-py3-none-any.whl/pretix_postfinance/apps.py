d1f2ac07e7feed2305c596b06071336632fb0d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:12:09 +0000

    Add package pretix-postfinance/1.1.0/pretix_postfinance-1.1.0-py3-none-any.whl

---
+++ packages/pretix-postfinance/pretix_postfinance-1.1.0-py3-none-any.whl/pretix_postfinance/apps.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from django.utils.translation import gettext_lazy as _
+
+from . import __version__
+
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!") from None
+
+
+class PluginApp(PluginConfig):
+    default = True
+    name = "pretix_postfinance"
+    verbose_name = "PostFinance"
+
+    class PretixPluginMeta:
+        name = _("PostFinance")
+        author = "Sweenu"
+        description = _("PostFinance Checkout payment plugin for pretix")
+        visible = True
+        picture = "pretix_postfinance/pf_logo.svg"
+        version = __version__
+        category = "PAYMENT"
+        compatibility = "pretix>=2.7.0"
+        settings_links = (
+            (
+                (_("Payment"), _("PostFinance")),
+                "control:event.settings.payment.provider",
+                {"provider": "postfinance"},
+            ),
+        )
+
+    def ready(self) -> None:
+        from . import signals  # noqa: F401
