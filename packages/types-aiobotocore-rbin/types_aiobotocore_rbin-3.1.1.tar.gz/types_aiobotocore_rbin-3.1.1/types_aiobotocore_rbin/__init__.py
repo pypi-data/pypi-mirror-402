5029aa90a04b62c3c4262a1dc91984f3bf6423ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:42 +0000

    Add package types-aiobotocore-rbin/3.1.1/types_aiobotocore_rbin-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-rbin/types_aiobotocore_rbin-3.1.1.tar.gz/types_aiobotocore_rbin-3.1.1/types_aiobotocore_rbin/__init__.py
@@ -0,0 +1,34 @@
+"""
+Main interface for rbin service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_rbin/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_rbin import (
+        Client,
+        ListRulesPaginator,
+        RecycleBinClient,
+    )
+
+    session = get_session()
+    async with session.create_client("rbin") as client:
+        client: RecycleBinClient
+        ...
+
+
+    list_rules_paginator: ListRulesPaginator = client.get_paginator("list_rules")
+    ```
+"""
+
+from .client import RecycleBinClient
+from .paginator import ListRulesPaginator
+
+Client = RecycleBinClient
+
+
+__all__ = ("Client", "ListRulesPaginator", "RecycleBinClient")
