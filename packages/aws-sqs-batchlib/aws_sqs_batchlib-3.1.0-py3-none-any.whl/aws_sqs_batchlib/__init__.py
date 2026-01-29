2a190aa2fdc796b200c1025859e94981afbc0572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:53 +0000

    Add package aws-sqs-batchlib/3.1.0/aws_sqs_batchlib-3.1.0-py3-none-any.whl

---
+++ packages/aws-sqs-batchlib/aws_sqs_batchlib-3.1.0-py3-none-any.whl/aws_sqs_batchlib/__init__.py
@@ -0,0 +1,17 @@
+"""Amazon SQS Batchlib"""
+
+__version__ = "3.1.0"
+
+from .aws_sqs_batchlib import (
+    create_sqs_client,
+    delete_message_batch,
+    receive_message,
+    send_message_batch,
+)
+
+__all__ = [
+    "create_sqs_client",
+    "delete_message_batch",
+    "receive_message",
+    "send_message_batch",
+]
