d51e28d325e4b0de50587dcd524c056e62ce24e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:17:17 +0000

    Add package adamixture/1.0.0/adamixture-1.0.0-cp311-cp311-macosx_14_0_arm64.whl

---
+++ packages/adamixture/adamixture-1.0.0-cp311-cp311-macosx_14_0_arm64.whl/adamixture/src/snp_reader.py
@@ -0,0 +1,81 @@
+import logging
+import numpy as np
+import sys
+
+from .utils_c import tools
+from math import ceil
+from pathlib import Path
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
+log = logging.getLogger(__name__)
+
+class SNPReader:
+    """Wrapper to read genotype data from several formats
+    """
+
+    def _read_bed(self, file: str) -> np.ndarray:
+        """Reader wrapper for BED files
+
+        Args:
+            file (str): path to file.
+            master (bool): Wheter or not this process is the master for printing the output.
+
+        Returns:
+            da.core.Array: averaged genotype Dask array of shape (n_samples, n_snps)
+        """
+        log.info("    Input format is BED.")
+
+        file_path = Path(file)
+        fam_file = file_path.with_suffix(".fam")
+        bed_file = file_path.with_suffix(".bed")
+        
+        with open(fam_file, "r") as fam:
+            N = sum(1 for _ in fam)
+        N_bytes = ceil(N / 4)
+        
+        with open(bed_file, "rb") as bed:
+            B = np.fromfile(bed, dtype=np.uint8, offset=3)
+        
+        assert (B.shape[0] % N_bytes) == 0, "bim file doesn't match!"
+        M = B.shape[0] // N_bytes
+        B.shape = (M, N_bytes)
+        
+        G = np.zeros((M, N), dtype=np.uint8)
+        tools.read_bed(B, G)
+        del B
+        return G
+    
+    def _read_vcf(self, file: str) -> np.ndarray:
+        """Reader wrapper for VCF files
+
+        Args:
+            file (str): path to file.
+            master (bool): Wheter or not this process is the master for printing the output.
+
+        Returns:
+            np.ndarray: averaged genotype array of shape (n_samples, n_snps)
+        """
+        log.info("    Input format is VCF.")
+        import allel
+        f_tr = allel.read_vcf(file)
+        calldata = f_tr["calldata/GT"].astype(np.uint8)
+        return np.ascontiguousarray(np.sum(calldata, axis=2, dtype=np.uint8))
+        
+    def read_data(self, file: str) -> np.ndarray:
+        """Wrapper of readers
+
+        Args:
+            file (str): path to file
+        Returns:
+            np.ndarray: averaged genotype numpy array of shape (n_samples, n_snps)
+        """
+        file_extensions = Path(file).suffixes
+    
+        if '.bed' in file_extensions:
+            G = self._read_bed(file)
+        elif '.vcf' in file_extensions:
+            G = self._read_vcf(file)
+        else:
+            log.error("    Invalid format. Unrecognized file format. Make sure file ends with .bed, .pgen or .vcf .")
+            sys.exit(1)
+        return G if G.mean() < 1 else 2 - G
