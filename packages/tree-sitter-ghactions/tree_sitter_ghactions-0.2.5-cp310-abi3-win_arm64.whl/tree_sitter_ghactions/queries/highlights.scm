b4e165ae1037939101631ab9d1c2a6c6889ed747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:47:07 +0000

    Add package tree-sitter-ghactions/0.2.5/tree_sitter_ghactions-0.2.5-cp310-abi3-win_arm64.whl

---
+++ packages/tree-sitter-ghactions/tree_sitter_ghactions-0.2.5-cp310-abi3-win_arm64.whl/tree_sitter_ghactions/queries/highlights.scm
@@ -0,0 +1,82 @@
+; note: all highlights (except @string) are set to priority 101, for compatibility with bash injections in yaml
+([
+  "${{"
+  "}}"
+] @keyword
+  (#set! priority 101))
+
+([
+  "&&"
+  "||"
+  "=="
+  "!="
+  "<"
+  "<="
+  ">"
+  ">="
+  "!"
+] @operator
+  (#set! priority 101))
+
+([
+  "("
+  ")"
+  "["
+  "]"
+] @punctuation.bracket
+  (#set! priority 101))
+
+([
+  ","
+  "."
+] @punctuation.delimiter
+  (#set! priority 101))
+
+("*" @punctuation.special
+  (#set! priority 101))
+
+((number) @number
+  (#set! priority 101))
+
+((identifier) @variable
+  (#set! priority 101))
+
+; https://docs.github.com/en/actions/reference/workflows-and-actions/contexts
+(dereference_expression
+  object: (identifier) @module.builtin
+  ; TODO: technically case-insensitive
+  (#any-of? @module.builtin
+    "github" "env" "vars" "job" "jobs" "steps" "runner" "secrets" "strategy" "matrix" "needs"
+    "inputs")
+  (#set! priority 101))
+
+((dereference_expression
+  property: (identifier) @property)
+  (#set! priority 101))
+
+((function_call
+  name: (identifier) @function.call)
+  (#set! priority 101))
+
+; https://docs.github.com/en/actions/reference/workflows-and-actions/expressions#functions
+((function_call
+  name: (identifier) @function.builtin
+  ; TODO: technically case-insensitive
+  (#any-of? @function.builtin
+    "contains" "startsWith" "endsWith" "format" "join" "toJSON" "toJson" "fromJSON" "fromJson"
+    "hashFiles" "success" "always" "cancelled" "failure" "case"))
+  (#set! priority 101))
+
+([
+  (true)
+  (false)
+] @boolean
+  (#set! priority 101))
+
+((null) @constant.builtin
+  (#set! priority 101))
+
+(string) @string
+
+((escape) @string.escape
+  (#set! priority 101))
