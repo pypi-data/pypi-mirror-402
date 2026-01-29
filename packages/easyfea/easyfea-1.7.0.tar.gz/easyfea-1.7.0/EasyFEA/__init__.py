367cc1d5a49d94bec3eef74136b42dad73921f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:42 +0000

    Add package easyfea/1.7.0/easyfea-1.7.0.tar.gz

---
+++ packages/easyfea/easyfea-1.7.0.tar.gz/easyfea-1.7.0/EasyFEA/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2021-2025 Université Gustave Eiffel.
+# This file is part of the EasyFEA project.
+# EasyFEA is distributed under the terms of the GNU General Public License v3, see LICENSE.txt and CREDITS.md for more information.
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+BUILDING_GALLERY = False
+
+# utilities
+from .Utilities import Display, Folder, Paraview, PyVista, Vizir, MeshIO, Tic
+
+# fem
+from .FEM import Mesher, ElemType, Mesh, MatrixType
+import gmsh
+
+# simulations
+from .Simulations.Solvers import SolverType
+
+# version
+from .__about__ import __version__
