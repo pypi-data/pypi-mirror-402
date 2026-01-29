a81ff5e5d79cb6a4c35afa87d4faeb59d1eb11a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:11:19 +0000

    Add package langwatch/0.9.0/langwatch-0.9.0.tar.gz

---
+++ packages/langwatch/langwatch-0.9.0.tar.gz/langwatch-0.9.0/examples/guardrails.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import chainlit as cl
+from openai import OpenAI
+
+client = OpenAI()
+
+import langwatch
+import langwatch.guardrails
+
+
+@cl.on_message
+@langwatch.trace()
+async def main(message: cl.Message):
+    langwatch.get_current_trace().autotrack_openai_calls(client)
+    langwatch.get_current_trace().update(
+        metadata={"label": "guardrails"},
+    )
+
+    msg = cl.Message(
+        content="",
+    )
+
+    jailbreak_guardrail = langwatch.get_current_span().evaluate(
+        "jailbreak-detection",
+        name="Jailbreak Detection",
+        as_guardrail=True,
+        input=message.content,
+    )
+    if not jailbreak_guardrail.passed:
+        await msg.stream_token(f"I'm sorry, I can't help you with that.")
+        await msg.update()
+        return
+
+    completion = client.chat.completions.create(
+        model="gpt-5",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that only reply in short tweet-like responses, using lots of emojis.",
+            },
+            {"role": "user", "content": message.content},
+        ],
+        stream=True,
+    )
+
+    for part in completion:
+        if token := part.choices[0].delta.content or "":
+            await msg.stream_token(token)
+
+    await msg.update()
