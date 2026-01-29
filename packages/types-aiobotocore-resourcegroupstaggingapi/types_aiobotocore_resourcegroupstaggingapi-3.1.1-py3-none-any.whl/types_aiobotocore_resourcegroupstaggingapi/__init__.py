1cfd1d2e015d5cc24dc01242937a2ae05b1cf39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:43 +0000

    Add package types-aiobotocore-resourcegroupstaggingapi/3.1.1/types_aiobotocore_resourcegroupstaggingapi-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-resourcegroupstaggingapi/types_aiobotocore_resourcegroupstaggingapi-3.1.1-py3-none-any.whl/types_aiobotocore_resourcegroupstaggingapi/__init__.py
@@ -0,0 +1,56 @@
+"""
+Main interface for resourcegroupstaggingapi service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_resourcegroupstaggingapi/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_resourcegroupstaggingapi import (
+        Client,
+        GetComplianceSummaryPaginator,
+        GetResourcesPaginator,
+        GetTagKeysPaginator,
+        GetTagValuesPaginator,
+        ListRequiredTagsPaginator,
+        ResourceGroupsTaggingAPIClient,
+    )
+
+    session = get_session()
+    async with session.create_client("resourcegroupstaggingapi") as client:
+        client: ResourceGroupsTaggingAPIClient
+        ...
+
+
+    get_compliance_summary_paginator: GetComplianceSummaryPaginator = client.get_paginator("get_compliance_summary")
+    get_resources_paginator: GetResourcesPaginator = client.get_paginator("get_resources")
+    get_tag_keys_paginator: GetTagKeysPaginator = client.get_paginator("get_tag_keys")
+    get_tag_values_paginator: GetTagValuesPaginator = client.get_paginator("get_tag_values")
+    list_required_tags_paginator: ListRequiredTagsPaginator = client.get_paginator("list_required_tags")
+    ```
+"""
+
+from .client import ResourceGroupsTaggingAPIClient
+from .paginator import (
+    GetComplianceSummaryPaginator,
+    GetResourcesPaginator,
+    GetTagKeysPaginator,
+    GetTagValuesPaginator,
+    ListRequiredTagsPaginator,
+)
+
+Client = ResourceGroupsTaggingAPIClient
+
+
+__all__ = (
+    "Client",
+    "GetComplianceSummaryPaginator",
+    "GetResourcesPaginator",
+    "GetTagKeysPaginator",
+    "GetTagValuesPaginator",
+    "ListRequiredTagsPaginator",
+    "ResourceGroupsTaggingAPIClient",
+)
