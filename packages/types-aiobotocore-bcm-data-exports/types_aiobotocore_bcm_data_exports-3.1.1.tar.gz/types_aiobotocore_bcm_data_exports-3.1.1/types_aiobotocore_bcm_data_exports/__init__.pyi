1dc03d116027679c16e681c9ebe9d77892d08638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:52 +0000

    Add package types-aiobotocore-bcm-data-exports/3.1.1/types_aiobotocore_bcm_data_exports-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-bcm-data-exports/types_aiobotocore_bcm_data_exports-3.1.1.tar.gz/types_aiobotocore_bcm_data_exports-3.1.1/types_aiobotocore_bcm_data_exports/__init__.pyi
@@ -0,0 +1,43 @@
+"""
+Main interface for bcm-data-exports service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bcm_data_exports/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_bcm_data_exports import (
+        BillingandCostManagementDataExportsClient,
+        Client,
+        ListExecutionsPaginator,
+        ListExportsPaginator,
+        ListTablesPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("bcm-data-exports") as client:
+        client: BillingandCostManagementDataExportsClient
+        ...
+
+
+    list_executions_paginator: ListExecutionsPaginator = client.get_paginator("list_executions")
+    list_exports_paginator: ListExportsPaginator = client.get_paginator("list_exports")
+    list_tables_paginator: ListTablesPaginator = client.get_paginator("list_tables")
+    ```
+"""
+
+from .client import BillingandCostManagementDataExportsClient
+from .paginator import ListExecutionsPaginator, ListExportsPaginator, ListTablesPaginator
+
+Client = BillingandCostManagementDataExportsClient
+
+__all__ = (
+    "BillingandCostManagementDataExportsClient",
+    "Client",
+    "ListExecutionsPaginator",
+    "ListExportsPaginator",
+    "ListTablesPaginator",
+)
---
+++ packages/types-aiobotocore-bcm-data-exports/types_aiobotocore_bcm_data_exports-3.1.1.tar.gz/types_aiobotocore_bcm_data_exports-3.1.1/types_aiobotocore_bcm_data_exports/paginator.py
@@ -0,0 +1,115 @@
+"""
+Type annotations for bcm-data-exports service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bcm_data_exports/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_bcm_data_exports.client import BillingandCostManagementDataExportsClient
+    from types_aiobotocore_bcm_data_exports.paginator import (
+        ListExecutionsPaginator,
+        ListExportsPaginator,
+        ListTablesPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("bcm-data-exports") as client:
+        client: BillingandCostManagementDataExportsClient
+
+        list_executions_paginator: ListExecutionsPaginator = client.get_paginator("list_executions")
+        list_exports_paginator: ListExportsPaginator = client.get_paginator("list_exports")
+        list_tables_paginator: ListTablesPaginator = client.get_paginator("list_tables")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    ListExecutionsRequestPaginateTypeDef,
+    ListExecutionsResponseTypeDef,
+    ListExportsRequestPaginateTypeDef,
+    ListExportsResponseTypeDef,
+    ListTablesRequestPaginateTypeDef,
+    ListTablesResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ListExecutionsPaginator", "ListExportsPaginator", "ListTablesPaginator")
+
+
+if TYPE_CHECKING:
+    _ListExecutionsPaginatorBase = AioPaginator[ListExecutionsResponseTypeDef]
+else:
+    _ListExecutionsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListExecutionsPaginator(_ListExecutionsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bcm-data-exports/paginator/ListExecutions.html#BillingandCostManagementDataExports.Paginator.ListExecutions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bcm_data_exports/paginators/#listexecutionspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListExecutionsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListExecutionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bcm-data-exports/paginator/ListExecutions.html#BillingandCostManagementDataExports.Paginator.ListExecutions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bcm_data_exports/paginators/#listexecutionspaginator)
+        """
+
+
+if TYPE_CHECKING:
+    _ListExportsPaginatorBase = AioPaginator[ListExportsResponseTypeDef]
+else:
+    _ListExportsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListExportsPaginator(_ListExportsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bcm-data-exports/paginator/ListExports.html#BillingandCostManagementDataExports.Paginator.ListExports)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bcm_data_exports/paginators/#listexportspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListExportsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListExportsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bcm-data-exports/paginator/ListExports.html#BillingandCostManagementDataExports.Paginator.ListExports.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bcm_data_exports/paginators/#listexportspaginator)
+        """
+
+
+if TYPE_CHECKING:
+    _ListTablesPaginatorBase = AioPaginator[ListTablesResponseTypeDef]
+else:
+    _ListTablesPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListTablesPaginator(_ListTablesPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bcm-data-exports/paginator/ListTables.html#BillingandCostManagementDataExports.Paginator.ListTables)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bcm_data_exports/paginators/#listtablespaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListTablesRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListTablesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bcm-data-exports/paginator/ListTables.html#BillingandCostManagementDataExports.Paginator.ListTables.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_bcm_data_exports/paginators/#listtablespaginator)
+        """
