bd06bc7bfa7fd9f57c12119aff98c12fe941ff5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:15:56 +0000

    Add package topsis-Bhaumik-102303067/1.0.0/topsis_bhaumik_102303067-1.0.0.tar.gz

---
+++ packages/topsis-Bhaumik-102303067/topsis_bhaumik_102303067-1.0.0.tar.gz/topsis_bhaumik_102303067-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topsis-Bhaumik-102303067",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy"
+    ],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.__main__:main"
+        ]
+    },
+    author="Bhaumik Verma",
+    description="TOPSIS implementation for multi-criteria decision analysis",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bhaumik181/TOPSIS",
+    license="MIT"
+)
---
+++ packages/topsis-Bhaumik-102303067/topsis_bhaumik_102303067-1.0.0.tar.gz/topsis_bhaumik_102303067-1.0.0/topsis/__main__.py
@@ -0,0 +1,12 @@
+import sys
+from .topsis import topsis
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: topsis <InputFile> <Weights> <Impacts> <OutputFile>")
+        sys.exit(1)
+
+    topsis(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+if __name__ == "__main__":
+    main()
