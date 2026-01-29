759980a87eba5cce43271e99b7a6f9b8be35b23f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:33 +0000

    Add package types-aiobotocore-neptune-graph/3.1.1/types_aiobotocore_neptune_graph-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-neptune-graph/types_aiobotocore_neptune_graph-3.1.1.tar.gz/types_aiobotocore_neptune_graph-3.1.1/types_aiobotocore_neptune_graph/paginator.pyi
@@ -0,0 +1,155 @@
+"""
+Type annotations for neptune-graph service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_neptune_graph.client import NeptuneGraphClient
+    from types_aiobotocore_neptune_graph.paginator import (
+        ListExportTasksPaginator,
+        ListGraphSnapshotsPaginator,
+        ListGraphsPaginator,
+        ListImportTasksPaginator,
+        ListPrivateGraphEndpointsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("neptune-graph") as client:
+        client: NeptuneGraphClient
+
+        list_export_tasks_paginator: ListExportTasksPaginator = client.get_paginator("list_export_tasks")
+        list_graph_snapshots_paginator: ListGraphSnapshotsPaginator = client.get_paginator("list_graph_snapshots")
+        list_graphs_paginator: ListGraphsPaginator = client.get_paginator("list_graphs")
+        list_import_tasks_paginator: ListImportTasksPaginator = client.get_paginator("list_import_tasks")
+        list_private_graph_endpoints_paginator: ListPrivateGraphEndpointsPaginator = client.get_paginator("list_private_graph_endpoints")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    ListExportTasksInputPaginateTypeDef,
+    ListExportTasksOutputTypeDef,
+    ListGraphsInputPaginateTypeDef,
+    ListGraphSnapshotsInputPaginateTypeDef,
+    ListGraphSnapshotsOutputTypeDef,
+    ListGraphsOutputTypeDef,
+    ListImportTasksInputPaginateTypeDef,
+    ListImportTasksOutputTypeDef,
+    ListPrivateGraphEndpointsInputPaginateTypeDef,
+    ListPrivateGraphEndpointsOutputTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+__all__ = (
+    "ListExportTasksPaginator",
+    "ListGraphSnapshotsPaginator",
+    "ListGraphsPaginator",
+    "ListImportTasksPaginator",
+    "ListPrivateGraphEndpointsPaginator",
+)
+
+if TYPE_CHECKING:
+    _ListExportTasksPaginatorBase = AioPaginator[ListExportTasksOutputTypeDef]
+else:
+    _ListExportTasksPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListExportTasksPaginator(_ListExportTasksPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/paginator/ListExportTasks.html#NeptuneGraph.Paginator.ListExportTasks)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/#listexporttaskspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListExportTasksInputPaginateTypeDef]
+    ) -> AioPageIterator[ListExportTasksOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/paginator/ListExportTasks.html#NeptuneGraph.Paginator.ListExportTasks.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/#listexporttaskspaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListGraphSnapshotsPaginatorBase = AioPaginator[ListGraphSnapshotsOutputTypeDef]
+else:
+    _ListGraphSnapshotsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListGraphSnapshotsPaginator(_ListGraphSnapshotsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/paginator/ListGraphSnapshots.html#NeptuneGraph.Paginator.ListGraphSnapshots)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/#listgraphsnapshotspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListGraphSnapshotsInputPaginateTypeDef]
+    ) -> AioPageIterator[ListGraphSnapshotsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/paginator/ListGraphSnapshots.html#NeptuneGraph.Paginator.ListGraphSnapshots.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/#listgraphsnapshotspaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListGraphsPaginatorBase = AioPaginator[ListGraphsOutputTypeDef]
+else:
+    _ListGraphsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListGraphsPaginator(_ListGraphsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/paginator/ListGraphs.html#NeptuneGraph.Paginator.ListGraphs)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/#listgraphspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListGraphsInputPaginateTypeDef]
+    ) -> AioPageIterator[ListGraphsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/paginator/ListGraphs.html#NeptuneGraph.Paginator.ListGraphs.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/#listgraphspaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListImportTasksPaginatorBase = AioPaginator[ListImportTasksOutputTypeDef]
+else:
+    _ListImportTasksPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListImportTasksPaginator(_ListImportTasksPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/paginator/ListImportTasks.html#NeptuneGraph.Paginator.ListImportTasks)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/#listimporttaskspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListImportTasksInputPaginateTypeDef]
+    ) -> AioPageIterator[ListImportTasksOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/paginator/ListImportTasks.html#NeptuneGraph.Paginator.ListImportTasks.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/#listimporttaskspaginator)
+        """
+
+if TYPE_CHECKING:
+    _ListPrivateGraphEndpointsPaginatorBase = AioPaginator[ListPrivateGraphEndpointsOutputTypeDef]
+else:
+    _ListPrivateGraphEndpointsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+class ListPrivateGraphEndpointsPaginator(_ListPrivateGraphEndpointsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/paginator/ListPrivateGraphEndpoints.html#NeptuneGraph.Paginator.ListPrivateGraphEndpoints)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/#listprivategraphendpointspaginator)
+    """
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListPrivateGraphEndpointsInputPaginateTypeDef]
+    ) -> AioPageIterator[ListPrivateGraphEndpointsOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/paginator/ListPrivateGraphEndpoints.html#NeptuneGraph.Paginator.ListPrivateGraphEndpoints.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/paginators/#listprivategraphendpointspaginator)
+        """
---
+++ packages/types-aiobotocore-neptune-graph/types_aiobotocore_neptune_graph-3.1.1.tar.gz/types_aiobotocore_neptune_graph-3.1.1/types_aiobotocore_neptune_graph/waiter.py
@@ -0,0 +1,249 @@
+"""
+Type annotations for neptune-graph service client waiters.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_neptune_graph.client import NeptuneGraphClient
+    from types_aiobotocore_neptune_graph.waiter import (
+        ExportTaskCancelledWaiter,
+        ExportTaskSuccessfulWaiter,
+        GraphAvailableWaiter,
+        GraphDeletedWaiter,
+        GraphSnapshotAvailableWaiter,
+        GraphSnapshotDeletedWaiter,
+        GraphStoppedWaiter,
+        ImportTaskCancelledWaiter,
+        ImportTaskSuccessfulWaiter,
+        PrivateGraphEndpointAvailableWaiter,
+        PrivateGraphEndpointDeletedWaiter,
+    )
+
+    session = get_session()
+    async with session.create_client("neptune-graph") as client:
+        client: NeptuneGraphClient
+
+        export_task_cancelled_waiter: ExportTaskCancelledWaiter = client.get_waiter("export_task_cancelled")
+        export_task_successful_waiter: ExportTaskSuccessfulWaiter = client.get_waiter("export_task_successful")
+        graph_available_waiter: GraphAvailableWaiter = client.get_waiter("graph_available")
+        graph_deleted_waiter: GraphDeletedWaiter = client.get_waiter("graph_deleted")
+        graph_snapshot_available_waiter: GraphSnapshotAvailableWaiter = client.get_waiter("graph_snapshot_available")
+        graph_snapshot_deleted_waiter: GraphSnapshotDeletedWaiter = client.get_waiter("graph_snapshot_deleted")
+        graph_stopped_waiter: GraphStoppedWaiter = client.get_waiter("graph_stopped")
+        import_task_cancelled_waiter: ImportTaskCancelledWaiter = client.get_waiter("import_task_cancelled")
+        import_task_successful_waiter: ImportTaskSuccessfulWaiter = client.get_waiter("import_task_successful")
+        private_graph_endpoint_available_waiter: PrivateGraphEndpointAvailableWaiter = client.get_waiter("private_graph_endpoint_available")
+        private_graph_endpoint_deleted_waiter: PrivateGraphEndpointDeletedWaiter = client.get_waiter("private_graph_endpoint_deleted")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+
+from aiobotocore.waiter import AIOWaiter
+
+from .type_defs import (
+    GetExportTaskInputWaitExtraTypeDef,
+    GetExportTaskInputWaitTypeDef,
+    GetGraphInputWaitExtraExtraTypeDef,
+    GetGraphInputWaitExtraTypeDef,
+    GetGraphInputWaitTypeDef,
+    GetGraphSnapshotInputWaitExtraTypeDef,
+    GetGraphSnapshotInputWaitTypeDef,
+    GetImportTaskInputWaitExtraTypeDef,
+    GetImportTaskInputWaitTypeDef,
+    GetPrivateGraphEndpointInputWaitExtraTypeDef,
+    GetPrivateGraphEndpointInputWaitTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = (
+    "ExportTaskCancelledWaiter",
+    "ExportTaskSuccessfulWaiter",
+    "GraphAvailableWaiter",
+    "GraphDeletedWaiter",
+    "GraphSnapshotAvailableWaiter",
+    "GraphSnapshotDeletedWaiter",
+    "GraphStoppedWaiter",
+    "ImportTaskCancelledWaiter",
+    "ImportTaskSuccessfulWaiter",
+    "PrivateGraphEndpointAvailableWaiter",
+    "PrivateGraphEndpointDeletedWaiter",
+)
+
+
+class ExportTaskCancelledWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/ExportTaskCancelled.html#NeptuneGraph.Waiter.ExportTaskCancelled)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#exporttaskcancelledwaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetExportTaskInputWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/ExportTaskCancelled.html#NeptuneGraph.Waiter.ExportTaskCancelled.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#exporttaskcancelledwaiter)
+        """
+
+
+class ExportTaskSuccessfulWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/ExportTaskSuccessful.html#NeptuneGraph.Waiter.ExportTaskSuccessful)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#exporttasksuccessfulwaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetExportTaskInputWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/ExportTaskSuccessful.html#NeptuneGraph.Waiter.ExportTaskSuccessful.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#exporttasksuccessfulwaiter)
+        """
+
+
+class GraphAvailableWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/GraphAvailable.html#NeptuneGraph.Waiter.GraphAvailable)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#graphavailablewaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetGraphInputWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/GraphAvailable.html#NeptuneGraph.Waiter.GraphAvailable.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#graphavailablewaiter)
+        """
+
+
+class GraphDeletedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/GraphDeleted.html#NeptuneGraph.Waiter.GraphDeleted)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#graphdeletedwaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetGraphInputWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/GraphDeleted.html#NeptuneGraph.Waiter.GraphDeleted.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#graphdeletedwaiter)
+        """
+
+
+class GraphSnapshotAvailableWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/GraphSnapshotAvailable.html#NeptuneGraph.Waiter.GraphSnapshotAvailable)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#graphsnapshotavailablewaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetGraphSnapshotInputWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/GraphSnapshotAvailable.html#NeptuneGraph.Waiter.GraphSnapshotAvailable.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#graphsnapshotavailablewaiter)
+        """
+
+
+class GraphSnapshotDeletedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/GraphSnapshotDeleted.html#NeptuneGraph.Waiter.GraphSnapshotDeleted)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#graphsnapshotdeletedwaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetGraphSnapshotInputWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/GraphSnapshotDeleted.html#NeptuneGraph.Waiter.GraphSnapshotDeleted.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#graphsnapshotdeletedwaiter)
+        """
+
+
+class GraphStoppedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/GraphStopped.html#NeptuneGraph.Waiter.GraphStopped)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#graphstoppedwaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetGraphInputWaitExtraExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/GraphStopped.html#NeptuneGraph.Waiter.GraphStopped.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#graphstoppedwaiter)
+        """
+
+
+class ImportTaskCancelledWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/ImportTaskCancelled.html#NeptuneGraph.Waiter.ImportTaskCancelled)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#importtaskcancelledwaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetImportTaskInputWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/ImportTaskCancelled.html#NeptuneGraph.Waiter.ImportTaskCancelled.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#importtaskcancelledwaiter)
+        """
+
+
+class ImportTaskSuccessfulWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/ImportTaskSuccessful.html#NeptuneGraph.Waiter.ImportTaskSuccessful)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#importtasksuccessfulwaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetImportTaskInputWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/ImportTaskSuccessful.html#NeptuneGraph.Waiter.ImportTaskSuccessful.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#importtasksuccessfulwaiter)
+        """
+
+
+class PrivateGraphEndpointAvailableWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/PrivateGraphEndpointAvailable.html#NeptuneGraph.Waiter.PrivateGraphEndpointAvailable)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#privategraphendpointavailablewaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetPrivateGraphEndpointInputWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/PrivateGraphEndpointAvailable.html#NeptuneGraph.Waiter.PrivateGraphEndpointAvailable.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#privategraphendpointavailablewaiter)
+        """
+
+
+class PrivateGraphEndpointDeletedWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/PrivateGraphEndpointDeleted.html#NeptuneGraph.Waiter.PrivateGraphEndpointDeleted)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#privategraphendpointdeletedwaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[GetPrivateGraphEndpointInputWaitExtraTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/neptune-graph/waiter/PrivateGraphEndpointDeleted.html#NeptuneGraph.Waiter.PrivateGraphEndpointDeleted.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_neptune_graph/waiters/#privategraphendpointdeletedwaiter)
+        """
