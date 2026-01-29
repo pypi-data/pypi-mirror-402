30b392f7f19b5c21d4f3207ba9e8163434cd93b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:47:57 +0000

    Add package vivisect/1.3.0/vivisect-1.3.0-py3-none-any.whl

---
+++ packages/vivisect/vivisect-1.3.0-py3-none-any.whl/vivisect/tests/openbsd_amd64_ls_data.py
@@ -0,0 +1,23 @@
+ls_amd64_data = {
+    'imports': [
+    ],
+
+    'exports': [
+        (0x400230, 0, '__entry', 'ls'),
+    ],
+
+    'relocs': [
+    ],
+
+    'names': [
+        (0x400018, 'ls.ptr___entry_00400018'),
+        (0x400220, 'ls.init_function'),
+        (0x400230, 'ls.__entry'),
+        (0x42ec10, 'ls.fini_function'),
+        (0x739f60, 'ls.ptr_fini_function_00739f60'),
+    ],
+
+    'pltgot': [
+    ],
+
+}
