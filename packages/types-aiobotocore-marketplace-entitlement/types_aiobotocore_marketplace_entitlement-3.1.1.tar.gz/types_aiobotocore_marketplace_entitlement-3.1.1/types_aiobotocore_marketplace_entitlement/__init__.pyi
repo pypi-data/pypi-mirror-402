96f14f3aa3ca92495c13e73d5120be4b1fe54797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:30 +0000

    Add package types-aiobotocore-marketplace-entitlement/3.1.1/types_aiobotocore_marketplace_entitlement-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-marketplace-entitlement/types_aiobotocore_marketplace_entitlement-3.1.1.tar.gz/types_aiobotocore_marketplace_entitlement-3.1.1/types_aiobotocore_marketplace_entitlement/__init__.pyi
@@ -0,0 +1,33 @@
+"""
+Main interface for marketplace-entitlement service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_marketplace_entitlement/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_marketplace_entitlement import (
+        Client,
+        GetEntitlementsPaginator,
+        MarketplaceEntitlementServiceClient,
+    )
+
+    session = get_session()
+    async with session.create_client("marketplace-entitlement") as client:
+        client: MarketplaceEntitlementServiceClient
+        ...
+
+
+    get_entitlements_paginator: GetEntitlementsPaginator = client.get_paginator("get_entitlements")
+    ```
+"""
+
+from .client import MarketplaceEntitlementServiceClient
+from .paginator import GetEntitlementsPaginator
+
+Client = MarketplaceEntitlementServiceClient
+
+__all__ = ("Client", "GetEntitlementsPaginator", "MarketplaceEntitlementServiceClient")
