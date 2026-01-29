bde6d157a9b9eb3f9648ad54e9a7f56278b65141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:34 +0000

    Add package topsis-garima-102303542/0.0.1/topsis_garima_102303542-0.0.1.tar.gz

---
+++ packages/topsis-garima-102303542/topsis_garima_102303542-0.0.1.tar.gz/topsis_garima_102303542-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topsis_garima_102303542",
+    version="0.0.1",
+    author="Garima Singla",
+    author_email="garimasingla732@gmail.com",
+    description="TOPSIS implementation in Python (Command Line Tool)",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas"],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.topsis:main"
+        ]
+    },
+    python_requires=">=3.7",
+)
