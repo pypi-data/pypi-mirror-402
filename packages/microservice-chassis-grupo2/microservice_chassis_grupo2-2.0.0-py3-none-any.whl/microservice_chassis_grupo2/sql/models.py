d05d060131c4621d2822b67fc64d76d926d7fd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:11:17 +0000

    Add package microservice-chassis-grupo2/2.0.0/microservice_chassis_grupo2-2.0.0-py3-none-any.whl

---
+++ packages/microservice-chassis-grupo2/microservice_chassis_grupo2-2.0.0-py3-none-any.whl/microservice_chassis_grupo2/sql/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, DateTime
+from sqlalchemy.sql import func
+from microservice_chassis_grupo2.sql.database import Base
+
+class BaseModel(Base):
+    """Base database table representation to reuse."""
+    __abstract__ = True
+    creation_date = Column(DateTime(timezone=True), server_default=func.now())
+    update_date = Column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
+
+    def __repr__(self):
+        fields = ""
+        for column in self.__table__.columns:
+            if fields == "":
+                fields = f"{column.name}='{getattr(self, column.name)}'"
+                # fields = "{}='{}'".format(column.name, getattr(self, column.name))
+            else:
+                fields = f"{fields}, {column.name}='{getattr(self, column.name)}'"
+                # fields = "{}, {}='{}'".format(fields, column.name, getattr(self, column.name))
+        return f"<{self.__class__.__name__}({fields})>"
+        # return "<{}({})>".format(self.__class__.__name__, fields)
+
+    @staticmethod
+    def list_as_dict(items):
+        """Returns list of items as dict."""
+        return [i.as_dict() for i in items]
+
+    def as_dict(self):
+        """Return the item as dict."""
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
