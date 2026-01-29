7ebb763e8a89679a9c22579ee3ef230085508f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 12:07:47 +0000

    Add package topsis-yash-102303796/1.0.4/topsis_yash_102303796-1.0.4.tar.gz

---
+++ packages/topsis-yash-102303796/topsis_yash_102303796-1.0.4.tar.gz/topsis_yash_102303796-1.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="topsis_yash_102303796",
+    version="1.0.4",
+    author="Yash",
+    author_email="yash@example.com",
+    description="A Python package for TOPSIS (Technique for Order Preference by Similarity to Ideal Solution)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy"
+    ],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis_yash_102303796.topsis:main"
+        ]
+    },
+    python_requires=">=3.6",
+)
