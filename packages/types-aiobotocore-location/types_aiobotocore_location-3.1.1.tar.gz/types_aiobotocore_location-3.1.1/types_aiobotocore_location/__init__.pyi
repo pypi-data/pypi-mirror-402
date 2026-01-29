ff849135e97b9a35b18b21c8c2f18d02f7f84254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:28 +0000

    Add package types-aiobotocore-location/3.1.1/types_aiobotocore_location-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-location/types_aiobotocore_location-3.1.1.tar.gz/types_aiobotocore_location-3.1.1/types_aiobotocore_location/__init__.pyi
@@ -0,0 +1,79 @@
+"""
+Main interface for location service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_location/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_location import (
+        Client,
+        ForecastGeofenceEventsPaginator,
+        GetDevicePositionHistoryPaginator,
+        ListDevicePositionsPaginator,
+        ListGeofenceCollectionsPaginator,
+        ListGeofencesPaginator,
+        ListKeysPaginator,
+        ListMapsPaginator,
+        ListPlaceIndexesPaginator,
+        ListRouteCalculatorsPaginator,
+        ListTrackerConsumersPaginator,
+        ListTrackersPaginator,
+        LocationServiceClient,
+    )
+
+    session = get_session()
+    async with session.create_client("location") as client:
+        client: LocationServiceClient
+        ...
+
+
+    forecast_geofence_events_paginator: ForecastGeofenceEventsPaginator = client.get_paginator("forecast_geofence_events")
+    get_device_position_history_paginator: GetDevicePositionHistoryPaginator = client.get_paginator("get_device_position_history")
+    list_device_positions_paginator: ListDevicePositionsPaginator = client.get_paginator("list_device_positions")
+    list_geofence_collections_paginator: ListGeofenceCollectionsPaginator = client.get_paginator("list_geofence_collections")
+    list_geofences_paginator: ListGeofencesPaginator = client.get_paginator("list_geofences")
+    list_keys_paginator: ListKeysPaginator = client.get_paginator("list_keys")
+    list_maps_paginator: ListMapsPaginator = client.get_paginator("list_maps")
+    list_place_indexes_paginator: ListPlaceIndexesPaginator = client.get_paginator("list_place_indexes")
+    list_route_calculators_paginator: ListRouteCalculatorsPaginator = client.get_paginator("list_route_calculators")
+    list_tracker_consumers_paginator: ListTrackerConsumersPaginator = client.get_paginator("list_tracker_consumers")
+    list_trackers_paginator: ListTrackersPaginator = client.get_paginator("list_trackers")
+    ```
+"""
+
+from .client import LocationServiceClient
+from .paginator import (
+    ForecastGeofenceEventsPaginator,
+    GetDevicePositionHistoryPaginator,
+    ListDevicePositionsPaginator,
+    ListGeofenceCollectionsPaginator,
+    ListGeofencesPaginator,
+    ListKeysPaginator,
+    ListMapsPaginator,
+    ListPlaceIndexesPaginator,
+    ListRouteCalculatorsPaginator,
+    ListTrackerConsumersPaginator,
+    ListTrackersPaginator,
+)
+
+Client = LocationServiceClient
+
+__all__ = (
+    "Client",
+    "ForecastGeofenceEventsPaginator",
+    "GetDevicePositionHistoryPaginator",
+    "ListDevicePositionsPaginator",
+    "ListGeofenceCollectionsPaginator",
+    "ListGeofencesPaginator",
+    "ListKeysPaginator",
+    "ListMapsPaginator",
+    "ListPlaceIndexesPaginator",
+    "ListRouteCalculatorsPaginator",
+    "ListTrackerConsumersPaginator",
+    "ListTrackersPaginator",
+    "LocationServiceClient",
+)
