9a19ee9869fca577d014e26cf2f55f5d794bcc4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:09:42 +0000

    Add package lib50/3.2.1/lib50-3.2.1.tar.gz

---
+++ packages/lib50/lib50-3.2.1.tar.gz/lib50-3.2.1/setup.py
@@ -0,0 +1,32 @@
+if __import__("os").name == "nt":
+    raise RuntimeError("lib50 does not support Windows directly. Instead, you should install the Windows Subsystem for Linux (https://docs.microsoft.com/en-us/windows/wsl/install-win10) and then install lib50 within that.")
+
+from setuptools import setup
+
+setup(
+    author="CS50",
+    author_email="sysadmins@cs50.harvard.edu",
+    classifiers=[
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Topic :: Education",
+        "Topic :: Utilities"
+    ],
+    message_extractors = {
+        'lib50': [('**.py', 'python', None),],
+    },
+    license="GPLv3",
+    description="This is lib50, CS50's own internal library used in many of its tools.",
+    long_description="This is lib50, CS50's own internal library used in many of its tools.",
+    install_requires=["packaging", "pexpect>=4.6,<5", "pyyaml<7", "requests>=2.13,<3", "setuptools", "termcolor>=1.1,<2", "jellyfish>=1,<2", "cryptography>=2.7"],
+    extras_require = {
+        "develop": ["sphinx", "sphinx-autobuild", "sphinx_rtd_theme"]
+    },
+    keywords=["lib50"],
+    name="lib50",
+    python_requires=">= 3.10",
+    packages=["lib50"],
+    url="https://github.com/cs50/lib50",
+    version="3.2.1",
+    include_package_data=True
+)
