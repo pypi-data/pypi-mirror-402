e0c6940af64d4335e0d1228e64f53e714aa8883b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:08:03 +0000

    Add package types-aiobotocore-comprehend/3.1.1/types_aiobotocore_comprehend-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-comprehend/types_aiobotocore_comprehend-3.1.1.tar.gz/types_aiobotocore_comprehend-3.1.1/types_aiobotocore_comprehend/__init__.pyi
@@ -0,0 +1,75 @@
+"""
+Main interface for comprehend service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_comprehend/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_comprehend import (
+        Client,
+        ComprehendClient,
+        ListDocumentClassificationJobsPaginator,
+        ListDocumentClassifiersPaginator,
+        ListDominantLanguageDetectionJobsPaginator,
+        ListEndpointsPaginator,
+        ListEntitiesDetectionJobsPaginator,
+        ListEntityRecognizersPaginator,
+        ListKeyPhrasesDetectionJobsPaginator,
+        ListPiiEntitiesDetectionJobsPaginator,
+        ListSentimentDetectionJobsPaginator,
+        ListTopicsDetectionJobsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("comprehend") as client:
+        client: ComprehendClient
+        ...
+
+
+    list_document_classification_jobs_paginator: ListDocumentClassificationJobsPaginator = client.get_paginator("list_document_classification_jobs")
+    list_document_classifiers_paginator: ListDocumentClassifiersPaginator = client.get_paginator("list_document_classifiers")
+    list_dominant_language_detection_jobs_paginator: ListDominantLanguageDetectionJobsPaginator = client.get_paginator("list_dominant_language_detection_jobs")
+    list_endpoints_paginator: ListEndpointsPaginator = client.get_paginator("list_endpoints")
+    list_entities_detection_jobs_paginator: ListEntitiesDetectionJobsPaginator = client.get_paginator("list_entities_detection_jobs")
+    list_entity_recognizers_paginator: ListEntityRecognizersPaginator = client.get_paginator("list_entity_recognizers")
+    list_key_phrases_detection_jobs_paginator: ListKeyPhrasesDetectionJobsPaginator = client.get_paginator("list_key_phrases_detection_jobs")
+    list_pii_entities_detection_jobs_paginator: ListPiiEntitiesDetectionJobsPaginator = client.get_paginator("list_pii_entities_detection_jobs")
+    list_sentiment_detection_jobs_paginator: ListSentimentDetectionJobsPaginator = client.get_paginator("list_sentiment_detection_jobs")
+    list_topics_detection_jobs_paginator: ListTopicsDetectionJobsPaginator = client.get_paginator("list_topics_detection_jobs")
+    ```
+"""
+
+from .client import ComprehendClient
+from .paginator import (
+    ListDocumentClassificationJobsPaginator,
+    ListDocumentClassifiersPaginator,
+    ListDominantLanguageDetectionJobsPaginator,
+    ListEndpointsPaginator,
+    ListEntitiesDetectionJobsPaginator,
+    ListEntityRecognizersPaginator,
+    ListKeyPhrasesDetectionJobsPaginator,
+    ListPiiEntitiesDetectionJobsPaginator,
+    ListSentimentDetectionJobsPaginator,
+    ListTopicsDetectionJobsPaginator,
+)
+
+Client = ComprehendClient
+
+__all__ = (
+    "Client",
+    "ComprehendClient",
+    "ListDocumentClassificationJobsPaginator",
+    "ListDocumentClassifiersPaginator",
+    "ListDominantLanguageDetectionJobsPaginator",
+    "ListEndpointsPaginator",
+    "ListEntitiesDetectionJobsPaginator",
+    "ListEntityRecognizersPaginator",
+    "ListKeyPhrasesDetectionJobsPaginator",
+    "ListPiiEntitiesDetectionJobsPaginator",
+    "ListSentimentDetectionJobsPaginator",
+    "ListTopicsDetectionJobsPaginator",
+)
