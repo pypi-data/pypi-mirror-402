a4ded7a980b041f1bf3bc35781ddaef69a9d5ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 13:26:23 +0000

    Add package pyxcp/0.25.8/pyxcp-0.25.8-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/pyxcp/pyxcp-0.25.8-cp312-cp312-macosx_11_0_arm64.whl/pyxcp/master/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""Lowlevel API reflecting available XCP services
+
+.. note:: For technical reasons the API is split into two parts;
+          common methods and a Python version specific part.
+
+.. [1] XCP Specification, Part 2 - Protocol Layer Specification
+"""
+
+from .master import Master  # noqa: F401
