f93df1f03f9eaea4062d873fcca147f93f4a3d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:12:43 +0000

    Add package ipl3checksum/1.3.1/ipl3checksum-1.3.1-cp314-cp314t-win_amd64.whl

---
+++ packages/ipl3checksum/ipl3checksum-1.3.1-cp314-cp314t-win_amd64.whl/ipl3checksum/detect.pyi
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2023-2024 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from .cickinds import CICKind
+
+def detectCICRaw(rawBytes: bytes) -> CICKind | None:
+    """Tries to detect an IPL3 binary.
+
+    The argument to this function must be exactly the IPL3 binary.
+
+    Args:
+        rawBytes (bytes): IPL3 binary in big endian format.
+
+    Returns:
+        CICKind|None: The detected CIC kind, or `None` if was not able to detect the CIC kind.
+    """
+
+def detectCIC(romBytes: bytes) -> CICKind | None:
+    """Tries to detect an IPL3 in a ROM.
+
+    The argument to this function must be a ROM in big endian format.
+
+    Args:
+        romBytes (bytes): ROMbinary in big endian format.
+
+    Returns:
+        CICKind|None: The detected CIC kind, or `None` if was not able to detect the CIC kind.
+    """
