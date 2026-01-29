51cfa74fe654cc1609838bd0cba44bec83729b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 12:07:44 +0000

    Add package py-local/0.5.3/py_local-0.5.3.tar.gz

---
+++ packages/py-local/py_local-0.5.3.tar.gz/py_local-0.5.3/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+
+path = os.path.dirname(__file__)
+with open(path + '/README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path + '/py_local/__init__.py', 'r') as metadata:
+    content = metadata.read()
+result = content.split()
+__version__ = result[-1][1:-1]
+
+setup(
+    name='py-local',
+    version=__version__,
+    description='Local Network Module for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Lao',
+    url='https://github.com/codelao/py-local',
+    license='MIT',
+    install_requires=[
+        'fake-useragent>=1.3.0', 'colorama>=0.4.6'
+    ],
+    packages=[
+        'py_local'
+    ]
+)
