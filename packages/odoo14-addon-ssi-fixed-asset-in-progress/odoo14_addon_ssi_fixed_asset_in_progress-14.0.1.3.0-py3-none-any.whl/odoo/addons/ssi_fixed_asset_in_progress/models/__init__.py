7e30ab95dbaacc5dfcf45ea487d697cf234314ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:10 +0000

    Add package odoo14-addon-ssi-fixed-asset-in-progress/14.0.1.3.0/odoo14_addon_ssi_fixed_asset_in_progress-14.0.1.3.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-fixed-asset-in-progress/odoo14_addon_ssi_fixed_asset_in_progress-14.0.1.3.0-py3-none-any.whl/odoo/addons/ssi_fixed_asset_in_progress/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (  # noqa: F401
+    fixed_asset_in_progress,
+    account_move_line,
+    fixed_asset_asset,
+    fixed_asset_category,
+)
