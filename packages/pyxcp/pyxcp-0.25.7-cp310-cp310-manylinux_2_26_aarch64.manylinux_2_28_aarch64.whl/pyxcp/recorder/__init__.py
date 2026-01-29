5ff18e67721f2673e6dcac3b799dd34028a2b406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:34:03 +0000

    Add package pyxcp/0.25.7/pyxcp-0.25.7-cp310-cp310-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/pyxcp/pyxcp-0.25.7-cp310-cp310-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/pyxcp/recorder/__init__.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+"""XCP Frame Recording Facility."""
+
+from dataclasses import dataclass
+from typing import Union
+
+from pyxcp.transport.base import FrameCategory
+
+
+try:
+    import pandas as pd
+except ImportError:
+    HAS_PANDAS = False
+else:
+    HAS_PANDAS = True
+
+from pyxcp.recorder.rekorder import DaqOnlinePolicy  # noqa: F401
+from pyxcp.recorder.rekorder import DaqRecorderPolicy  # noqa: F401
+from pyxcp.recorder.rekorder import Deserializer  # noqa: F401
+from pyxcp.recorder.rekorder import MeasurementParameters  # noqa: F401
+from pyxcp.recorder.rekorder import _PyXcpLogFileReader, _PyXcpLogFileWriter, data_types
+
+
+DATA_TYPES = data_types()
+
+
+@dataclass
+class XcpLogFileHeader:
+    """ """
+
+    version: int
+    options: int
+    num_containers: int
+    record_count: int
+    size_uncompressed: int
+    size_compressed: int
+    compression_ratio: float
+
+
+COUNTER_MAX = 0xFFFF
+
+
+class XcpLogFileReader:
+    """ """
+
+    def __init__(self, file_name):
+        self._reader = _PyXcpLogFileReader(file_name)
+
+    @property
+    def header(self):
+        return self._reader.get_header()
+
+    def get_header(self):
+        return XcpLogFileHeader(*self._reader.get_header_as_tuple())
+
+    def get_metadata(self):
+        return self._reader.get_metadata()
+
+    def __iter__(self):
+        while True:
+            frames = self._reader.next_block()
+            if frames is None:
+                break
+            for category, counter, timestamp, _, payload in frames:
+                yield (FrameCategory(category), counter, timestamp, payload)
+
+    def reset_iter(self):
+        self._reader.reset()
+
+    def as_dataframe(self):
+        if HAS_PANDAS:
+            df = pd.DataFrame((f for f in self), columns=["category", "counter", "timestamp", "payload"])
+            df = df.set_index("timestamp")
+            df.counter = df.counter.astype("uint16")
+            df.category = df.category.map({v: k for k, v in FrameCategory.__members__.items()}).astype("category")
+            return df
+        else:
+            raise NotImplementedError("method as_dataframe() requires 'pandas' package")
+
+
+class XcpLogFileWriter:
+    """ """
+
+    def __init__(self, file_name: str, prealloc=500, chunk_size=1):
+        self._writer = _PyXcpLogFileWriter(file_name, prealloc, chunk_size)
+        self._finalized = False
+
+    def __del__(self):
+        if not self._finalized:
+            self.finalize()
+
+    def add_frame(self, category: FrameCategory, counter: int, timestamp: float, payload: Union[bytes, bytearray]):
+        self._writer.add_frame(category, counter % (COUNTER_MAX + 1), timestamp, len(payload), payload)
+
+    def finalize(self):
+        self._writer.finalize()
