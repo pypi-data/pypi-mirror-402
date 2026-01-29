ed55e36bc5bb2e2a7eed6ac83aa7083be19a627c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:09:58 +0000

    Add package feagi-bv-macos/2.0.3/feagi_bv_macos-2.0.3-py3-none-any.whl

---
+++ packages/feagi-bv-macos/feagi_bv_macos-2.0.3-py3-none-any.whl/feagi_bv_macos/__init__.py
@@ -0,0 +1,8 @@
+"""
+FEAGI Brain Visualizer binaries for macOS.
+
+This package contains macOS universal (arm64+x86_64) binaries for Brain Visualizer.
+Install via: pip install feagi-bv (auto-installs this package on macOS)
+"""
+
+__version__ = "2.0.0"
