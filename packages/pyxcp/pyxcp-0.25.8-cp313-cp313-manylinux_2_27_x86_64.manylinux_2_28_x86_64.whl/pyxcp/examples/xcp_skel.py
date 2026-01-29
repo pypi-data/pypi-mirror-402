caf749b7d7a4a67f2eb9a7a652f860c9502ee7c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 13:26:25 +0000

    Add package pyxcp/0.25.8/pyxcp-0.25.8-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pyxcp/pyxcp-0.25.8-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/pyxcp/examples/xcp_skel.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""Use this as a copy-and-paste template for your own scripts."""
+
+from pyxcp.cmdline import ArgumentParser
+
+
+def callout(master, args):
+    if args.sk_dll:
+        master.seedNKeyDLL = args.sk_dll
+
+
+ap = ArgumentParser(description="pyXCP skeleton.", callout=callout)
+
+# Add command-line option for seed-and-key DLL.
+ap.parser.add_argument(
+    "-s",
+    "--sk-dll",
+    dest="sk_dll",
+    help="Seed-and-Key .DLL name",
+    type=str,
+    default=None,
+)
+
+with ap.run() as x:
+    x.connect()
+    if x.slaveProperties.optionalCommMode:
+        # Collect additional properties.
+        x.getCommModeInfo()
+
+    # getId() is not strictly required.
+    slave_name = x.identifier(0x01)
+
+    # Unlock resources, if necessary.
+    # Could be more specific, like cond_unlock("DAQ")
+    # Note: Unlocking requires a seed-and-key DLL.
+    x.cond_unlock()
+
+    ##
+    # Your own code goes here.
+    ##
+
+    x.disconnect()
+
+# Print some useful information.
+# print("\nSlave properties:")
+# print("=================")
+# print("ID: '{}'".format(slave_name.decode("utf8")))
+# pprint(x.slaveProperties)
