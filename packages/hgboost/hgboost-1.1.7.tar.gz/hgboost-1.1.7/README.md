065b0934f5128846a9bd1341f9c4e686ea450044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:47 +0000

    Add package hgboost/1.1.7/hgboost-1.1.7.tar.gz

---
+++ packages/hgboost/hgboost-1.1.7.tar.gz/hgboost-1.1.7/README.md
@@ -0,0 +1,235 @@
+# hgboost - Hyperoptimized Gradient Boosting
+
+[![Python](https://img.shields.io/pypi/pyversions/hgboost)](https://img.shields.io/pypi/pyversions/hgboost)
+[![PyPI Version](https://img.shields.io/pypi/v/hgboost)](https://pypi.org/project/hgboost/)
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](https://github.com/erdogant/hgboost/blob/master/LICENSE)
+[![Github Forks](https://img.shields.io/github/forks/erdogant/hgboost.svg)](https://github.com/erdogant/hgboost/network)
+[![GitHub Open Issues](https://img.shields.io/github/issues/erdogant/hgboost.svg)](https://github.com/erdogant/hgboost/issues)
+[![Project Status](http://www.repostatus.org/badges/latest/active.svg)](http://www.repostatus.org/#active)
+[![Downloads](https://pepy.tech/badge/hgboost/month)](https://pepy.tech/project/hgboost/month)
+[![Downloads](https://pepy.tech/badge/hgboost)](https://pepy.tech/project/hgboost)
+[![DOI](https://zenodo.org/badge/257025146.svg)](https://zenodo.org/badge/latestdoi/257025146)
+[![Sphinx](https://img.shields.io/badge/Sphinx-Docs-Green)](https://erdogant.github.io/hgboost/)
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://erdogant.github.io/hgboost/pages/html/Documentation.html#colab-classification-notebook)
+[![Medium](https://img.shields.io/badge/Medium-Blog-green)](https://erdogant.github.io/hgboost/pages/html/Documentation.html#medium-blog)
+<!---[![BuyMeCoffee](https://img.shields.io/badge/buymea-coffee-yellow.svg)](https://www.buymeacoffee.com/erdogant)-->
+<!---[![Coffee](https://img.shields.io/badge/coffee-black-grey.svg)](https://erdogant.github.io/donate/?currency=USD&amount=5)-->
+
+--------------------------------------------------------------------
+
+``hgboost`` is short for **Hyperoptimized Gradient Boosting** and is a python package for hyperparameter optimization for *xgboost*, *catboost* and *lightboost* using cross-validation, and evaluating the results on an independent validation set.
+``hgboost`` can be applied for classification and regression tasks.
+
+``hgboost`` is fun because:
+
+    * 1. Hyperoptimization of the Parameter-space using bayesian approach.
+    * 2. Determines the best scoring model(s) using k-fold cross validation.
+    * 3. Evaluates best model on independent evaluation set.
+    * 4. Fit model on entire input-data using the best model.
+    * 5. Works for classification and regression
+    * 6. Creating a super-hyperoptimized model by an ensemble of all individual optimized models.
+    * 7. Return model, space and test/evaluation results.
+    * 8. Makes insightful plots.
+
+--------------------------------------------------------------------
+
+**⭐️ Star this repo if you like it ⭐️**
+
+--------------------------------------------------------------------
+
+### Blogs
+
+Medium Blog 1: 
+[The Best Boosting Model using Bayesian Hyperparameter Tuning but without Overfitting.](https://erdogant.github.io/hgboost/pages/html/Documentation.html#medium-blog)
+
+Medium Blog 2: 
+[Create Explainable Gradient Boosting Classification models using Bayesian Hyperparameter Optimization.](https://erdogant.github.io/hgboost/pages/html/Documentation.html#medium-blog)
+
+--------------------------------------------------------------------
+
+### [Documentation pages](https://erdogant.github.io/hgboost/)
+
+On the [documentation pages](https://erdogant.github.io/hgboost/) you can find detailed information about the working of the ``hgboost`` with many examples. 
+
+--------------------------------------------------------------------
+
+
+## Colab Notebooks
+
+* <a href="https://erdogant.github.io/hgboost/pages/html/Documentation.html#colab-regression-notebook"> <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open regression example In Colab"/> </a> Regression example 
+
+* <a href="https://erdogant.github.io/hgboost/pages/html/Documentation.html#colab-classification-notebook"> <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open classification example In Colab"/> </a> Classification example 
+
+--------------------------------------------------------------------
+
+
+### Schematic overview of hgboost
+
+<p align="center">
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/schematic_overview.png" width="600" />
+</p>
+
+
+### Installation Environment
+
+```python
+conda create -n env_hgboost python=3.8
+conda activate env_hgboost
+```
+
+### Install from pypi
+
+```bash
+pip install hgboost
+pip install -U hgboost # Force update
+
+```
+
+#### Import hgboost package
+```python
+import hgboost as hgboost
+```
+
+#### Examples
+
+* [Example: Fit catboost by hyperoptimization and cross-validation](https://erdogant.github.io/hgboost/pages/html/Examples.html#catboost)
+
+#
+
+* [Example: Fit lightboost by hyperoptimization and cross-validation](https://erdogant.github.io/hgboost/pages/html/Examples.html#lightboost)
+
+#
+
+* [Example: Fit xgboost by hyperoptimization and cross-validation](https://erdogant.github.io/hgboost/pages/html/Examples.html#xgboost-two-class)
+
+#
+
+* [Example: Plot searched parameter space](https://erdogant.github.io/hgboost/pages/html/Examples.html#plot-params)
+
+<p align="left">
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/plot_params_clf_1.png" width="400" />
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/plot_params_clf_2.png" width="400" />
+  </a>
+</p>
+
+#
+
+* [Example: plot summary](https://erdogant.github.io/hgboost/pages/html/Examples.html#plot-summary)
+
+<p align="left">
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/plot_clf.png" width="600" />
+  </a>
+</p>
+
+
+#
+
+* [Example: Tree plot](https://erdogant.github.io/hgboost/pages/html/Examples.html#treeplot)
+
+<p align="left">
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/treeplot_clf_1.png" width="400" />
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/treeplot_clf_2.png" width="400" />
+  </a>
+</p>
+
+
+#
+
+* [Example: Plot the validation results](https://erdogant.github.io/hgboost/pages/html/Examples.html#plot-validation)
+
+<p align="left">
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/plot_validation_clf_1.png" width="600" />
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/plot_validation_clf_2.png" width="400" />
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/plot_validation_clf_3.png" width="600" />
+</p>
+
+#
+
+* [Example: Plot the cross-validation results](https://erdogant.github.io/hgboost/pages/html/Examples.html#plot-cv)
+
+<p align="left">
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/plot_cv_clf.png" width="600" />
+</p>
+
+
+#
+
+* [Example: use the learned model to make new predictions](https://erdogant.github.io/hgboost/pages/html/hgboost.hgboost.html?highlight=predict#hgboost.hgboost.hgboost.predict)
+
+#
+
+* [Example: Create ensemble model for Classification](https://erdogant.github.io/hgboost/pages/html/Examples.html#ensemble-classification)
+
+#
+
+* [Example: Create ensemble model for Regression](https://erdogant.github.io/hgboost/pages/html/Examples.html#ensemble-regression)
+
+#
+
+#### Classification example for xgboost, catboost and lightboost:
+```python
+
+# Load library
+from hgboost import hgboost
+
+# Initialization
+hgb = hgboost(max_eval=10, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42)
+
+# Fit xgboost by hyperoptimization and cross-validation
+results = hgb.xgboost(X, y, pos_label='survived')
+
+# [hgboost] >Start hgboost classification..
+# [hgboost] >Collecting xgb_clf parameters.
+# [hgboost] >Number of variables in search space is [11], loss function: [auc].
+# [hgboost] >method: xgb_clf
+# [hgboost] >eval_metric: auc
+# [hgboost] >greater_is_better: True
+# [hgboost] >pos_label: True
+# [hgboost] >Total dataset: (891, 204) 
+# [hgboost] >Hyperparameter optimization..
+#  100% |----| 500/500 [04:39<05:21,  1.33s/trial, best loss: -0.8800619834710744]
+# [hgboost] >Best performing [xgb_clf] model: auc=0.881198
+# [hgboost] >5-fold cross validation for the top 10 scoring models, Total nr. tests: 50
+# 100%|██████████| 10/10 [00:42<00:00,  4.27s/it]
+# [hgboost] >Evalute best [xgb_clf] model on independent validation dataset (179 samples, 20.00%).
+# [hgboost] >[auc] on independent validation dataset: -0.832
+# [hgboost] >Retrain [xgb_clf] on the entire dataset with the optimal parameters settings.
+```
+
+
+```python
+
+# Plot the ensemble classification validation results
+hgb.plot_validation()
+
+```
+
+<p align="center">
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/plot_ensemble_clf_1.png" width="600" />
+</p>
+
+<p align="center">
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/plot_ensemble_clf_2.png" width="400" />
+  <img src="https://github.com/erdogant/hgboost/blob/master/docs/figs/plot_ensemble_clf_3.png" width="400" />
+</p>
+
+<hr>
+
+**References**
+
+    * http://hyperopt.github.io/hyperopt/
+    * https://github.com/dmlc/xgboost
+    * https://github.com/microsoft/LightGBM
+    * https://github.com/catboost/catboost
+    
+**Maintainers**
+* Erdogan Taskesen, github: [erdogant](https://github.com/erdogant)
+
+**Contribute**
+* Contributions are welcome.
+
+**Licence**
+See [LICENSE](LICENSE) for details.
+
+**Coffee**
+* If you wish to buy me a <a href="https://www.buymeacoffee.com/erdogant">Coffee</a> for this work, it is very appreciated :)
---
+++ packages/hgboost/hgboost-1.1.7.tar.gz/hgboost-1.1.7/hgboost/examples.py
@@ -0,0 +1,376 @@
+#%%
+# https://scikit-learn.org/stable/modules/cross_validation.html
+# https://towardsdatascience.com/fine-tuning-xgboost-in-python-like-a-boss-b4543ed8b1e
+# https://scikit-learn.org/stable/auto_examples/model_selection/plot_cv_indices.html#sphx-glr-auto-examples-model-selection-plot-cv-indices-py
+# https://github.com/WillKoehrsen/hyperparameter-optimization/blob/master/Bayesian%20Hyperparameter%20Optimization%20of%20Gradient%20Boosting%20Machine.ipynb
+# https://www.kaggle.com/henrylidgley/xgboost-with-hyperopt-tuning
+# https://towardsdatascience.com/tree-boosted-mixed-effects-models-4df610b624cb
+# https://machinelearningmastery.com/gradient-boosting-with-scikit-learn-xgboost-lightgbm-and-catboost/
+# http://hyperopt.github.io/hyperopt/getting-started/minimizing_functions/
+
+import numpy as np
+from hgboost import hgboost
+
+hgb = hgboost(max_eval=25, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42, verbose=3)
+
+# Import data
+df = hgb.import_example()
+y = df['Age'].values
+df.drop(['Age', 'PassengerId', 'Name'], axis=1, inplace=True)
+
+# Preprocessing
+X = hgb.preprocessing(df, verbose=0)
+I = ~np.isnan(y)
+X = X.loc[I, :]
+y = y[I]
+
+# Fit
+hgb.lightboost_reg(X, y, eval_metric='mae');
+
+# %%
+# Objective is to demonstrate:
+
+# regression
+# binary classification
+# multiclass classification
+# cross-validation
+# hyperparameter searching
+# feature importance
+# early stopping
+# plotting
+
+# %%
+# import numpy as np
+# from hgboost import hgboost
+# print(dir(hgboost))
+# print(hgboost.__version__)
+
+# %%
+from hgboost import hgboost
+hgb = hgboost()
+df = hgb.import_example(data='ds_salaries')
+
+y = df['salary_in_usd'].values
+df.drop(['salary_in_usd', 'salary_currency', 'salary'], axis=1, inplace=True)
+X = hgb.preprocessing(df, verbose=4)
+
+import numpy as np
+I = ~np.isnan(y)
+X = X.loc[I, :]
+y = y[I]
+
+results  = hgb.xgboost_reg(X, y, eval_metric='mae')      # XGBoost
+results  = hgb.xgboost_reg(X, y, eval_metric='rmse')      # XGBoost
+
+# %% HYPEROPTIMIZED REGRESSION-XGBOOST
+import numpy as np
+from hgboost import hgboost
+
+hgb_xgb = hgboost(max_eval=25, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42, verbose=3)
+hgb_cat = hgboost(max_eval=25, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42, verbose=3)
+hgb_light = hgboost(max_eval=25, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42, verbose=3)
+
+# Import data
+df = hgb_xgb.import_example()
+y = df['Age'].values
+df.drop(['Age', 'PassengerId', 'Name'], axis=1, inplace=True)
+
+# Preprocessing
+X = hgb_xgb.preprocessing(df, verbose=0)
+I = ~np.isnan(y)
+X = X.loc[I, :]
+y = y[I]
+
+# Fit
+results = hgb_xgb.xgboost_reg(X, y, eval_metric='mae')
+# results2 = hgb_cat.catboost_reg(X, y, eval_metric='mae')
+# results3 = hgb_light.lightboost_reg(X, y, eval_metric='mae')
+
+# hgb_xgb.save('c:\\temp\\hgb_xgb.pkl')
+# hgb_cat.save('c:\\temp\\hgb_cat.pkl')
+# hgb_light.save('c:\\temp\\hgb_light.pkl')
+
+# Make some plots
+hgb_xgb.plot_params()
+hgb_xgb.plot(ylim=[8.5, 13], plot2=False)
+hgb_xgb.plot_validation()
+hgb_xgb.plot_cv()
+hgb_xgb.treeplot(plottype='vertical')
+
+hgb_cat.plot_params()
+hgb_cat.plot(ylim=[8, 12])
+hgb_cat.treeplot()
+hgb_cat.plot_validation()
+hgb_cat.plot_cv()
+
+hgb_light.plot_params()
+hgb_light.plot(ylim=[8, 12])
+hgb_light.treeplot()
+hgb_light.plot_validation()
+hgb_light.plot_cv()
+
+# use the predictor
+# y_pred, y_proba = hgb.predict(X)
+
+# %% HYPEROPTIMIZED regression XGBOOST
+from hgboost import hgboost
+import pandas as pd
+file = "C://Users//playground//Downloads//Dataset//Dataset//Training//Features_Variant_1.csv"
+df = pd.read_csv(file, header=None)
+y = df.loc[:,53].values
+X  = df.loc[:,:52] 
+
+hgb_xgb = hgboost(max_eval=10, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42, verbose=4)
+results = hgb_xgb.xgboost_reg(X, y, eval_metric='mae')
+# Baseline MAE is 11.31
+
+hgb_xgb.plot_params()
+hgb_xgb.plot()
+hgb_xgb.treeplot()
+hgb_xgb.plot_validation()
+hgb_xgb.plot_cv()
+
+
+
+from sklearn.metrics import mean_absolute_error
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.1, random_state=42)
+
+model = LinearRegression().fit(hgb_xgb.X_train, hgb_xgb.y_train)
+model.score(hgb_xgb.X_train, hgb_xgb.y_train)
+model.score(hgb_xgb.X_val, hgb_xgb.y_val)
+ 
+# "Learn" the mean from the training data
+mean_train = np.mean(y_train)
+# Get predictions on the test set
+baseline_predictions = np.ones(y_test.shape) * mean_train
+# Compute MAE
+mae_baseline = mean_absolute_error(y_test, baseline_predictions)
+print("Baseline MAE is {:.2f}".format(mae_baseline))
+# Baseline MAE is 11.31
+
+
+# %% HYPEROPTIMIZED regression XGBOOST
+from hgboost import hgboost
+
+from sklearn.datasets import fetch_california_housing, load_diabetes
+df = fetch_california_housing(as_frame=True)
+df = df['frame']
+y = df['MedHouseVal'].values
+del df['MedHouseVal']
+del df['Latitude']
+del df['Longitude']
+
+df = load_diabetes(as_frame=True)
+df = df['frame']
+y = df['target'].values
+del df['target']
+
+
+hgb_xgb = hgboost(max_eval=50, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42, verbose=4)
+results = hgb_xgb.xgboost_reg(df, y, eval_metric='mae')
+
+# Make some plots
+hgb_xgb.plot_params()
+hgb_xgb.plot()
+hgb_xgb.treeplot()
+hgb_xgb.plot_validation()
+hgb_xgb.plot_cv()
+
+# BAseline simple regression
+model = LinearRegression().fit(hgb_xgb.X_train, hgb_xgb.y_train)
+model.score(hgb_xgb.X_train, hgb_xgb.y_train)
+model.score(hgb_xgb.X_test, hgb_xgb.y_test)
+model.score(hgb_xgb.X_val, hgb_xgb.y_val)
+ 
+# "Learn" the mean from the training data
+mean_train = np.mean(hgb_xgb.y_train)
+# Get predictions on the test set
+baseline_predictions = np.ones(hgb_xgb.y_test.shape) * mean_train
+# Compute MAE
+mae_baseline = mean_absolute_error(hgb_xgb.y_test, baseline_predictions)
+print("Baseline MAE is {:.2f}".format(mae_baseline))
+# Baseline MAE is 11.31
+
+
+# %% HYPEROPTIMIZED CLASSIFICATION XGBOOST
+from hgboost import hgboost
+hgb_xgb = hgboost(max_eval=25, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=0, gpu=False, verbose=3)
+hgb_cat = hgboost(max_eval=25, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=0, gpu=False, verbose=3)
+hgb_light = hgboost(max_eval=25, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=0, gpu=False, verbose=3)
+
+# Import data
+df = hgb_xgb.import_example()
+del df['PassengerId']
+del df['Name']
+
+y = df['Survived'].values
+del df['Survived']
+X = hgb_xgb.preprocessing(df, verbose=0)
+
+# Fit
+results = hgb_xgb.xgboost(df, y<0, pos_label=1)
+results = hgb_cat.catboost(X, y, pos_label=1)
+results = hgb_light.lightboost(X, y, pos_label=1)
+
+# Make some plots
+hgb_xgb.plot_params()
+hgb_xgb.plot()
+hgb_xgb.treeplot()
+hgb_xgb.plot_validation()
+hgb_xgb.plot_cv()
+
+hgb_cat.plot_params()
+hgb_cat.plot()
+hgb_cat.treeplot()
+hgb_cat.plot_validation()
+hgb_cat.plot_cv()
+
+hgb_light.plot_params()
+hgb_light.plot()
+hgb_light.treeplot()
+hgb_light.plot_validation()
+hgb_light.plot_cv()
+
+# use the predictor
+y_pred, y_proba = hgb_xgb.predict(X)
+y_pred, y_proba = hgb_cat.predict(X)
+y_pred, y_proba = hgb_light.predict(X)
+
+import matplotlib.pyplot as plt
+plt.figure();plt.plot(results['summary']['loss'])
+
+#     booster colsample_bytree gamma  ... best_cv loss_validation default_params
+# 0    gbtree             0.55     2  ...     0.0             NaN          False
+# 1    gbtree             0.56     1  ...     0.0             NaN          False
+# 2    gbtree             0.64     5  ...     0.0             NaN          False
+# 3    gbtree             0.59     5  ...     0.0             NaN          False
+# 4    gbtree             0.22   1.5  ...     0.0             NaN          False
+# ..      ...              ...   ...  ...     ...             ...            ...
+# 246  gbtree              0.8     2  ...     0.0             NaN          False
+# 247  gbtree             0.45     1  ...     0.0             NaN          False
+# 248  gbtree             0.88   0.5  ...     0.0             NaN          False
+# 249  gbtree             0.57     1  ...     0.0             NaN          False
+# 250     NaN                1     0  ...     NaN         0.85415           True
+
+# [251 rows x 23 columns]
+
+# %%
+hg = hgboost(max_eval=10, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=0, gpu=True, verbose=3)
+df = hg.import_example()
+y = df['Survived'].values
+del df['Survived']
+X = hg.preprocessing(df, verbose=0)
+
+# # Fit
+results = hg.xgboost(X, y, pos_label=1)
+
+# %%
+hg.save('test')
+hg.load('test')
+# results['model']
+# results['params']
+
+
+
+
+# %% HYPEROPTIMIZED MULTI-CLASS XGBOOST
+hgb = hgboost(max_eval=10, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42)
+
+# Import data
+df = hgb.import_example()
+y = df['Parch'].values
+y[y>=3]=3
+del df['Parch']
+X = hgb.preprocessing(df, verbose=0)
+
+# FIT MULTI-CLASS CLASSIFIER
+results = hgb.xgboost(X, y, method='xgb_clf_multi')
+
+# Make some plots
+hgb.plot_params()
+hgb.plot()
+hgb.treeplot()
+hgb.plot_validation()
+hgb.plot_cv()
+
+# use the predictor
+y_pred, y_proba = hgb.predict(X)
+
+
+# %% ENSEMBLE CLASSIFIER
+hgb = hgboost(max_eval=10, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=None, gpu=True, verbose=3)
+
+# Import data
+df = hgb.import_example()
+y = df['Survived'].values
+del df['Survived']
+X = hgb.preprocessing(df, verbose=0)
+
+results = hgb.ensemble(X, y, pos_label=1, methods=['xgb_clf', 'ctb_clf', 'lgb_clf'])
+
+# use the predictor
+y_pred, y_proba = hgb.predict(X)
+
+# Plot
+hgb.plot_validation()
+hgb.plot()
+
+# %% ENSEMBLE REGRESSION
+hgb = hgboost(max_eval=10, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=None, verbose=3)
+
+# Import data
+df = hgb.import_example()
+y = df['Age'].values
+del df['Age']
+I = ~np.isnan(y)
+X = hgb.preprocessing(df, verbose=0)
+X = X.loc[I, :]
+y = y[I]
+
+results = hgb.ensemble(X, y, methods=['xgb_reg', 'ctb_reg', 'lgb_reg'])
+
+# use the predictor
+y_pred, y_proba = hgb.predict(X)
+
+# Plot
+hgb.plot_validation()
+
+# %% CLASSIFICATION TWO-CLASS #####
+from sklearn import datasets
+import pandas as pd
+
+iris = datasets.load_iris()
+X = pd.DataFrame(iris.data, columns=iris['feature_names'])
+y = iris.target
+
+hgb = hgboost(max_eval=10, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42)
+results = hgb.xgboost(X, y, pos_label=0)
+
+# Plot
+hgb.plot_params()
+hgb.plot()
+hgb.treeplot()
+hgb.plot_validation()
+hgb.plot_cv()
+
+# %% CLASSIFICATION MULTI-CLASS #####
+from sklearn import datasets
+import pandas as pd
+
+iris = datasets.load_iris()
+X = pd.DataFrame(iris.data, columns=iris['feature_names'])
+y = iris.target
+
+hgb = hgboost(max_eval=10, threshold=0.5, cv=5, test_size=0.2, val_size=0.2, top_cv_evals=10, random_state=42)
+results = hgb.xgboost(X, y, method="xgb_clf_multi")
+
+hgb.plot_params()
+hgb.plot()
+hgb.treeplot()
+hgb.plot_validation()
+hgb.plot_cv()
+
+# %%
