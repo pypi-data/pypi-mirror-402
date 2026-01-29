357aacc86295daf68bda8481d0a6e44a3af3ba44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:12:52 +0000

    Add package openneuro-py/2026.1.0/openneuro_py-2026.1.0-py3-none-any.whl

---
+++ packages/openneuro-py/openneuro_py-2026.1.0-py3-none-any.whl/openneuro/_cli.py
@@ -0,0 +1,109 @@
+from typing import Annotated, Optional
+
+import typer
+
+import openneuro
+from openneuro._download import download, login
+
+app = typer.Typer(no_args_is_help=True, pretty_exceptions_show_locals=False)
+
+
+@app.command(name="download")
+def download_cli(
+    dataset: Annotated[
+        str, typer.Option(help="The OpenNeuro dataset identifier.", show_default=False)
+    ],
+    tag: Annotated[
+        Optional[str],
+        typer.Option(help="The tag (version) of the dataset.", show_default=False),
+    ] = None,
+    target_dir: Annotated[
+        Optional[str],
+        typer.Option(help="The directory to download to.", show_default=False),
+    ] = None,
+    include: Annotated[
+        Optional[str],
+        typer.Option(
+            help="Only include the specified file or directory. "
+            "Can be passed multiple times.",
+            show_default=False,
+        ),
+    ] = None,
+    exclude: Annotated[
+        Optional[str],
+        typer.Option(
+            help="Exclude the specified file or directory. "
+            "Can be passed multiple times.",
+            show_default=False,
+        ),
+    ] = None,
+    verify_hash: Annotated[
+        bool,
+        typer.Option(
+            help="Whether to check the SHA256 hash of each downloaded file.",
+        ),
+    ] = True,
+    verify_size: Annotated[
+        bool,
+        typer.Option(help="Whether to check the size of each downloaded file."),
+    ] = True,
+    max_retries: Annotated[
+        int,
+        typer.Option(
+            help="Try the specified number of times to download a file before failing.",
+        ),
+    ] = 5,
+    max_concurrent_downloads: Annotated[
+        int,
+        typer.Option(
+            help="The maximum number of downloads to run in parallel.",
+        ),
+    ] = 5,
+    metadata_timeout: Annotated[
+        float,
+        typer.Option(
+            help="Timeout in seconds for metadata queries.",
+        ),
+    ] = 15.0,
+) -> None:
+    """Download datasets from OpenNeuro."""
+    download(
+        dataset=dataset,
+        tag=tag,
+        target_dir=target_dir,
+        include=include,
+        exclude=exclude,
+        verify_hash=verify_hash,
+        verify_size=verify_size,
+        max_retries=max_retries,
+        max_concurrent_downloads=max_concurrent_downloads,
+        metadata_timeout=metadata_timeout,
+    )
+
+
+@app.command(name="login")
+def login_cli() -> None:
+    """Login to OpenNeuro and store an access token."""
+    login()
+
+
+def show_version_callback(show_version: bool) -> None:
+    if show_version:
+        typer.echo(f"This is openneuro-py {openneuro.__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: Annotated[
+        Optional[bool],
+        typer.Option(
+            "--version",
+            help="Show the version of openneuro-py.",
+            callback=show_version_callback,
+            is_eager=True,
+        ),
+    ] = False,
+) -> None:
+    """Access OpenNeuro datasets."""
+    pass
---
+++ packages/openneuro-py/openneuro_py-2026.1.0-py3-none-any.whl/openneuro/_config.py
@@ -0,0 +1,84 @@
+import getpass
+import json
+import os
+import stat
+from pathlib import Path
+from typing import TypedDict
+
+import platformdirs
+from tqdm.auto import tqdm
+
+CONFIG_DIR = Path(
+    platformdirs.user_config_dir(appname="openneuro-py", appauthor=False, roaming=True)
+)
+CONFIG_DIR.mkdir(parents=True, exist_ok=True)
+CONFIG_PATH = CONFIG_DIR / "config.json"
+BASE_URL = "https://openneuro.org/"
+
+
+class Config(TypedDict):
+    """Configuration container."""
+
+    endpoint: str
+    apikey: str
+
+
+def init_config() -> None:
+    """Initialize a new OpenNeuro configuration file."""
+    tqdm.write(
+        "🙏 Please login to your OpenNeuro account and go to: "
+        "My Account → Obtain an API Key"
+    )
+    api_key = getpass.getpass("OpenNeuro API key (input hidden): ")
+
+    config: Config = {
+        "endpoint": BASE_URL,
+        "apikey": api_key,
+    }
+
+    with open(CONFIG_PATH, "w", encoding="utf-8") as f:
+        json.dump(config, f, indent=2)
+    os.chmod(CONFIG_PATH, stat.S_IRUSR | stat.S_IWUSR)
+
+
+def load_config() -> dict[str, str]:
+    """Load an OpenNeuro configuration file, and return its contents.
+
+    Returns
+    -------
+    dict
+        The configuration options.
+
+    """
+    with open(CONFIG_PATH, encoding="utf-8") as f:
+        config = json.load(f)
+    assert isinstance(config, dict)
+    return config
+
+
+def get_token() -> str:
+    """Get the OpenNeuro API token if configured with the 'login' command.
+
+    Returns
+    -------
+    The API token if configured.
+
+    Raises
+    ------
+    ValueError
+        When no token has been configured yet.
+
+    """
+    if not CONFIG_PATH.exists():
+        raise ValueError(
+            "Could not read API token as no openneuro-py configuration "
+            'file exists. Run "openneuro login" to generate it.'
+        )
+    config = load_config()
+    if "apikey" not in config:
+        raise ValueError(
+            "An openneuro-py configuration file was found, but did not "
+            'contain an "apikey" entry. Run "openneuro login" to '
+            "add such an entry."
+        )
+    return config["apikey"]
