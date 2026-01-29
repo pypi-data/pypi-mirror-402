b4dd3d87e9dcdf724085cd80ea96a5cb09f3ef89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:12:59 +0000

    Add package wrenfold/0.5.1/wrenfold-0.5.1-cp311-cp311-win_amd64.whl

---
+++ packages/wrenfold/wrenfold-0.5.1-cp311-cp311-win_amd64.whl/pywrenfold/__init__.pyi
@@ -0,0 +1,13 @@
+from . import (
+    ast as ast,
+    enumerations as enumerations,
+    exceptions as exceptions,
+    gen as gen,
+    geometry as geometry,
+    sym as sym,
+    sympy_conversion as sympy_conversion,
+    type_info as type_info
+)
+
+
+def set_tracing_output_path(path: str) -> None: ...
