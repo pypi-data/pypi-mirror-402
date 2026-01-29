7f3b21cba9c3b59795b0a6bfafc324688bffd058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:45 +0000

    Add package libsemigroups-pybind11/1.3.0/libsemigroups_pybind11-1.3.0-cp313-cp313-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.3.0-cp313-cp313-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/libsemigroups_pybind11/froidure_pin.py
@@ -0,0 +1,275 @@
+# Copyright (c) 2021-2024 J. D. Mitchell
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+# The module doc string is what appears at the top of the helper function
+# doc page, and so is omitted.
+
+"""This page contains the documentation for various helper functions for
+manipulating :any:`FroidurePin` objects. All such functions
+are contained in the submodule ``libsemigroups_pybind11.froidure_pin``.
+"""
+
+from collections.abc import Iterator as _Iterator
+from typing import TypeVar as _TypeVar
+
+from typing_extensions import Self as _Self
+
+from _libsemigroups_pybind11 import (
+    LIBSEMIGROUPS_HPCOMBI_ENABLED as _LIBSEMIGROUPS_HPCOMBI_ENABLED,
+    PBR as _PBR,
+    Bipartition as _Bipartition,
+    BMat as _BMat,
+    BMat8 as _BMat8,
+    FroidurePinBipartition as _FroidurePinBipartition,
+    FroidurePinBMat as _FroidurePinBMat,
+    FroidurePinBMat8 as _FroidurePinBMat8,
+    FroidurePinIntMat as _FroidurePinIntMat,
+    FroidurePinKBEStringRewriteFromLeft as _FroidurePinKBEStringRewriteFromLeft,
+    FroidurePinKBEStringRewriteTrie as _FroidurePinKBEStringRewriteTrie,
+    FroidurePinKBEWordRewriteFromLeft as _FroidurePinKBEWordRewriteFromLeft,
+    FroidurePinKBEWordRewriteTrie as _FroidurePinKBEWordRewriteTrie,
+    FroidurePinKEMultiViewString as _FroidurePinKEMultiViewString,
+    FroidurePinKEString as _FroidurePinKEString,
+    FroidurePinKEWord as _FroidurePinKEWord,
+    FroidurePinMaxPlusMat as _FroidurePinMaxPlusMat,
+    FroidurePinMaxPlusTruncMat as _FroidurePinMaxPlusTruncMat,
+    FroidurePinMinPlusMat as _FroidurePinMinPlusMat,
+    FroidurePinMinPlusTruncMat as _FroidurePinMinPlusTruncMat,
+    FroidurePinNTPMat as _FroidurePinNTPMat,
+    FroidurePinPBR as _FroidurePinPBR,
+    FroidurePinPerm1 as _FroidurePinPerm1,
+    FroidurePinPerm2 as _FroidurePinPerm2,
+    FroidurePinPerm4 as _FroidurePinPerm4,
+    FroidurePinPPerm1 as _FroidurePinPPerm1,
+    FroidurePinPPerm2 as _FroidurePinPPerm2,
+    FroidurePinPPerm4 as _FroidurePinPPerm4,
+    FroidurePinProjMaxPlusMat as _FroidurePinProjMaxPlusMat,
+    FroidurePinTCE as _FroidurePinTCE,
+    FroidurePinTransf1 as _FroidurePinTransf1,
+    FroidurePinTransf2 as _FroidurePinTransf2,
+    FroidurePinTransf4 as _FroidurePinTransf4,
+    IntMat as _IntMat,
+    KBEStringFromLeft as _KBEStringFromLeft,
+    KBEStringTrie as _KBEStringTrie,
+    KBEWordFromLeft as _KBEWordFromLeft,
+    KBEWordTrie as _KBEWordTrie,
+    MaxPlusMat as _MaxPlusMat,
+    MaxPlusTruncMat as _MaxPlusTruncMat,
+    MinPlusMat as _MinPlusMat,
+    MinPlusTruncMat as _MinPlusTruncMat,
+    NTPMat as _NTPMat,
+    Perm1 as _Perm1,
+    Perm2 as _Perm2,
+    Perm4 as _Perm4,
+    PPerm1 as _PPerm1,
+    PPerm2 as _PPerm2,
+    PPerm4 as _PPerm4,
+    ProjMaxPlusMat as _ProjMaxPlusMat,
+    Transf1 as _Transf1,
+    Transf2 as _Transf2,
+    Transf4 as _Transf4,
+    froidure_pin_current_minimal_factorisation as _froidure_pin_current_minimal_factorisation,
+    froidure_pin_current_normal_forms as _froidure_pin_current_normal_forms,
+    froidure_pin_current_position as _froidure_pin_current_position,
+    froidure_pin_current_rules as _froidure_pin_current_rules,
+    froidure_pin_equal_to as _froidure_pin_equal_to,
+    froidure_pin_factorisation as _froidure_pin_factorisation,
+    froidure_pin_minimal_factorisation as _froidure_pin_minimal_factorisation,
+    froidure_pin_normal_forms as _froidure_pin_normal_forms,
+    froidure_pin_position as _froidure_pin_position,
+    froidure_pin_product_by_reduction as _froidure_pin_product_by_reduction,
+    froidure_pin_rules as _froidure_pin_rules,
+    froidure_pin_to_element as _froidure_pin_to_element,
+)
+
+from .detail.cxx_wrapper import (
+    CxxWrapper as _CxxWrapper,
+    copy_cxx_mem_fns as _copy_cxx_mem_fns,
+    register_cxx_wrapped_type as _register_cxx_wrapped_type,
+    to_cxx as _to_cxx,
+    to_py as _to_py,
+    wrap_cxx_free_fn as _wrap_cxx_free_fn,
+)
+from .detail.decorators import copydoc as _copydoc
+
+if _LIBSEMIGROUPS_HPCOMBI_ENABLED:
+    # Disable pylint which complains if HPCOMBI is not enabled
+    # pylint: disable=no-name-in-module
+    from _libsemigroups_pybind11 import (
+        FroidurePinHPCombiPerm16 as _FroidurePinHPCombiPerm16,
+        FroidurePinHPCombiPPerm16 as _FroidurePinHPCombiPPerm16,
+        FroidurePinHPCombiPTransf16 as _FroidurePinHPCombiPTransf16,
+        FroidurePinHPCombiTransf16 as _FroidurePinHPCombiTransf16,
+        hpcombi_Perm16 as _HPCombiPerm16,
+        hpcombi_PPerm16 as _HPCombiPPerm16,
+        hpcombi_PTransf16 as _HPCombiPTransf16,
+        hpcombi_Transf16 as _HPCombiTransf16,
+    )
+
+
+########################################################################
+# The FroidurePin python class
+########################################################################
+
+
+class FroidurePin(_CxxWrapper):
+    Element = _TypeVar("Element")
+
+    __doc__ = _FroidurePinPBR.__doc__
+
+    _py_template_params_to_cxx_type = {
+        (_BMat,): _FroidurePinBMat,
+        (_BMat8,): _FroidurePinBMat8,
+        (_Bipartition,): _FroidurePinBipartition,
+        (_IntMat,): _FroidurePinIntMat,
+        (_MaxPlusMat,): _FroidurePinMaxPlusMat,
+        (_MaxPlusTruncMat,): _FroidurePinMaxPlusTruncMat,
+        (_MinPlusMat,): _FroidurePinMinPlusMat,
+        (_MinPlusTruncMat,): _FroidurePinMinPlusTruncMat,
+        (_NTPMat,): _FroidurePinNTPMat,
+        (_PBR,): _FroidurePinPBR,
+        (_PPerm1,): _FroidurePinPPerm1,
+        (_PPerm2,): _FroidurePinPPerm2,
+        (_PPerm4,): _FroidurePinPPerm4,
+        (_Perm1,): _FroidurePinPerm1,
+        (_Perm2,): _FroidurePinPerm2,
+        (_Perm4,): _FroidurePinPerm4,
+        (_ProjMaxPlusMat,): _FroidurePinProjMaxPlusMat,
+        (_Transf1,): _FroidurePinTransf1,
+        (_Transf2,): _FroidurePinTransf2,
+        (_Transf4,): _FroidurePinTransf4,
+        (_KBEStringTrie,): _FroidurePinKBEStringRewriteTrie,
+        (_KBEStringFromLeft,): _FroidurePinKBEStringRewriteFromLeft,
+        (_KBEWordTrie,): _FroidurePinKBEWordRewriteTrie,
+        (_KBEWordFromLeft,): _FroidurePinKBEWordRewriteFromLeft,
+    } | (
+        {
+            (_HPCombiPTransf16,): _FroidurePinHPCombiPTransf16,
+            (_HPCombiTransf16,): _FroidurePinHPCombiTransf16,
+            (_HPCombiPerm16,): _FroidurePinHPCombiPerm16,
+            (_HPCombiPPerm16,): _FroidurePinHPCombiPPerm16,
+        }
+        if _LIBSEMIGROUPS_HPCOMBI_ENABLED
+        else {}
+    )
+
+    _cxx_type_to_py_template_params = dict(
+        zip(
+            _py_template_params_to_cxx_type.values(),
+            _py_template_params_to_cxx_type.keys(),
+            strict=True,
+        )
+    )
+
+    _all_wrapped_cxx_types = {*_py_template_params_to_cxx_type.values()} | {
+        _FroidurePinKEMultiViewString,
+        _FroidurePinKEString,
+        _FroidurePinKEWord,
+        _FroidurePinTCE,
+    }
+
+    ########################################################################
+    # Protected methods
+    ########################################################################
+
+    def _raise_if_element_not_implemented(self: _Self):
+        if type(_to_cxx(self)) not in self._cxx_type_to_py_template_params:
+            raise NotImplementedError(
+                "It is not yet possible to call functions that return an element "
+                "on FroidurePin objects with this type of element."
+            )
+
+    ########################################################################
+    # Special methods
+    ########################################################################
+
+    # TODO(1) Add a keyword argument for element type to the __init__ function,
+    # so that we know which FroidurePin type to construct based on the element
+    # and/or the underlying cxx type.
+    @_copydoc(_FroidurePinPBR.__init__)
+    def __init__(self: _Self, *args) -> None:
+        super().__init__(*args)
+        if _to_cxx(self) is not None:
+            return
+        if len(args) == 0:
+            raise TypeError("expected at least 1 argument, found 0")
+
+        if isinstance(args[0], list) and len(args) == 1:
+            gens = args[0]
+        else:
+            gens = list(args)
+        gens = [_to_cxx(x) for x in gens]
+        self.py_template_params = (type(gens[0]),)
+        self.init_cxx_obj(gens)
+
+    def __getitem__(self: _Self, i: int) -> Element:
+        self._raise_if_element_not_implemented()
+        return _to_py(_to_cxx(self)[i])
+
+    def __iter__(self: _Self) -> _Iterator[Element]:
+        self._raise_if_element_not_implemented()
+        return (_to_py(x) for x in iter(_to_cxx(self)))
+
+    # TODO(1) __contains__?
+
+    ########################################################################
+    # Iterators
+    ########################################################################
+
+    @_copydoc(_FroidurePinPBR.current_elements)
+    def current_elements(  # pylint: disable=missing-function-docstring
+        self: _Self,
+    ) -> _Iterator[Element]:
+        self._raise_if_element_not_implemented()
+        return (_to_py(x) for x in _to_cxx(self).current_elements())
+
+    @_copydoc(_FroidurePinPBR.idempotents)
+    def idempotents(  # pylint: disable=missing-function-docstring
+        self: _Self,
+    ) -> _Iterator[Element]:
+        self._raise_if_element_not_implemented()
+        return (_to_py(x) for x in _to_cxx(self).idempotents())
+
+    @_copydoc(_FroidurePinPBR.sorted_elements)
+    def sorted_elements(  # pylint: disable=missing-function-docstring
+        self: _Self,
+    ) -> _Iterator[Element]:
+        self._raise_if_element_not_implemented()
+        return (_to_py(x) for x in _to_cxx(self).sorted_elements())
+
+
+_copy_cxx_mem_fns(_FroidurePinBMat, FroidurePin)
+
+for _fp_type in (
+    FroidurePin._py_template_params_to_cxx_type.values()  # pylint:disable=protected-access
+):
+    _register_cxx_wrapped_type(_fp_type, FroidurePin)
+
+
+_register_cxx_wrapped_type(_FroidurePinKEMultiViewString, FroidurePin)
+_register_cxx_wrapped_type(_FroidurePinKEString, FroidurePin)
+_register_cxx_wrapped_type(_FroidurePinKEWord, FroidurePin)
+_register_cxx_wrapped_type(_FroidurePinTCE, FroidurePin)
+
+########################################################################
+# Helpers -- from froidure-pin.cpp
+########################################################################
+
+# TODO(1) be good to get the notes about enumeration being triggered or not, in
+# this doc
+
+current_minimal_factorisation = _wrap_cxx_free_fn(_froidure_pin_current_minimal_factorisation)
+current_normal_forms = _wrap_cxx_free_fn(_froidure_pin_current_normal_forms)
+current_position = _wrap_cxx_free_fn(_froidure_pin_current_position)
+current_rules = _wrap_cxx_free_fn(_froidure_pin_current_rules)
+equal_to = _wrap_cxx_free_fn(_froidure_pin_equal_to)
+factorisation = _wrap_cxx_free_fn(_froidure_pin_factorisation)
+minimal_factorisation = _wrap_cxx_free_fn(_froidure_pin_minimal_factorisation)
+normal_forms = _wrap_cxx_free_fn(_froidure_pin_normal_forms)
+position = _wrap_cxx_free_fn(_froidure_pin_position)
+product_by_reduction = _wrap_cxx_free_fn(_froidure_pin_product_by_reduction)
+rules = _wrap_cxx_free_fn(_froidure_pin_rules)
+to_element = _wrap_cxx_free_fn(_froidure_pin_to_element)
---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.3.0-cp313-cp313-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl/libsemigroups_pybind11/to.py
@@ -0,0 +1,149 @@
+# Copyright (c) 2025 Joseph Edwards
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+"""Subpackage containing the :any:`to` function for converting
+``libsemigroups_pybind11`` objects from one type to another.
+"""
+
+from typing import _GenericAlias
+
+from _libsemigroups_pybind11 import (
+    to_congruence_string as _to_congruence_string,
+    to_congruence_word as _to_congruence_word,
+    to_froidure_pin as _to_froidure_pin,
+    to_inverse_presentation as _to_inverse_presentation,
+    to_inverse_presentation_string as _to_inverse_presentation_string,
+    to_inverse_presentation_word as _to_inverse_presentation_word,
+    to_knuth_bendix as _to_knuth_bendix,
+    to_knuth_bendix_RewriteFromLeft as _to_knuth_bendix_RewriteFromLeft,
+    to_knuth_bendix_RewriteTrie as _to_knuth_bendix_RewriteTrie,
+    to_knuth_bendix_string_RewriteFromLeft as _to_knuth_bendix_string_RewriteFromLeft,
+    to_knuth_bendix_string_RewriteTrie as _to_knuth_bendix_string_RewriteTrie,
+    to_knuth_bendix_word_RewriteFromLeft as _to_knuth_bendix_word_RewriteFromLeft,
+    to_knuth_bendix_word_RewriteTrie as _to_knuth_bendix_word_RewriteTrie,
+    to_presentation as _to_presentation,
+    to_presentation_string as _to_presentation_string,
+    to_presentation_word as _to_presentation_word,
+    to_todd_coxeter as _to_todd_coxeter,
+    to_todd_coxeter_string as _to_todd_coxeter_string,
+    to_todd_coxeter_word as _to_todd_coxeter_word,
+)
+
+from .congruence import Congruence as _Congruence
+from .detail.cxx_wrapper import to_cxx as _to_cxx
+from .froidure_pin import FroidurePin as _FroidurePin
+from .knuth_bendix import KnuthBendix as _KnuthBendix
+from .presentation import InversePresentation as _InversePresentation, Presentation as _Presentation
+from .todd_coxeter import ToddCoxeter as _ToddCoxeter
+
+
+def _nice_name(type_list):
+    """Convert an iterable of type-like things into a string"""
+    single_element = False
+    if not isinstance(type_list, tuple):
+        single_element = True
+        type_list = [type_list]
+    out = []
+    for t in type_list:
+        if isinstance(t, str):
+            out.append(t)
+        elif isinstance(t, _GenericAlias):
+            out.append(str(t))
+        elif hasattr(t, "__name__"):
+            out.append(t.__name__)
+        else:
+            out.append(str(t))
+    if single_element:
+        return out[0]
+    return f"({', '.join(out)})"
+
+
+_RETURN_TYPE_TO_CONVERTER_FUNCTION = {
+    (_Congruence, str): _to_congruence_string,
+    (_Congruence, list[int]): _to_congruence_word,
+    (_FroidurePin,): _to_froidure_pin,
+    (_InversePresentation,): _to_inverse_presentation,
+    (_InversePresentation, list[int]): _to_inverse_presentation_word,
+    (_InversePresentation, str): _to_inverse_presentation_string,
+    (_KnuthBendix,): _to_knuth_bendix,
+    (_KnuthBendix, "RewriteTrie"): _to_knuth_bendix_RewriteTrie,
+    (_KnuthBendix, "RewriteFromLeft"): _to_knuth_bendix_RewriteFromLeft,
+    (_KnuthBendix, list[int], "RewriteFromLeft"): _to_knuth_bendix_word_RewriteFromLeft,
+    (_KnuthBendix, list[int], "RewriteTrie"): _to_knuth_bendix_word_RewriteTrie,
+    (_KnuthBendix, str, "RewriteFromLeft"): _to_knuth_bendix_string_RewriteFromLeft,
+    (_KnuthBendix, str, "RewriteTrie"): _to_knuth_bendix_string_RewriteTrie,
+    (_Presentation,): _to_presentation,
+    (_Presentation, str): _to_presentation_string,
+    (_Presentation, list[int]): _to_presentation_word,
+    (_ToddCoxeter,): _to_todd_coxeter,
+    (_ToddCoxeter, str): _to_todd_coxeter_string,
+    (_ToddCoxeter, list[int]): _to_todd_coxeter_word,
+}
+
+_VALID_TYPES = (_nice_name(x) for x in _RETURN_TYPE_TO_CONVERTER_FUNCTION)
+_VALID_TYPES_STRING = "\n    * " + "\n    * ".join(_VALID_TYPES) + "\n"
+
+
+def to(*args, rtype: tuple):
+    """Convert from one type of ``libsemigroups_pybind11`` object to another.
+
+    This function converts the the arguments specified in *args* to object of
+    type *rtype*.
+
+    :param args: the objects to convert.
+    :param rtype: the type of object to convert to.
+    :type rtype: tuple
+
+    :returns: an object of type *rtype*.
+
+    .. seealso::
+
+        See the following pages for a detailed description of the various use
+        cases of this function:
+
+            * :doc:`/main-algorithms/congruence/to-cong`;
+            * :doc:`/main-algorithms/froidure-pin/to-froidure-pin`;
+            * :doc:`/data-structures/presentations/to-inverse-present`;
+            * :doc:`/main-algorithms/knuth-bendix/to-knuth-bendix`;
+            * :doc:`/data-structures/presentations/to-present`; and
+            * :doc:`/main-algorithms/todd-coxeter/to-todd-coxeter`.
+
+
+    .. doctest::
+
+        >>> from libsemigroups_pybind11 import (
+        ...     congruence_kind,
+        ...     FroidurePin,
+        ...     KnuthBendix,
+        ...     Presentation,
+        ...     presentation,
+        ...     to,
+        ... )
+
+        >>> p = Presentation([0, 1])
+        >>> presentation.add_rule(p, [0, 1], [1, 0])
+        >>> presentation.add_rule(p, [0, 0], [0])
+        >>> presentation.add_rule(p, [1, 1], [1])
+        >>> kb = KnuthBendix(congruence_kind.twosided, p)
+        >>> fp = to(kb, rtype=(FroidurePin,))
+        >>> fp  # doctest: +NORMALIZE_WHITESPACE
+        <partially enumerated FroidurePin with 2 generators, 2 elements,
+         Cayley graph ⌀ 1, & 0 rules>
+
+        >>> fp.run()
+        >>> fp
+        <fully enumerated FroidurePin with 2 generators, 3 elements, Cayley graph ⌀ 2, & 3 rules>
+
+    """
+    cxx_args = [_to_cxx(arg) for arg in args]
+    if rtype not in _RETURN_TYPE_TO_CONVERTER_FUNCTION:
+        raise TypeError(
+            "expected the first keyword argument to be one of:"
+            f"{_VALID_TYPES_STRING}"
+            f"but found: {_nice_name(rtype)}"
+        )
+    constructor = rtype[0]
+    return constructor(_RETURN_TYPE_TO_CONVERTER_FUNCTION[rtype](*cxx_args))
