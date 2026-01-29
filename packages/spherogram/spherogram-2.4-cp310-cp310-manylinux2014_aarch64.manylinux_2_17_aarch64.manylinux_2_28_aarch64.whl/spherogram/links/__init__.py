58dba4abfbd83f2540980ec281173efc13be24dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 12:07:12 +0000

    Add package spherogram/2.4/spherogram-2.4-cp310-cp310-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/spherogram/spherogram-2.4-cp310-cp310-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/spherogram/links/__init__.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+from .links import Crossing, Strand, Link, ClosedBraid
+from .tangles import Tangle, CapTangle, CupTangle, RationalTangle, ZeroTangle, InfinityTangle, MinusOneTangle, OneTangle, IntegerTangle, IdentityBraid, ComponentTangle, join_strands
+from . import orthogonal
+from .random_links import random_link
+from . import bands
+
+Link.view = orthogonal.orthogonal_draw
+
+
+def pdf_docs():
+    "Open the PDF docs for links and tangles using the default application"
+    file = __path__[0] + os.sep + "doc.pdf"
+    if sys.platform.startswith('darwin'):
+        command = 'open'
+    elif sys.platform.startswith('win'):
+        command = 'start'
+    else:
+        command = 'xdg-open'
+    os.system(command + ' ' + file)
+
+
+__all__ = ['Crossing', 'Strand', 'Link', 'ClosedBraid',
+           'Tangle', 'CapTangle', 'CupTangle', 'RationalTangle',
+           'ZeroTangle', 'InfinityTangle', 'MinusOneTangle', 'OneTangle', 'IntegerTangle',
+           'IdentityBraid', 'join_strands',
+           'pdf_docs', 'random_link', 'bands']
