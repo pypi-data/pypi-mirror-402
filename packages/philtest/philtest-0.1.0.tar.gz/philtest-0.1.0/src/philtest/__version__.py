8c947fdee95bac98ba8aa35c830205cc1d02d821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:30 +0000

    Add package philtest/0.1.0/philtest-0.1.0.tar.gz

---
+++ packages/philtest/philtest-0.1.0.tar.gz/philtest-0.1.0/src/philtest/__version__.py
@@ -0,0 +1,11 @@
+# .-. .-. .-. . . .-. .-. .-. .-.
+# |(  |-  |.| | | |-  `-.  |  `-.
+# ' ' `-' `-`.`-' `-' `-'  '  `-'
+
+__title__ = "philtest"
+__description__ = "philtest"
+__url__ = ""
+__version__ = "0.0.1"
+__author__ = "PixelPhilOnGit"
+__author_email__ = "2648212924#qq.com"
+__license__ = "Apache-2.0"
