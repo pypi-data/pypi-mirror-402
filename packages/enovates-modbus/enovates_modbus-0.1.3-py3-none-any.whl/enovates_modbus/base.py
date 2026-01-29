cee4013bea9d7698e21e7065353683eb047280d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:12:27 +0000

    Add package enovates-modbus/0.1.3/enovates_modbus-0.1.3-py3-none-any.whl

---
+++ packages/enovates-modbus/enovates_modbus-0.1.3-py3-none-any.whl/enovates_modbus/base.py
@@ -0,0 +1,227 @@
+"""
+Common base set of Modus register based types and the RegisterMap base class.
+"""
+
+import inspect
+import struct
+import typing
+from abc import ABCMeta, abstractmethod
+from functools import cached_property
+from typing import Annotated, ClassVar
+
+from pymodbus import ModbusException
+from pymodbus.client import AsyncModbusTcpClient
+
+
+class ModbusRegisterType[T](metaclass=ABCMeta):
+    """
+    Abstract base class for Modbus register type.
+    Used by the RegistryMap class to do type conversions.
+    """
+
+    def __init__(self, count: int):
+        self.count = count
+        assert self.count > 0, f"Invalid register count {count!r} for {self.__class__.__name__}."
+
+    @abstractmethod
+    def from_registers(self, registers: list[int]) -> T:
+        """
+        Pops all needed registers of the input list and returns the transformed value.
+        --> registers is mutated!
+        """
+        raise NotImplementedError
+
+
+class ModbusRegisterEnumType[T](ModbusRegisterType[T]):
+    """
+    Enum based Modbus register type.
+    """
+
+    # Unfortunately there is no nice (supported) way to get T at runtime,
+    # so we also have to pass the type to the constructor.
+    def __init__(self, enum: type[T], count: int = 1):
+        super().__init__(count)
+        self.enum = enum
+
+    def from_registers(self, registers: list[int]) -> T:
+        assert len(registers) >= self.count
+        return self.enum(registers.pop(0))
+
+
+class ModbusRegisterBoolType(ModbusRegisterType[bool]):
+    """
+    Boolean based Modbus register type.
+    0 = False, anything else = True.
+    """
+
+    def from_registers(self, registers: list[int]) -> bool:
+        assert len(registers) >= self.count
+        return registers.pop(0) != 0
+
+
+class ModbusRegisterStrType(ModbusRegisterType[str]):
+    """
+    String based Modbus register type.
+    The register count is double the nr of 8 bytes ASCII chars.
+    """
+
+    def from_registers(self, registers: list[int]) -> str:
+        assert len(registers) >= self.count
+        data = b"".join(struct.pack(">H", registers.pop(0)) for _ in range(self.count))
+        return data.decode("ascii", "replace").rstrip("\x00")
+
+
+class ModbusRegisterIntType(ModbusRegisterType[int]):
+    """
+    Integer based Modbus register type.
+    Big endian support only.
+    Signed = Two's compliment
+    """
+
+    def __init__(self, count: int, signed: bool):
+        super().__init__(count)
+        self.signed = signed
+
+    def from_registers(self, registers: list[int]) -> int:
+        assert len(registers) >= self.count
+        value = registers.pop(0)
+        for _ in range(self.count - 1):
+            value <<= 16
+            value |= registers.pop(0)
+        if self.signed and value >= 1 << (16 * self.count - 1):
+            value -= 1 << (16 * self.count)
+        return value
+
+
+MB_BOOL = Annotated[bool, ModbusRegisterBoolType(1)]
+MB_UINT16 = Annotated[int, ModbusRegisterIntType(1, False)]
+MB_INT16 = Annotated[int, ModbusRegisterIntType(1, True)]
+MB_UINT32 = Annotated[int, ModbusRegisterIntType(2, False)]
+MB_INT32 = Annotated[int, ModbusRegisterIntType(2, True)]
+
+
+class RegisterMap:
+    """
+    Represents a continues region of registers available via modbus.
+
+    Implement by extending, as a (frozen) dataclass.
+    Every field that is a ModbusRegisterType gets added to the kwargs in the constructor when using from_registers.
+    """
+
+    BASE_ADDRESS: ClassVar[int]
+    REGISTER_COUNT: ClassVar[int]
+    _REGISTER_MAP: ClassVar[dict[str, ModbusRegisterType]]
+
+    def __init_subclass__(cls, base_address: int, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls.BASE_ADDRESS = base_address
+        cls.REGISTER_COUNT = 0
+        cls._REGISTER_MAP = {}
+        for field, annotation in inspect.get_annotations(cls).items():
+            for a in typing.get_args(annotation):
+                if not isinstance(a, ModbusRegisterType):
+                    continue
+                cls.REGISTER_COUNT += a.count
+                cls._REGISTER_MAP[field] = a
+
+    @classmethod
+    def from_registers(cls, registers: list[int], /, *args, **kwargs) -> typing.Self:
+        assert len(registers) >= cls.REGISTER_COUNT, (
+            f"Not enough registers for {cls.__name__}. Expected {cls.REGISTER_COUNT} but got {len(registers)}."
+        )
+        assert cls._REGISTER_MAP.keys().isdisjoint(kwargs.keys()), "kwargs overlaps with register mapping."
+        for field, register_type in cls._REGISTER_MAP.items():
+            kwargs[field] = register_type.from_registers(registers)
+        return cls(*args, **kwargs)
+
+
+class EnoClient:
+    """
+    Base class for Modbus connection to Enovates EVSE over Modbus TCP
+
+    Implementation classes should extend, fill REGISTER_MAPS and provide fetch functions for every RegisterMap.
+    """
+
+    REGISTER_MAPS: ClassVar[tuple[type[RegisterMap], ...]]
+
+    def __init__(self, host: str, port: int = 502, device_id: int = 1):
+        self.host = host
+        self.port = port
+        self.device_id = device_id
+
+    @cached_property
+    def client(self) -> AsyncModbusTcpClient:
+        return AsyncModbusTcpClient(self.host, port=self.port, name=self.__class__.__qualname__, timeout=1, retries=0)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(host={self.host!r}, port={self.port!r}, device_id={self.device_id!r})"
+
+    async def __aenter__(self) -> typing.Self:
+        await self.ensure_connected()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        self.client.close()
+
+    async def ensure_connected(self):
+        if not self.client.connected:
+            try:
+                r = await self.client.connect()
+            except Exception as e:
+                raise ConnectionError(f"Failed to connect to {self.host}:{self.port}: {e}") from e
+            else:
+                if not r:
+                    raise ConnectionError(f"Failed to connect to {self.host}:{self.port}.")
+
+    async def read(self, address: int, count: int = 1) -> list[int]:
+        """
+        Read one or more (holding) registers.
+        """
+        await self.ensure_connected()
+        reply = await self.client.read_holding_registers(address=address, count=count, device_id=self.device_id)
+        if reply.isError():
+            raise ModbusException(f"Failed to read modbus registers. Got: {reply!r}")
+        return reply.registers
+
+    async def write(self, address: int, registers: list[int]):
+        """
+        Write one or more (holding) registers.
+        """
+        await self.ensure_connected()
+        reply = await self.client.write_registers(address=address, values=registers, device_id=self.device_id)
+        if reply.isError():
+            raise ModbusException(f"Failed to write modbus registers. Got: {reply!r}")
+        return reply.registers
+
+    async def write_single(self, address: int, data: int):
+        """
+        Write single (holding) register.
+        """
+        await self.ensure_connected()
+        reply = await self.client.write_register(address=address, value=data, device_id=self.device_id)
+        if reply.isError():
+            raise ModbusException(f"Failed to write modbus registers. Got: {reply!r}")
+        return reply.registers
+
+    async def fetch[T: RegisterMap](self, register_map: type[T]) -> T:
+        """
+        Get an entire register map in bulk.
+        """
+        registers = await self.read(register_map.BASE_ADDRESS, register_map.REGISTER_COUNT)
+        return register_map.from_registers(registers)
+
+    async def dump_all(self, file=None):
+        """
+        Dump all register maps to a file, or stdout if file is None.
+        Exceptions are logged but otherwise ignored.
+        """
+        print("Register map dump for ", self, ":", file=file, sep="")
+        for register_map in self.REGISTER_MAPS:
+            try:
+                rm = await self.fetch(register_map)
+            except KeyboardInterrupt:
+                raise
+            except (ModbusException, ConnectionError) as e:
+                print("\t", "ERROR\t", register_map.__name__, ": ", repr(e), file=file, sep="")
+            else:
+                print("\t", repr(rm), file=file, sep="")
---
+++ packages/enovates-modbus/enovates_modbus-0.1.3-py3-none-any.whl/enovates_modbus/eno_one.py
@@ -0,0 +1,218 @@
+"""
+Implementation of EnoONE register mappings and client class
+"""
+
+import dataclasses
+import enum
+import logging
+from typing import Annotated
+
+from .base import MB_BOOL, MB_INT16, MB_INT32, MB_UINT16, EnoClient, ModbusRegisterEnumType, ModbusRegisterStrType, RegisterMap
+
+LOGGER = logging.getLogger(__name__)
+
+
+@enum.verify(enum.UNIQUE, enum.CONTINUOUS)
+class LockState(enum.IntEnum):
+    UNLOCKED = 0
+    LOCKED = 1
+    NO_LOCK_PRESENT = 2
+
+
+MB_LOCK_STATE = Annotated[LockState, ModbusRegisterEnumType[LockState](LockState)]
+
+
+@enum.verify(enum.UNIQUE, enum.CONTINUOUS)
+class LEDColor(enum.IntEnum):
+    OFF = 0
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+    CYAN = 4
+    YELLOW = 5
+    PINK = 6
+    WHITE = 7
+    ORANGE = 8
+    PURPLE = 9
+
+
+MB_LED_COLOR = Annotated[LEDColor, ModbusRegisterEnumType[LEDColor](LEDColor)]
+
+
+@enum.verify(enum.UNIQUE, enum.CONTINUOUS)
+class Mode3State(enum.IntEnum):
+    A1 = 0
+    A2 = 1
+    B1 = 2
+    B2 = 3
+    C1 = 4
+    C2 = 5
+    D1 = 6
+    D2 = 7
+    E = 8
+    F = 9
+
+
+MB_MODE3_STATE = Annotated[Mode3State, ModbusRegisterEnumType[Mode3State](Mode3State)]
+
+
+@dataclasses.dataclass(frozen=True)
+class APIVersion(RegisterMap, base_address=0):
+    major: MB_UINT16
+    minor: MB_UINT16
+
+
+assert APIVersion.REGISTER_COUNT == 2
+
+
+@dataclasses.dataclass(frozen=True)
+class State(RegisterMap, base_address=50):
+    number_of_phases: MB_UINT16
+    max_amp_per_phase: MB_UINT16
+    ocpp_state: MB_BOOL
+    load_shedding_state: MB_BOOL
+    lock_state: MB_LOCK_STATE
+    contactor_state: MB_BOOL
+    led_color: MB_LED_COLOR
+
+
+assert State.REGISTER_COUNT == 7
+
+
+@dataclasses.dataclass(frozen=True)
+class Measurements(RegisterMap, base_address=200):
+    current_l1: MB_UINT16  # mA
+    current_l2: MB_UINT16  # mA
+    current_l3: MB_UINT16  # mA
+    voltage_l1: MB_INT16  # V
+    voltage_l2: MB_INT16  # V
+    voltage_l3: MB_INT16  # V
+    charger_active_power_total: MB_INT16  # W
+    charger_active_power_l1: MB_INT16  # W
+    charger_active_power_l2: MB_INT16  # W
+    charger_active_power_l3: MB_INT16  # W
+    installation_current_l1: MB_INT32  # mA
+    installation_current_l2: MB_INT32  # mA
+    installation_current_l3: MB_INT32  # mA
+    active_energy_import_total: MB_INT32  # Wh
+
+
+assert Measurements.REGISTER_COUNT == 18
+
+
+@dataclasses.dataclass(frozen=True)
+class Mode3Details(RegisterMap, base_address=300):
+    state_num: MB_MODE3_STATE
+    state_str: Annotated[str, ModbusRegisterStrType(2)]
+    pwm_amp: MB_INT16  # mA
+    pwm: MB_INT16  # ‰
+    pp: MB_INT16  # A
+    CP_pos: MB_INT16  # V
+    CP_neg: MB_INT16  # V
+
+
+assert Mode3Details.REGISTER_COUNT == 8
+
+
+@dataclasses.dataclass(frozen=True)
+class EMSLimit(RegisterMap, base_address=400):
+    """
+    Writable only if EMS-over-modbus is enabled in device settings.
+    Always readable.
+    """
+
+    ems_limit: MB_INT16  # mA
+
+
+assert EMSLimit.REGISTER_COUNT == 1
+
+
+@dataclasses.dataclass(frozen=True)
+class TransactionToken(RegisterMap, base_address=401):
+    """
+    Readable only if EMS-over-modbus is enabled in device settings.
+    """
+
+    transaction_token: Annotated[str, ModbusRegisterStrType(16)]
+
+
+assert TransactionToken.REGISTER_COUNT == 16
+
+
+@dataclasses.dataclass(frozen=True)
+class CurrentOffered(RegisterMap, base_address=417):
+    active_current_offered: MB_UINT16  # mA
+
+
+assert CurrentOffered.REGISTER_COUNT == 1
+
+
+@dataclasses.dataclass(frozen=True)
+class Diagnostics(RegisterMap, base_address=5000):
+    manufacturer: Annotated[str, ModbusRegisterStrType(16)]  # Enovates NV
+    vendor_id: Annotated[str, ModbusRegisterStrType(16)]
+    serial_nr: Annotated[str, ModbusRegisterStrType(16)]
+    model_id: Annotated[str, ModbusRegisterStrType(16)]
+    firmware_version: Annotated[str, ModbusRegisterStrType(16)]
+
+
+assert Diagnostics.REGISTER_COUNT == 80
+
+
+class EnoOneClient(EnoClient):
+    REGISTER_MAPS = (
+        APIVersion,
+        State,
+        Measurements,
+        Mode3Details,
+        EMSLimit,
+        TransactionToken,
+        CurrentOffered,
+        Diagnostics,
+    )
+
+    async def check_version(self) -> bool:
+        version = await self.get_api_version()
+        return version.major == 1 and version.minor >= 2
+
+    async def get_api_version(self) -> APIVersion:
+        return await self.fetch(APIVersion)
+
+    async def get_state(self) -> State:
+        return await self.fetch(State)
+
+    async def get_measurements(self) -> Measurements:
+        return await self.fetch(Measurements)
+
+    async def get_mode3_details(self) -> Mode3Details:
+        return await self.fetch(Mode3Details)
+
+    async def get_transaction_token(self) -> TransactionToken:
+        return await self.fetch(TransactionToken)
+
+    async def get_diagnostics(self) -> Diagnostics:
+        return await self.fetch(Diagnostics)
+
+    # Functions below are
+    async def get_ems_limit(self) -> int:
+        """
+        Get EMS limit in mA.
+        """
+        value = (await self.read(EMSLimit.BASE_ADDRESS, 1))[0]
+        if value > 0x8000:
+            value -= 0x10000
+        return value
+
+    async def set_ems_limit(self, limit: int):
+        """
+        Set EMS limit in mA.
+        """
+        if limit < -0x7FFF or limit > 0x7FFF:
+            raise ValueError("EMS limit is out of range.")
+        return await self.write_single(EMSLimit.BASE_ADDRESS, limit)
+
+    async def get_current_offered(self) -> int:
+        """
+        Get current offered to the car in mA.
+        """
+        return (await self.fetch(CurrentOffered)).active_current_offered
