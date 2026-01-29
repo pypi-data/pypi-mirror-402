6d4e1b04ff18dc3012dda6a428c15aba4f4381c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:10:23 +0000

    Add package antspyx/0.6.2/antspyx-0.6.2-cp310-cp310-macosx_15_0_x86_64.whl

---
+++ packages/antspyx/antspyx-0.6.2-cp310-cp310-macosx_15_0_x86_64.whl/ants/__init__.py
@@ -0,0 +1,14 @@
+
+__version__ = '0.6.2'
+
+from .core import *
+from .label import *
+from .learn import *
+from .math import *
+from .ops import *
+from .plotting import *
+from .registration import *
+from .segmentation import *
+from .utils import *
+from .contrib import *
+from .deeplearn import *
