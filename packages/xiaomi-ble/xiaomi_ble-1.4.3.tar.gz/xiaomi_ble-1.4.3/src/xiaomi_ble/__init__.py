f31fd3a415f5160464e895b7ca9bd3259fb69687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:19:44 +0000

    Add package xiaomi-ble/1.4.3/xiaomi_ble-1.4.3.tar.gz

---
+++ packages/xiaomi-ble/xiaomi_ble-1.4.3.tar.gz/xiaomi_ble-1.4.3/src/xiaomi_ble/__init__.py
@@ -0,0 +1,54 @@
+"""Parser for Xiaomi BLE advertisements.
+
+This file is shamlessly copied from the following repository:
+https://github.com/Ernst79/bleparser/blob/c42ae922e1abed2720c7fac993777e1bd59c0c93/package/bleparser/Xiaomi.py
+
+MIT License applies.
+"""
+
+from __future__ import annotations
+
+from sensor_state_data import (
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .cloud import (
+    XiaomiCloudBLEDevice,
+    XiaomiCloudException,
+    XiaomiCloudInvalidAuthenticationException,
+    XiaomiCloudInvalidPasswordException,
+    XiaomiCloudInvalidUsernameException,
+    XiaomiCloudTokenFetch,
+    XiaomiCloudTwoFactorAuthenticationException,
+)
+from .devices import SLEEPY_DEVICE_MODELS
+from .parser import EncryptionScheme, XiaomiBluetoothDeviceData
+
+__version__ = "1.4.3"
+
+__all__ = [
+    "SLEEPY_DEVICE_MODELS",
+    "EncryptionScheme",
+    "XiaomiBluetoothDeviceData",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+    "XiaomiCloudBLEDevice",
+    "XiaomiCloudException",
+    "XiaomiCloudInvalidAuthenticationException",
+    "XiaomiCloudInvalidPasswordException",
+    "XiaomiCloudInvalidUsernameException",
+    "XiaomiCloudTokenFetch",
+    "XiaomiCloudTwoFactorAuthenticationException",
+]
