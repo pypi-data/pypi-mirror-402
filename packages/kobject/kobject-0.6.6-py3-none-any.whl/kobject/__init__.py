1185aa2c39bac62bc1f2d4cd3bd88a1b5d271644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:11:04 +0000

    Add package kobject/0.6.6/kobject-0.6.6-py3-none-any.whl

---
+++ packages/kobject/kobject-0.6.6-py3-none-any.whl/kobject/__init__.py
@@ -0,0 +1,39 @@
+"""
+Know your object a __init__ type validator
+"""
+
+from enum import Enum
+
+from kobject.validator import Kobject
+
+
+class EmptyType:
+    def __repr__(self):
+        return "<EMPTY>"
+
+    def __bool__(self):
+        return False
+
+
+Empty = EmptyType()
+
+Kobject.set_decoder_resolver(bool, lambda attr_type, value: value)
+Kobject.set_decoder_resolver(
+    float, lambda attr_type, value: float(value) if isinstance(value, int) else value
+)
+Kobject.set_decoder_resolver(int, lambda attr_type, value: value)
+Kobject.set_decoder_resolver(str, lambda attr_type, value: value)
+Kobject.set_decoder_resolver(
+    Kobject,
+    lambda attr_type, value: attr_type.from_dict(value)
+    if isinstance(value, dict)
+    else value,
+)
+Kobject.set_decoder_resolver(
+    Enum,
+    lambda attr_type, value: attr_type(value)
+    if any(value.__eq__(i.value) for i in attr_type)  # pylint: disable=C2801
+    else value,
+)
+
+__all__ = ["Kobject", "Empty"]
