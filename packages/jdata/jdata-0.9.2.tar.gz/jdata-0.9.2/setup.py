e830826c9afe5261c9c727315be7fa66bcc72cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:34:46 +0000

    Add package jdata/0.9.2/jdata-0.9.2.tar.gz

---
+++ packages/jdata/jdata-0.9.2.tar.gz/jdata-0.9.2/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name="jdata",
+    packages=["jdata"],
+    version="0.9.2",
+    license="Apache license 2.0",
+    description="JSON/binary JSON formats for exchanging Python and Numpy data",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Qianqian Fang",
+    author_email="fangqq@gmail.com",
+    maintainer="Qianqian Fang",
+    url="https://github.com/NeuroJSON/pyjdata",
+    keywords=[
+        "JSON",
+        "JData",
+        "UBJSON",
+        "BJData",
+        "OpenJData",
+        "NeuroJSON",
+        "JNIfTI",
+        "JMesh",
+        "Encoder",
+        "Decoder",
+    ],
+    platforms="any",
+    install_requires=["numpy>=1.8.0"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
