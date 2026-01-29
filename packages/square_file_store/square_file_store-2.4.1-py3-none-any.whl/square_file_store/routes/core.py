2495b3458435903705153df006425efc2335fcca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:28 +0000

    Add package square_file_store/2.4.1/square_file_store-2.4.1-py3-none-any.whl

---
+++ packages/square_file_store/square_file_store-2.4.1-py3-none-any.whl/square_file_store/routes/core.py
@@ -0,0 +1,112 @@
+from typing import Annotated, List, Optional
+from uuid import UUID
+
+from fastapi import APIRouter, Form, UploadFile, status
+from fastapi.exceptions import HTTPException
+from fastapi.params import Query
+from fastapi.responses import JSONResponse, FileResponse
+from square_commons import get_api_output_in_standard_format
+from square_commons.api_utils import StandardResponse
+
+from square_file_store.configuration import (
+    global_object_square_logger,
+)
+from square_file_store.messages import messages
+from square_file_store.pydantic_models.core import (
+    UploadFileV0Response,
+    DeleteFilesV0Response,
+)
+from square_file_store.utils.routes.core import (
+    util_upload_file_v0,
+    util_download_file_v0,
+    util_delete_files_v0,
+)
+
+router = APIRouter(
+    tags=["core"],
+)
+
+
+@router.post(
+    "/upload_file/v0",
+    status_code=status.HTTP_201_CREATED,
+    response_model=StandardResponse[UploadFileV0Response],
+)
+@global_object_square_logger.auto_logger()
+async def upload_file_v0(
+    file: UploadFile,
+    app_id: Annotated[
+        Optional[int],
+        Form(title="App id", description="Specify the app id"),
+    ] = None,
+    system_relative_path: Annotated[
+        str,
+        Form(
+            title="System relative path",
+            description="Specify the path using '/'. For e.g. home/user_document",
+        ),
+    ] = "others/misc",
+):
+    try:
+        return await util_upload_file_v0(
+            file=file, app_id=app_id, system_relative_path=system_relative_path
+        )
+    except HTTPException as he:
+        global_object_square_logger.logger.error(he, exc_info=True)
+        return JSONResponse(status_code=he.status_code, content=he.detail)
+    except Exception as e:
+        global_object_square_logger.logger.error(e, exc_info=True)
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_500"], log=str(e)
+        )
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
+        )
+
+
+@router.get(
+    "/download_file/v0",
+    status_code=status.HTTP_200_OK,
+    response_class=FileResponse,
+)
+@global_object_square_logger.auto_logger()
+async def download_file_v0(file_storage_token: UUID):
+    try:
+        return util_download_file_v0(
+            file_storage_token=file_storage_token,
+        )
+    except HTTPException as he:
+        global_object_square_logger.logger.error(he, exc_info=True)
+        return JSONResponse(status_code=he.status_code, content=he.detail)
+    except Exception as e:
+        global_object_square_logger.logger.error(e, exc_info=True)
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_500"], log=str(e)
+        )
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
+        )
+
+
+@router.delete(
+    "/delete_files/v0",
+    status_code=status.HTTP_200_OK,
+    response_model=StandardResponse[DeleteFilesV0Response],
+)
+@global_object_square_logger.auto_logger()
+async def delete_files_v0(file_storage_tokens: List[UUID] = Query()):
+    try:
+        return util_delete_files_v0(
+            file_storage_tokens=file_storage_tokens,
+        )
+    except HTTPException as he:
+        global_object_square_logger.logger.error(he, exc_info=True)
+        return JSONResponse(status_code=he.status_code, content=he.detail)
+    except Exception as e:
+        global_object_square_logger.logger.error(e, exc_info=True)
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_500"], log=str(e)
+        )
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
+        )
---
+++ packages/square_file_store/square_file_store-2.4.1-py3-none-any.whl/square_file_store/utils/helper.py
@@ -0,0 +1,87 @@
+from fastapi import status
+from fastapi.exceptions import HTTPException
+from square_commons import get_api_output_in_standard_format
+from square_database_helper import SquareDatabaseHelper, FiltersV0
+from square_database_helper.pydantic_models import FilterConditionsV0
+from square_database_structure.square import global_string_database_name
+from square_database_structure.square.file_storage import global_string_schema_name
+from square_database_structure.square.file_storage.tables import File
+
+from square_file_store.configuration import (
+    config_str_square_database_protocol,
+    config_str_square_database_ip,
+    config_int_square_database_port,
+    global_object_square_logger,
+)
+from square_file_store.messages import messages
+
+local_object_square_database_helper = SquareDatabaseHelper(
+    param_str_square_database_ip=config_str_square_database_ip,
+    param_str_square_database_protocol=config_str_square_database_protocol,
+    param_int_square_database_port=config_int_square_database_port,
+)
+
+
+@global_object_square_logger.auto_logger()
+def create_entry_in_file_store(
+    file_name_with_extension: str,
+    content_type: str,
+    system_file_name_with_extension: str,
+    file_storage_token: str,
+    app_id: int,
+    system_relative_path: str,
+):
+    try:
+
+        data = [
+            {
+                File.file_name_with_extension.name: file_name_with_extension,
+                File.file_content_type.name: content_type,
+                File.file_system_file_name_with_extension.name: system_file_name_with_extension,
+                File.file_system_relative_path.name: system_relative_path,
+                File.file_storage_token.name: file_storage_token,
+                File.app_id.name: app_id,
+            }
+        ]
+
+        response = local_object_square_database_helper.insert_rows_v0(
+            data=data,
+            database_name=global_string_database_name,
+            schema_name=global_string_schema_name,
+            table_name=File.__tablename__,
+            response_as_pydantic=True,
+        ).data.main
+
+        return response
+    except Exception as e:
+        raise e
+
+
+@global_object_square_logger.auto_logger()
+def get_file_row(file_storage_token):
+    try:
+        response = local_object_square_database_helper.get_rows_v0(
+            database_name=global_string_database_name,
+            schema_name=global_string_schema_name,
+            table_name=File.__tablename__,
+            filters=FiltersV0(
+                root={
+                    File.file_storage_token.name: FilterConditionsV0(
+                        eq=file_storage_token
+                    ),
+                }
+            ),
+            response_as_pydantic=True,
+        ).data.main
+        if len(response) != 1:
+            output_content = get_api_output_in_standard_format(
+                message=messages["GENERIC_400"],
+                log=f"incorrect file_storage_token: {file_storage_token}.",
+            )
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=output_content,
+            )
+        return response[0]
+    except Exception as e:
+        raise e
