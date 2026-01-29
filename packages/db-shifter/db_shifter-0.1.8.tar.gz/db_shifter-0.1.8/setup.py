3d73cd364193ce81ed3a804a96d7791507cb614e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:13:37 +0000

    Add package db-shifter/0.1.8/db_shifter-0.1.8.tar.gz

---
+++ packages/db-shifter/db_shifter-0.1.8.tar.gz/db_shifter-0.1.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="db-shifter",
+    version="0.1.8",
+    packages=find_packages(),
+    install_requires=[
+        "psycopg2-binary",
+    ],
+    entry_points={
+        "console_scripts": [
+            "db-shifter=db_shifter.__main__:main"
+        ]
+    },
+    author="superman",
+    author_email="goodnesskolapo@gmail.com",
+    description="A smart tool to sync missing rows between two PostgreSQL databases.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/goodness5/db-shifter",
+    project_urls={
+        "Changelog": "https://github.com/goodness5/db-shifter/blob/master/CHANGELOG.md",
+        "Source": "https://github.com/goodness5/db-shifter",
+        "Tracker": "https://github.com/goodness5/db-shifter/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
