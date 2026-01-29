8ec2e490983a542ba31ee91aa484e8bf0147a252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:09:27 +0000

    Add package thothai-cli-core/1.0.1/thothai_cli_core-1.0.1-py3-none-any.whl

---
+++ packages/thothai-cli-core/thothai_cli_core-1.0.1-py3-none-any.whl/thothai_cli_core/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2025 Marco Pancotti
+# This file is part of ThothAI and is released under the Apache 2.0.
+# See the LICENSE.md file in the project root for full license information.
+
+__version__ = "1.0.0"
