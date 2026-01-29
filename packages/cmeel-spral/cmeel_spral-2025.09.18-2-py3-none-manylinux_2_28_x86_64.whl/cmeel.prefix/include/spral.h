66b52931f1a27b46f28192267854ca1ac102ca12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:14:53 +0000

    Add package cmeel-spral/2025.9.18/cmeel_spral-2025.09.18-2-py3-none-manylinux_2_28_x86_64.whl

---
+++ packages/cmeel-spral/cmeel_spral-2025.09.18-2-py3-none-manylinux_2_28_x86_64.whl/cmeel.prefix/include/spral.h
@@ -0,0 +1,17 @@
+#ifndef SPRAL_H
+#define SPRAL_H
+
+#define SPRAL_VERSION "2025.09.18"
+
+#include "spral_lsmr.h"
+#include "spral_matrix_util.h"
+#include "spral_random.h"
+#include "spral_random_matrix.h"
+#include "spral_rutherford_boeing.h"
+#include "spral_scaling.h"
+#include "spral_ssids.h"
+#include "spral_ssmfe.h"
+
+void get_spral_version(char version[11]);
+
+#endif // SPRAL_H
