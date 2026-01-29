57a677d377dd22294039f381d7c04687b9226698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:35:14 +0000

    Add package telegram-async/0.1.2.1/telegram_async-0.1.2.1.tar.gz

---
+++ packages/telegram-async/telegram_async-0.1.2.1.tar.gz/telegram_async-0.1.2.1/telegram_async/roles.py
@@ -0,0 +1,18 @@
+from enum import IntEnum
+class Roles:
+    ADMIN = "admin"
+    MODERATOR = "moderator"
+    USER = "user"
+    GUEST = "guest"
+
+    def __init__(self):
+        self.user_roles = {}
+
+    def set_role(self, user_id: int, role: str):
+        self.user_roles[user_id] = role
+
+    def get_role(self, user_id: int):
+        return self.user_roles.get(user_id, self.USER)
+
+    def get_role(self, user_id: int):
+        return self.user_roles.get(user_id, self.USER)
