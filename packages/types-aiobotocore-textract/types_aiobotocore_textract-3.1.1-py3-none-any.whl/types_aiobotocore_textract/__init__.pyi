405452b156a601fa17baad21d8b3b98fec157016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:51 +0000

    Add package types-aiobotocore-textract/3.1.1/types_aiobotocore_textract-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-textract/types_aiobotocore_textract-3.1.1-py3-none-any.whl/types_aiobotocore_textract/__init__.pyi
@@ -0,0 +1,35 @@
+"""
+Main interface for textract service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_textract/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_textract import (
+        Client,
+        ListAdapterVersionsPaginator,
+        ListAdaptersPaginator,
+        TextractClient,
+    )
+
+    session = get_session()
+    async with session.create_client("textract") as client:
+        client: TextractClient
+        ...
+
+
+    list_adapter_versions_paginator: ListAdapterVersionsPaginator = client.get_paginator("list_adapter_versions")
+    list_adapters_paginator: ListAdaptersPaginator = client.get_paginator("list_adapters")
+    ```
+"""
+
+from .client import TextractClient
+from .paginator import ListAdaptersPaginator, ListAdapterVersionsPaginator
+
+Client = TextractClient
+
+__all__ = ("Client", "ListAdapterVersionsPaginator", "ListAdaptersPaginator", "TextractClient")
---
+++ packages/types-aiobotocore-textract/types_aiobotocore_textract-3.1.1-py3-none-any.whl/types_aiobotocore_textract/paginator.py
@@ -0,0 +1,90 @@
+"""
+Type annotations for textract service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_textract/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_textract.client import TextractClient
+    from types_aiobotocore_textract.paginator import (
+        ListAdapterVersionsPaginator,
+        ListAdaptersPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("textract") as client:
+        client: TextractClient
+
+        list_adapter_versions_paginator: ListAdapterVersionsPaginator = client.get_paginator("list_adapter_versions")
+        list_adapters_paginator: ListAdaptersPaginator = client.get_paginator("list_adapters")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    ListAdaptersRequestPaginateTypeDef,
+    ListAdaptersResponseTypeDef,
+    ListAdapterVersionsRequestPaginateTypeDef,
+    ListAdapterVersionsResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ListAdapterVersionsPaginator", "ListAdaptersPaginator")
+
+
+if TYPE_CHECKING:
+    _ListAdapterVersionsPaginatorBase = AioPaginator[ListAdapterVersionsResponseTypeDef]
+else:
+    _ListAdapterVersionsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListAdapterVersionsPaginator(_ListAdapterVersionsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/textract/paginator/ListAdapterVersions.html#Textract.Paginator.ListAdapterVersions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_textract/paginators/#listadapterversionspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListAdapterVersionsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListAdapterVersionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/textract/paginator/ListAdapterVersions.html#Textract.Paginator.ListAdapterVersions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_textract/paginators/#listadapterversionspaginator)
+        """
+
+
+if TYPE_CHECKING:
+    _ListAdaptersPaginatorBase = AioPaginator[ListAdaptersResponseTypeDef]
+else:
+    _ListAdaptersPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListAdaptersPaginator(_ListAdaptersPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/textract/paginator/ListAdapters.html#Textract.Paginator.ListAdapters)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_textract/paginators/#listadapterspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListAdaptersRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListAdaptersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/textract/paginator/ListAdapters.html#Textract.Paginator.ListAdapters.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_textract/paginators/#listadapterspaginator)
+        """
