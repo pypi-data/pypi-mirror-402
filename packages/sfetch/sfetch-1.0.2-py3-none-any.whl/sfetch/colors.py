76fe859c06049d2a8a519d8d7d43bdf02ab055ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:30 +0000

    Add package sfetch/1.0.2/sfetch-1.0.2-py3-none-any.whl

---
+++ packages/sfetch/sfetch-1.0.2-py3-none-any.whl/sfetch/colors.py
@@ -0,0 +1,8 @@
+class colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    LIGHTGREEN = '\033[93m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
