67b7982c842fa8d288e7c0d149165278144cd949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:08:18 +0000

    Add package fastembed-bio/0.1.0/fastembed_bio-0.1.0.tar.gz

---
+++ packages/fastembed-bio/fastembed_bio-0.1.0.tar.gz/fastembed_bio-0.1.0/fastembed/__init__.py
@@ -0,0 +1,24 @@
+import importlib.metadata
+
+from fastembed.bio import ProteinEmbedding
+from fastembed.image import ImageEmbedding
+from fastembed.late_interaction import LateInteractionTextEmbedding
+from fastembed.late_interaction_multimodal import LateInteractionMultimodalEmbedding
+from fastembed.sparse import SparseEmbedding, SparseTextEmbedding
+from fastembed.text import TextEmbedding
+
+try:
+    version = importlib.metadata.version("fastembed")
+except importlib.metadata.PackageNotFoundError as _:
+    version = importlib.metadata.version("fastembed-gpu")
+
+__version__ = version
+__all__ = [
+    "TextEmbedding",
+    "SparseTextEmbedding",
+    "SparseEmbedding",
+    "ImageEmbedding",
+    "LateInteractionTextEmbedding",
+    "LateInteractionMultimodalEmbedding",
+    "ProteinEmbedding",
+]
