0c27738e18ceb045ef41d89cbe1a4402c6931a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:18:13 +0000

    Add package phenolrs/0.5.11/phenolrs-0.5.11-cp312-cp312-macosx_10_12_x86_64.whl

---
+++ packages/phenolrs/phenolrs-0.5.11-cp312-cp312-macosx_10_12_x86_64.whl/phenolrs/__init__.py
@@ -0,0 +1,5 @@
+from .phenolrs import *  # noqa: F403
+
+__doc__ = phenolrs.__doc__  # type: ignore[name-defined]  # noqa: F405
+if hasattr(phenolrs, "__all__"):  # type: ignore[name-defined]  # noqa: F405
+    __all__ = phenolrs.__all__  # type: ignore[name-defined]  # noqa: F405
