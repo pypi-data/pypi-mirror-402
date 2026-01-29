709d7a49de089a4f8417242ffa44c3252eee7015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:14 +0000

    Add package Bharat-sm-data-avinash/4.1.0/bharat_sm_data_avinash-4.1.0.tar.gz

---
+++ packages/Bharat-sm-data-avinash/bharat_sm_data_avinash-4.1.0.tar.gz/bharat_sm_data_avinash-4.1.0/setup.py
@@ -0,0 +1,49 @@
+from setuptools import find_packages, setup
+
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="Bharat_sm_data_avinash",
+    version="4.1.0",
+    description="Bharat SM Data stands for Bharat(India) Stock Market Data."
+                "\nIt is a Bharat (India) Stock Market Data Fetch Library for all kind of data, Derivatives, Equity "
+                "(both Technical and Fundamentals), Commodities, Currencies from NSE, BSE, MoneyControl and "
+                "Tickertape websites. Now includes NSE Charting API v2 with segment filtering and derivatives support.",
+    package_dir={"": "Bharat_sm_data"},
+    packages=find_packages(where="Bharat_sm_data"),
+    exclude_package_data={
+        '': ['Base/*'],  # Exclude specific file types from the root directory
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avinashsahu/Bharat-SM-Data-Avinash",
+    author="Avinash Sahu (Fork of Sampad Hegde's work)",
+    author_email="avinashsahu@example.com",
+    license="Apache License 2.0",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'Brotli>=1.0.0',
+        'requests>=2.31.0',
+        'pandas>=2.0.3',
+        'pydash>=7.0.6',
+        'beautifulsoup4>=4.12.2',
+        'numpy>=1.25.2',
+        'html5lib>=1.1',
+        'lxml>=4.9.3'
+    ],
+
+    extras_require={
+        "dev": ["twine>=4.0.2"],
+    },
+    python_requires=">=3.7",
+    project_urls={
+        "Documentation": "https://github.com/avinashsahu/Bharat-SM-Data-Avinash",
+        "Original Project": "https://github.com/Sampad-Hegde/Bharat-SM-Data"
+    },
+)
