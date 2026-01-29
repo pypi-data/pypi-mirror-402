c62986bf4a7e3273303413b66af4181d13cad73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:18:33 +0000

    Add package wxPython-zombie/3.1.5.9/wxpython_zombie-3.1.5.9-cp314-cp314-win_amd64.whl

---
+++ packages/wxPython-zombie/wxpython_zombie-3.1.5.9-cp314-cp314-win_amd64.whl/wx/__version__.py
@@ -0,0 +1,9 @@
+# This file was generated from wxPython's pkg_info.
+
+VERSION_STRING    = '3.1.5.9'
+MAJOR_VERSION     = 3
+MINOR_VERSION     = 1
+RELEASE_NUMBER    = 5
+BUILD_TYPE        = 'release'
+
+VERSION = (MAJOR_VERSION, MINOR_VERSION, RELEASE_NUMBER, '9')
