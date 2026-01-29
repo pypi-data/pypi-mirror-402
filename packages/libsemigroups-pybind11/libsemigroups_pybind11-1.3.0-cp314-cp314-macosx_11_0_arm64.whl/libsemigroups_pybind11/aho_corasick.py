8636d4dc0511fe24cd38f31a9f07e578b2828943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 09:14:45 +0000

    Add package libsemigroups-pybind11/1.3.0/libsemigroups_pybind11-1.3.0-cp314-cp314-macosx_11_0_arm64.whl

---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-1.3.0-cp314-cp314-macosx_11_0_arm64.whl/libsemigroups_pybind11/aho_corasick.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2024, Joseph Edwards
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+"""This module contains various helper functions for the class :any:`AhoCorasick`.
+
+These functions could be functions of :any:`AhoCorasick` but they only use
+public member functions of :any:`AhoCorasick`, and so they are declared
+as free functions instead.
+"""
+
+from _libsemigroups_pybind11 import (  # pylint: disable=unused-import
+    aho_corasick_add_word as add_word,
+    aho_corasick_dot as dot,
+    aho_corasick_rm_word as rm_word,
+    aho_corasick_traverse_word as traverse_word,
+)
