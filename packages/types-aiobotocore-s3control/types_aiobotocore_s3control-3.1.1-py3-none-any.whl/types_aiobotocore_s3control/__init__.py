422eab9dee5a0a3848db6b67af80a4b0440773ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:45 +0000

    Add package types-aiobotocore-s3control/3.1.1/types_aiobotocore_s3control-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-s3control/types_aiobotocore_s3control-3.1.1-py3-none-any.whl/types_aiobotocore_s3control/__init__.py
@@ -0,0 +1,48 @@
+"""
+Main interface for s3control service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_s3control/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_s3control import (
+        Client,
+        ListAccessPointsForDirectoryBucketsPaginator,
+        ListAccessPointsForObjectLambdaPaginator,
+        ListCallerAccessGrantsPaginator,
+        S3ControlClient,
+    )
+
+    session = get_session()
+    async with session.create_client("s3control") as client:
+        client: S3ControlClient
+        ...
+
+
+    list_access_points_for_directory_buckets_paginator: ListAccessPointsForDirectoryBucketsPaginator = client.get_paginator("list_access_points_for_directory_buckets")
+    list_access_points_for_object_lambda_paginator: ListAccessPointsForObjectLambdaPaginator = client.get_paginator("list_access_points_for_object_lambda")
+    list_caller_access_grants_paginator: ListCallerAccessGrantsPaginator = client.get_paginator("list_caller_access_grants")
+    ```
+"""
+
+from .client import S3ControlClient
+from .paginator import (
+    ListAccessPointsForDirectoryBucketsPaginator,
+    ListAccessPointsForObjectLambdaPaginator,
+    ListCallerAccessGrantsPaginator,
+)
+
+Client = S3ControlClient
+
+
+__all__ = (
+    "Client",
+    "ListAccessPointsForDirectoryBucketsPaginator",
+    "ListAccessPointsForObjectLambdaPaginator",
+    "ListCallerAccessGrantsPaginator",
+    "S3ControlClient",
+)
