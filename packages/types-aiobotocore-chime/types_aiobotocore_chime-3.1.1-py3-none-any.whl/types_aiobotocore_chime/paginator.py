9afac003ed2e37b612e2ff909dc23848f3bafc90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:55 +0000

    Add package types-aiobotocore-chime/3.1.1/types_aiobotocore_chime-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-chime/types_aiobotocore_chime-3.1.1-py3-none-any.whl/types_aiobotocore_chime/paginator.py
@@ -0,0 +1,90 @@
+"""
+Type annotations for chime service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_chime/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_chime.client import ChimeClient
+    from types_aiobotocore_chime.paginator import (
+        ListAccountsPaginator,
+        ListUsersPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("chime") as client:
+        client: ChimeClient
+
+        list_accounts_paginator: ListAccountsPaginator = client.get_paginator("list_accounts")
+        list_users_paginator: ListUsersPaginator = client.get_paginator("list_users")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import (
+    ListAccountsRequestPaginateTypeDef,
+    ListAccountsResponseTypeDef,
+    ListUsersRequestPaginateTypeDef,
+    ListUsersResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ListAccountsPaginator", "ListUsersPaginator")
+
+
+if TYPE_CHECKING:
+    _ListAccountsPaginatorBase = AioPaginator[ListAccountsResponseTypeDef]
+else:
+    _ListAccountsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListAccountsPaginator(_ListAccountsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/chime/paginator/ListAccounts.html#Chime.Paginator.ListAccounts)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_chime/paginators/#listaccountspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListAccountsRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListAccountsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/chime/paginator/ListAccounts.html#Chime.Paginator.ListAccounts.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_chime/paginators/#listaccountspaginator)
+        """
+
+
+if TYPE_CHECKING:
+    _ListUsersPaginatorBase = AioPaginator[ListUsersResponseTypeDef]
+else:
+    _ListUsersPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListUsersPaginator(_ListUsersPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/chime/paginator/ListUsers.html#Chime.Paginator.ListUsers)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_chime/paginators/#listuserspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListUsersRequestPaginateTypeDef]
+    ) -> AioPageIterator[ListUsersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/chime/paginator/ListUsers.html#Chime.Paginator.ListUsers.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_chime/paginators/#listuserspaginator)
+        """
