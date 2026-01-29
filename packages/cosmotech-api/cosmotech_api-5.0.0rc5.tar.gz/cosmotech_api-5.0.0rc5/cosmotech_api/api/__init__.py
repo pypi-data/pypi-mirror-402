65a23a30a55e53fba3048c65c158801799457e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:15:47 +0000

    Add package cosmotech-api/5.0.0rc5/cosmotech_api-5.0.0rc5.tar.gz

---
+++ packages/cosmotech-api/cosmotech_api-5.0.0rc5.tar.gz/cosmotech_api-5.0.0rc5/cosmotech_api/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from cosmotech_api.api.dataset_api import DatasetApi
+from cosmotech_api.api.meta_api import MetaApi
+from cosmotech_api.api.organization_api import OrganizationApi
+from cosmotech_api.api.run_api import RunApi
+from cosmotech_api.api.runner_api import RunnerApi
+from cosmotech_api.api.solution_api import SolutionApi
+from cosmotech_api.api.workspace_api import WorkspaceApi
+
