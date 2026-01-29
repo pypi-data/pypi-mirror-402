543aedcbb713911ff457ea10a6b243af04df9232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 13:27:24 +0000

    Add package ocicoeaidpml/0.1.4/ocicoeaidpml-0.1.4-py3-none-any.whl

---
+++ packages/ocicoeaidpml/ocicoeaidpml-0.1.4-py3-none-any.whl/ocicoeaidpml/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.1.0"
+
+from .core import run_schema_analysis
+from .core import generate_spark_ml_code          # ← add this line
+
+__all__ = [
+    "run_schema_analysis",
+    "generate_spark_ml_code"                     # ← add this too (good practice)
+]
+# pypi-AgEIcHlwaS5vcmcCJDEyYzg3ZDc3LWEwZmItNGY4My1iM2E2LWZlZmI1ZDFmNmZjNgACFFsxLFsib2NpY29lYWlkcG1sIl1dAAIsWzIsWyJjNzM4NTNkMC1jYzcwLTQxNGQtOWJkYS1mY2U4ZTc4MjJmZmYiXV0AAAYgOzDTP-3DLMdQ5ogzFa72HOC-9Rye8WfXX8WfL0ZLfvw
+
+# TEST
+
+# pypi-AgENdGVzdC5weXBpLm9yZwIkYTQxNTgxODktMjg3My00NDA1LWJiYzgtZmYzMjRlMWQ5NzFkAAIqWzMsIjUwNzRlZTY5LTRiMTctNGVjZS1hNWFmLWJkMjc2OWRhYWU0NiJdAAAGIBQiWzyV0qMw9Giv1cygoBxd9Ipc3Vj_QzPl0wkXICS3
