a8b0960544e058abae5c3e1bf0b79fbc9955cab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:11:32 +0000

    Add package python-library-template-secretary/0.1.0a2/python_library_template_secretary-0.1.0a2.tar.gz

---
+++ packages/python-library-template-secretary/python_library_template_secretary-0.1.0a2.tar.gz/python_library_template_secretary-0.1.0a2/setup.py
@@ -0,0 +1,16 @@
+# this is the setup.py file for the project
+from setuptools import setup, find_packages
+
+setup(
+    name='python_library_template_secretary',
+    version='0.1.0a2',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],  # list your project dependencies here
+    entry_points={
+        'console_scripts': [
+            'test_function=python_library_template_secretary:test_function',
+        ],
+    },
+)
