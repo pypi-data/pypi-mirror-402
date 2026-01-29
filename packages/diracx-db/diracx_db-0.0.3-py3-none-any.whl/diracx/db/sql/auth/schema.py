05f0150d157ecc2a032f395d10078978dc17a97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 13:26:57 +0000

    Add package diracx-db/0.0.3/diracx_db-0.0.3-py3-none-any.whl

---
+++ packages/diracx-db/diracx_db-0.0.3-py3-none-any.whl/diracx/db/sql/auth/schema.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+from enum import Enum, auto
+
+from sqlalchemy import (
+    JSON,
+    Index,
+    String,
+    Uuid,
+)
+from sqlalchemy.orm import declarative_base
+
+from diracx.db.sql.utils import (
+    Column,
+    DateNowColumn,
+    EnumColumn,
+    NullColumn,
+)
+
+USER_CODE_LENGTH = 8
+
+Base = declarative_base()
+
+
+class FlowStatus(Enum):
+    """The normal flow is
+    PENDING -> READY -> DONE
+    Pending is upon insertion
+    Ready/Error is set in response to IdP
+    Done means the user has been issued the dirac token.
+    """
+
+    # The flow is ongoing
+    PENDING = auto()
+    # The user has been authenticated
+    # and we are ready to issue a token
+    READY = auto()
+    # The token has been minted,
+    # the user can no longer login with this flow
+    DONE = auto()
+
+    # Something went wrong.
+    # Most likely the user did not login in the
+    # external IdP
+    ERROR = auto()
+
+
+class DeviceFlows(Base):
+    __tablename__ = "DeviceFlows"
+    user_code = Column("UserCode", String(USER_CODE_LENGTH), primary_key=True)
+    status = EnumColumn("Status", FlowStatus, server_default=FlowStatus.PENDING.name)
+    creation_time = DateNowColumn("CreationTime")
+    client_id = Column("ClientID", String(255))
+    scope = Column("Scope", String(1024))
+    device_code = Column("DeviceCode", String(128), unique=True)  # Should be a hash
+    id_token = NullColumn("IDToken", JSON())
+
+
+class AuthorizationFlows(Base):
+    __tablename__ = "AuthorizationFlows"
+    uuid = Column("UUID", Uuid(as_uuid=False), primary_key=True)
+    status = EnumColumn("Status", FlowStatus, server_default=FlowStatus.PENDING.name)
+    client_id = Column("ClientID", String(255))
+    creation_time = DateNowColumn("CreationTime")
+    scope = Column("Scope", String(1024))
+    code_challenge = Column("CodeChallenge", String(255))
+    code_challenge_method = Column("CodeChallengeMethod", String(8))
+    redirect_uri = Column("RedirectURI", String(255))
+    code = NullColumn("Code", String(255))  # Should be a hash
+    id_token = NullColumn("IDToken", JSON())
+
+
+class RefreshTokenStatus(Enum):
+    """The normal flow is
+    CREATED -> REVOKED.
+
+    Note1: There is no EXPIRED status as it can be calculated from a creation time
+    Note2: As part of the refresh token rotation mechanism, the revoked token should be retained
+    """
+
+    # The token has been created
+    # It does not indicate whether the token is valid or not
+    CREATED = auto()
+
+    # The token has been revoked
+    REVOKED = auto()
+
+
+class RefreshTokens(Base):
+    """Store attributes bound to a refresh token, as well as specific user attributes
+    that might be then used to generate access tokens.
+    """
+
+    __tablename__ = "RefreshTokens"
+    # Refresh token attributes
+    jti = Column("JTI", Uuid(as_uuid=False), primary_key=True)
+    status = EnumColumn(
+        "Status", RefreshTokenStatus, server_default=RefreshTokenStatus.CREATED.name
+    )
+    scope = Column("Scope", String(1024))
+
+    # User attributes bound to the refresh token
+    sub = Column("Sub", String(256), index=True)
+
+    __table_args__ = (Index("index_status_sub", status, sub),)
---
+++ packages/diracx-db/diracx_db-0.0.3-py3-none-any.whl/diracx/db/sql/task_queue/db.py
@@ -0,0 +1,180 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from sqlalchemy import delete, func, select, update
+
+if TYPE_CHECKING:
+    pass
+
+from ..utils import BaseSQLDB
+from .schema import (
+    BannedSitesQueue,
+    GridCEsQueue,
+    JobsQueue,
+    JobTypesQueue,
+    PlatformsQueue,
+    SitesQueue,
+    TagsQueue,
+    TaskQueueDBBase,
+    TaskQueues,
+)
+
+
+class TaskQueueDB(BaseSQLDB):
+    metadata = TaskQueueDBBase.metadata
+
+    async def get_tq_infos_for_jobs(
+        self, job_ids: list[int]
+    ) -> set[tuple[int, str, str, str]]:
+        """Get the task queue info for given jobs."""
+        stmt = (
+            select(
+                TaskQueues.TQId, TaskQueues.Owner, TaskQueues.OwnerGroup, TaskQueues.VO
+            )
+            .join(JobsQueue, TaskQueues.TQId == JobsQueue.TQId)
+            .where(JobsQueue.JobId.in_(job_ids))
+        )
+        return set(
+            (int(row[0]), str(row[1]), str(row[2]), str(row[3]))
+            for row in (await self.conn.execute(stmt)).all()
+        )
+
+    async def get_owner_for_task_queue(self, tq_id: int) -> dict[str, str]:
+        """Get the owner and owner group for a task queue."""
+        stmt = select(TaskQueues.Owner, TaskQueues.OwnerGroup, TaskQueues.VO).where(
+            TaskQueues.TQId == tq_id
+        )
+        return dict((await self.conn.execute(stmt)).one()._mapping)
+
+    async def get_task_queue_owners_by_group(self, group: str) -> dict[str, int]:
+        """Get the owners for a task queue and group."""
+        stmt = (
+            select(TaskQueues.Owner, func.count(TaskQueues.Owner))
+            .where(TaskQueues.OwnerGroup == group)
+            .group_by(TaskQueues.Owner)
+        )
+        rows = await self.conn.execute(stmt)
+        # Get owners in this group and the amount of times they appear
+        # TODO: I guess the rows are already a list of tuples
+        # maybe refactor
+        return {r[0]: r[1] for r in rows if r}
+
+    async def get_task_queue_priorities(
+        self, group: str, owner: str | None = None
+    ) -> dict[int, float]:
+        """Get the priorities for a list of task queues."""
+        stmt = (
+            select(
+                TaskQueues.TQId,
+                func.sum(JobsQueue.RealPriority) / func.count(JobsQueue.RealPriority),
+            )
+            .join(JobsQueue, TaskQueues.TQId == JobsQueue.TQId)
+            .where(TaskQueues.OwnerGroup == group)
+            .group_by(TaskQueues.TQId)
+        )
+        if owner:
+            stmt = stmt.where(TaskQueues.Owner == owner)
+        rows = await self.conn.execute(stmt)
+        return {tq_id: priority for tq_id, priority in rows}
+
+    async def remove_jobs(self, job_ids: list[int]):
+        """Remove jobs from the task queues."""
+        stmt = delete(JobsQueue).where(JobsQueue.JobId.in_(job_ids))
+        await self.conn.execute(stmt)
+
+    async def is_task_queue_empty(self, tq_id: int) -> bool:
+        """Check if a task queue is empty."""
+        stmt = (
+            select(TaskQueues.TQId)
+            .where(TaskQueues.Enabled >= 1)
+            .where(TaskQueues.TQId == tq_id)
+            .where(~TaskQueues.TQId.in_(select(JobsQueue.TQId)))
+        )
+        rows = await self.conn.execute(stmt)
+        return not rows.rowcount
+
+    async def delete_task_queue(
+        self,
+        tq_id: int,
+    ):
+        """Delete a task queue."""
+        # Deleting the task queue (the other tables will be deleted in cascade)
+        stmt = delete(TaskQueues).where(TaskQueues.TQId == tq_id)
+        await self.conn.execute(stmt)
+
+    async def set_priorities_for_entity(
+        self,
+        tq_ids: list[int],
+        priority: float,
+    ):
+        """Set the priority for a user/userGroup combo given a split share."""
+        update_stmt = (
+            update(TaskQueues)
+            .where(TaskQueues.TQId.in_(tq_ids))
+            .values(Priority=priority)
+        )
+        await self.conn.execute(update_stmt)
+
+    async def retrieve_task_queues(self, tq_id_list=None):
+        """Get all the task queues."""
+        if tq_id_list is not None and not tq_id_list:
+            # Empty list => Fast-track no matches
+            return {}
+
+        stmt = (
+            select(
+                TaskQueues.TQId,
+                TaskQueues.Priority,
+                func.count(JobsQueue.TQId).label("Jobs"),
+                TaskQueues.Owner,
+                TaskQueues.OwnerGroup,
+                TaskQueues.VO,
+                TaskQueues.CPUTime,
+            )
+            .join(JobsQueue, TaskQueues.TQId == JobsQueue.TQId)
+            .join(SitesQueue, TaskQueues.TQId == SitesQueue.TQId)
+            .join(GridCEsQueue, TaskQueues.TQId == GridCEsQueue.TQId)
+            .group_by(
+                TaskQueues.TQId,
+                TaskQueues.Priority,
+                TaskQueues.Owner,
+                TaskQueues.OwnerGroup,
+                TaskQueues.VO,
+                TaskQueues.CPUTime,
+            )
+        )
+        if tq_id_list is not None:
+            stmt = stmt.where(TaskQueues.TQId.in_(tq_id_list))
+
+        tq_data: dict[int, dict[str, list[str]]] = dict(
+            dict(row._mapping) for row in await self.conn.execute(stmt)
+        )
+        # TODO: the line above should be equivalent to the following commented code, check this is the case
+        # for record in rows:
+        #     tqId = record[0]
+        #     tqData[tqId] = {
+        #         "Priority": record[1],
+        #         "Jobs": record[2],
+        #         "Owner": record[3],
+        #         "OwnerGroup": record[4],
+        #         "VO": record[5],
+        #         "CPUTime": record[6],
+        #     }
+
+        for tq_id in tq_data:
+            # TODO: maybe factorize this handy tuple list
+            for table, field in {
+                (SitesQueue, "Sites"),
+                (GridCEsQueue, "GridCEs"),
+                (BannedSitesQueue, "BannedSites"),
+                (PlatformsQueue, "Platforms"),
+                (JobTypesQueue, "JobTypes"),
+                (TagsQueue, "Tags"),
+            }:
+                stmt = select(table.Value).where(table.TQId == tq_id)
+                tq_data[tq_id][field] = list(
+                    row[0] for row in await self.conn.execute(stmt)
+                )
+
+        return tq_data
