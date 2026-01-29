29efbe9ae75484bb360107f6ad80333df7ac836f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:16:22 +0000

    Add package endf-parserpy/0.15.0/endf_parserpy-0.15.0-cp313-cp313-musllinux_1_2_aarch64.whl

---
+++ packages/endf-parserpy/endf_parserpy-0.15.0-cp313-cp313-musllinux_1_2_aarch64.whl/endf_parserpy/compiler/cpp_templates/endf_float_cpp.hpp
@@ -0,0 +1,141 @@
+#ifndef CPP_ENDF_FLOAT_HPP
+#define CPP_ENDF_FLOAT_HPP
+
+
+#include <pybind11/pybind11.h>
+#include <iostream>
+#include <string>
+
+
+namespace py = pybind11;
+
+
+#define DOUBLE_TYPE EndfFloatCpp
+
+
+// This class must be in sync with the
+// Python class EndfFloat class in endf_parserpy.utils.math_utils
+
+class EndfFloatCpp {
+public:
+    EndfFloatCpp()
+        : _value(0.0), _orig_str("") {}
+
+    EndfFloatCpp(double value)
+        : _value(value), _orig_str("") {}
+
+    EndfFloatCpp(double value, const std::string& orig_str)
+        : _value(value), _orig_str(orig_str) {}
+
+    EndfFloatCpp(const EndfFloatCpp& other)
+        : _value(other._value), _orig_str(other._orig_str) {}
+
+    std::string get_original_string() const {
+        return _orig_str; }
+    bool operator==(const EndfFloatCpp& other) const {
+        return static_cast<double>(_value) == static_cast<double>(other._value);
+    }
+
+    bool operator!=(const EndfFloatCpp& other) const {
+        return static_cast<double>(_value) != static_cast<double>(other._value);
+    }
+
+    bool operator<(const EndfFloatCpp& other) const {
+        return static_cast<double>(_value) < static_cast<double>(other._value);
+    }
+
+
+    template<typename T>
+    bool operator==(T other) const {
+        return static_cast<double>(_value) == static_cast<double>(other);
+    }
+
+    template<typename T>
+    bool operator!=(T other) const {
+        return static_cast<double>(_value) != static_cast<double>(other);
+    }
+
+    template<typename T>
+    bool operator<(T other) const {
+        return static_cast<double>(_value) < static_cast<double>(other);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const EndfFloatCpp& obj) {
+        os << static_cast<double>(obj);
+        return os;
+    }
+
+    operator double() const {
+        return _value;
+    }
+
+    operator int() const {
+        return static_cast<int>(_value);
+    }
+
+private:
+    double _value;
+    std::string _orig_str;
+};
+
+
+namespace PYBIND11_NAMESPACE { namespace detail {
+
+    // Establish conversion between Python float and EndfFloatCpp
+    template <> struct type_caster<EndfFloatCpp> {
+    public:
+        PYBIND11_TYPE_CASTER(EndfFloatCpp, const_name("EndfFloatCpp"));
+
+        bool load(handle src, bool) {
+            static py::object PyEndfFloat = (
+                py::module::import("endf_parserpy.utils.math_utils").attr("EndfFloat")
+            );
+            // case 1: cast Python float to EndfFloatCpp
+            if (py::isinstance<py::float_>(src)) {
+                py::float_ tmp = py::cast<py::float_>(src);
+                double float_value = tmp.cast<double>();
+                value = EndfFloatCpp(float_value);
+                return !(float_value == -1 && PyErr_Occurred());
+            }
+
+            // case 2: cast Python int to EndfFloatCpp
+            else if (py::isinstance<py::int_>(src)) {
+                py::int_ tmp = py::cast<py::int_>(src);
+                double int_value = tmp.cast<double>() ;
+                value = EndfFloatCpp(int_value);
+                return true;
+            }
+
+            // case 3: cast Python float to EndfFloatCpp
+            else if (py::isinstance(src, PyEndfFloat)) {
+				auto float_method = src.attr("__float__");
+				double float_value = float_method().cast<double>();
+				std::string orig_str = src.attr("get_original_string")().cast<std::string>();
+				value = EndfFloatCpp(float_value, orig_str);
+                return true;
+            }
+            return false;
+        }
+
+        static handle cast(const EndfFloatCpp& src, return_value_policy, handle) {
+            static py::object PyEndfFloat = (
+                py::module::import("endf_parserpy.utils.math_utils").attr("EndfFloat")
+            );
+            double float_value = static_cast<double>(src);
+            std::string orig_str = src.get_original_string();
+            if (orig_str.empty()) {
+                return py::float_(static_cast<double>(src)).release();
+            } else {
+                py::object endf_float = PyEndfFloat(
+                   py::float_(float_value), py::str(orig_str)
+                );
+                return endf_float.release();
+            }
+        }
+    };
+
+
+}} // namespace PYBIND11_NAMESPACE::detail
+
+
+#endif // CPP_ENDF_FLOAT_HPP
