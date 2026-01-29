0eb808e49b41e5b0ffee01562e839d84912182d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:11:55 +0000

    Add package napari-rosettasciio/0.0.3/napari_rosettasciio-0.0.3.tar.gz

---
+++ packages/napari-rosettasciio/napari_rosettasciio-0.0.3.tar.gz/napari_rosettasciio-0.0.3/src/napari_rosettasciio/__init__.py
@@ -0,0 +1,14 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+
+from ._reader import napari_get_reader
+from ._writer import write_single_image, write_multiple
+
+__all__ = (
+    "napari_get_reader",
+    "write_single_image",
+    "write_multiple",
+)
