d22a9291f44e0ba3f65b3309769aa5fda8b8bd70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:09:49 +0000

    Add package fastapi-crudrouter-v2/0.1.2/fastapi_crudrouter_v2-0.1.2-py3-none-any.whl

---
+++ packages/fastapi-crudrouter-v2/fastapi_crudrouter_v2-0.1.2-py3-none-any.whl/fastapi_crudrouter_v2/__init__.py
@@ -0,0 +1,9 @@
+from .core import (
+    TortoiseCRUDRouter,
+)
+
+from ._version import __version__  # noqa: F401
+
+__all__ = [
+    "TortoiseCRUDRouter",
+]
---
+++ packages/fastapi-crudrouter-v2/fastapi_crudrouter_v2-0.1.2-py3-none-any.whl/fastapi_crudrouter_v2/core/_types.py
@@ -0,0 +1,12 @@
+# mypy: ignore-errors
+
+from typing import Dict, Optional, Sequence, TypeVar
+
+from fastapi.params import Depends
+from pydantic import BaseModel
+
+PAGINATION = Dict[str, Optional[int]]
+PYDANTIC_SCHEMA = BaseModel
+
+T = TypeVar("T", bound=BaseModel)
+DEPENDENCIES = Optional[Sequence[Depends]]
