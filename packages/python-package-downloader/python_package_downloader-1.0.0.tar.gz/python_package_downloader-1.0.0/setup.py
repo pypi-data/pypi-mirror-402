009f1dfe1383d55cb91d1b47b07c856142254fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 05:19:38 +0000

    Add package python-package-downloader/1.0.0/python_package_downloader-1.0.0.tar.gz

---
+++ packages/python-package-downloader/python_package_downloader-1.0.0.tar.gz/python_package_downloader-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from pip_setuptools import setup, clean, find_packages, requirements
+
+clean()
+setup(
+    name='python-package-downloader',
+    version='1.0.0',
+    py_modules=[],
+    packages=find_packages(),
+    entry_points=dict(console_scripts=['ppd=python_package_downloader:main']),
+    install_requires=requirements(),
+    author="Маг Ильяс DOMA (MagIlyasDOMA)",
+    author_email='magilyas.doma.09@list.ru',
+    url='https://github.com/MagIlyasDOMA/python-package-downloader',
+    python_requires='>=3.10',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    description="A CLI application for easily downloading Python packages"
+)
