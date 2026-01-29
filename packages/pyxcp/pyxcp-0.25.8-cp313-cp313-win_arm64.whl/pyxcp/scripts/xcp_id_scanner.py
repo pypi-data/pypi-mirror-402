7fe4c5aa5a63e09e9238024bd51d2e5faddb0c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 13:26:25 +0000

    Add package pyxcp/0.25.8/pyxcp-0.25.8-cp313-cp313-win_arm64.whl

---
+++ packages/pyxcp/pyxcp-0.25.8-cp313-cp313-win_arm64.whl/pyxcp/scripts/xcp_id_scanner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""Scan for available IDs."""
+
+from pyxcp.cmdline import ArgumentParser
+
+
+def main():
+    ap = ArgumentParser(description="Scan for available IDs.")
+    with ap.run() as x:
+        x.connect()
+        result = x.id_scanner()
+        for key, value in result.items():
+            print(f"{key}: {value}", end="\n\n")
+        x.disconnect()
+
+
+if __name__ == "__main__":
+    main()
