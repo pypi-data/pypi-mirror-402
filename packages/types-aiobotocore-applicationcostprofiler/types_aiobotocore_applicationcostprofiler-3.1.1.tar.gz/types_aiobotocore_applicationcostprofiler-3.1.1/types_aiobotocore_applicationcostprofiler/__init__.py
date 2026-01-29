3580e8f6d4b842eb0fe92c7bf1d824f003fd3196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:48 +0000

    Add package types-aiobotocore-applicationcostprofiler/3.1.1/types_aiobotocore_applicationcostprofiler-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-applicationcostprofiler/types_aiobotocore_applicationcostprofiler-3.1.1.tar.gz/types_aiobotocore_applicationcostprofiler-3.1.1/types_aiobotocore_applicationcostprofiler/__init__.py
@@ -0,0 +1,34 @@
+"""
+Main interface for applicationcostprofiler service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_applicationcostprofiler/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_applicationcostprofiler import (
+        ApplicationCostProfilerClient,
+        Client,
+        ListReportDefinitionsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("applicationcostprofiler") as client:
+        client: ApplicationCostProfilerClient
+        ...
+
+
+    list_report_definitions_paginator: ListReportDefinitionsPaginator = client.get_paginator("list_report_definitions")
+    ```
+"""
+
+from .client import ApplicationCostProfilerClient
+from .paginator import ListReportDefinitionsPaginator
+
+Client = ApplicationCostProfilerClient
+
+
+__all__ = ("ApplicationCostProfilerClient", "Client", "ListReportDefinitionsPaginator")
