0979a105b103ea3d9f1899ac70c76b6de1f4ce41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:20 +0000

    Add package types-aiobotocore-freetier/3.1.1/types_aiobotocore_freetier-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-freetier/types_aiobotocore_freetier-3.1.1.tar.gz/types_aiobotocore_freetier-3.1.1/types_aiobotocore_freetier/client.py
@@ -0,0 +1,184 @@
+"""
+Type annotations for freetier service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_freetier.client import FreeTierClient
+
+    session = get_session()
+    async with session.create_client("freetier") as client:
+        client: FreeTierClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import GetFreeTierUsagePaginator, ListAccountActivitiesPaginator
+from .type_defs import (
+    GetAccountActivityRequestTypeDef,
+    GetAccountActivityResponseTypeDef,
+    GetAccountPlanStateResponseTypeDef,
+    GetFreeTierUsageRequestTypeDef,
+    GetFreeTierUsageResponseTypeDef,
+    ListAccountActivitiesRequestTypeDef,
+    ListAccountActivitiesResponseTypeDef,
+    UpgradeAccountPlanRequestTypeDef,
+    UpgradeAccountPlanResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Self, Unpack
+else:
+    from typing_extensions import Literal, Self, Unpack
+
+
+__all__ = ("FreeTierClient",)
+
+
+class Exceptions(BaseClientExceptions):
+    AccessDeniedException: type[BotocoreClientError]
+    ClientError: type[BotocoreClientError]
+    InternalServerException: type[BotocoreClientError]
+    ResourceNotFoundException: type[BotocoreClientError]
+    ThrottlingException: type[BotocoreClientError]
+    ValidationException: type[BotocoreClientError]
+
+
+class FreeTierClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier.html#FreeTier.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        FreeTierClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier.html#FreeTier.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/#generate_presigned_url)
+        """
+
+    async def get_account_activity(
+        self, **kwargs: Unpack[GetAccountActivityRequestTypeDef]
+    ) -> GetAccountActivityResponseTypeDef:
+        """
+        Returns a specific activity record that is available to the customer.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier/client/get_account_activity.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/#get_account_activity)
+        """
+
+    async def get_account_plan_state(self) -> GetAccountPlanStateResponseTypeDef:
+        """
+        This returns all of the information related to the state of the account plan
+        related to Free Tier.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier/client/get_account_plan_state.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/#get_account_plan_state)
+        """
+
+    async def get_free_tier_usage(
+        self, **kwargs: Unpack[GetFreeTierUsageRequestTypeDef]
+    ) -> GetFreeTierUsageResponseTypeDef:
+        """
+        Returns a list of all Free Tier usage objects that match your filters.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier/client/get_free_tier_usage.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/#get_free_tier_usage)
+        """
+
+    async def list_account_activities(
+        self, **kwargs: Unpack[ListAccountActivitiesRequestTypeDef]
+    ) -> ListAccountActivitiesResponseTypeDef:
+        """
+        Returns a list of activities that are available.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier/client/list_account_activities.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/#list_account_activities)
+        """
+
+    async def upgrade_account_plan(
+        self, **kwargs: Unpack[UpgradeAccountPlanRequestTypeDef]
+    ) -> UpgradeAccountPlanResponseTypeDef:
+        """
+        The account plan type for the Amazon Web Services account.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier/client/upgrade_account_plan.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/#upgrade_account_plan)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["get_free_tier_usage"]
+    ) -> GetFreeTierUsagePaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_account_activities"]
+    ) -> ListAccountActivitiesPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier.html#FreeTier.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/freetier.html#FreeTier.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_freetier/client/)
+        """
