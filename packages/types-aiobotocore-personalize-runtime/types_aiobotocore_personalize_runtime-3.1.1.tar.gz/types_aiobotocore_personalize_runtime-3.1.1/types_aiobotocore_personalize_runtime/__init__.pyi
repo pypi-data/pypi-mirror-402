efb1d41183b09229386d89c7c3224a3dd90fdd4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:40 +0000

    Add package types-aiobotocore-personalize-runtime/3.1.1/types_aiobotocore_personalize_runtime-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-personalize-runtime/types_aiobotocore_personalize_runtime-3.1.1.tar.gz/types_aiobotocore_personalize_runtime-3.1.1/types_aiobotocore_personalize_runtime/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for personalize-runtime service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_runtime/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_personalize_runtime import (
+        Client,
+        PersonalizeRuntimeClient,
+    )
+
+    session = get_session()
+    async with session.create_client("personalize-runtime") as client:
+        client: PersonalizeRuntimeClient
+        ...
+
+    ```
+"""
+
+from .client import PersonalizeRuntimeClient
+
+Client = PersonalizeRuntimeClient
+
+__all__ = ("Client", "PersonalizeRuntimeClient")
