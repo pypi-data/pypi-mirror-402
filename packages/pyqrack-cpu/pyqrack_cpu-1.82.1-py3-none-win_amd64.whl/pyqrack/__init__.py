027695b158ec9324acf763fac64085139693290d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 12:07:22 +0000

    Add package pyqrack-cpu/1.82.1/pyqrack_cpu-1.82.1-py3-none-win_amd64.whl

---
+++ packages/pyqrack-cpu/pyqrack_cpu-1.82.1-py3-none-win_amd64.whl/pyqrack/__init__.py
@@ -0,0 +1,20 @@
+# (C) Daniel Strano and the Qrack contributors 2017-2025. All rights reserved.
+#
+# Use of this source code is governed by an MIT-style license that can be
+# found in the LICENSE file or at https://opensource.org/licenses/MIT.
+
+from .pauli import Pauli
+from .neuron_activation_fn import NeuronActivationFn
+from .quimb_circuit_type import QuimbCircuitType
+from .qrack_ace_backend import QrackAceBackend
+from .qrack_circuit import QrackCircuit
+from .qrack_neuron import QrackNeuron
+from .qrack_neuron_torch_layer import (
+    QrackNeuronTorch,
+    QrackNeuronTorchFunction,
+    QrackNeuronTorchLayer,
+)
+from .qrack_simulator import QrackSimulator
+from .qrack_stabilizer import QrackStabilizer
+from .qrack_system import QrackSystem, Qrack
+from .stats.quantize_by_range import quantize_by_range
