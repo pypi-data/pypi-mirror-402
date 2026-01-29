03c2a85736d0878d470d04cbeca85d7d7ce8a7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:10:00 +0000

    Add package logikal-browser/3.1.1/logikal_browser-3.1.1.tar.gz

---
+++ packages/logikal-browser/logikal_browser-3.1.1.tar.gz/logikal_browser-3.1.1/README.rst
@@ -0,0 +1,8 @@
+logikal-browser
+===============
+Common browser automation utilities.
+
+Getting Started
+---------------
+You can find the project documentation under `docs.logikal.io/logikal-browser/
+<https://docs.logikal.io/logikal-browser/>`_.
---
+++ packages/logikal-browser/logikal_browser-3.1.1.tar.gz/logikal_browser-3.1.1/browser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import logging
+from dataclasses import replace
+
+from logikal_browser.chrome import ChromeBrowser
+from logikal_browser.scenarios import desktop
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
+browser = ChromeBrowser(settings=replace(desktop.settings, headless=False))
+browser.get('https://github.com/logikal-io/')
+input('Press <ENTER> to stop the program... ')
