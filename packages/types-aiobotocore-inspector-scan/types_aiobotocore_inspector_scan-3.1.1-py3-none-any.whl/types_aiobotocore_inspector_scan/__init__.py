180023c14ac58e0754a1b650c4c9d5d2d9208844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:22 +0000

    Add package types-aiobotocore-inspector-scan/3.1.1/types_aiobotocore_inspector_scan-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-inspector-scan/types_aiobotocore_inspector_scan-3.1.1-py3-none-any.whl/types_aiobotocore_inspector_scan/__init__.py
@@ -0,0 +1,30 @@
+"""
+Main interface for inspector-scan service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_inspector_scan/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_inspector_scan import (
+        Client,
+        InspectorscanClient,
+    )
+
+    session = get_session()
+    async with session.create_client("inspector-scan") as client:
+        client: InspectorscanClient
+        ...
+
+    ```
+"""
+
+from .client import InspectorscanClient
+
+Client = InspectorscanClient
+
+
+__all__ = ("Client", "InspectorscanClient")
