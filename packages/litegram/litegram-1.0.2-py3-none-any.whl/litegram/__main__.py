b715bb566851d4c55e42b155fc7a0465bedfcfab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:15:30 +0000

    Add package litegram/1.0.2/litegram-1.0.2-py3-none-any.whl

---
+++ packages/litegram/litegram-1.0.2-py3-none-any.whl/litegram/__main__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from .cli import litegram_group
+
+if __name__ == "__main__":
+    litegram_group()
---
+++ packages/litegram/litegram-1.0.2-py3-none-any.whl/litegram/utils/magic_filter/exceptions.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+
+class MagicFilterException(Exception):
+    pass
+
+
+class SwitchMode(MagicFilterException):
+    pass
+
+
+class SwitchModeToAll(SwitchMode):
+    def __init__(self, key: slice) -> None:
+        self.key = key
+
+
+class SwitchModeToAny(SwitchMode):
+    pass
+
+
+class RejectOperations(MagicFilterException):
+    pass
+
+
+class ParamsConflict(MagicFilterException):
+    pass
