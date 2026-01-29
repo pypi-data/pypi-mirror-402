5083a7e4fb1a4bddecd5eb4157a98af1f7b8f2ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:39 +0000

    Add package types-aiobotocore-personalize-events/3.1.1/types_aiobotocore_personalize_events-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-personalize-events/types_aiobotocore_personalize_events-3.1.1-py3-none-any.whl/types_aiobotocore_personalize_events/__init__.py
@@ -0,0 +1,30 @@
+"""
+Main interface for personalize-events service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_personalize_events/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_personalize_events import (
+        Client,
+        PersonalizeEventsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("personalize-events") as client:
+        client: PersonalizeEventsClient
+        ...
+
+    ```
+"""
+
+from .client import PersonalizeEventsClient
+
+Client = PersonalizeEventsClient
+
+
+__all__ = ("Client", "PersonalizeEventsClient")
