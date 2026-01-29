f90d6cf85d8c9bc24c712e3655c3962007c80248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:15:30 +0000

    Add package guildpack/0.1.12/guildpack-0.1.12-py3-none-any.whl

---
+++ packages/guildpack/guildpack-0.1.12-py3-none-any.whl/guildpack/__init__.py
@@ -0,0 +1,11 @@
+"""Guildpack - Alias package for oguild.
+
+This package provides an alternative installation name for oguild.
+All functionality is re-exported from oguild.
+
+Install with: pip install guildpack
+"""
+
+# Re-export everything from oguild
+from oguild import *
+from oguild import __all__
