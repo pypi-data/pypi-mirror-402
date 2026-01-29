e034c137cb070153b8bbd8f15aeceb9928b349d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:36 +0000

    Add package Topsis-102303752/0.1.0/topsis_102303752-0.1.0.tar.gz

---
+++ packages/Topsis-102303752/topsis_102303752-0.1.0.tar.gz/topsis_102303752-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Topsis-102303752',  # Unique package name
+    version='0.1.0',
+    author='Raunaq Mittal',
+    author_email='raunaqmittal2004@gmail.com',
+    description='TOPSIS implementation for multi-criteria decision making',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/raunaqmittal/Topsis-Package',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
