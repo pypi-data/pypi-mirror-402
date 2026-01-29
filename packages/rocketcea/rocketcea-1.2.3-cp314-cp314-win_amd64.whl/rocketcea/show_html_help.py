08e74b0140b301bc6b7f1fa20479faf15f577440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:12:36 +0000

    Add package rocketcea/1.2.3/rocketcea-1.2.3-cp314-cp314-win_amd64.whl

---
+++ packages/rocketcea/rocketcea-1.2.3-cp314-cp314-win_amd64.whl/rocketcea/show_html_help.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os, sys
+import webbrowser
+
+#here = os.path.abspath(os.path.dirname(__file__))
+#INDEX_PAGE = os.path.join( here, 'sphinx_html', 'index.html' )
+
+INDEX_PAGE = "https://rocketcea.readthedocs.io/en/latest/index.html"
+
+def main():
+    webbrowser.open(INDEX_PAGE)
+
+if __name__ == '__main__':
+    main()
