1717915203a68ade15113f8669e70fcac6d92c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:08:16 +0000

    Add package discordcn-dpy/0.0.0/discordcn_dpy-0.0.0.tar.gz

---
+++ packages/discordcn-dpy/discordcn_dpy-0.0.0.tar.gz/discordcn_dpy-0.0.0/README.md
@@ -0,0 +1,4 @@
+
+## Overview
+discordcn-dpy is a Python library for doing awesome things.
+This name has been reserved using [Reserver](https://github.com/openscilab/reserver).
---
+++ packages/discordcn-dpy/discordcn_dpy-0.0.0.tar.gz/discordcn_dpy-0.0.0/setup.py
@@ -0,0 +1,47 @@
+
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# invalid email
+# download url
+# url
+# project urls
+
+setup(
+    name ="discordcn-dpy",
+    packages=[".",],
+    version='0.0.0',
+    description="Reserved for future use by nicebots.xyz. Please contact me@paillat.dev for more information.",
+    long_description="""
+## Overview
+discordcn-dpy is a Python library for doing awesome things.
+This name has been reserved using [Reserver](https://github.com/openscilab/reserver).
+""",
+    long_description_content_type='text/markdown',
+    author="Paillat-dev",
+    author_email="me@paillat.dev",
+    url="https://url.com",
+    download_url="https://download_url.com",
+    keywords="python3 python reserve reserver reserved",
+    project_urls={
+            'Source':"https://github.com/nicebots-xyz/discordcn-dpy",
+    },
+    install_requires="",
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    license="MIT",
+)
+
