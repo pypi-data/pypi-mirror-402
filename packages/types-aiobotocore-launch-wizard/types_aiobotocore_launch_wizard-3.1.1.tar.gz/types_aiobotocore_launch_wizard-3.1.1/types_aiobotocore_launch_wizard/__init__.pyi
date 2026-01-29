126e5fa82ca9518c98d57122ba47fa41a292ff8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:26 +0000

    Add package types-aiobotocore-launch-wizard/3.1.1/types_aiobotocore_launch_wizard-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-launch-wizard/types_aiobotocore_launch_wizard-3.1.1.tar.gz/types_aiobotocore_launch_wizard-3.1.1/types_aiobotocore_launch_wizard/__init__.pyi
@@ -0,0 +1,55 @@
+"""
+Main interface for launch-wizard service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_launch_wizard/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_launch_wizard import (
+        Client,
+        LaunchWizardClient,
+        ListDeploymentEventsPaginator,
+        ListDeploymentPatternVersionsPaginator,
+        ListDeploymentsPaginator,
+        ListWorkloadDeploymentPatternsPaginator,
+        ListWorkloadsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("launch-wizard") as client:
+        client: LaunchWizardClient
+        ...
+
+
+    list_deployment_events_paginator: ListDeploymentEventsPaginator = client.get_paginator("list_deployment_events")
+    list_deployment_pattern_versions_paginator: ListDeploymentPatternVersionsPaginator = client.get_paginator("list_deployment_pattern_versions")
+    list_deployments_paginator: ListDeploymentsPaginator = client.get_paginator("list_deployments")
+    list_workload_deployment_patterns_paginator: ListWorkloadDeploymentPatternsPaginator = client.get_paginator("list_workload_deployment_patterns")
+    list_workloads_paginator: ListWorkloadsPaginator = client.get_paginator("list_workloads")
+    ```
+"""
+
+from .client import LaunchWizardClient
+from .paginator import (
+    ListDeploymentEventsPaginator,
+    ListDeploymentPatternVersionsPaginator,
+    ListDeploymentsPaginator,
+    ListWorkloadDeploymentPatternsPaginator,
+    ListWorkloadsPaginator,
+)
+
+Client = LaunchWizardClient
+
+__all__ = (
+    "Client",
+    "LaunchWizardClient",
+    "ListDeploymentEventsPaginator",
+    "ListDeploymentPatternVersionsPaginator",
+    "ListDeploymentsPaginator",
+    "ListWorkloadDeploymentPatternsPaginator",
+    "ListWorkloadsPaginator",
+)
