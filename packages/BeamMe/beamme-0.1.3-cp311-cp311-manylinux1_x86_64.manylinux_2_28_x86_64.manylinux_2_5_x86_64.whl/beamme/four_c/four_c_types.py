7b67b3b703805167d5f6e0de2b9785aca8511e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:07:42 +0000

    Add package BeamMe/0.1.3/beamme-0.1.3-cp311-cp311-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl

---
+++ packages/BeamMe/beamme-0.1.3-cp311-cp311-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl/beamme/four_c/four_c_types.py
@@ -0,0 +1,47 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2018-2026 BeamMe Authors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""This module defines enums for beams in 4C."""
+
+from enum import Enum as _Enum
+from enum import auto as _auto
+
+
+class BeamType(_Enum):
+    """Enum for beam types."""
+
+    reissner = _auto()
+    kirchhoff = _auto()
+    euler_bernoulli = _auto()
+
+
+class BeamKirchhoffConstraintType(_Enum):
+    """Enum for type of Kirchhoff constraint enforcement."""
+
+    weak = _auto()
+    strong = _auto()
+
+
+class BeamKirchhoffParametrizationType(_Enum):
+    """Enum for type of Kirchhoff parameterization."""
+
+    rot = _auto()
+    tan = _auto()
