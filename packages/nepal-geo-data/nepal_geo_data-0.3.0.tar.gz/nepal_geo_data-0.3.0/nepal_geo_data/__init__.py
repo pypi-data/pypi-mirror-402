78e6bde266d0aa499e655fef59c663244e4c08b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:13 +0000

    Add package nepal-geo-data/0.3.0/nepal_geo_data-0.3.0.tar.gz

---
+++ packages/nepal-geo-data/nepal_geo_data-0.3.0.tar.gz/nepal_geo_data-0.3.0/nepal_geo_data/__init__.py
@@ -0,0 +1,30 @@
+from .core import (
+    get_geojson,
+    get_provinces_geojson,
+    get_municipalities_geojson,
+    get_districts,
+    get_provinces,
+    get_municipalities,
+    get_district,
+    get_municipality,
+    get_province_districts,
+    get_boundaries,
+    get_wards,
+    help
+)
+
+__version__ = "0.3.0"
+__all__ = [
+    "get_geojson",
+    "get_provinces_geojson",
+    "get_municipalities_geojson",
+    "get_districts",
+    "get_provinces",
+    "get_municipalities",
+    "get_district",
+    "get_municipality",
+    "get_province_districts",
+    "get_boundaries",
+    "get_wards",
+    "help"
+]
---
+++ packages/nepal-geo-data/nepal_geo_data-0.3.0.tar.gz/nepal_geo_data-0.3.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nepal-geo-data",
+    version="0.3.0",
+    author="Bedbyas Pokhrel",
+    author_email="bedbyaspokhrel@gmail.com",
+    description="Comprehensive Python package offering Nepal's geographical data including GeoJSON for all 77 districts and 7 provinces.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bedbyaspokhrel/nepal-geo-data",
+    project_urls={
+        "Bug Tracker": "https://github.com/bedbyaspokhrel/nepal-geo-data/issues",
+    },
+    keywords="nepal geojson gis nepal-map districts provinces nepal-geography kathmandu data-visualization python-nepal",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.8",
+    include_package_data=True,
+    package_data={
+        "nepal_geo_data": ["data/*.geojson"],
+    },
+)
