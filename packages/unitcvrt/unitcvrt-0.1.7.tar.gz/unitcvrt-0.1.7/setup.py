64e9773a75aff931692d377ed423971c90c787a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:13:18 +0000

    Add package unitcvrt/0.1.7/unitcvrt-0.1.7.tar.gz

---
+++ packages/unitcvrt/unitcvrt-0.1.7.tar.gz/unitcvrt-0.1.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="unitcvrt",
+    version="0.1.7",
+    description="Unit Conversion System",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Shinsuke Sakai",
+    author_email='sakaishin0321@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=2.0.2",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    python_requires='>=3.6',
+)
