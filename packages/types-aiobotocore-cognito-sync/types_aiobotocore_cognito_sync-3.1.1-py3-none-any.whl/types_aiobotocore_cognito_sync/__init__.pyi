a617142e26eeed2bbd862d3aee2e32d655d75300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:08:02 +0000

    Add package types-aiobotocore-cognito-sync/3.1.1/types_aiobotocore_cognito_sync-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-cognito-sync/types_aiobotocore_cognito_sync-3.1.1-py3-none-any.whl/types_aiobotocore_cognito_sync/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for cognito-sync service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cognito_sync/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_cognito_sync import (
+        Client,
+        CognitoSyncClient,
+    )
+
+    session = get_session()
+    async with session.create_client("cognito-sync") as client:
+        client: CognitoSyncClient
+        ...
+
+    ```
+"""
+
+from .client import CognitoSyncClient
+
+Client = CognitoSyncClient
+
+__all__ = ("Client", "CognitoSyncClient")
