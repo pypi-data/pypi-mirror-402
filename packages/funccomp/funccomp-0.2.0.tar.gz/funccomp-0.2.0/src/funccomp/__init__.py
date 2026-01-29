71a9a536699eb39bb9ba469cae9f548c3fc5e425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:17:29 +0000

    Add package funccomp/0.2.0/funccomp-0.2.0.tar.gz

---
+++ packages/funccomp/funccomp-0.2.0.tar.gz/funccomp-0.2.0/src/funccomp/__init__.py
@@ -0,0 +1,4 @@
+from funccomp.core.BaseComposite import BaseComposite
+from funccomp.core.Composite import Composite
+from funccomp.core.StarComposite import StarComposite
+from funccomp.tests import *
