7b9940bd98c8ae0dbce3fe4d5ba1bb58c0a77c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:12:16 +0000

    Add package django-env-loader/1.0.3/django_env_loader-1.0.3-py3-none-any.whl

---
+++ packages/django-env-loader/django_env_loader-1.0.3-py3-none-any.whl/django_env_loader/__init__.py
@@ -0,0 +1,19 @@
+"""Django Env Loader - Gerenciamento robusto de variáveis de ambiente."""
+
+from django_env_loader.exceptions import EnvLoaderError, SecretNotFoundError, ValidationError
+from django_env_loader.loader import DjangoEnvLoader, EnvConfig, EnvLoader
+
+__version__ = "1.0.0"
+__all__ = [
+    "EnvLoader",
+    "DjangoEnvLoader",
+    "EnvConfig",
+    "EnvLoaderError",
+    "SecretNotFoundError",
+    "ValidationError",
+    "env_loader",
+]
+
+# Instância singleton padrão para importação direta
+# Uso: from django_env_loader import env_loader
+env_loader = EnvLoader()
