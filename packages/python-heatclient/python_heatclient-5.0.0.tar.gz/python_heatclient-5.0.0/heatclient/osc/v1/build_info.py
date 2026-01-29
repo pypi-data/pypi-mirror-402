cd70c61d1aac9d99b1a01531ce982cca2bf4a21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:17:57 +0000

    Add package python-heatclient/5.0.0/python_heatclient-5.0.0.tar.gz

---
+++ packages/python-heatclient/python_heatclient-5.0.0.tar.gz/python_heatclient-5.0.0/heatclient/osc/v1/build_info.py
@@ -0,0 +1,45 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+"""Orchestration v1 build info action implementations"""
+
+import logging
+
+from osc_lib.command import command
+from osc_lib import utils
+
+from heatclient.osc.v1 import common
+
+
+class BuildInfo(command.ShowOne):
+    """Retrieve build information."""
+
+    log = logging.getLogger(__name__ + ".BuildInfo")
+
+    def get_parser(self, prog_name):
+        parser = super().get_parser(prog_name)
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug("take_action(%s)", parsed_args)
+
+        heat_client = self.app.client_manager.orchestration
+        result = heat_client.build_info.build_info()
+
+        formatters = {
+            'api': common.JsonColumn,
+            'engine': common.JsonColumn,
+        }
+        columns = sorted(list(result.keys()))
+        return columns, utils.get_dict_properties(result, columns,
+                                                  formatters=formatters)
