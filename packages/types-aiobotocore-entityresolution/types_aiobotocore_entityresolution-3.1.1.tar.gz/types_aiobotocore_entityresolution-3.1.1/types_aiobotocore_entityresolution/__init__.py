033e9916a612f98eb3ffad39ad5daccce26ca84a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:18 +0000

    Add package types-aiobotocore-entityresolution/3.1.1/types_aiobotocore_entityresolution-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-entityresolution/types_aiobotocore_entityresolution-3.1.1.tar.gz/types_aiobotocore_entityresolution-3.1.1/types_aiobotocore_entityresolution/__init__.py
@@ -0,0 +1,64 @@
+"""
+Main interface for entityresolution service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_entityresolution/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_entityresolution import (
+        Client,
+        EntityResolutionClient,
+        ListIdMappingJobsPaginator,
+        ListIdMappingWorkflowsPaginator,
+        ListIdNamespacesPaginator,
+        ListMatchingJobsPaginator,
+        ListMatchingWorkflowsPaginator,
+        ListProviderServicesPaginator,
+        ListSchemaMappingsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("entityresolution") as client:
+        client: EntityResolutionClient
+        ...
+
+
+    list_id_mapping_jobs_paginator: ListIdMappingJobsPaginator = client.get_paginator("list_id_mapping_jobs")
+    list_id_mapping_workflows_paginator: ListIdMappingWorkflowsPaginator = client.get_paginator("list_id_mapping_workflows")
+    list_id_namespaces_paginator: ListIdNamespacesPaginator = client.get_paginator("list_id_namespaces")
+    list_matching_jobs_paginator: ListMatchingJobsPaginator = client.get_paginator("list_matching_jobs")
+    list_matching_workflows_paginator: ListMatchingWorkflowsPaginator = client.get_paginator("list_matching_workflows")
+    list_provider_services_paginator: ListProviderServicesPaginator = client.get_paginator("list_provider_services")
+    list_schema_mappings_paginator: ListSchemaMappingsPaginator = client.get_paginator("list_schema_mappings")
+    ```
+"""
+
+from .client import EntityResolutionClient
+from .paginator import (
+    ListIdMappingJobsPaginator,
+    ListIdMappingWorkflowsPaginator,
+    ListIdNamespacesPaginator,
+    ListMatchingJobsPaginator,
+    ListMatchingWorkflowsPaginator,
+    ListProviderServicesPaginator,
+    ListSchemaMappingsPaginator,
+)
+
+Client = EntityResolutionClient
+
+
+__all__ = (
+    "Client",
+    "EntityResolutionClient",
+    "ListIdMappingJobsPaginator",
+    "ListIdMappingWorkflowsPaginator",
+    "ListIdNamespacesPaginator",
+    "ListMatchingJobsPaginator",
+    "ListMatchingWorkflowsPaginator",
+    "ListProviderServicesPaginator",
+    "ListSchemaMappingsPaginator",
+)
