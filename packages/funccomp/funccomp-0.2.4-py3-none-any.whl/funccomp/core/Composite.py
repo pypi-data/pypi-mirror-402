684ab34fb327c9eb93c1749138b17ab6ba27dfe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:17:34 +0000

    Add package funccomp/0.2.4/funccomp-0.2.4-py3-none-any.whl

---
+++ packages/funccomp/funccomp-0.2.4-py3-none-any.whl/funccomp/core/Composite.py
@@ -0,0 +1,20 @@
+import operator
+from typing import *
+
+from .BaseComposite import BaseComposite
+
+__all__ = [
+    "Composite",
+]
+
+
+class Composite(BaseComposite):
+    factors: list
+    __slots__ = ()
+
+    @classmethod
+    def _call(cls: type[Self], factor: Any, answer: Any) -> Any:
+        if callable(factor):
+            return factor(answer)
+        else:
+            return operator.mul(factor, answer)
