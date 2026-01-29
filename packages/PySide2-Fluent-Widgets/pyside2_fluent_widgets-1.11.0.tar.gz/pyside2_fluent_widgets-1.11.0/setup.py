7f7138fdd5ce523adb66d9c62ab7585a10ba8cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:09:10 +0000

    Add package PySide2-Fluent-Widgets/1.11.0/pyside2_fluent_widgets-1.11.0.tar.gz

---
+++ packages/PySide2-Fluent-Widgets/pyside2_fluent_widgets-1.11.0.tar.gz/pyside2_fluent_widgets-1.11.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="PySide2-Fluent-Widgets",
+    version="1.11.0",
+    keywords="pyside2 fluent widgets",
+    author="zhiyiYo",
+    author_email="shokokawaii@outlook.com",
+    description="A fluent design widgets library based on PySide2",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="GPLv3",
+    url="https://qfluentwidgets.com",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "PySide2>=5.15.0",
+        "PySide2-Frameless-Window>=0.4.0",
+        "darkdetect",
+    ],
+    extras_require = {
+        'full': ['scipy', 'pillow', 'colorthief']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent'
+    ],
+    project_urls={
+        'Documentation': 'https://qfluentwidgets.com/',
+        'Source Code': 'https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/PySide2',
+        'Bug Tracker': 'https://github.com/zhiyiYo/PyQt-Fluent-Widgets/issues',
+    }
+)
