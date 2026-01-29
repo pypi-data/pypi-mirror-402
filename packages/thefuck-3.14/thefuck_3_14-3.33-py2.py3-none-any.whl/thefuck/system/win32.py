3aea4b7ecc0ed1b6ab613c4b9fb570199b1370e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:13:07 +0000

    Add package thefuck-3.14/3.33/thefuck_3_14-3.33-py2.py3-none-any.whl

---
+++ packages/thefuck-3.14/thefuck_3_14-3.33-py2.py3-none-any.whl/thefuck/system/win32.py
@@ -0,0 +1,41 @@
+import os
+import msvcrt
+from .. import const
+
+
+def init_output():
+    import colorama
+    colorama.init()
+
+
+def get_key():
+    ch = msvcrt.getwch()
+    if ch in ('\x00', '\xe0'):  # arrow or function key prefix?
+        ch = msvcrt.getwch()  # second call returns the actual key code
+
+    if ch in const.KEY_MAPPING:
+        return const.KEY_MAPPING[ch]
+    if ch == 'H':
+        return const.KEY_UP
+    if ch == 'P':
+        return const.KEY_DOWN
+
+    return ch
+
+
+def open_command(arg):
+    return 'cmd /c start ' + arg
+
+
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path
+
+
+def _expanduser(self):
+    return self.__class__(os.path.expanduser(str(self)))
+
+
+# pathlib's expanduser fails on windows, see http://bugs.python.org/issue19776
+Path.expanduser = _expanduser
