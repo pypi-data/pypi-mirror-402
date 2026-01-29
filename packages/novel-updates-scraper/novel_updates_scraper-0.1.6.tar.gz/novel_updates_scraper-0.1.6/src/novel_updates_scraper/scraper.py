392767f2be80402f8cc595232f09445c6df9c866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 03:47:04 +0000

    Add package novel-updates-scraper/0.1.6/novel_updates_scraper-0.1.6.tar.gz

---
+++ packages/novel-updates-scraper/novel_updates_scraper-0.1.6.tar.gz/novel_updates_scraper-0.1.6/src/novel_updates_scraper/scraper.py
@@ -0,0 +1,38 @@
+from curl_cffi import requests
+from bs4 import BeautifulSoup
+from .constants import BASE_URL
+import asyncio
+from .cf_bypass import get_cf_clearance
+
+session = {
+    "token": None,
+    "user_agent": None
+}
+
+async def refresh_cf_token():
+    session["token"],session["user_agent"]=await get_cf_clearance(BASE_URL)
+
+def get_html(url,query_params={}):
+    
+    for i in range(5):
+        headers={
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.9",
+        }
+        res = requests.get(
+            url,
+            headers=headers,
+            cookies={"cf_clearance":session["token"]},
+            impersonate="chrome",
+            params=query_params
+        )
+        if res.status_code!=403:
+            break
+        else:  
+            asyncio.run(refresh_cf_token())
+
+    soup = BeautifulSoup(res.content, 'html.parser')
+    return soup
+
+
