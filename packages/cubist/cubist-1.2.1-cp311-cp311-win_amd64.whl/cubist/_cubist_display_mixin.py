9a9b7a94a11d6495da2f265c10c8962280c9ca3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:12:24 +0000

    Add package cubist/1.2.1/cubist-1.2.1-cp311-cp311-win_amd64.whl

---
+++ packages/cubist/cubist-1.2.1-cp311-cp311-win_amd64.whl/cubist/_cubist_display_mixin.py
@@ -0,0 +1,135 @@
+"""Base class for Cubist plotting utilities"""
+
+import math
+
+import numpy as np
+import pandas as pd
+from sklearn.utils._optional_dependencies import check_matplotlib_support
+
+
+class _CubistDisplayMixin:
+    """Mixin class to be used in Displays for Cubist.
+
+    The aim of this class is to centralize some validations for generating the
+    matplotlib figure and axes as well as for processing the data to be plotted.
+    """
+
+    def _validate_plot_params(
+        self,
+        *,
+        ax=None,
+        df: pd.DataFrame,
+        y_label_map=None,
+        gridspec_kwargs=None,
+    ):
+        check_matplotlib_support(f"{self.__class__.__name__}.plot")
+        import matplotlib.pyplot as plt
+
+        if y_label_map is None:
+            y_label_map = {}
+
+        if gridspec_kwargs is None:
+            gridspec_kwargs = {}
+
+        if ax is None:
+            # number of plots to be created
+            nplots = df.variable.nunique()
+            # get the number of columns and rows required to create this many
+            # plots as a square-ish grid
+            nrows = ncols = int(round(math.sqrt(nplots)))
+            # if the square root of the number of plots rounds down
+            # then add 1 to the number of rows
+            if nplots > (nrows * ncols):
+                nrows += 1
+            # create subplots with ncols/nrows, sharing the x and y axes, and
+            # merging in gridspec configurations
+            fig, ax = plt.subplots(
+                ncols=ncols,
+                nrows=nrows,
+                sharex="all",
+                sharey="all",
+                gridspec_kw={"hspace": 0.5, "wspace": 0} | gridspec_kwargs,
+            )
+
+            # ax is a numpy array and so we reshape it to one dimension for
+            # simpler processing
+            if isinstance(ax, np.ndarray):
+                ax = ax.reshape(-1)
+            # if there is only one plot created, place it in a numpy array
+            else:
+                ax = np.array([ax])
+        # if ax is provided, get the current figure
+        else:
+            fig = plt.gcf()
+        return fig, ax, y_label_map, gridspec_kwargs
+
+    @classmethod
+    def _validate_from_estimator_params(
+        cls,
+        *,
+        df: pd.DataFrame,
+        committee: int | None = None,
+        rule: int | None = None,
+        feature_names: list[str] | None = None,
+    ):
+        check_matplotlib_support(f"{cls.__name__}.from_estimator")
+
+        # make sure dataframe is sorted by committee and rule
+        df = df.sort_values(by=["committee", "rule"]).reset_index(drop=True)
+
+        # if the committee parameter is passed
+        if committee is not None:
+            # verify committee parameter is integer
+            if not isinstance(committee, int):
+                raise TypeError(
+                    f"`committee` must be an integer but got {type(committee)}."
+                )
+            # filter for committees below requested committee number
+            df = df.loc[df.committee <= committee]
+
+        # if rule parameter is passed
+        if rule is not None:
+            # verify rule parameter is integer
+            if not isinstance(rule, int):
+                raise TypeError(f"`rule` must be an integer but got {type(rule)}.")
+            # filter for rules below requested rule number
+            df = df.loc[df.rule <= rule]
+
+        # if feature_names parameter is passed
+        if feature_names is not None:
+            # verify feature_names is a list
+            if not isinstance(feature_names, list):
+                raise TypeError(f"`feature_names` must be a list but got {type(rule)}.")
+            # filter for rows where the attribute/variable name is found in
+            # the list
+            df = df.loc[df.variable.isin(feature_names)]
+            # verify the dataframe is not empty now
+            if df.empty:
+                raise ValueError(
+                    f"No rows are available given `feature_names`: {feature_names}."
+                )
+
+        # reset the index since this is used for labelling
+        df = df.reset_index(drop=True)
+
+        # create the y-axis labels and y-tick values
+        if df.committee.max() == 1:
+            # if there is only one committee, this is a rule-only model
+            y_axis_label = "Rule"
+            df["label"] = df.rule
+        else:
+            # otherwise report by committee and rule
+            y_axis_label = "Committee/Rule"
+            # create label column for committee/rule pair
+            df["label"] = df[["committee", "rule"]].apply(
+                lambda x: f"{x.committee}/{x.rule}",
+                axis=1,
+            )
+        # get the distinct ordered labels
+        y_label_map = df.label.drop_duplicates().reset_index(drop=True).to_dict()
+        # get the labels as a list
+        y_labels = list(y_label_map.values())
+        # replace the dataframe label column values with the index of the
+        # same value in y_labels
+        df.label = df.label.apply(y_labels.index)
+        return df, y_axis_label, y_label_map
---
+++ packages/cubist/cubist-1.2.1-cp311-cp311-win_amd64.whl/cubist/_make_data_string.py
@@ -0,0 +1,80 @@
+"""Function to create the Cubist datav_ input"""
+
+import numpy as np
+import pandas as pd
+from pandas.api.types import is_numeric_dtype, is_string_dtype
+
+from ._make_names_string import _escapes
+from ._utils import _format
+
+
+def _make_data_string(x, y=None, w=None):
+    """
+    Converts input dataset array X into a string.
+
+    Parameters
+    ----------
+    x : {pd.DataFrame} of shape (n_samples, n_features)
+        The input samples.
+
+    y : pd.Series
+        The predicted values.
+
+    w : ndarray of shape (n_samples,)
+        Instance weights.
+
+    Returns
+    -------
+    x : str
+        Input dataset converted to a string and formatted per Cubist's
+        requirements.
+    """
+    # apply the escapes function to all string columns
+    for col in x:
+        if is_string_dtype(x[col]):
+            x[col] = _escapes(x[col].astype(str))
+
+    # if y is None for model predictions, set y as a column of NaN values,
+    # # which will become ?'s later
+    if y is None:
+        y = [np.nan] * x.shape[0]
+        y = pd.Series(y)
+    else:
+        y = y.copy(deep=True)
+
+    # format the y column for special charactesrs
+    y = pd.Series(_escapes(y.astype(str)))
+
+    # insert the y column as the first column of x
+    x.insert(0, "y", y)
+
+    # handle weights matrix (?) TODO: validate
+    if w is not None:
+        # [f"w{i}" for i in range(x.shape[1])]
+        column_names = list(x.columns) + ["w"]
+        x = x.assign(w=w)
+        x.columns = column_names
+
+    # convert all columns to strings
+    for col in x:
+        if is_numeric_dtype(x[col]):
+            x[col] = x[col].apply(_format)
+        else:
+            x[col] = x[col].astype(str)
+
+    # remove leading whitespace from all elements
+    x = x.map(lambda a: a.lstrip())
+
+    # replace missing values with ?
+    x = x.fillna("?")
+    x = x.replace("nan", "?")
+
+    # convert dataframe to list of lists
+    x = x.to_numpy().tolist()
+
+    # merge each sublist into single strings with entries separated by commas
+    x = [",".join(row) for row in x]
+
+    # join all row strings into a single string separated by \n's
+    x = "\n".join(x)
+    return x
