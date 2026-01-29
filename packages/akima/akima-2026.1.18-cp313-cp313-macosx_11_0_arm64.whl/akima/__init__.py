3499daabac6e68af076bc8d4ab9fb9ea467c4ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 07:17:55 +0000

    Add package akima/2026.1.18/akima-2026.1.18-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/akima/akima-2026.1.18-cp313-cp313-macosx_11_0_arm64.whl/akima/__init__.py
@@ -0,0 +1,9 @@
+# akima/__init__.py
+
+from .akima import *
+from .akima import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Akima version string."""
