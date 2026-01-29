2a466a1b35d2a1bfe10c150e42b059bd4f7719e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:12:36 +0000

    Add package Topsis-Avneet-102303289/1.0.0/topsis_avneet_102303289-1.0.0.tar.gz

---
+++ packages/Topsis-Avneet-102303289/topsis_avneet_102303289-1.0.0.tar.gz/topsis_avneet_102303289-1.0.0/README.md
@@ -0,0 +1,12 @@
+# TOPSIS Package – Avneet Sandhu (102303289)
+
+This package implements the **TOPSIS (Technique for Order Preference by Similarity to Ideal Solution)** method as a **command line tool**.
+
+It is developed as part of the Predictive Analytics assignment.
+
+---
+
+## Installation
+
+```bash
+pip install Topsis-Avneet-102303289
---
+++ packages/Topsis-Avneet-102303289/topsis_avneet_102303289-1.0.0.tar.gz/topsis_avneet_102303289-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-Avneet-102303289",
+    version="1.0.0",
+    author="Avneet Sandhu",
+    author_email="your_email@gmail.com",
+    description="TOPSIS command line tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["pandas", "numpy"],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis.topsis:main"
+        ]
+    },
+)
