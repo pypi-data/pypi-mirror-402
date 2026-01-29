0309b7663e2d2519abd3e9ef6898224389a7bab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:15:06 +0000

    Add package piton-konsol/0.1.0/piton_konsol-0.1.0.tar.gz

---
+++ packages/piton-konsol/piton_konsol-0.1.0.tar.gz/piton_konsol-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="piton-konsol",
+    version="0.1.0",
+    author="Eray",
+    description="Türkçe Python kütüphanesi - Kodlamayı ana dilinde öğren!",
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
