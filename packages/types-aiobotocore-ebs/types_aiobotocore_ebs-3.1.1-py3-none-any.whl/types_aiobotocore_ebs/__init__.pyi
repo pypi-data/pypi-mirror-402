bd5e85de3b203270ff125593d97d6a2645ffcb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:16 +0000

    Add package types-aiobotocore-ebs/3.1.1/types_aiobotocore_ebs-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-ebs/types_aiobotocore_ebs-3.1.1-py3-none-any.whl/types_aiobotocore_ebs/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for ebs service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_ebs/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_ebs import (
+        Client,
+        EBSClient,
+    )
+
+    session = get_session()
+    async with session.create_client("ebs") as client:
+        client: EBSClient
+        ...
+
+    ```
+"""
+
+from .client import EBSClient
+
+Client = EBSClient
+
+__all__ = ("Client", "EBSClient")
