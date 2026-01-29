7b86928b6c93a74952df06c89473bc42904e8bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:43 +0000

    Add package types-aiobotocore-resiliencehub/3.1.1/types_aiobotocore_resiliencehub-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-resiliencehub/types_aiobotocore_resiliencehub-3.1.1.tar.gz/types_aiobotocore_resiliencehub-3.1.1/types_aiobotocore_resiliencehub/__init__.pyi
@@ -0,0 +1,47 @@
+"""
+Main interface for resiliencehub service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_resiliencehub/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_resiliencehub import (
+        Client,
+        ListAppAssessmentResourceDriftsPaginator,
+        ListMetricsPaginator,
+        ListResourceGroupingRecommendationsPaginator,
+        ResilienceHubClient,
+    )
+
+    session = get_session()
+    async with session.create_client("resiliencehub") as client:
+        client: ResilienceHubClient
+        ...
+
+
+    list_app_assessment_resource_drifts_paginator: ListAppAssessmentResourceDriftsPaginator = client.get_paginator("list_app_assessment_resource_drifts")
+    list_metrics_paginator: ListMetricsPaginator = client.get_paginator("list_metrics")
+    list_resource_grouping_recommendations_paginator: ListResourceGroupingRecommendationsPaginator = client.get_paginator("list_resource_grouping_recommendations")
+    ```
+"""
+
+from .client import ResilienceHubClient
+from .paginator import (
+    ListAppAssessmentResourceDriftsPaginator,
+    ListMetricsPaginator,
+    ListResourceGroupingRecommendationsPaginator,
+)
+
+Client = ResilienceHubClient
+
+__all__ = (
+    "Client",
+    "ListAppAssessmentResourceDriftsPaginator",
+    "ListMetricsPaginator",
+    "ListResourceGroupingRecommendationsPaginator",
+    "ResilienceHubClient",
+)
