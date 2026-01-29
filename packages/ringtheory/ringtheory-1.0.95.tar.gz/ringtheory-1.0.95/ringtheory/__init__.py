4759c581f156e1c915710d1564d375f090777f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:32:01 +0000

    Add package ringtheory/1.0.95/ringtheory-1.0.95.tar.gz

---
+++ packages/ringtheory/ringtheory-1.0.95.tar.gz/ringtheory-1.0.95/ringtheory/__init__.py
@@ -0,0 +1,30 @@
+"""
+RingTheory 1.0 - High-Precision Energy-Efficient Computing
+Implementation of Recursive Autopattern Theory for GPU optimization
+"""
+
+__version__ = "1.0.95"
+__author__ = "RingTheory Team"
+__license__ = "MIT"
+
+from .gpu_optimizer import (
+    GPURingOptimizer,
+    EnergyMode,
+    gpu_energy_monitor,
+    get_gpu_power,
+    verify_precision_thorough,
+    calculate_ring_parameters,
+    create_ring_pattern,
+    calculate_informational_distance
+)
+
+__all__ = [
+    'GPURingOptimizer',
+    'EnergyMode',
+    'gpu_energy_monitor',
+    'get_gpu_power',
+    'verify_precision_thorough',
+    'calculate_ring_parameters',
+    'create_ring_pattern',
+    'calculate_informational_distance'
+]
