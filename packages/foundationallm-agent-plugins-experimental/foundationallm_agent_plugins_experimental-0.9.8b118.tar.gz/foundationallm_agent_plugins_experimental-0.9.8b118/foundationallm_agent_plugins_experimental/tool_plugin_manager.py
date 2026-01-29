348b7ed898f2077616653f69c1d180e99ab80b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:08:51 +0000

    Add package foundationallm-agent-plugins-experimental/0.9.8b118/foundationallm_agent_plugins_experimental-0.9.8b118.tar.gz

---
+++ packages/foundationallm-agent-plugins-experimental/foundationallm_agent_plugins_experimental-0.9.8b118.tar.gz/foundationallm_agent_plugins_experimental-0.9.8b118/foundationallm_agent_plugins_experimental/tool_plugin_manager.py
@@ -0,0 +1,32 @@
+from foundationallm.config import Configuration, UserIdentity
+from foundationallm.models.agents import AgentTool
+from foundationallm.langchain.common import FoundationaLLMToolBase
+from foundationallm.plugins import ToolPluginManagerBase
+
+from foundationallm_agent_plugins_experimental.tools import (
+    FoundationaLLMNopTool
+)
+
+class FoundationaLLMAgentToolExperimentalPluginManager(ToolPluginManagerBase):
+
+    FOUNDATIONALLM_NOP_TOOL_CLASS = 'FoundationaLLMNopTool'
+
+    def __init__(self):
+        super().__init__()
+
+    def create_tool(self,
+        tool_config: AgentTool,
+        objects: dict,
+        user_identity: UserIdentity,
+        config: Configuration,
+        intercept_http_calls: bool = False
+    ) -> FoundationaLLMToolBase:
+
+        match tool_config.class_name:
+            case FoundationaLLMAgentToolExperimentalPluginManager.FOUNDATIONALLM_NOP_TOOL_CLASS:
+                return FoundationaLLMNopTool(tool_config, objects, user_identity, config, intercept_http_calls=intercept_http_calls)
+            case _:
+                raise ValueError(f'Unknown tool class: {tool_config.class_name}')
+
+    def refresh_tools(self):
+        print('Refreshing tools...')
