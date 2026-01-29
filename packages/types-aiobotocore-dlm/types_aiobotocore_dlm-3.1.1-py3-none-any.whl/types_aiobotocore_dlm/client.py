c4a017e296a862e8c721e07dd3a258314ec99d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:15 +0000

    Add package types-aiobotocore-dlm/3.1.1/types_aiobotocore_dlm-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-dlm/types_aiobotocore_dlm-3.1.1-py3-none-any.whl/types_aiobotocore_dlm/client.py
@@ -0,0 +1,191 @@
+"""
+Type annotations for dlm service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_dlm.client import DLMClient
+
+    session = get_session()
+    async with session.create_client("dlm") as client:
+        client: DLMClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .type_defs import (
+    CreateLifecyclePolicyRequestTypeDef,
+    CreateLifecyclePolicyResponseTypeDef,
+    DeleteLifecyclePolicyRequestTypeDef,
+    GetLifecyclePoliciesRequestTypeDef,
+    GetLifecyclePoliciesResponseTypeDef,
+    GetLifecyclePolicyRequestTypeDef,
+    GetLifecyclePolicyResponseTypeDef,
+    ListTagsForResourceRequestTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    TagResourceRequestTypeDef,
+    UntagResourceRequestTypeDef,
+    UpdateLifecyclePolicyRequestTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Self, Unpack
+else:
+    from typing_extensions import Self, Unpack
+
+
+__all__ = ("DLMClient",)
+
+
+class Exceptions(BaseClientExceptions):
+    ClientError: type[BotocoreClientError]
+    InternalServerException: type[BotocoreClientError]
+    InvalidRequestException: type[BotocoreClientError]
+    LimitExceededException: type[BotocoreClientError]
+    ResourceNotFoundException: type[BotocoreClientError]
+
+
+class DLMClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm.html#DLM.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        DLMClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm.html#DLM.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#generate_presigned_url)
+        """
+
+    async def create_lifecycle_policy(
+        self, **kwargs: Unpack[CreateLifecyclePolicyRequestTypeDef]
+    ) -> CreateLifecyclePolicyResponseTypeDef:
+        """
+        Creates an Amazon Data Lifecycle Manager lifecycle policy.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm/client/create_lifecycle_policy.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#create_lifecycle_policy)
+        """
+
+    async def delete_lifecycle_policy(
+        self, **kwargs: Unpack[DeleteLifecyclePolicyRequestTypeDef]
+    ) -> dict[str, Any]:
+        """
+        Deletes the specified lifecycle policy and halts the automated operations that
+        the policy specified.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm/client/delete_lifecycle_policy.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#delete_lifecycle_policy)
+        """
+
+    async def get_lifecycle_policies(
+        self, **kwargs: Unpack[GetLifecyclePoliciesRequestTypeDef]
+    ) -> GetLifecyclePoliciesResponseTypeDef:
+        """
+        Gets summary information about all or the specified data lifecycle policies.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm/client/get_lifecycle_policies.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#get_lifecycle_policies)
+        """
+
+    async def get_lifecycle_policy(
+        self, **kwargs: Unpack[GetLifecyclePolicyRequestTypeDef]
+    ) -> GetLifecyclePolicyResponseTypeDef:
+        """
+        Gets detailed information about the specified lifecycle policy.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm/client/get_lifecycle_policy.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#get_lifecycle_policy)
+        """
+
+    async def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceRequestTypeDef]
+    ) -> ListTagsForResourceResponseTypeDef:
+        """
+        Lists the tags for the specified resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm/client/list_tags_for_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#list_tags_for_resource)
+        """
+
+    async def tag_resource(self, **kwargs: Unpack[TagResourceRequestTypeDef]) -> dict[str, Any]:
+        """
+        Adds the specified tags to the specified resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm/client/tag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#tag_resource)
+        """
+
+    async def untag_resource(self, **kwargs: Unpack[UntagResourceRequestTypeDef]) -> dict[str, Any]:
+        """
+        Removes the specified tags from the specified resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm/client/untag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#untag_resource)
+        """
+
+    async def update_lifecycle_policy(
+        self, **kwargs: Unpack[UpdateLifecyclePolicyRequestTypeDef]
+    ) -> dict[str, Any]:
+        """
+        Updates the specified lifecycle policy.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm/client/update_lifecycle_policy.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/#update_lifecycle_policy)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm.html#DLM.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dlm.html#DLM.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_dlm/client/)
+        """
