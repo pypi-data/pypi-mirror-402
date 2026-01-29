552f9b310dbb9134e4113396fdeeb298909be59c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 12:07:25 +0000

    Add package porringer/0.1.1.dev11/porringer-0.1.1.dev11.tar.gz

---
+++ packages/porringer/porringer-0.1.1.dev11.tar.gz/porringer-0.1.1.dev11/porringer/backend/resolver.py
@@ -0,0 +1,50 @@
+"""Resolves"""
+
+from porringer.backend.schema import Configuration, GlobalConfiguration
+from porringer.core.plugin_schema.environment import Environment
+from porringer.schema import ListPluginResults, LocalConfiguration
+from porringer.utility.utility import canonicalize_type
+
+
+def resolve_configuration(
+    local_configuration: LocalConfiguration, global_configuration: GlobalConfiguration
+) -> Configuration:
+    """Resolves the configuration.
+
+    Args:
+        local_configuration: The local configuration.
+        global_configuration: The global configuration.
+
+    Returns:
+        The resolved configuration.
+    """
+    local_configuration.cache_directory.mkdir(parents=True, exist_ok=True)
+
+    global_configuration.config_directory.mkdir(parents=True, exist_ok=True)
+    global_configuration.data_directory.mkdir(parents=True, exist_ok=True)
+
+    return Configuration(
+        cache_directory=local_configuration.cache_directory,
+        config_directory=global_configuration.config_directory,
+        data_directory=global_configuration.data_directory,
+    )
+
+
+def resolve_list_plugins_parameters(environment: list[Environment]) -> list[ListPluginResults]:
+    """Resolves the list plugins parameters.
+
+    Args:
+        environment: The environment.
+
+    Returns:
+        A list of plugin metadata.
+    """
+    plugin_metadata: list[ListPluginResults] = []
+
+    for plugin in environment:
+        canonicalized = canonicalize_type(type(plugin))
+
+        resolved_metadata = ListPluginResults(canonicalized.name, plugin.distribution.version, False)
+        plugin_metadata.append(resolved_metadata)
+
+    return plugin_metadata
