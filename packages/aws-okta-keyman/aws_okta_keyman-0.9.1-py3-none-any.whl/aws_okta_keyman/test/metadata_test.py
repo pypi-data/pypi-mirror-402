af61f6a14a619fcafd7e4d6fe25bcaa3032405c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 12:07:59 +0000

    Add package aws-okta-keyman/0.9.1/aws_okta_keyman-0.9.1-py3-none-any.whl

---
+++ packages/aws-okta-keyman/aws_okta_keyman-0.9.1-py3-none-any.whl/aws_okta_keyman/test/metadata_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+from aws_okta_keyman import metadata
+
+
+class MetadataTest(unittest.TestCase):
+    def test_version(self):
+        assert metadata.__version__
+
+    def test_init_blank_args(self):
+        assert metadata.__desc__
