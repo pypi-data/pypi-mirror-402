077edc3c35aa7778d854bf33e52e00970b0b94df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:09:15 +0000

    Add package attoworld/2026.1.1/attoworld-2026.1.1-pp311-pypy311_pp73-musllinux_1_2_aarch64.whl

---
+++ packages/attoworld/attoworld-2026.1.1-pp311-pypy311_pp73-musllinux_1_2_aarch64.whl/attoworld/personal/nick/__init__.py
@@ -0,0 +1,6 @@
+"""Contains functions primarily used by Nick Karpowicz."""
+
+from .impulse_response import get_effective_response
+from .nick import dark_plot
+
+__all__ = ["dark_plot", "get_effective_response"]
