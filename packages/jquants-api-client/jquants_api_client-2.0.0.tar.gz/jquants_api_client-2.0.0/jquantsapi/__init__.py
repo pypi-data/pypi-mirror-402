c3c8d1a2e756c418cd33bc66e58b952667cc6c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 13:27:24 +0000

    Add package jquants-api-client/2.0.0/jquants_api_client-2.0.0.tar.gz

---
+++ packages/jquants-api-client/jquants_api_client-2.0.0.tar.gz/jquants_api_client-2.0.0/jquantsapi/__init__.py
@@ -0,0 +1,6 @@
+# this version will be overwritten by poetry-dynamic-versioning
+__version__ = "2.0.0"
+
+from .client import Client
+from .client_v2 import ClientV2
+from .enums import MARKET_API_SECTIONS, BulkEndpoint
