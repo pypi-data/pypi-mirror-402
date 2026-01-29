4656cc765a17d30a70019c06397cbe24117e4af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:58 +0000

    Add package topsis-harshit-kansal-102303554/1.0.1/topsis_harshit_kansal_102303554-1.0.1.tar.gz

---
+++ packages/topsis-harshit-kansal-102303554/topsis_harshit_kansal_102303554-1.0.1.tar.gz/topsis_harshit_kansal_102303554-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="topsis-harshit-kansal-102303554",
+    version="1.0.1",
+    author="Harshit Kansal",
+    author_email="hkansal_be23@thapar.edu",
+    description="TOPSIS (MCDM) implementation for ranking alternatives using multiple criteria.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas"],
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "topsis-hk=topsis_harshit_102303554.cli:main"
+        ]
+    },
+)
