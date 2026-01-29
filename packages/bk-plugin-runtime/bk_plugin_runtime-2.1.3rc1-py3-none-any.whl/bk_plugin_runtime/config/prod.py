46f9f9fd4dcd5d4354ec51dfcf2495431add7952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 13:26:31 +0000

    Add package bk-plugin-runtime/2.1.3rc1/bk_plugin_runtime-2.1.3rc1-py3-none-any.whl

---
+++ packages/bk-plugin-runtime/bk_plugin_runtime-2.1.3rc1-py3-none-any.whl/bk_plugin_runtime/config/prod.py
@@ -0,0 +1,28 @@
+"""
+Tencent is pleased to support the open source community by making 蓝鲸智云 - PaaS平台 (BlueKing - PaaS System) available.
+Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+
+from bk_plugin_runtime.config import RUN_VER
+
+if RUN_VER == "open":
+    from blueapps.patch.settings_open_saas import *  # noqa
+else:
+    from blueapps.patch.settings_paas_services import *  # noqa
+
+# 正式环境
+RUN_MODE = "PRODUCT"
+
+default.logging_addition_settings(LOGGING)  # noqa
+
+# 插件开发者自定义配置变量
+try:
+    from bk_plugin.settings import *  # noqa
+except ImportError:
+    pass
