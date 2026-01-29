ae7b759be4a60d0b164fe694a7d9f8d0a70d647d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:09:47 +0000

    Add package pyxcp/0.25.10/pyxcp-0.25.10-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/pyxcp/pyxcp-0.25.10-cp310-cp310-macosx_11_0_arm64.whl/pyxcp/examples/xcphello.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+"""Very basic hello-world example."""
+
+from pprint import pprint
+
+from pyxcp.cmdline import ArgumentParser
+from pyxcp.utils import decode_bytes
+
+
+daq_info = False
+
+
+ap = ArgumentParser(description="pyXCP hello world.")
+
+with ap.run() as x:
+    x.connect()
+    if x.slaveProperties.optionalCommMode:
+        x.getCommModeInfo()
+    identifier = x.identifier(0x01)
+    print("\nSlave Properties:")
+    print("=================")
+    print(f"ID: {identifier!r}")
+    pprint(x.slaveProperties)
+    x.cond_unlock()
+    cps = x.getCurrentProtectionStatus()
+    print("\nProtection Status")
+    print("=================")
+    for k, v in cps.items():
+        print(f"    {k:6s}: {v}")
+    daq = x.getDaqInfo()
+    pprint(daq)
+    if daq_info:
+        dqp = x.getDaqProcessorInfo()
+        print("\nDAQ Processor Info:")
+        print("===================")
+        print(dqp)
+        print("\nDAQ Events:")
+        print("===========")
+        for idx in range(dqp.maxEventChannel):
+            evt = x.getDaqEventInfo(idx)
+            length = evt.eventChannelNameLength
+            name = decode_bytes(x.pull(length))
+            dq = "DAQ" if evt.daqEventProperties.daq else ""
+            st = "STIM" if evt.daqEventProperties.stim else ""
+            dq_st = dq + " " + st
+            print(f"    [{idx:04}] {name:r}")
+            print(f"        dir:            {dq_st}")
+            print(f"        packed:         {evt.daqEventProperties.packed}")
+            PFX_CONS = "CONSISTENCY_"
+            print(f"        consistency:    {evt.daqEventProperties.consistency.strip(PFX_CONS)}")
+            print(f"        max. DAQ lists: {evt.maxDaqList}")
+            PFX_TU = "EVENT_CHANNEL_TIME_UNIT_"
+            print(f"        unit:           {evt.eventChannelTimeUnit.strip(PFX_TU)}")
+            print(f"        cycle:          {evt.eventChannelTimeCycle or 'SPORADIC'}")
+            print(f"        priority        {evt.eventChannelPriority}")
+
+        dqr = x.getDaqResolutionInfo()
+        print("\nDAQ Resolution Info:")
+        print("====================")
+        print(dqr)
+        for idx in range(dqp.maxDaq):
+            print(f"\nDAQ List Info #{idx}")
+            print("=================")
+            print(f"{x.getDaqListInfo(idx)}")
+    x.disconnect()
