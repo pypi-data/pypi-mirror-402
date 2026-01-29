f278ebcdf31d3ad59390a080e82f998ef0e38efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:34 +0000

    Add package cchecksum/0.4.0/cchecksum-0.4.0-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/cchecksum/cchecksum-0.4.0-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/cchecksum/__init__.py
@@ -0,0 +1,15 @@
+"""CChecksum is a ~8x faster drop-in replacement for eth_utils.to_checksum_address,
+with the most cpu-intensive part implemented in c.
+
+It keeps the exact same API as the existing implementation, exceptions and all.
+"""
+
+from cchecksum._checksum import to_checksum_address, to_checksum_address_many
+from cchecksum.monkey_patch import monkey_patch_eth_utils, monkey_patch_web3py
+
+__all__ = [
+    "to_checksum_address",
+    "to_checksum_address_many",
+    "monkey_patch_eth_utils",
+    "monkey_patch_web3py",
+]
