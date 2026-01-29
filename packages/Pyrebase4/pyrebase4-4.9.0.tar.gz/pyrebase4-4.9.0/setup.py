fd871e5b952fa74293536c64e8670c3329a55e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:11:08 +0000

    Add package Pyrebase4/4.9.0/pyrebase4-4.9.0.tar.gz

---
+++ packages/Pyrebase4/pyrebase4-4.9.0.tar.gz/pyrebase4-4.9.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Pyrebase4',
+    version='4.9.0',
+    url='https://github.com/nhorvath/Pyrebase4',
+    description='A simple python wrapper for the Firebase API with current deps',
+    author='nhorvath',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='Firebase',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'requests-toolbelt>=1.0.0',
+        'requests>=2.31',
+        'urllib3>=1.21.1,<2',
+        'google-cloud-storage>=2.18.2',
+        'oauth2client>=4.1.2',
+        'python-jwt>=2.0.1',
+        'pycryptodome>=3.6.4'
+    ]
+)
