1608ad05a92c55b73c8ffca3b28fa2aba01b7cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:26 +0000

    Add package types-aiobotocore-kinesis-video-webrtc-storage/3.1.1/types_aiobotocore_kinesis_video_webrtc_storage-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-kinesis-video-webrtc-storage/types_aiobotocore_kinesis_video_webrtc_storage-3.1.1.tar.gz/types_aiobotocore_kinesis_video_webrtc_storage-3.1.1/types_aiobotocore_kinesis_video_webrtc_storage/__init__.py
@@ -0,0 +1,30 @@
+"""
+Main interface for kinesis-video-webrtc-storage service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_kinesis_video_webrtc_storage/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_kinesis_video_webrtc_storage import (
+        Client,
+        KinesisVideoWebRTCStorageClient,
+    )
+
+    session = get_session()
+    async with session.create_client("kinesis-video-webrtc-storage") as client:
+        client: KinesisVideoWebRTCStorageClient
+        ...
+
+    ```
+"""
+
+from .client import KinesisVideoWebRTCStorageClient
+
+Client = KinesisVideoWebRTCStorageClient
+
+
+__all__ = ("Client", "KinesisVideoWebRTCStorageClient")
