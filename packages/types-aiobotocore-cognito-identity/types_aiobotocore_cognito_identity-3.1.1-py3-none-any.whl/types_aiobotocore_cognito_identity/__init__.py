ed5c30e932918372c1aa66391ae62fd14fba4421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:08:02 +0000

    Add package types-aiobotocore-cognito-identity/3.1.1/types_aiobotocore_cognito_identity-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-cognito-identity/types_aiobotocore_cognito_identity-3.1.1-py3-none-any.whl/types_aiobotocore_cognito_identity/__init__.py
@@ -0,0 +1,34 @@
+"""
+Main interface for cognito-identity service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cognito_identity/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_cognito_identity import (
+        Client,
+        CognitoIdentityClient,
+        ListIdentityPoolsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("cognito-identity") as client:
+        client: CognitoIdentityClient
+        ...
+
+
+    list_identity_pools_paginator: ListIdentityPoolsPaginator = client.get_paginator("list_identity_pools")
+    ```
+"""
+
+from .client import CognitoIdentityClient
+from .paginator import ListIdentityPoolsPaginator
+
+Client = CognitoIdentityClient
+
+
+__all__ = ("Client", "CognitoIdentityClient", "ListIdentityPoolsPaginator")
---
+++ packages/types-aiobotocore-cognito-identity/types_aiobotocore_cognito_identity-3.1.1-py3-none-any.whl/types_aiobotocore_cognito_identity/paginator.py
@@ -0,0 +1,62 @@
+"""
+Type annotations for cognito-identity service client paginators.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cognito_identity/paginators/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_cognito_identity.client import CognitoIdentityClient
+    from types_aiobotocore_cognito_identity.paginator import (
+        ListIdentityPoolsPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("cognito-identity") as client:
+        client: CognitoIdentityClient
+
+        list_identity_pools_paginator: ListIdentityPoolsPaginator = client.get_paginator("list_identity_pools")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from aiobotocore.paginate import AioPageIterator, AioPaginator
+
+from .type_defs import ListIdentityPoolsInputPaginateTypeDef, ListIdentityPoolsResponseTypeDef
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ListIdentityPoolsPaginator",)
+
+
+if TYPE_CHECKING:
+    _ListIdentityPoolsPaginatorBase = AioPaginator[ListIdentityPoolsResponseTypeDef]
+else:
+    _ListIdentityPoolsPaginatorBase = AioPaginator  # type: ignore[assignment]
+
+
+class ListIdentityPoolsPaginator(_ListIdentityPoolsPaginatorBase):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cognito-identity/paginator/ListIdentityPools.html#CognitoIdentity.Paginator.ListIdentityPools)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cognito_identity/paginators/#listidentitypoolspaginator)
+    """
+
+    def paginate(  # type: ignore[override]
+        self, **kwargs: Unpack[ListIdentityPoolsInputPaginateTypeDef]
+    ) -> AioPageIterator[ListIdentityPoolsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cognito-identity/paginator/ListIdentityPools.html#CognitoIdentity.Paginator.ListIdentityPools.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_cognito_identity/paginators/#listidentitypoolspaginator)
+        """
