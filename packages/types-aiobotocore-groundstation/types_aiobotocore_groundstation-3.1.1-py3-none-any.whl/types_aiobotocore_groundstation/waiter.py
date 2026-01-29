2d4c48eca662edd4d4085b8aa71285f4df6fa182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:21 +0000

    Add package types-aiobotocore-groundstation/3.1.1/types_aiobotocore_groundstation-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-groundstation/types_aiobotocore_groundstation-3.1.1-py3-none-any.whl/types_aiobotocore_groundstation/waiter.py
@@ -0,0 +1,55 @@
+"""
+Type annotations for groundstation service client waiters.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_groundstation/waiters/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_groundstation.client import GroundStationClient
+    from types_aiobotocore_groundstation.waiter import (
+        ContactScheduledWaiter,
+    )
+
+    session = get_session()
+    async with session.create_client("groundstation") as client:
+        client: GroundStationClient
+
+        contact_scheduled_waiter: ContactScheduledWaiter = client.get_waiter("contact_scheduled")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+
+from aiobotocore.waiter import AIOWaiter
+
+from .type_defs import DescribeContactRequestWaitTypeDef
+
+if sys.version_info >= (3, 12):
+    from typing import Unpack
+else:
+    from typing_extensions import Unpack
+
+
+__all__ = ("ContactScheduledWaiter",)
+
+
+class ContactScheduledWaiter(AIOWaiter):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/groundstation/waiter/ContactScheduled.html#GroundStation.Waiter.ContactScheduled)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_groundstation/waiters/#contactscheduledwaiter)
+    """
+
+    async def wait(  # type: ignore[override]
+        self, **kwargs: Unpack[DescribeContactRequestWaitTypeDef]
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/groundstation/waiter/ContactScheduled.html#GroundStation.Waiter.ContactScheduled.wait)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_groundstation/waiters/#contactscheduledwaiter)
+        """
