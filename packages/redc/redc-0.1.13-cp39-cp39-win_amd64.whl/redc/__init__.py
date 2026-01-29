3bb3e029f055a7ce77102689e72aad6f9cbdca36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 11:11:12 +0000

    Add package redc/0.1.13/redc-0.1.13-cp39-cp39-win_amd64.whl

---
+++ packages/redc/redc-0.1.13-cp39-cp39-win_amd64.whl/redc/__init__.py
@@ -0,0 +1,50 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_11_2():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'redc.libs'))
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-redc-0.1.13')
+        if os.path.isfile(load_order_filepath):
+            import ctypes.wintypes
+            with open(os.path.join(libs_dir, '.load-order-redc-0.1.13')) as file:
+                load_order = file.read().split()
+            kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+            kernel32.LoadLibraryExW.restype = ctypes.wintypes.HMODULE
+            kernel32.LoadLibraryExW.argtypes = ctypes.wintypes.LPCWSTR, ctypes.wintypes.HANDLE, ctypes.wintypes.DWORD
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(lib_path, None, 8):
+                    raise OSError('Error loading {}; {}'.format(lib, ctypes.FormatError(ctypes.get_last_error())))
+
+
+_delvewheel_patch_1_11_2()
+del _delvewheel_patch_1_11_2
+# end delvewheel patch
+
+from . import utils
+from .callbacks import ProgressCallback, StreamCallback
+from .client import Client
+from .codes import HTTPStatus
+from .exceptions import HTTPError
+from .response import Response
+from ._version import __version__, __copyright__, __license__
+
+__all__ = [
+    "utils",
+    "ProgressCallback",
+    "StreamCallback",
+    "Client",
+    "HTTPStatus",
+    "HTTPError",
+    "Response",
+    "__version__",
+    "__copyright__",
+    "__license__",
+]
