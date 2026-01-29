4a2ec7f97280d098f819c2929b96a26aea31a8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:48 +0000

    Add package types-aiobotocore-sesv2/3.1.1/types_aiobotocore_sesv2-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-sesv2/types_aiobotocore_sesv2-3.1.1-py3-none-any.whl/types_aiobotocore_sesv2/__init__.py
@@ -0,0 +1,56 @@
+"""
+Main interface for sesv2 service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_sesv2/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_sesv2 import (
+        Client,
+        ListMultiRegionEndpointsPaginator,
+        ListReputationEntitiesPaginator,
+        ListResourceTenantsPaginator,
+        ListTenantResourcesPaginator,
+        ListTenantsPaginator,
+        SESV2Client,
+    )
+
+    session = get_session()
+    async with session.create_client("sesv2") as client:
+        client: SESV2Client
+        ...
+
+
+    list_multi_region_endpoints_paginator: ListMultiRegionEndpointsPaginator = client.get_paginator("list_multi_region_endpoints")
+    list_reputation_entities_paginator: ListReputationEntitiesPaginator = client.get_paginator("list_reputation_entities")
+    list_resource_tenants_paginator: ListResourceTenantsPaginator = client.get_paginator("list_resource_tenants")
+    list_tenant_resources_paginator: ListTenantResourcesPaginator = client.get_paginator("list_tenant_resources")
+    list_tenants_paginator: ListTenantsPaginator = client.get_paginator("list_tenants")
+    ```
+"""
+
+from .client import SESV2Client
+from .paginator import (
+    ListMultiRegionEndpointsPaginator,
+    ListReputationEntitiesPaginator,
+    ListResourceTenantsPaginator,
+    ListTenantResourcesPaginator,
+    ListTenantsPaginator,
+)
+
+Client = SESV2Client
+
+
+__all__ = (
+    "Client",
+    "ListMultiRegionEndpointsPaginator",
+    "ListReputationEntitiesPaginator",
+    "ListResourceTenantsPaginator",
+    "ListTenantResourcesPaginator",
+    "ListTenantsPaginator",
+    "SESV2Client",
+)
