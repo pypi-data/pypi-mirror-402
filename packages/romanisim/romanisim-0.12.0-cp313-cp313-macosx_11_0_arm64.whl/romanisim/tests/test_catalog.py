6d5610001b6db155f7d061ea1846e9dfffb747e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:08:50 +0000

    Add package romanisim/0.12.0/romanisim-0.12.0-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/romanisim/romanisim-0.12.0-cp313-cp313-macosx_11_0_arm64.whl/romanisim/tests/test_catalog.py
@@ -0,0 +1,278 @@
+"""
+Unit tests for catalog functions.
+"""
+
+import os
+import importlib.resources
+import pytest
+import numpy as np
+import galsim
+# from pathlib import Path
+from romanisim import catalog
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from astropy.time import Time
+from romanisim import log
+import asdf
+
+OPTICAL_ELEMS = ["F062", "F087", "F106", "F129", "F146", "F158", "F184", "F213"]
+
+
+def test_make_dummy_catalog():
+    cen = SkyCoord(ra=5 * u.deg, dec=-10 * u.deg)
+    radius = 0.2
+    nobj = 100
+    fn = os.environ.get('GALSIM_CAT_PATH', None)
+    if fn is not None:
+        fn = str(fn)
+    cat = catalog.make_dummy_catalog(
+        cen, radius=radius, seed=11, nobj=nobj, chromatic=True,
+        galaxy_sample_file_name=fn)
+    assert len(cat) == nobj
+    skycoord = SkyCoord(
+        ra=[c.sky_pos.ra / galsim.degrees * u.deg for c in cat],
+        dec=[c.sky_pos.dec / galsim.degrees * u.deg for c in cat])
+    assert np.max(cen.separation(skycoord).to(u.deg).value) < radius
+    assert cat[0].profile.spectral
+    assert cat[0].flux is None  # fluxes built into profile
+    cat = catalog.make_dummy_catalog(
+        cen, radius=radius, nobj=nobj, chromatic=False,
+        galaxy_sample_file_name=fn)
+    assert not cat[0].profile.spectral
+
+
+def test_table_catalog(tmp_path):
+    """Test generation of sources with different magnitudes and sizes
+    Demonstrates DMS217: generate parametric distributions
+    """
+    cen = SkyCoord(ra=5 * u.deg, dec=-10 * u.deg)
+    radius = 0.2
+    nobj = 200
+    bands = ['F087', 'hello']
+    table = catalog.make_dummy_table_catalog(cen, radius=radius, nobj=nobj,
+                                             bandpasses=bands)
+    coord = SkyCoord(ra=table['ra'] * u.deg, dec=table['dec'] * u.deg)
+    assert len(table) == nobj
+    assert np.max(cen.separation(coord).to(u.deg).value) < radius
+    assert np.sum(table['type'] == 'PSF') != 0
+    assert np.sum(table['type'] == 'SER') != 0
+    m = table['type'] == 'PSF'
+    assert np.all(table['n'][m] == -1)
+    assert np.all(table['n'][~m] != -1)
+    assert np.all(table['half_light_radius'][m] == 0)
+    assert np.all(table['half_light_radius'][~m] != 0)
+    assert np.all(table['pa'][m] == 0)
+    assert np.all(table['ba'][m] == 1)
+    assert np.all(table['pa'][~m] != 0)
+    assert np.all(table['ba'][~m] != 0)
+    for b in bands:
+        assert b in table.dtype.names
+    cat = catalog.table_to_catalog(table, bands)
+    assert not cat[0].profile.spectral  # table-based profiles are not chromatic
+    assert len(cat[0].flux) == len(bands)  # has some fluxes
+    assert cat[0].flux[bands[0]] == table[bands[0]][0]
+
+    tabpath = tmp_path / 'table.ecsv'
+    table.write(tabpath)
+    newtab = catalog.read_catalog(tabpath, bands)
+    newcat = catalog.table_to_catalog(newtab, bands)
+    assert len(newcat) == len(cat)
+    for c1, c2 in zip(cat, newcat):
+        assert c1.sky_pos == c2.sky_pos
+        assert c1.profile == c2.profile
+        assert np.all([c1.flux[b] == c2.flux[b] for b in bands])
+
+    table = catalog.make_dummy_table_catalog(cen, radius=radius, nobj=nobj)
+    assert ('F087' in table.dtype.names) or ('Z087' in table.dtype.names)
+
+    faintmag = 30
+    n = 1000
+    tstar = catalog.make_stars(
+        cen, n=n, radius=1, index=1, faintmag=faintmag,
+        truncation_radius=None, bandpasses=bands)
+    assert len(tstar) == n
+    assert np.all(tstar['type'] == 'PSF')
+    assert np.max(-2.5 * np.log10(tstar[bands[0]])) < faintmag + 6
+    assert np.max(-2.5 * np.log10(tstar[bands[0]])) > faintmag
+    # sigma of 1 mag dispersion added into mags, so it's okay if we go fainter
+    # than faintmag
+    # some test of index or truncation radius?
+    # large index -> more faint stars
+    tstar2 = catalog.make_stars(
+        cen, n=n, radius=1, index=10, faintmag=faintmag,
+        truncation_radius=None, bandpasses=bands)
+    assert np.median(tstar2[bands[0]]) < np.median(tstar[bands[0]])
+    # median star gets fainter when a large index is used
+    # i.e., more stars are close to the faint limit
+    tstar3 = catalog.make_stars(
+        cen, n=n, radius=1, index=1, faintmag=faintmag, truncation_radius=1,
+        bandpasses=bands)
+    tstar4 = catalog.make_stars(
+        cen, n=n, radius=1, index=1, faintmag=faintmag, truncation_radius=2,
+        bandpasses=bands)
+    cc3 = SkyCoord(ra=tstar3['ra'] * u.deg, dec=tstar3['dec'] * u.deg)
+    cc4 = SkyCoord(ra=tstar4['ra'] * u.deg, dec=tstar4['dec'] * u.deg)
+    maxsep3 = np.max(cc3.separation(cen).to(u.deg).value)
+    maxsep4 = np.max(cc4.separation(cen).to(u.deg).value)
+    assert maxsep3 < 1
+    assert maxsep4 < 2
+    assert maxsep4 > maxsep3
+
+    # make three galaxy distributions with different parameterizations
+    # distribution 1: 1" half light radius at faintmag
+    # distribution 2: very steep distribution (many faint sources)
+    # distribution 3: 100" half light radius at faint mag; bigger
+    # galaxies than distribution 1.
+    tgal1 = catalog.make_galaxies(
+        cen, n=n, radius=1, index=1, faintmag=faintmag,
+        bandpasses=bands, hlr_at_faintmag=1)
+    tgal2 = catalog.make_galaxies(
+        cen, n=n, radius=1, index=10, faintmag=faintmag,
+        bandpasses=bands)
+    tgal3 = catalog.make_galaxies(
+        cen, n=n, radius=1, index=1, faintmag=faintmag,
+        bandpasses=bands, hlr_at_faintmag=100)
+    assert np.median(tgal2[bands[0]]) < np.median(tgal1[bands[0]])
+    assert np.all(tgal1['type'] == 'SER')
+    assert np.all((tgal1['pa'] > -360) & (tgal1['pa'] < 360))
+    assert np.all((tgal1['ba'] > 0) & (tgal1['ba'] <= 1))
+    assert np.all((tgal1['n'] > 0) & (tgal1['n'] <= 8))
+    # not insane Sersic indices
+    assert np.all((tgal1['half_light_radius'] > 0)
+                  & (tgal1['half_light_radius'] < 3600))
+    # check that distribution 3 has bigger galaxies than distribution 1
+    assert (np.median(tgal3['half_light_radius'])
+            > np.median(tgal1['half_light_radius']))
+    # check that distribution 1 has brighter sources than distribution 2
+    assert (np.median(tgal1[bands[0]])
+            > np.median(tgal2[bands[0]]))
+
+    artifactdir = os.environ.get('TEST_ARTIFACT_DIR', None)
+    if artifactdir is not None:
+        af = asdf.AsdfFile()
+        af.tree = {'galsourcedist1': tgal1,
+                   'galsourcedist2': tgal2,
+                   'galsourcedist3': tgal3}
+        af.write_to(os.path.join(artifactdir, 'dms217.asdf'))
+
+    log.info('DMS217: successfully generated parametric distributions of '
+             'sources with different magnitudes and sizes.')
+
+
+def test_cosmos_table_catalog(tmp_path):
+    """Test population of sources with COSMOS objects
+    """
+    # Please do not change the seed or reduce the radius as these
+    # parameters are neccesary to make the positive flux test useful
+    cen = SkyCoord(ra=5 * u.deg, dec=-10 * u.deg)
+    radius = 0.01
+
+    cat = catalog.make_cosmos_galaxies(
+        cen, radius=radius, seed=11)
+    assert len(cat) > 0
+    skycoord = SkyCoord(
+        ra=[c['ra'] * u.deg for c in cat],
+        dec=[c['dec'] * u.deg for c in cat])
+
+    assert np.max(cen.separation(skycoord).to(u.deg).value) < radius
+
+    for bp in OPTICAL_ELEMS:
+        assert cat[0][bp] is not None
+        assert np.all(cat[bp] >= 0)
+
+
+def test_make_gaia_stars(tmp_path):
+    """Test population of sources from Gaia catalog
+    """
+    cen = SkyCoord(ra=5 * u.deg, dec=-10 * u.deg)
+    radius = 0.1
+
+    cat = catalog.make_gaia_stars(
+        cen, radius=radius)
+
+    assert len(cat) > 0
+    skycoord = SkyCoord(
+        ra=[c['ra'] * u.deg for c in cat],
+        dec=[c['dec'] * u.deg for c in cat])
+    assert np.nanmax(cen.separation(skycoord).to(u.deg).value) < radius
+
+    for bp in OPTICAL_ELEMS:
+        assert cat[0][bp] is not None
+
+
+def test_read_catalog(tmp_path):
+    """Test reading directory of healpix files of sources from Gaia catalog
+    """
+    cen = SkyCoord(ra=270.0 * u.deg, dec=66.0 * u.deg)
+    radius = 0.05
+
+    # Catalog from Gaia file
+    gf_name = str(importlib.resources.files('romanisim').joinpath('data/gaia-270-66-2027-06-01.ecsv'))
+    gf_cat = catalog.read_catalog(
+        filename=gf_name, coord=cen, date=Time('2026-01-01T00:00:00'),
+        bandpasses=OPTICAL_ELEMS, radius=radius
+    )
+
+    # Catalog from healpix directory
+    dir_name = str(importlib.resources.files('romanisim').joinpath('data'))
+    dir_cat = catalog.read_catalog(
+        filename=dir_name, coord=cen, date=Time('2026-01-01T00:00:00'),
+        bandpasses=OPTICAL_ELEMS, radius=radius
+    )
+
+    # Trim catalogs to objects within radius and sort them
+    gf_skycoord = SkyCoord(
+        ra=[c['ra'] * u.deg for c in gf_cat],
+        dec=[c['dec'] * u.deg for c in gf_cat])
+    gf_mask = cen.separation(gf_skycoord).to(u.deg).value < radius
+    gf_cat = gf_cat[gf_mask]
+    gf_cat.sort('ra')
+
+    dir_skycoord = SkyCoord(
+        ra=[c['ra'] * u.deg for c in dir_cat],
+        dec=[c['dec'] * u.deg for c in dir_cat])
+    dir_mask = cen.separation(dir_skycoord).to(u.deg).value < radius
+    dir_cat = dir_cat[dir_mask]
+    dir_cat.sort('ra')
+
+    # Both catalogs have the same number of objects,
+    # and that number is greater than 0
+    assert len(dir_cat) > 0
+    assert len(gf_cat) == len(dir_cat)
+
+    # The catalog objects are the same
+    gf_skycoord = SkyCoord(
+        ra=[c['ra'] * u.deg for c in gf_cat],
+        dec=[c['dec'] * u.deg for c in gf_cat])
+    dir_skycoord = SkyCoord(
+        ra=[c['ra'] * u.deg for c in dir_cat],
+        dec=[c['dec'] * u.deg for c in dir_cat])
+    assert np.allclose(gf_cat["ra"], dir_cat["ra"], rtol=1e-06)
+    assert np.allclose(gf_cat["dec"], dir_cat["dec"], rtol=1e-06)
+    for bp in OPTICAL_ELEMS:
+        assert np.allclose(gf_cat[bp], dir_cat[bp], rtol=1e-06)
+
+
+@pytest.mark.parametrize("cosmos", [True, False])
+@pytest.mark.parametrize("gaia", [True, False])
+@pytest.mark.parametrize("filename",
+    [None,
+     str(importlib.resources.files('romanisim').joinpath('data/COSMOS2020_CLASSIC_R1_v2.2_p3_Streamlined.fits'))])
+@pytest.mark.parametrize("date", [None, Time('2026-01-01T00:00:00')])
+def test_full_table_catalog(cosmos, gaia, filename, date, tmp_path):
+    """Test permutations of source population
+    """
+    cen = SkyCoord(ra=5 * u.deg, dec=-10 * u.deg)
+    radius = 0.1
+    cat = catalog.make_dummy_table_catalog(
+        cen, radius=radius, seed=11, cosmos=cosmos, gaia=gaia,
+        filename=filename, date=date)
+    assert len(cat) > 0
+    skycoord = SkyCoord(
+        ra=[c['ra'] * u.deg for c in cat],
+        dec=[c['dec'] * u.deg for c in cat])
+
+    assert np.nanmax(cen.separation(skycoord).to(u.deg).value) < radius
+
+    for bp in OPTICAL_ELEMS:
+        assert cat[0][bp] is not None
---
+++ packages/romanisim/romanisim-0.12.0-cp313-cp313-macosx_11_0_arm64.whl/romanisim/tests/test_wcs.py
@@ -0,0 +1,182 @@
+"""
+Unit tests for wcs module.
+"""
+import copy
+import numpy as np
+from astropy.modeling import rotations, projections, models
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from astropy.time import Time
+from romanisim import wcs, util, parameters
+import galsim
+
+import roman_datamodels
+
+
+def make_fake_distortion_function():
+    """A very simple distortion function.
+
+    This returns a very simple distortion function that scales pixels
+    by roughly their angular size on the sky and does a tangent plane
+    projection.  To make this more right we'd have to start by looking
+    at where detectors actually are relative to the boresight and put in
+    a more complicated radial distortion function than just tangent plane.
+    One could imagine that going into pix2tan.  But I don't think that
+    actually improves our tests at present so I'm leaving this as a
+    vaguely plausible ("does kind of the right thing") distortion function.
+    """
+    # distortion takes from pixels to V2 V3
+    # V2V3 is a weird spherical coordinate system where 0, 0 -> the boresight.
+    zen2v2v3 = (rotations.EulerAngleRotation(0, -90, 0, 'xyz')
+                | (models.Scale(3600) & models.Scale(3600)))
+    # zenith -> (0, 0) rotation
+    tanproj = projections.Pix2Sky_Gnomonic()
+    pix2tan = models.Scale(0.11 / 3600) & models.Scale(0.11 / 3600)
+    return pix2tan | tanproj | zen2v2v3
+
+
+def test_wcs():
+    distortion = make_fake_distortion_function()
+    cc = SkyCoord(ra=0 * u.deg, dec=0 * u.deg)
+    gwcs = wcs.make_wcs(cc, distortion)
+    assert cc.separation(gwcs.pixel_to_world(0, 0)).to(u.arcsec).value < 1e-3
+    cc2 = SkyCoord(ra=0 * u.deg, dec=0.11 * u.arcsec)
+    assert cc2.separation(gwcs.pixel_to_world(0, 1)).to(u.arcsec).value < 1e-2
+    # a tenth of a pixel
+    cc3 = SkyCoord(ra=0.11 * u.arcsec, dec=0 * u.deg)
+    assert cc3.separation(gwcs.pixel_to_world(1, 0)).to(u.arcsec).value < 1e-2
+    wcsgalsim = wcs.GWCS(gwcs)
+    assert wcsgalsim.wcs is gwcs
+    assert ((wcsgalsim.origin.x == 0) and (wcsgalsim.origin.y == 0))
+    assert repr(wcsgalsim)[:10] == 'romanisim.'
+    wcsgalsim2 = wcsgalsim.copy()
+    pos = galsim.PositionD(128, 256)
+    cc1 = wcsgalsim.toWorld(pos)
+    cc2 = wcsgalsim2.toWorld(pos)
+    assert np.allclose([cc1.ra / galsim.degrees, cc1.dec / galsim.degrees],
+                       [cc2.ra / galsim.degrees, cc2.dec / galsim.degrees])
+    pos2 = wcsgalsim.toImage(cc1)
+    assert np.allclose([pos.x, pos.y], [pos2.x, pos2.y])
+
+    # Using direct unit input to Galsim we should get identical results
+    pos3 = wcsgalsim.toImage(cc1.ra.deg, cc1.dec.deg, units="deg")
+    assert np.all((pos2.x, pos2.y) == pos3)
+ 
+    # also try some arrays
+    xx = np.random.uniform(0, 4096, 100)
+    yy = np.random.uniform(0, 4096, 100)
+    rr, dd = wcsgalsim._radec(xx, yy)
+    xx2, yy2 = wcsgalsim._xy(rr, dd)
+    assert np.allclose(xx, xx2)
+    assert np.allclose(yy, yy2)
+
+    metadata = {'instrument':
+                {
+                    'detector': 'WFI01',
+                },
+                'exposure':
+                {
+                    'start_time': Time('2026-01-01T00:00:00'),
+                }
+                }
+    wcs.fill_in_parameters(metadata, cc, boresight=True)
+    wcswrap = wcs.get_wcs(metadata, distortion=distortion)
+    cc2 = util.skycoord(wcswrap.toWorld(galsim.PositionD(0, 0)))
+    assert cc.separation(cc2).to(u.arcsec).value < 1e-3
+    celpole = SkyCoord(270 * u.deg, 66 * u.deg)
+    wcs.fill_in_parameters(metadata, celpole, boresight=True)
+    wcsgalsim = wcs.get_wcs(metadata, usecrds=False)
+    cc3 = util.skycoord(wcsgalsim.toWorld(galsim.PositionD(128, 128)))
+    assert cc3.separation(celpole).to(u.degree).value < 3
+    # big margin here of 3 deg!  The CCDs are not really at the boresight.
+    wcs.fill_in_parameters(metadata, cc, boresight=True)
+    wcswrap2 = wcs.get_wcs(metadata, distortion=distortion)
+    cc3 = util.skycoord(wcswrap2.toWorld(galsim.PositionD(0, 0)))
+    assert cc.separation(cc3).to(u.arcsec).value < 1e-3
+    wcs.fill_in_parameters(metadata, cc, boresight=False)
+    wcswrap3 = wcs.get_wcs(metadata, distortion=distortion)
+    cc4 = util.skycoord(wcswrap3.toWorld(galsim.PositionD(0, 0)))
+    # The difference between locations with and without the boresight offset
+    # should be close to the reference v2 & v3 offset.
+    assert np.abs(cc3.separation(cc4).to(u.arcsec).value
+                  - np.hypot(*parameters.v2v3_wficen)) < 1
+    metadata = dict(pointing=dict(), instrument=dict(), wcsinfo=dict(), velocity_aberration=dict())
+    metadata['instrument']['detector'] = 'WFI01'
+    util.update_pointing_and_wcsinfo_metadata(metadata, wcs.GWCS(gwcs))
+    assert metadata['wcsinfo']['aperture_name'] == 'WFI01_FULL'
+
+
+def test_wcs_from_fits_header():
+    wcs1 = wcs.get_wcs(parameters.default_parameters_dictionary,
+                       usecrds=False)
+    wcs2 = wcs.wcs_from_fits_header(wcs1.header.header)
+    xg, yg = np.meshgrid(np.linspace(0, 4088, 100), np.linspace(0, 4088, 100))
+    rd1 = np.degrees(wcs1._radec(xg.copy(), yg.copy()))
+    rd2 = wcs2.pixel_to_world(xg.copy(), yg.copy())
+    rd1 = SkyCoord(ra=rd1[0] * u.deg, dec=rd1[1] * u.deg, frame=rd2.frame)
+    sep = rd1.separation(rd2).to(u.arcsec).value
+
+    # really end up with 10^-10 arcsec in my tests, but let's say that 10^-5
+    # is fine.
+    assert np.max(sep) < 1e-5
+
+
+def test_wcs_crds_match():
+    # Set up parameters for simulation run
+
+    metadata = copy.deepcopy(parameters.default_parameters_dictionary)
+    metadata['instrument']['detector'] = 'WFI07'
+    metadata['instrument']['optical_element'] = 'F158'
+    metadata['exposure']['ma_table_number'] = 1
+
+    image_mod = roman_datamodels.datamodels.ImageModel.create_fake_data({"meta": metadata})
+    image_mod.meta.wcs = None
+
+    twcs = wcs.get_wcs(image_mod, usecrds=True)
+
+    # Expect a GWCS object as opposed to a dictionary
+    assert type(twcs) is wcs.GWCS
+
+
+def test_scale_factor():
+    """Test that specifying a scale factor actually calculates a new wcs"""
+
+    # Truth aiming for.
+    cc = SkyCoord(ra=0 * u.deg, dec=0 * u.deg)
+    scale_factor = 1.1
+    truth = SkyCoord([(0.        , 0.        ), (0.03361111, 0.0336111 ),
+                      (0.0672222 , 0.06722216), (0.10083325, 0.10083312),
+                      (0.13444424, 0.13444393)],
+                     unit='deg')
+
+    # Make a simple grid of pixel coordinates to calculate sky for
+    grid = range(0, 4096, 1000)
+
+    # Create the wcs and generate results
+    distortion = make_fake_distortion_function()
+    gwcs = wcs.make_wcs(cc, distortion, scale_factor=scale_factor)
+    sky = gwcs.pixel_to_world(grid, grid)
+
+    assert all(truth.separation(sky).to(u.arcsec).value < 1e-3)
+
+
+def test_scale_factor_negative():
+    """Test that specifying a scale factor actually calculates a new wcs"""
+
+    # Truth aiming for.
+    cc = SkyCoord(ra=0 * u.deg, dec=0 * u.deg)
+    scale_factor = -99999
+    truth = SkyCoord([(0.        , 0.        ), (0.03055555, 0.03055555),
+                      (0.06111109, 0.06111105), (0.09166659, 0.09166647),
+                      (0.12222204, 0.12222176)],
+                     unit='deg')
+
+    # Make a simple grid of pixel coordinates to calculate sky for
+    grid = range(0, 4096, 1000)
+
+    # Create the wcs and generate results
+    distortion = make_fake_distortion_function()
+    gwcs = wcs.make_wcs(cc, distortion, scale_factor=scale_factor)
+    sky = gwcs.pixel_to_world(grid, grid)
+
+    assert all(truth.separation(sky).to(u.arcsec).value < 1e-3)
