78f2c173b03490941a381a288585eea650e9e178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:48 +0000

    Add package uzoncalc/1.0.1/uzoncalc-1.0.1.tar.gz

---
+++ packages/uzoncalc/uzoncalc-1.0.1.tar.gz/uzoncalc-1.0.1/core/tests/example.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import sys
+import numpy as np
+
+# Ensure project root is on sys.path so `import core` works when running
+# this script from the `core` folder.
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
+
+from core.setup import uzon_calc
+from core.utils import *
+
+
+@uzon_calc()
+def sheet():
+    gamma_0 = 9.81 * unit.meter / unit.second**2
+
+    save("../../output/example.html")
+
+
+if __name__ == "__main__":
+    import time
+
+    t0 = time.perf_counter()
+    props = {"field1"}  # 这里按你的业务填入 inputs
+    # 异步调用 setup
+    sheet()  # type: ignore
+    t1 = time.perf_counter()
+    print(f"Execution time: {t1 - t0} seconds")
