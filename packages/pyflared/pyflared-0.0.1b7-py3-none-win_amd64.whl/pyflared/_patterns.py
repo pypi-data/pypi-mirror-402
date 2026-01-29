0b1d9cd95cdad42e9047fa377ceb1212062bbd31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 12:07:44 +0000

    Add package pyflared/0.0.1b7/pyflared-0.0.1b7-py3-none-win_amd64.whl

---
+++ packages/pyflared/pyflared-0.0.1b7-py3-none-win_amd64.whl/pyflared/_patterns.py
@@ -0,0 +1,4 @@
+starting_tunnel: bytes = b"Starting tunnel tunnelID="
+config_pattern: bytes = b"Updated to new configuration config="
+tunnel_connection_pattern: bytes = b"connIndex="
+all_tunnels_connected: bytes = b"INF Registered tunnel connection connIndex=3"
