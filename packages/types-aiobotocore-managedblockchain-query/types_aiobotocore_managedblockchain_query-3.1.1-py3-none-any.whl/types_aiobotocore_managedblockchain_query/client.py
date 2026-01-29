1be20fc4bbb52420be20e29c65ebc4f46da105a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:29 +0000

    Add package types-aiobotocore-managedblockchain-query/3.1.1/types_aiobotocore_managedblockchain_query-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-managedblockchain-query/types_aiobotocore_managedblockchain_query-3.1.1-py3-none-any.whl/types_aiobotocore_managedblockchain_query/client.py
@@ -0,0 +1,277 @@
+"""
+Type annotations for managedblockchain-query service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_managedblockchain_query.client import ManagedBlockchainQueryClient
+
+    session = get_session()
+    async with session.create_client("managedblockchain-query") as client:
+        client: ManagedBlockchainQueryClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import (
+    ListAssetContractsPaginator,
+    ListFilteredTransactionEventsPaginator,
+    ListTokenBalancesPaginator,
+    ListTransactionEventsPaginator,
+    ListTransactionsPaginator,
+)
+from .type_defs import (
+    BatchGetTokenBalanceInputTypeDef,
+    BatchGetTokenBalanceOutputTypeDef,
+    GetAssetContractInputTypeDef,
+    GetAssetContractOutputTypeDef,
+    GetTokenBalanceInputTypeDef,
+    GetTokenBalanceOutputTypeDef,
+    GetTransactionInputTypeDef,
+    GetTransactionOutputTypeDef,
+    ListAssetContractsInputTypeDef,
+    ListAssetContractsOutputTypeDef,
+    ListFilteredTransactionEventsInputTypeDef,
+    ListFilteredTransactionEventsOutputTypeDef,
+    ListTokenBalancesInputTypeDef,
+    ListTokenBalancesOutputTypeDef,
+    ListTransactionEventsInputTypeDef,
+    ListTransactionEventsOutputTypeDef,
+    ListTransactionsInputTypeDef,
+    ListTransactionsOutputTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal, Self, Unpack
+else:
+    from typing_extensions import Literal, Self, Unpack
+
+
+__all__ = ("ManagedBlockchainQueryClient",)
+
+
+class Exceptions(BaseClientExceptions):
+    AccessDeniedException: type[BotocoreClientError]
+    ClientError: type[BotocoreClientError]
+    InternalServerException: type[BotocoreClientError]
+    ResourceNotFoundException: type[BotocoreClientError]
+    ServiceQuotaExceededException: type[BotocoreClientError]
+    ThrottlingException: type[BotocoreClientError]
+    ValidationException: type[BotocoreClientError]
+
+
+class ManagedBlockchainQueryClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query.html#ManagedBlockchainQuery.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        ManagedBlockchainQueryClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query.html#ManagedBlockchainQuery.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#generate_presigned_url)
+        """
+
+    async def batch_get_token_balance(
+        self, **kwargs: Unpack[BatchGetTokenBalanceInputTypeDef]
+    ) -> BatchGetTokenBalanceOutputTypeDef:
+        """
+        Gets the token balance for a batch of tokens by using the
+        <code>BatchGetTokenBalance</code> action for every token in the request.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/batch_get_token_balance.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#batch_get_token_balance)
+        """
+
+    async def get_asset_contract(
+        self, **kwargs: Unpack[GetAssetContractInputTypeDef]
+    ) -> GetAssetContractOutputTypeDef:
+        """
+        Gets the information about a specific contract deployed on the blockchain.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/get_asset_contract.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#get_asset_contract)
+        """
+
+    async def get_token_balance(
+        self, **kwargs: Unpack[GetTokenBalanceInputTypeDef]
+    ) -> GetTokenBalanceOutputTypeDef:
+        """
+        Gets the balance of a specific token, including native tokens, for a given
+        address (wallet or contract) on the blockchain.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/get_token_balance.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#get_token_balance)
+        """
+
+    async def get_transaction(
+        self, **kwargs: Unpack[GetTransactionInputTypeDef]
+    ) -> GetTransactionOutputTypeDef:
+        """
+        Gets the details of a transaction.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/get_transaction.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#get_transaction)
+        """
+
+    async def list_asset_contracts(
+        self, **kwargs: Unpack[ListAssetContractsInputTypeDef]
+    ) -> ListAssetContractsOutputTypeDef:
+        """
+        Lists all the contracts for a given contract type deployed by an address
+        (either a contract address or a wallet address).
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/list_asset_contracts.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#list_asset_contracts)
+        """
+
+    async def list_filtered_transaction_events(
+        self, **kwargs: Unpack[ListFilteredTransactionEventsInputTypeDef]
+    ) -> ListFilteredTransactionEventsOutputTypeDef:
+        """
+        Lists all the transaction events for an address on the blockchain.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/list_filtered_transaction_events.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#list_filtered_transaction_events)
+        """
+
+    async def list_token_balances(
+        self, **kwargs: Unpack[ListTokenBalancesInputTypeDef]
+    ) -> ListTokenBalancesOutputTypeDef:
+        """
+        This action returns the following for a given blockchain network:.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/list_token_balances.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#list_token_balances)
+        """
+
+    async def list_transaction_events(
+        self, **kwargs: Unpack[ListTransactionEventsInputTypeDef]
+    ) -> ListTransactionEventsOutputTypeDef:
+        """
+        Lists all the transaction events for a transaction.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/list_transaction_events.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#list_transaction_events)
+        """
+
+    async def list_transactions(
+        self, **kwargs: Unpack[ListTransactionsInputTypeDef]
+    ) -> ListTransactionsOutputTypeDef:
+        """
+        Lists all the transaction events for a transaction.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/list_transactions.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#list_transactions)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_asset_contracts"]
+    ) -> ListAssetContractsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_filtered_transaction_events"]
+    ) -> ListFilteredTransactionEventsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_token_balances"]
+    ) -> ListTokenBalancesPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_transaction_events"]
+    ) -> ListTransactionEventsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_transactions"]
+    ) -> ListTransactionsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query.html#ManagedBlockchainQuery.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/managedblockchain-query.html#ManagedBlockchainQuery.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_managedblockchain_query/client/)
+        """
