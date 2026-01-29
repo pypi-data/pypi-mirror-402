f88303f6080ad11ea184bcc1daac44a56a84c861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:12:39 +0000

    Add package ipl3checksum/1.3.1/ipl3checksum-1.3.1-pp311-pypy311_pp73-macosx_11_0_arm64.whl

---
+++ packages/ipl3checksum/ipl3checksum-1.3.1-pp311-pypy311_pp73-macosx_11_0_arm64.whl/ipl3checksum/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2023-2024 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+# Version should be synced with pyproject.toml, Cargo.toml and src/rs/version.rs
+__version_info__: tuple[int, int, int] = (1, 3, 1)
+__version__ = ".".join(map(str, __version_info__))  # + "-dev0"
+__author__ = "Decompollaborate"
+
+from .ipl3checksum import *  # noqa: F403
+
+from . import frontends as frontends
