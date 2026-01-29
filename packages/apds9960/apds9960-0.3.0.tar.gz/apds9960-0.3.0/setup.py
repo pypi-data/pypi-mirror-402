cda803dd9e29445748424dd0ada1c10c30896baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:36 +0000

    Add package apds9960/0.3.0/apds9960-0.3.0.tar.gz

---
+++ packages/apds9960/apds9960-0.3.0.tar.gz/apds9960-0.3.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+def version():
+    with open("apds9960/__init__.py") as fd:
+        for line in fd:
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+setup(
+    name = 'apds9960',
+    packages = ['apds9960'],
+    version = version(),
+    description = 'Python APDS-9960 Library',
+    author = 'Thomas Liske',
+    author_email = 'thomas@fiasko-nw.net',
+    url = 'https://github.com/liske/python-apds9960/',
+    keywords = 'apds9960',
+    license='GPLv3+',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
