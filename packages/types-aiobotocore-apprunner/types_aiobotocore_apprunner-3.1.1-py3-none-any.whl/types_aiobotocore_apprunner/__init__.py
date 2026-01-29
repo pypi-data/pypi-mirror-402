d827170ac2f146e745271f84ad15cabf9eb76efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:48 +0000

    Add package types-aiobotocore-apprunner/3.1.1/types_aiobotocore_apprunner-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-apprunner/types_aiobotocore_apprunner-3.1.1-py3-none-any.whl/types_aiobotocore_apprunner/__init__.py
@@ -0,0 +1,30 @@
+"""
+Main interface for apprunner service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_apprunner/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_apprunner import (
+        AppRunnerClient,
+        Client,
+    )
+
+    session = get_session()
+    async with session.create_client("apprunner") as client:
+        client: AppRunnerClient
+        ...
+
+    ```
+"""
+
+from .client import AppRunnerClient
+
+Client = AppRunnerClient
+
+
+__all__ = ("AppRunnerClient", "Client")
