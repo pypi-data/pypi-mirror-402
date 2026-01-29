e1163636fe98e62777f3471ec731db7ccb00faa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 08:08:11 +0000

    Add package csvnorm/1.1.8/csvnorm-1.1.8.tar.gz

---
+++ packages/csvnorm/csvnorm-1.1.8.tar.gz/csvnorm-1.1.8/src/csvnorm/__init__.py
@@ -0,0 +1,8 @@
+"""csvnorm - Validate and normalize CSV files."""
+
+"""csvnorm package."""
+__all__ = ["normalize_csv", "detect_encoding", "process_csv"]
+
+from csvnorm.core import process_csv
+from csvnorm.encoding import detect_encoding
+from csvnorm.validation import normalize_csv
