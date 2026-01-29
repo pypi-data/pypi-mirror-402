5a925f0499b124c07c14228c89fce6db70a6df14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 06:12:38 +0000

    Add package ipl3checksum/1.3.1/ipl3checksum-1.3.1-cp38-abi3-win_amd64.whl

---
+++ packages/ipl3checksum/ipl3checksum-1.3.1-cp38-abi3-win_amd64.whl/ipl3checksum/ipl3checksum.pyi
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2023-2024 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from .cickinds import CICKind as CICKind
+
+from .checksum import calculateChecksum as calculateChecksum
+from .checksum import calculateChecksumAutodetect as calculateChecksumAutodetect
+
+from .detect import detectCIC as detectCIC
+from .detect import detectCICRaw as detectCICRaw
+
+from .exceptions import exceptions as exceptions
