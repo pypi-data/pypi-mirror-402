2aa312fee162a343f279579a4c57ce97ada930f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:24 +0000

    Add package types-aiobotocore-iottwinmaker/3.1.1/types_aiobotocore_iottwinmaker-3.1.1-py3-none-any.whl

---
+++ packages/types-aiobotocore-iottwinmaker/types_aiobotocore_iottwinmaker-3.1.1-py3-none-any.whl/types_aiobotocore_iottwinmaker/__init__.pyi
@@ -0,0 +1,29 @@
+"""
+Main interface for iottwinmaker service.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_iottwinmaker/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_iottwinmaker import (
+        Client,
+        IoTTwinMakerClient,
+    )
+
+    session = get_session()
+    async with session.create_client("iottwinmaker") as client:
+        client: IoTTwinMakerClient
+        ...
+
+    ```
+"""
+
+from .client import IoTTwinMakerClient
+
+Client = IoTTwinMakerClient
+
+__all__ = ("Client", "IoTTwinMakerClient")
