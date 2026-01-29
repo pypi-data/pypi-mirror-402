42eb0f44e6f695fc950de78cef7bc4783432e177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 04:34:21 +0000

    Add package colorful-logger/0.2.1/colorful_logger-0.2.1-py3-none-any.whl

---
+++ packages/colorful-logger/colorful_logger-0.2.1-py3-none-any.whl/colorful_logger/handlers.py
@@ -0,0 +1,56 @@
+import logging
+from logging.handlers import QueueHandler, QueueListener
+
+from colorful_logger.formatter import ColorfulFormatter
+from colorful_logger.types import StrPath
+
+
+def console_handler(
+    datefmt: str, add_file_path=True, disable_line_number_filter=False
+) -> logging.StreamHandler:
+    console_handler = logging.StreamHandler()
+
+    fmt = ColorfulFormatter(
+        datefmt=datefmt,
+        add_file_path=add_file_path,
+        disable_line_number_filter=disable_line_number_filter,
+    )
+    console_handler.setFormatter(fmt)
+    return console_handler
+
+
+def file_handler(
+    file_path: StrPath,
+    colorful: bool,
+    datefmt: str,
+    add_file_path=True,
+    disable_line_number_filter=False,
+) -> logging.FileHandler:
+    file_handler = logging.FileHandler(
+        filename=file_path,
+        mode="w",
+        encoding="utf-8",
+    )
+    if colorful:
+        fmt = ColorfulFormatter(
+            datefmt=datefmt,
+            add_file_path=add_file_path,
+            disable_line_number_filter=disable_line_number_filter,
+        )
+    else:
+        fmt = ColorfulFormatter(
+            datefmt=datefmt,
+            add_file_path=add_file_path,
+            to_file=True,
+            disable_line_number_filter=disable_line_number_filter,
+        )
+    file_handler.setFormatter(fmt)
+    return file_handler
+
+
+class ColorfulQueueHandler(QueueHandler):
+    pass
+
+
+class ColorfulQueueListener(QueueListener):
+    pass
