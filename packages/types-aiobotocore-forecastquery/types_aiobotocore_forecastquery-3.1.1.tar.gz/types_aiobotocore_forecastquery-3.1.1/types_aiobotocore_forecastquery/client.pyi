e47a0219235195bed4a1cc94df9fdf8fef690839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:07:19 +0000

    Add package types-aiobotocore-forecastquery/3.1.1/types_aiobotocore_forecastquery-3.1.1.tar.gz

---
+++ packages/types-aiobotocore-forecastquery/types_aiobotocore_forecastquery-3.1.1.tar.gz/types_aiobotocore_forecastquery-3.1.1/types_aiobotocore_forecastquery/client.pyi
@@ -0,0 +1,124 @@
+"""
+Type annotations for forecastquery service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_forecastquery/client/)
+
+Copyright 2026 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_forecastquery.client import ForecastQueryServiceClient
+
+    session = get_session()
+    async with session.create_client("forecastquery") as client:
+        client: ForecastQueryServiceClient
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from collections.abc import Mapping
+from types import TracebackType
+from typing import Any
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .type_defs import (
+    QueryForecastRequestTypeDef,
+    QueryForecastResponseTypeDef,
+    QueryWhatIfForecastRequestTypeDef,
+    QueryWhatIfForecastResponseTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Self, Unpack
+else:
+    from typing_extensions import Self, Unpack
+
+__all__ = ("ForecastQueryServiceClient",)
+
+class Exceptions(BaseClientExceptions):
+    ClientError: type[BotocoreClientError]
+    InvalidInputException: type[BotocoreClientError]
+    InvalidNextTokenException: type[BotocoreClientError]
+    LimitExceededException: type[BotocoreClientError]
+    ResourceInUseException: type[BotocoreClientError]
+    ResourceNotFoundException: type[BotocoreClientError]
+
+class ForecastQueryServiceClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/forecastquery.html#ForecastQueryService.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_forecastquery/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        ForecastQueryServiceClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/forecastquery.html#ForecastQueryService.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_forecastquery/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/forecastquery/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_forecastquery/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/forecastquery/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_forecastquery/client/#generate_presigned_url)
+        """
+
+    async def query_forecast(
+        self, **kwargs: Unpack[QueryForecastRequestTypeDef]
+    ) -> QueryForecastResponseTypeDef:
+        """
+        Retrieves a forecast for a single item, filtered by the supplied criteria.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/forecastquery/client/query_forecast.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_forecastquery/client/#query_forecast)
+        """
+
+    async def query_what_if_forecast(
+        self, **kwargs: Unpack[QueryWhatIfForecastRequestTypeDef]
+    ) -> QueryWhatIfForecastResponseTypeDef:
+        """
+        Retrieves a what-if forecast.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/forecastquery/client/query_what_if_forecast.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_forecastquery/client/#query_what_if_forecast)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/forecastquery.html#ForecastQueryService.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_forecastquery/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/forecastquery.html#ForecastQueryService.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_forecastquery/client/)
+        """
