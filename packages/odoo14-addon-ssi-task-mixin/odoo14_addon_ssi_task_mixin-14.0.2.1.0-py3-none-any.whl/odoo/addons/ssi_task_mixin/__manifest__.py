a985c9007b1963f77e4a45bbddb8e826ee712046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:16:57 +0000

    Add package odoo14-addon-ssi-task-mixin/14.0.2.1.0/odoo14_addon_ssi_task_mixin-14.0.2.1.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-task-mixin/odoo14_addon_ssi_task_mixin-14.0.2.1.0-py3-none-any.whl/odoo/addons/ssi_task_mixin/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Task Mixin",
+    "version": "14.0.2.1.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_task_timebox",
+        "ssi_project_template",
+        "ssi_decorator",
+    ],
+    "data": [
+        "templates/mixin_task_templates.xml",
+    ],
+    "demo": [],
+}
