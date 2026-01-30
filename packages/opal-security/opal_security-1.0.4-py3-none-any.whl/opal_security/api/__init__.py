16d7467b2ec3482c518e5f538bf34bdb4f05c255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:50:48 +0000

    Add package opal-security/1.0.4/opal_security-1.0.4-py3-none-any.whl

---
+++ packages/opal-security/opal_security-1.0.4-py3-none-any.whl/opal_security/api/__init__.py
@@ -0,0 +1,22 @@
+# flake8: noqa
+
+# import apis into api package
+from opal_security.api.access_rules_api import AccessRulesApi
+from opal_security.api.apps_api import AppsApi
+from opal_security.api.bundles_api import BundlesApi
+from opal_security.api.configuration_templates_api import ConfigurationTemplatesApi
+from opal_security.api.events_api import EventsApi
+from opal_security.api.group_bindings_api import GroupBindingsApi
+from opal_security.api.groups_api import GroupsApi
+from opal_security.api.idp_group_mappings_api import IdpGroupMappingsApi
+from opal_security.api.message_channels_api import MessageChannelsApi
+from opal_security.api.non_human_identities_api import NonHumanIdentitiesApi
+from opal_security.api.on_call_schedules_api import OnCallSchedulesApi
+from opal_security.api.owners_api import OwnersApi
+from opal_security.api.requests_api import RequestsApi
+from opal_security.api.resources_api import ResourcesApi
+from opal_security.api.sessions_api import SessionsApi
+from opal_security.api.tags_api import TagsApi
+from opal_security.api.uars_api import UarsApi
+from opal_security.api.users_api import UsersApi
+
