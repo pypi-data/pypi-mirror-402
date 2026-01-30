656239d0875f5f1e09f059bda42307cf696b60b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:33:54 +0000

    Add package lintrunner-adapters/0.13.0/lintrunner_adapters-0.13.0-py3-none-any.whl

---
+++ packages/lintrunner-adapters/lintrunner_adapters-0.13.0-py3-none-any.whl/lintrunner_adapters/__init__.py
@@ -0,0 +1,32 @@
+"""Adapters and tools for lintrunner."""
+
+from __future__ import annotations
+
+__all__ = [
+    "add_default_options",
+    "as_posix",
+    "available_adapters",
+    "IS_WINDOWS",
+    "LintMessage",
+    "LintSeverity",
+    "run_command",
+]
+
+import pathlib
+
+from ._common.lintrunner_common import (
+    IS_WINDOWS,
+    LintMessage,
+    LintSeverity,
+    add_default_options,
+    as_posix,
+    run_command,
+)
+
+
+def available_adapters() -> dict[str, pathlib.Path]:
+    """Return a mapping of available adapters and their paths."""
+    module_path = pathlib.Path(__file__).parent
+    adapter_paths = (module_path / "adapters").glob("*.py")
+    adapters = {path.stem: path for path in adapter_paths}
+    return adapters
