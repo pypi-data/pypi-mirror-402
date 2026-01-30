9f2e435b6bc7afc290b5dfe09c908f785d019701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:36:50 +0000

    Add package rmoney-tradion-uat/1.1.3/rmoney_tradion_uat-1.1.3-py3-none-any.whl

---
+++ packages/rmoney-tradion-uat/rmoney_tradion_uat-1.1.3-py3-none-any.whl/config/settings.py
@@ -0,0 +1,13 @@
+class SettingsProps:
+    """Settings Props settings for the SDK setup and metadata."""
+
+    # setup.py ---------------------------------------------------------------------------------------------------------
+    PIP_NAME = "rmoney-tradion-uat"
+    PIP_VERSION = "1.1.3"
+    SETUP_AUTHOR = "Codifi"
+    SETUP_AUTHOR_EMAIL = "periyasamy@codifi.in"
+    SETUP_DESCRIPTION = "Python SDK for integrating with Aliceblue trading platform."
+    SETUP_LICENSE = "MIT"
+    SETUP_URL = "https://weblive.rmoneyindia.net/"
+    SETUP_DOWNLOADABLE_URL = "https://github.com/Periyasamy-Dev/TRADEHUB_PySDK.git"
+    SETUP_APIDOCS = "https://weblive.rmoneyindia.net/apidocs/"
