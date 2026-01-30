c26d02f5729510349238cfb055c8c317b6bcedfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:16:47 +0000

    Add package jarbin-toolkit-config/0.1.0.0/jarbin_toolkit_config-0.1.0.0-py3-none-any.whl

---
+++ packages/jarbin-toolkit-config/jarbin_toolkit_config-0.1.0.0-py3-none-any.whl/jarbin_toolkit_config/__init__.py
@@ -0,0 +1,21 @@
+#############################
+###                       ###
+###     Jarbin-ToolKit    ###
+###        config         ###
+###  ----__init__.py----  ###
+###                       ###
+###=======================###
+### by JARJARBIN's STUDIO ###
+#############################
+
+
+from jarbin_toolkit_config import Config
+
+
+__all__ : list[str] = [
+    'Config'
+]
+
+
+__author__ : str = 'Nathan Jarjarbin'
+__email__ : str = 'nathan.amaraggi@epitech.eu'
