cfee171928608a7e6447fdb74372ba91c8c16af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:33:30 +0000

    Add package github-activity/1.1.5/github_activity-1.1.5-py3-none-any.whl

---
+++ packages/github-activity/github_activity-1.1.5-py3-none-any.whl/github_activity/cli.py
@@ -0,0 +1,261 @@
+import argparse
+import json
+import os
+import sys
+from subprocess import PIPE
+from subprocess import run
+
+from .git import _git_installed_check
+from .git import _git_toplevel_path
+from .github_activity import _parse_target
+from .github_activity import generate_activity_md
+from .github_activity import generate_all_activity_md
+
+DESCRIPTION = "Generate a markdown changelog of GitHub activity within a date window."
+
+# These defaults are managed by load_config_and_defaults so that they can be
+# overridden by a config file
+ARG_DEFAULTS = {
+    "heading-level": 1,
+    "include-issues": False,
+    "include-opened": False,
+    "strip-brackets": False,
+    "all": False,
+    "ignore-contributor": [],
+}
+
+parser = argparse.ArgumentParser(description=DESCRIPTION)
+parser.add_argument(
+    "-t",
+    "--target",
+    nargs="?",
+    default=None,
+    help="""The GitHub organization/repo for which you want to grab recent issues/PRs.
+    Can either be *just* an organization (e.g., `jupyter`), or a combination
+    organization and repo (e.g., `jupyter/notebook`). If the former, all
+    repositories for that org will be used. If the latter, only the specified
+    repository will be used. Can also be a GitHub URL to an organization or repo. If
+    None, the org/repo will attempt to be inferred from `git remote -v`.""",
+)
+parser.add_argument(
+    "-s",
+    "--since",
+    nargs="?",
+    default=None,
+    help="""Return issues/PRs with activity since this date or git reference. Can be
+    any string that is parsed with dateutil.parser.parse.""",
+)
+parser.add_argument(
+    "-u",
+    "--until",
+    nargs="?",
+    default=None,
+    help="""Return issues/PRs with activity until this date or git reference. Can be
+    any string that is parsed with dateutil.parser.parse. If none, today's
+    date will be used.""",
+)
+parser.add_argument(
+    "-o", "--output", default=None, help="""Write the markdown to a file if desired."""
+)
+parser.add_argument(
+    "--kind",
+    default=None,
+    help="""Return only issues or PRs. If None, both will be returned.""",
+)
+parser.add_argument(
+    "--auth",
+    default=None,
+    help=(
+        "An authentication token for GitHub. If None, then the environment "
+        "variable `GITHUB_ACCESS_TOKEN` will be tried. If it does not exist "
+        "then attempt to infer the token from `gh auth status -t`."
+    ),
+)
+parser.add_argument(
+    "--tags",
+    default=None,
+    help=(
+        "A list of the tags to use in generating subsets of PRs for the "
+        "markdown report. Must be one of:"
+        ""
+        "   ['api_change', 'new', 'enhancement', 'bug', 'maintenance', 'documentation', 'ci', 'deprecate']"
+        ""
+        "If None, all of the above tags will be used."
+    ),
+)
+parser.add_argument(
+    "--include-issues",
+    default=None,
+    action="store_true",
+    help="Include Issues in the markdown output",
+)
+parser.add_argument(
+    "--include-opened",
+    default=None,
+    action="store_true",
+    help="Include a list of opened items in the markdown output",
+)
+parser.add_argument(
+    "--strip-brackets",
+    default=None,
+    action="store_true",
+    help=(
+        "If True, strip any text between brackets at the beginning of the issue/PR title. "
+        "E.g., [MRG], [DOC], etc."
+    ),
+)
+parser.add_argument(
+    "--heading-level",
+    default=None,
+    type=int,
+    help=(
+        """Base heading level to add when generating markdown.
+
+        Useful when including changelog output in an existing document.
+
+        By default, changelog is emitted with one h1 and an h2 heading for each section.
+
+        --heading-level=2 starts at h2, etc.
+        """
+    ),
+)
+parser.add_argument(
+    "--branch",
+    "-b",
+    default=None,
+    help=(
+        """Filter pull requests by their target branch. Only PRs merged into this branch will be included. """
+    ),
+)
+parser.add_argument(
+    "--all",
+    default=None,
+    action="store_true",
+    help=("""Whether to include all the GitHub tags"""),
+)
+parser.add_argument(
+    "--ignore-contributor",
+    action="append",
+    help="Do not include this GitHub username as a contributor in the changelog",
+)
+
+# Hidden argument so that target can be optionally passed as a positional argument
+parser.add_argument(
+    "_target",
+    nargs="?",
+    default=None,
+    help=argparse.SUPPRESS,
+)
+
+
+def load_config_and_defaults(args):
+    """
+    Load .githubactivity.json from the Git top-level directory,
+    override unset args with values from .githubactivity.json,
+    and set defaults for remaining args.
+    """
+    config = {}
+    git_toplevel = _git_toplevel_path()
+    if git_toplevel:
+        try:
+            with open(os.path.join(git_toplevel, ".githubactivity.json")) as f:
+                config = json.load(f)
+        except FileNotFoundError:
+            pass
+
+    # Treat args as a dict
+    # https://docs.python.org/3/library/argparse.html#the-namespace-object
+    for argname in vars(args):
+        configname = argname.replace("_", "-")
+        if getattr(args, argname) is None:
+            setattr(args, argname, config.get(configname, ARG_DEFAULTS.get(configname)))
+
+
+def main():
+    if not _git_installed_check():
+        print("git is required to run github-activity", file=sys.stderr)
+        sys.exit(1)
+
+    args = parser.parse_args()
+    if args.target and args._target:
+        raise ValueError(
+            "target cannot be passed as both a positional and keyword argument"
+        )
+    if not args.target:
+        args.target = args._target
+
+    load_config_and_defaults(args)
+
+    tags = args.tags.split(",") if args.tags is not None else args.tags
+    # Automatically detect the target from remotes if we haven't had one passed.
+    if not args.target:
+        err = "Could not automatically detect remote, and none was given."
+        try:
+            out = run("git remote -v".split(), stdout=PIPE)
+            remotes = out.stdout.decode().split("\n")
+            remotes = [ii for ii in remotes if ii]
+            remotes = {
+                ii.split("\t")[0]: ii.split("\t")[1].split()[0] for ii in remotes
+            }
+            if "upstream" in remotes:
+                ref = remotes["upstream"]
+            elif "origin" in remotes:
+                ref = remotes["origin"]
+            else:
+                ref = None
+            if not ref:
+                raise ValueError(err)
+
+            org, repo = _parse_target(ref)
+            if repo:
+                args.target = f"{org}/{repo}"
+            else:
+                args.target = f"{org}"
+        except Exception:
+            raise ValueError(err)
+
+    common_kwargs = dict(
+        kind=args.kind,
+        auth=args.auth,
+        tags=tags,
+        include_issues=bool(args.include_issues),
+        include_opened=bool(args.include_opened),
+        strip_brackets=bool(args.strip_brackets),
+        branch=args.branch,
+        ignored_contributors=args.ignore_contributor,
+    )
+
+    # Wrap in a try/except so we don't have an ugly stack trace if there's an error
+    try:
+        if args.all:
+            md = generate_all_activity_md(args.target, **common_kwargs)
+
+        else:
+            md = generate_activity_md(
+                args.target,
+                since=args.since,
+                until=args.until,
+                heading_level=args.heading_level,
+                **common_kwargs,
+            )
+
+        if not md:
+            return
+
+        if args.output:
+            output = os.path.abspath(args.output)
+            output_dir = os.path.dirname(output)
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            with open(args.output, "w") as ff:
+                ff.write(md)
+            print(f"Finished writing markdown to: {args.output}", file=sys.stderr)
+        else:
+            print(md)
+    except ValueError as e:
+        print(f"Error: {e}", file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/github-activity/github_activity-1.1.5-py3-none-any.whl/github_activity/graphql.py
@@ -0,0 +1,375 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import requests
+from tqdm.auto import tqdm
+
+from .auth import TokenAuth
+
+comments_query = """\
+        comments(last: 100) {
+          edges {
+            node {
+              authorAssociation
+              createdAt
+              updatedAt
+              url
+              author {
+                login
+                __typename
+              }
+            }
+          }
+        }
+"""
+
+reviews_query = """\
+        reviews(last: 100) {
+          edges {
+            node {
+              authorAssociation
+              author {
+                login
+                __typename
+              }
+            }
+          }
+        }
+"""
+
+commits_query = """\
+        commits(first: 100) {
+          edges {
+            node {
+              commit {
+                committer {
+                  user {
+                    login
+                    __typename
+                  }
+                }
+                authors(first: 10) {
+                  edges {
+                    node {
+                      user {
+                        login
+                        __typename
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+"""
+
+base_elements = """\
+        state
+        id
+        title
+        url
+        createdAt
+        updatedAt
+        closedAt
+        labels(first: 10) {
+            edges {
+                node {
+                    name
+                }
+            }
+        }
+        number
+        authorAssociation
+        author {
+          login
+          __typename
+        }
+        reactions(content: THUMBS_UP) {
+          totalCount
+        }
+"""
+
+gql_template = """\
+{{
+  search({query}) {{
+    issueCount
+    pageInfo {{
+        endCursor
+        hasNextPage
+    }}
+    nodes {{
+      ... on PullRequest {{
+        {base_elements}
+        mergedBy {{
+          login
+          __typename
+        }}
+        mergeCommit {{
+          oid
+        }}
+        baseRefName
+        {comments}
+        {commits}
+        {reviews}
+      }}
+      ... on Issue {{
+        {base_elements}
+        {comments}
+      }}
+    }}
+  }}
+}}
+"""
+
+
+# Define our query object that we'll re-use for github search
+class GitHubGraphQlQuery:
+    def __init__(self, query, display_progress=True, auth=None):
+        """Run a GitHub GraphQL query and return the issue/PR data from it.
+
+        Parameters
+        ----------
+        query : string
+          The GitHub search query to run. This is similar to whatever you'd use
+          to search on GitHub.com.
+        display_progress : bool
+          Whether to display a progress bar as data is fetched.
+        auth : string | None
+          An authentication token for GitHub. If None, then the environment
+          variable `GITHUB_ACCESS_TOKEN` will be tried.
+        """
+        self.query = query
+        self.bot_users = set()  # Store detected bot usernames
+
+        # Authentication
+        token = auth or os.environ.get("GITHUB_ACCESS_TOKEN")
+        if not token:
+            raise ValueError(
+                "Either the environment variable GITHUB_ACCESS_TOKEN or the "
+                "--auth flag or must be used to pass a Personal Access Token "
+                "needed by the GitHub API. You can generate a token at "
+                "https://github.com/settings/tokens/new. Note that while "
+                "working with a public repository, you don't need to set any "
+                "scopes on the token you create."
+            )
+        self.auth = TokenAuth(token)
+
+        self.gql_template = gql_template
+        self.display_progress = display_progress
+
+    def request(self, n_pages=100, n_per_page=50):
+        """Make a request to the GitHub GraphQL API.
+
+        This generates an attribute `self.data` with a pandas
+        DataFrame of the issue / PR activity corresponding to
+        the query you ran.
+        """
+
+        # NOTE: This main search query has a type, but the query string also has a type.
+        # ref ("search"): https://developer.github.com/v4/query/#connections
+        # Collect paginated issues
+        pageInfo = None
+        self.issues_and_or_prs = []
+        for ii in range(n_pages):
+            github_search_query = [
+                "first: %s" % n_per_page,
+                'query: "%s"' % self.query,
+                "type: ISSUE",
+            ]
+            if ii != 0:
+                github_search_query.append('after: "%s"' % pageInfo["endCursor"])
+
+            ii_gql_query = self.gql_template.format(
+                query=", ".join(github_search_query),
+                comments=comments_query,
+                base_elements=base_elements,
+                reviews=reviews_query,
+                commits=commits_query,
+            )
+            ii_request = requests.post(
+                "https://api.github.com/graphql",
+                json={"query": ii_gql_query},
+                auth=self.auth,
+            )
+            if ii_request.status_code != 200:
+                # Check for common error cases and provide helpful messages
+                if ii_request.status_code == 403:
+                    try:
+                        error_data = ii_request.json()
+                        error_message = error_data.get("message", "")
+                        if "rate limit" in error_message.lower():
+                            raise Exception(
+                                f"GitHub API rate limit exceeded. {error_message}\n"
+                                "Please wait before making more requests, or use an authentication token with higher rate limits."
+                            )
+                        else:
+                            # Generic 403 - likely a permissions issue
+                            raise Exception(
+                                f"GitHub API access forbidden. {error_message}\n"
+                                "This usually means your authentication token doesn't have the required permissions.\n"
+                                "Please check that your token has the necessary scopes for this repository."
+                            )
+                    except (ValueError, KeyError):
+                        pass
+                raise Exception(
+                    "Query failed to run by returning code of {}. {}".format(
+                        ii_request.status_code, ii_gql_query
+                    )
+                )
+            errors = ii_request.json().get("errors")
+            if errors:
+                # Check for rate limit errors in GraphQL response
+                for error in errors:
+                    if error.get("type") == "RATE_LIMITED":
+                        error_message = error.get("message", "Rate limit exceeded")
+                        raise Exception(
+                            f"GitHub API rate limit exceeded. {error_message}\n"
+                            "Please wait before making more requests, or use an authentication token with higher rate limits."
+                        )
+                raise Exception(
+                    "Query failed to run with error {}. {}".format(errors, ii_gql_query)
+                )
+            self.last_request = ii_request
+
+            # Parse the response for this pagination
+            json = ii_request.json()["data"]["search"]
+            if ii == 0:
+                if json["issueCount"] == 0:
+                    print("Found no entries for query.", file=sys.stderr)
+                    self.data = pd.DataFrame()
+                    return
+
+                n_pages = int(np.ceil(json["issueCount"] / n_per_page))
+                print(
+                    "Found {} items, which will take {} pages".format(
+                        json["issueCount"], n_pages
+                    ),
+                    file=sys.stderr,
+                )
+                prog = tqdm(
+                    total=json["issueCount"],
+                    desc="Downloading:",
+                    unit="issues",
+                    disable=n_pages == 1 or not self.display_progress,
+                )
+
+            # Add the JSON to the raw data list
+            self.issues_and_or_prs.extend(json["nodes"])
+            pageInfo = json["pageInfo"]
+            self.last_query = ii_gql_query
+
+            # Update progress and should we stop?
+            prog.update(len(json["nodes"]))
+            if not pageInfo["hasNextPage"]:
+                break
+
+        # Extract bot users from raw data before DataFrame conversion
+        def is_bot(user_dict):
+            """Check if a GraphQL user object represents a bot account."""
+            return user_dict and user_dict.get("__typename") == "Bot"
+
+        bot_users = set()
+        for item in self.issues_and_or_prs:
+            # Check author
+            author = item.get("author")
+            if is_bot(author):
+                bot_users.add(author["login"])
+
+            # Check mergedBy
+            merged_by = item.get("mergedBy")
+            if is_bot(merged_by):
+                bot_users.add(merged_by["login"])
+
+            # Check reviewers
+            reviews = item.get("reviews")
+            if reviews:
+                for review in reviews.get("edges", []):
+                    review_author = review["node"].get("author")
+                    if is_bot(review_author):
+                        bot_users.add(review_author["login"])
+
+            # Check commenters
+            comments = item.get("comments")
+            if comments:
+                for comment in comments.get("edges", []):
+                    comment_author = comment["node"].get("author")
+                    if is_bot(comment_author):
+                        bot_users.add(comment_author["login"])
+
+            # Check commit authors and committers
+            commits = item.get("commits")
+            if commits:
+                for commit_edge in commits.get("edges", []):
+                    commit = commit_edge["node"]["commit"]
+                    # Check committer
+                    committer = commit.get("committer")
+                    if committer and committer.get("user"):
+                        if is_bot(committer["user"]):
+                            bot_users.add(committer["user"]["login"])
+                    # Check authors
+                    authors = commit.get("authors")
+                    if authors:
+                        for author_edge in authors.get("edges", []):
+                            author_user = author_edge["node"].get("user")
+                            if author_user and is_bot(author_user):
+                                bot_users.add(author_user["login"])
+
+        # Create a dataframe of the issues and/or PRs
+        self.data = pd.DataFrame(self.issues_and_or_prs)
+        self.data.attrs["bot_users"] = bot_users
+
+        # Add some extra fields
+        def get_login(user):
+            return user["login"] if pd.notna(user) else user
+
+        self.data["author"] = self.data["author"].map(get_login)
+        self.data["mergedBy"] = self.data["mergedBy"].map(get_login)
+
+        self.data["org"] = self.data["url"].map(lambda a: a.split("/")[3])
+        self.data["repo"] = self.data["url"].map(lambda a: a.split("/")[4])
+        self.data["labels"] = self.data["labels"].map(
+            lambda a: [edge["node"]["name"] for edge in a["edges"]]
+        )
+        self.data["kind"] = self.data["url"].map(
+            lambda a: "issue" if "issues/" in a else "pr"
+        )
+
+        self.data["thumbsup"] = self.data["reactions"].map(lambda a: a["totalCount"])
+        self.data.drop(columns="reactions", inplace=True)
+
+        def get_reviewers(reviews):
+            """map review graph to unique list of reviewers"""
+            if pd.isna(reviews) or not reviews:
+                return []
+            return sorted(
+                set([review["node"]["author"]["login"] for review in reviews["edges"]])
+            )
+
+        self.data["reviewers"] = self.data["reviews"].map(get_reviewers)
+
+        def get_committers(commits):
+            """map commit graph to non-unique ordered list of commit authors"""
+            if pd.isna(commits) or not commits:
+                return []
+            committers = []
+            for commit in commits["edges"]:
+                commit = commit["node"]["commit"]
+                commit_authors = []
+                for author in commit["authors"]["edges"]:
+                    author = author["node"]
+                    if author and author["user"]:
+                        commit_authors.append(author["user"]["login"])
+                    committer = commit["committer"]
+                    if (
+                        committer
+                        and committer["user"]
+                        and committer["user"]["login"] not in commit_authors
+                    ):
+                        commit_authors.append(committer["user"]["login"])
+                committers.extend(commit_authors)
+            return committers
+
+        self.data["committers"] = self.data["commits"].map(get_committers)
