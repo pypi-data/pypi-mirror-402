f9b9be4741c85d1be82d1ff51b36af8271525cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:16:03 +0000

    Add package yoel-ar-bast-paketet/0.0.1/yoel_ar_bast_paketet-0.0.1-py3-none-any.whl

---
+++ packages/yoel-ar-bast-paketet/yoel_ar_bast_paketet-0.0.1-py3-none-any.whl/yoel_best/__init__.py
@@ -0,0 +1,6 @@
+# Denna kod körs direkt när någon skriver "import yoel_best"
+print("Yoel är bäst")
+
+# En extra funktion ifall man vill dubbelkolla
+def vem_ar_bast():
+    return "Det är fortfarande Yoel."
