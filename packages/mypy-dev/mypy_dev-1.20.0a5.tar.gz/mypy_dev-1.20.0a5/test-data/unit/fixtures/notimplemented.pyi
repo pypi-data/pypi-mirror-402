cd10d3486b3c4d856bcbbfcd455a98f8458870b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:35:41 +0000

    Add package mypy-dev/1.20.0a5/mypy_dev-1.20.0a5.tar.gz

---
+++ packages/mypy-dev/mypy_dev-1.20.0a5.tar.gz/mypy_dev-1.20.0a5/test-data/unit/fixtures/notimplemented.pyi
@@ -0,0 +1,26 @@
+# builtins stub used in NotImplemented related cases.
+from typing import Any
+
+class object:
+    def __init__(self) -> None: pass
+
+class type: pass
+class function: pass
+class bool: pass
+class int: pass
+class str: pass
+class dict: pass
+class tuple: pass
+class ellipsis: pass
+class list: pass
+
+import sys
+
+if sys.version_info >= (3, 10):  # type: ignore
+    from types import NotImplementedType
+    NotImplemented: NotImplementedType
+else:
+    class _NotImplementedType(Any): ...
+    NotImplemented: _NotImplementedType
+
+class BaseException: pass
