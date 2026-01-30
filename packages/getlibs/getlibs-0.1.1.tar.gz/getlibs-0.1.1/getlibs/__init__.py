bc1259b0b06fe74a3d479d4aed1df81168d6aded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:35:58 +0000

    Add package getlibs/0.1.1/getlibs-0.1.1.tar.gz

---
+++ packages/getlibs/getlibs-0.1.1.tar.gz/getlibs-0.1.1/getlibs/__init__.py
@@ -0,0 +1,10 @@
+"""
+getlibs
+
+Python projeleri için dependency ve import analiz aracı.
+"""
+
+from .cli import analyze_project  # cli.py içindeki fonksiyonu import et
+
+__all__ = ["analyze_project"]
+__version__ = "0.1.1"
