4b4096bba7898e56ba51290abc896b3d20be550d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:09:56 +0000

    Add package aspose-cells-gridjs-net-python/26.1.0/aspose_cells_gridjs_net_python-26.1.0-py3-none-win32.whl

---
+++ packages/aspose-cells-gridjs-net-python/aspose_cells_gridjs_net_python-26.1.0-py3-none-win32.whl/aspose/cellsgridjs/__pyinstaller/hook-aspose.cellsgridjs.py
@@ -0,0 +1,9 @@
+from PyInstaller.utils.hooks import get_package_paths
+import os.path
+
+(_, root) = get_package_paths('aspose')
+
+datas = [(os.path.join(root, 'assemblies', 'cellsgridjs'), os.path.join('aspose', 'assemblies', 'cellsgridjs'))]
+
+hiddenimports = [ 'aspose', 'aspose.pyreflection', 'aspose.pydrawing', 'aspose.pygc', 'aspose.pycore' ]
+
