082173803e8d88010c1d24c20d509d3269aa41e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:24:38 +0000

    Add package huaweicloudsdkdis/3.1.184/huaweicloudsdkdis-3.1.184-py3-none-any.whl

---
+++ packages/huaweicloudsdkdis/huaweicloudsdkdis-3.1.184-py3-none-any.whl/huaweicloudsdkdis/v2/model/consume_records_request.py
@@ -0,0 +1,136 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class ConsumeRecordsRequest:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'partition_cursor': 'str',
+        'max_fetch_bytes': 'int'
+    }
+
+    attribute_map = {
+        'partition_cursor': 'partition-cursor',
+        'max_fetch_bytes': 'max_fetch_bytes'
+    }
+
+    def __init__(self, partition_cursor=None, max_fetch_bytes=None):
+        r"""ConsumeRecordsRequest
+
+        The model defined in huaweicloud sdk
+
+        :param partition_cursor: 数据游标，需要先通过获取数据游标的接口获取。  取值范围：1~512个字符。  说明：  数据游标有效期为5分钟。
+        :type partition_cursor: str
+        :param max_fetch_bytes: 每个请求获取记录的最大字节数。  注意：  该值如果小于分区中单条记录的大小，会导致一直无法获取到记录。
+        :type max_fetch_bytes: int
+        """
+        
+        
+
+        self._partition_cursor = None
+        self._max_fetch_bytes = None
+        self.discriminator = None
+
+        self.partition_cursor = partition_cursor
+        if max_fetch_bytes is not None:
+            self.max_fetch_bytes = max_fetch_bytes
+
+    @property
+    def partition_cursor(self):
+        r"""Gets the partition_cursor of this ConsumeRecordsRequest.
+
+        数据游标，需要先通过获取数据游标的接口获取。  取值范围：1~512个字符。  说明：  数据游标有效期为5分钟。
+
+        :return: The partition_cursor of this ConsumeRecordsRequest.
+        :rtype: str
+        """
+        return self._partition_cursor
+
+    @partition_cursor.setter
+    def partition_cursor(self, partition_cursor):
+        r"""Sets the partition_cursor of this ConsumeRecordsRequest.
+
+        数据游标，需要先通过获取数据游标的接口获取。  取值范围：1~512个字符。  说明：  数据游标有效期为5分钟。
+
+        :param partition_cursor: The partition_cursor of this ConsumeRecordsRequest.
+        :type partition_cursor: str
+        """
+        self._partition_cursor = partition_cursor
+
+    @property
+    def max_fetch_bytes(self):
+        r"""Gets the max_fetch_bytes of this ConsumeRecordsRequest.
+
+        每个请求获取记录的最大字节数。  注意：  该值如果小于分区中单条记录的大小，会导致一直无法获取到记录。
+
+        :return: The max_fetch_bytes of this ConsumeRecordsRequest.
+        :rtype: int
+        """
+        return self._max_fetch_bytes
+
+    @max_fetch_bytes.setter
+    def max_fetch_bytes(self, max_fetch_bytes):
+        r"""Sets the max_fetch_bytes of this ConsumeRecordsRequest.
+
+        每个请求获取记录的最大字节数。  注意：  该值如果小于分区中单条记录的大小，会导致一直无法获取到记录。
+
+        :param max_fetch_bytes: The max_fetch_bytes of this ConsumeRecordsRequest.
+        :type max_fetch_bytes: int
+        """
+        self._max_fetch_bytes = max_fetch_bytes
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, ConsumeRecordsRequest):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/huaweicloudsdkdis/huaweicloudsdkdis-3.1.184-py3-none-any.whl/huaweicloudsdkdis/v2/model/show_cursor_response.py
@@ -0,0 +1,112 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.sdk_response import SdkResponse
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class ShowCursorResponse(SdkResponse):
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'partition_cursor': 'str'
+    }
+
+    attribute_map = {
+        'partition_cursor': 'partition_cursor'
+    }
+
+    def __init__(self, partition_cursor=None):
+        r"""ShowCursorResponse
+
+        The model defined in huaweicloud sdk
+
+        :param partition_cursor: 数据游标。  取值范围：1~512个字符。  说明：  数据游标有效期为5分钟。
+        :type partition_cursor: str
+        """
+        
+        super().__init__()
+
+        self._partition_cursor = None
+        self.discriminator = None
+
+        if partition_cursor is not None:
+            self.partition_cursor = partition_cursor
+
+    @property
+    def partition_cursor(self):
+        r"""Gets the partition_cursor of this ShowCursorResponse.
+
+        数据游标。  取值范围：1~512个字符。  说明：  数据游标有效期为5分钟。
+
+        :return: The partition_cursor of this ShowCursorResponse.
+        :rtype: str
+        """
+        return self._partition_cursor
+
+    @partition_cursor.setter
+    def partition_cursor(self, partition_cursor):
+        r"""Sets the partition_cursor of this ShowCursorResponse.
+
+        数据游标。  取值范围：1~512个字符。  说明：  数据游标有效期为5分钟。
+
+        :param partition_cursor: The partition_cursor of this ShowCursorResponse.
+        :type partition_cursor: str
+        """
+        self._partition_cursor = partition_cursor
+
+    def to_dict(self):
+        import warnings
+        warnings.warn("ShowCursorResponse.to_dict() is deprecated and no longer maintained, "
+                      "use to_json_object() to get the response content.", DeprecationWarning)
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, ShowCursorResponse):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
