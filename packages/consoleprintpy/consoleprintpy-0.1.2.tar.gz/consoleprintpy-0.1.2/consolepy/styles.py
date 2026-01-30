ede8a53a83409dbb2ffc7525dd8d2a160036cfce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:34:36 +0000

    Add package consoleprintpy/0.1.2/consoleprintpy-0.1.2.tar.gz

---
+++ packages/consoleprintpy/consoleprintpy-0.1.2.tar.gz/consoleprintpy-0.1.2/consolepy/styles.py
@@ -0,0 +1,30 @@
+ANSI = {
+    "reset": "\033[0m",
+
+    # estilos
+    "bold": "\033[1m",
+    "underline": "\033[4m",
+    "italic": "\033[3m",
+    "strike": "\033[9m",
+
+    # cores
+    "black": "\033[30m",
+    "red": "\033[31m",
+    "green": "\033[32m",
+    "yellow": "\033[33m",
+    "blue": "\033[34m",
+    "grey": "\033[90m",
+    "magenta": "\033[35m",
+    "cyan": "\033[36m",
+    "white": "\033[37m",
+
+    # backgrounds
+    "bg_black": "\033[40m",
+    "bg_red": "\033[41m",
+    "bg_green": "\033[42m",
+    "bg_yellow": "\033[43m",
+    "bg_blue": "\033[44m",
+    "bg_magenta": "\033[45m",
+    "bg_cyan": "\033[46m",
+    "bg_white": "\033[47m",
+}
