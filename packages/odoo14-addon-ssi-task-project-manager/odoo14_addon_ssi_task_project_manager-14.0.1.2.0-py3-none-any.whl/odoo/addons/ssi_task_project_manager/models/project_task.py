7bd3c64d8c2ae9d24fded690fe8431046b81b989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:16:57 +0000

    Add package odoo14-addon-ssi-task-project-manager/14.0.1.2.0/odoo14_addon_ssi_task_project_manager-14.0.1.2.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-task-project-manager/odoo14_addon_ssi_task_project_manager-14.0.1.2.0-py3-none-any.whl/odoo/addons/ssi_task_project_manager/models/project_task.py
@@ -0,0 +1,18 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProjectTask(models.Model):
+    _name = "project.task"
+    _inherit = "project.task"
+
+    project_manager_id = fields.Many2one(
+        string="Project Manager",
+        comodel_name="res.users",
+        related="project_id.user_id",
+        store=True,
+        compute_sudo=True,
+    )
