dd1fe0fed21e7bf78b972110d0e94a83d8d1687b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:47 +0000

    Add package Monei/2.5.2/monei-2.5.2-py3-none-any.whl

---
+++ packages/Monei/monei-2.5.2-py3-none-any.whl/Monei/models/__init__.py
@@ -0,0 +1,136 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from Monei.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from Monei.model.activate_apple_pay_certificate_request import (
+    ActivateApplePayCertificateRequest,
+)
+from Monei.model.activate_subscription_request import ActivateSubscriptionRequest
+from Monei.model.address import Address
+from Monei.model.api_exception import ApiException
+from Monei.model.apple_pay_certificate import ApplePayCertificate
+from Monei.model.apple_pay_certificates_remove200_response import (
+    ApplePayCertificatesRemove200Response,
+)
+from Monei.model.bad_request_error import BadRequestError
+from Monei.model.bad_request_error_all_of import BadRequestErrorAllOf
+from Monei.model.bizum_validate_phone200_response import BizumValidatePhone200Response
+from Monei.model.cancel_payment_request import CancelPaymentRequest
+from Monei.model.cancel_subscription_request import CancelSubscriptionRequest
+from Monei.model.capture_payment_request import CapturePaymentRequest
+from Monei.model.confirm_payment_request import ConfirmPaymentRequest
+from Monei.model.confirm_payment_request_payment_method import (
+    ConfirmPaymentRequestPaymentMethod,
+)
+from Monei.model.confirm_payment_request_payment_method_card import (
+    ConfirmPaymentRequestPaymentMethodCard,
+)
+from Monei.model.create_payment_request import CreatePaymentRequest
+from Monei.model.create_subscription_request import CreateSubscriptionRequest
+from Monei.model.internal_server_error import InternalServerError
+from Monei.model.internal_server_error_all_of import InternalServerErrorAllOf
+from Monei.model.not_found_error import NotFoundError
+from Monei.model.not_found_error_all_of import NotFoundErrorAllOf
+from Monei.model.pause_subscription_request import PauseSubscriptionRequest
+from Monei.model.payment import Payment
+from Monei.model.payment_billing_details import PaymentBillingDetails
+from Monei.model.payment_cancellation_reason import PaymentCancellationReason
+from Monei.model.payment_customer import PaymentCustomer
+from Monei.model.payment_last_refund_reason import PaymentLastRefundReason
+from Monei.model.payment_message_channel import PaymentMessageChannel
+from Monei.model.payment_message_language import PaymentMessageLanguage
+from Monei.model.payment_methods import PaymentMethods
+from Monei.model.payment_methods_metadata import PaymentMethodsMetadata
+from Monei.model.payment_methods_metadata_alipay import PaymentMethodsMetadataAlipay
+from Monei.model.payment_methods_metadata_apple_pay import (
+    PaymentMethodsMetadataApplePay,
+)
+from Monei.model.payment_methods_metadata_bancontact import (
+    PaymentMethodsMetadataBancontact,
+)
+from Monei.model.payment_methods_metadata_bizum import PaymentMethodsMetadataBizum
+from Monei.model.payment_methods_metadata_blik import PaymentMethodsMetadataBlik
+from Monei.model.payment_methods_metadata_card import PaymentMethodsMetadataCard
+from Monei.model.payment_methods_metadata_click_to_pay import (
+    PaymentMethodsMetadataClickToPay,
+)
+from Monei.model.payment_methods_metadata_click_to_pay_discover import (
+    PaymentMethodsMetadataClickToPayDiscover,
+)
+from Monei.model.payment_methods_metadata_click_to_pay_mastercard import (
+    PaymentMethodsMetadataClickToPayMastercard,
+)
+from Monei.model.payment_methods_metadata_click_to_pay_visa import (
+    PaymentMethodsMetadataClickToPayVisa,
+)
+from Monei.model.payment_methods_metadata_eps import PaymentMethodsMetadataEps
+from Monei.model.payment_methods_metadata_giropay import PaymentMethodsMetadataGiropay
+from Monei.model.payment_methods_metadata_google_pay import (
+    PaymentMethodsMetadataGooglePay,
+)
+from Monei.model.payment_methods_metadata_i_deal import PaymentMethodsMetadataIDeal
+from Monei.model.payment_methods_metadata_klarna import PaymentMethodsMetadataKlarna
+from Monei.model.payment_methods_metadata_mbway import PaymentMethodsMetadataMbway
+from Monei.model.payment_methods_metadata_sepa import PaymentMethodsMetadataSepa
+from Monei.model.payment_methods_metadata_sofort import PaymentMethodsMetadataSofort
+from Monei.model.payment_methods_metadata_trustly import PaymentMethodsMetadataTrustly
+from Monei.model.payment_methods_methods import PaymentMethodsMethods
+from Monei.model.payment_next_action import PaymentNextAction
+from Monei.model.payment_payment_method import PaymentPaymentMethod
+from Monei.model.payment_payment_method_bizum import PaymentPaymentMethodBizum
+from Monei.model.payment_payment_method_bizum_input import (
+    PaymentPaymentMethodBizumInput,
+)
+from Monei.model.payment_payment_method_card import PaymentPaymentMethodCard
+from Monei.model.payment_payment_method_card_input import PaymentPaymentMethodCardInput
+from Monei.model.payment_payment_method_input import PaymentPaymentMethodInput
+from Monei.model.payment_payment_method_klarna import PaymentPaymentMethodKlarna
+from Monei.model.payment_payment_method_mbway import PaymentPaymentMethodMbway
+from Monei.model.payment_payment_method_paypal import PaymentPaymentMethodPaypal
+from Monei.model.payment_payment_method_sepa import PaymentPaymentMethodSepa
+from Monei.model.payment_payment_method_trustly import PaymentPaymentMethodTrustly
+from Monei.model.payment_payment_methods import PaymentPaymentMethods
+from Monei.model.payment_refund_reason import PaymentRefundReason
+from Monei.model.payment_sequence import PaymentSequence
+from Monei.model.payment_sequence_recurring import PaymentSequenceRecurring
+from Monei.model.payment_session_details import PaymentSessionDetails
+from Monei.model.payment_shipping_details import PaymentShippingDetails
+from Monei.model.payment_shop import PaymentShop
+from Monei.model.payment_status import PaymentStatus
+from Monei.model.payment_trace_details import PaymentTraceDetails
+from Monei.model.payment_transaction_type import PaymentTransactionType
+from Monei.model.recurring_payment_request import RecurringPaymentRequest
+from Monei.model.refund_payment_request import RefundPaymentRequest
+from Monei.model.register_apple_pay_domain_request import RegisterApplePayDomainRequest
+from Monei.model.send_payment_link_request import SendPaymentLinkRequest
+from Monei.model.send_payment_receipt_request import SendPaymentReceiptRequest
+from Monei.model.send_payment_request_request import SendPaymentRequestRequest
+from Monei.model.send_subscription_link_request import SendSubscriptionLinkRequest
+from Monei.model.send_subscription_status_request import SendSubscriptionStatusRequest
+from Monei.model.service_unavailable_error import ServiceUnavailableError
+from Monei.model.service_unavailable_error_all_of import ServiceUnavailableErrorAllOf
+from Monei.model.subscription import Subscription
+from Monei.model.subscription_interval import SubscriptionInterval
+from Monei.model.subscription_last_payment import SubscriptionLastPayment
+from Monei.model.subscription_payment_method import SubscriptionPaymentMethod
+from Monei.model.subscription_payment_method_card import SubscriptionPaymentMethodCard
+from Monei.model.subscription_payment_methods import SubscriptionPaymentMethods
+from Monei.model.subscription_retry_schedule import SubscriptionRetrySchedule
+from Monei.model.subscription_retry_schedule_inner import SubscriptionRetryScheduleInner
+from Monei.model.subscription_status import SubscriptionStatus
+from Monei.model.unauthorized_error import UnauthorizedError
+from Monei.model.unauthorized_error_all_of import UnauthorizedErrorAllOf
+from Monei.model.unprocessable_entity_error import UnprocessableEntityError
+from Monei.model.unprocessable_entity_error_all_of import UnprocessableEntityErrorAllOf
+from Monei.model.update_apple_pay_certificate_request import (
+    UpdateApplePayCertificateRequest,
+)
+from Monei.model.update_subscription_request import UpdateSubscriptionRequest
+from Monei.model.validate_bizum_phone_request import ValidateBizumPhoneRequest
