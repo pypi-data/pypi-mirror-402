8a1d9b4e60ffe175823ee9587217fc02bf51f14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:09:36 +0000

    Add package lsst-ctrl-platform-s3df/30.0.0rc3/lsst_ctrl_platform_s3df-30.0.0rc3-py3-none-any.whl

---
+++ packages/lsst-ctrl-platform-s3df/lsst_ctrl_platform_s3df-30.0.0rc3-py3-none-any.whl/lsst/ctrl/platform/s3df/etc/config/slurmConfig.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+config.platform.queue = "$QUEUE"
+config.platform.scratchDirectory = "$USER_SCRATCH/condor_scratch"
+config.platform.loginHostName = "sdfiana012.sdf.slac.stanford.edu"
+config.platform.utilityPath = "/usr/sbin/"
