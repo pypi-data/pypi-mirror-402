62d00924a0f6ccf166354decd8cf7d5262c15995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:50 +0000

    Add package parangonar/3.2.0/parangonar-3.2.0-py3-none-any.whl

---
+++ packages/parangonar/parangonar-3.2.0-py3-none-any.whl/parangonar/__init__.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+The top level of the package 
+contains functions to
+align music data.
+"""
+
+import sys
+# Use importlib.metadata and importlib.resources for modern Python versions
+if sys.version_info >= (3, 9):
+    from importlib.metadata import version
+    from importlib.resources import files
+else:
+    # Backport for Python 3.7-3.8
+    try:
+        from importlib_metadata import version
+    except ImportError:
+        from importlib.metadata import version
+    try:
+        from importlib_resources import files
+    except ImportError:
+        from importlib.resources import files
+
+# define a version variable
+__version__ = version("parangonar")
+
+#: An example MusicXML file for didactic purposes
+EXAMPLE = str(files("parangonar") / "assets" / "mozart_k265_var1.match")
+ALIGNMENT_TRANSFORMER_CHECKPOINT = str(files("parangonar") / "assets" / "alignment_transformer_checkpoint.pt")
+THEGLUENOTE_CHECKPOINT = str(files("parangonar") / "assets" / "thegluenote_small_checkpoint.pt")
+
+from .match import (
+    AnchorPointNoteMatcher,
+    AutomaticNoteMatcher,
+    DualDTWNoteMatcher,
+    TheGlueNoteMatcher,
+)
+from .match import (
+    OnlineTransformerMatcher,
+    OnlinePureTransformerMatcher,
+    TOLTWMatcher,
+    OLTWMatcher,
+)
+from .mismatch import RepeatIdentifier, SubPartMatcher
+from .evaluate import (
+    fscore_alignments,
+    print_fscore_alignments,
+    plot_alignment,
+    plot_alignment_comparison,
+    save_piano_precision_csv,
+    save_sonic_visualizer_csvs,
+    save_maps,
+    match_midis
+)
+
+__all__ = [
+    "AnchorPointNoteMatcher",
+    "AutomaticNoteMatcher",
+    "DualDTWNoteMatcher",
+    "TheGlueNoteMatcher",
+    "OnlineTransformerMatcher",
+    "OnlinePureTransformerMatcher",
+    "TOLTWMatcher",
+    "fscore_alignments",
+    "print_fscore_alignments",
+    "plot_alignment_comparison",
+    "plot_alignment",
+    "save_piano_precision_csv",
+    "save_sonic_visualizer_csvs",
+    "save_maps",
+    "match_midis"
+]
