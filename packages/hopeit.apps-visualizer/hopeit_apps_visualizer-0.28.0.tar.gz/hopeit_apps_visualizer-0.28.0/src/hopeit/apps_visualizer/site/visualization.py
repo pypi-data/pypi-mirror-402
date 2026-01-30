1250de4ce288755db3833588eb14643778db50d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:50:24 +0000

    Add package hopeit.apps-visualizer/0.28.0/hopeit_apps_visualizer-0.28.0.tar.gz

---
+++ packages/hopeit.apps-visualizer/hopeit_apps_visualizer-0.28.0.tar.gz/hopeit_apps_visualizer-0.28.0/src/hopeit/apps_visualizer/site/visualization.py
@@ -0,0 +1,53 @@
+"""
+Visualization metadata
+"""
+
+from hopeit.app.context import EventContext
+from typing import Dict, Any, Optional
+
+from hopeit.dataobjects import dataclass, dataobject, field
+
+
+@dataobject
+@dataclass
+class CytoscapeGraph:
+    data: Dict[str, Dict[str, Any]] = field(default_factory=dict)
+
+
+@dataobject
+@dataclass
+class VisualizationOptions:
+    app_prefix: str = ""
+    host_filter: str = ""
+    expanded_view: bool = False
+    live: bool = False
+
+
+def visualization_options_api_args():
+    return [
+        ("app_prefix", Optional[str], "app name prefix to filter"),
+        ("host_filter", Optional[str], "host name filter substring"),
+        (
+            "expanded_view",
+            Optional[bool],
+            "if `true` shows each stream queue as a separated stream",
+        ),
+        ("live", Optional[bool], "if `true` enable live stats refreshing"),
+    ]
+
+
+async def visualization_options(
+    payload: None,
+    context: EventContext,
+    *,
+    app_prefix: str = "",
+    host_filter: str = "",
+    expanded_view: bool = False,
+    live: bool = False,
+) -> VisualizationOptions:
+    return VisualizationOptions(
+        app_prefix=app_prefix,
+        host_filter=host_filter,
+        expanded_view=expanded_view is True or expanded_view == "true",
+        live=live is True or live == "true",
+    )
