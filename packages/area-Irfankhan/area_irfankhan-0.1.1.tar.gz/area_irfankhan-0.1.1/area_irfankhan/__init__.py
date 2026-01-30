45aaebcd7f3b956b9d0628ac424c9cb9991e09c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:17:24 +0000

    Add package area-Irfankhan/0.1.1/area_irfankhan-0.1.1.tar.gz

---
+++ packages/area-Irfankhan/area_irfankhan-0.1.1.tar.gz/area_irfankhan-0.1.1/area_irfankhan/__init__.py
@@ -0,0 +1,1 @@
+from .library import square, rectangle, triangle, circle
---
+++ packages/area-Irfankhan/area_irfankhan-0.1.1.tar.gz/area_irfankhan-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="area-Irfankhan",       
+    version="0.1.1",        
+    description="A simple library to calculate areas of shapes",
+    author="Irfan Khan",
+    author_email="irfankhan016104@gmail.com",
+    packages=find_packages(),     
+)
