ec832cc6cbf97a3b0994f601aefef4a8d1b517a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:11 +0000

    Add package tennetsac/0.1.7/tennetsac-0.1.7-py3-none-any.whl

---
+++ packages/tennetsac/tennetsac-0.1.7-py3-none-any.whl/tennetsac/utils/model_io.py
@@ -0,0 +1,19 @@
+import torch
+from pathlib import Path
+
+def load_model(model, ckpt_file, device='cpu'):
+    model.to(torch.device(device))
+    model.load_state_dict(torch.load(ckpt_file, map_location=torch.device(device)))
+    
+    return model.eval()
+
+def load_all_Gamma_models(model_class, ckpt_dir, num_models=10):
+    models = []
+    for i in range(1, num_models + 1):
+        ckpt_file = Path(ckpt_dir) / f"{i}.ckpt"
+        model = model_class()
+        model.to(torch.device('cpu'))
+        model.load_state_dict(torch.load(ckpt_file, map_location=torch.device('cpu')))
+        model.eval()
+        models.append(model)
+    return models
