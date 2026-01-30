dc9c564e59b61f769a9b4958b65428a72baafeb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:52:08 +0000

    Add package restsession/1.0.3/restsession-1.0.3-py3-none-any.whl

---
+++ packages/restsession/restsession-1.0.3-py3-none-any.whl/restsession/defaults.py
@@ -0,0 +1,65 @@
+"""
+Set defaults for session instances when no parameters (or some parameters) are
+supplied. MappingProxyType prevents mutation of the default parameters inside
+the code; use property setters to change once an instance has been created.
+"""
+from types import MappingProxyType
+from restsession.default_hooks import default_request_exception_hook
+
+
+# 301: Moved Permanently
+# 302: Found / moved temporarily
+# 303: See other
+# 307: Temporary redirect (HTTP/1.1)
+# 308: Permanent redirect (no HTTP method change)
+REDIRECT_STATUS_CODES = (301, 302, 303, 307, 308)
+
+# 408: Request timeout
+# 413: Payload too large
+# 429: Too many requests
+CLIENT_ERROR_CODES = (408, 413, 429)
+
+# 503: Service Unavailable - should be temporary
+SERVER_ERROR_CODES = (503,)
+
+# Commonly used (?)
+DEFAULT_HEADERS = {
+    "User-Agent": "python-restsession/0.01",
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Connection": "keep-alive"
+}
+
+DEFAULT_EXCEPTION_HOOKS = default_request_exception_hook
+DEFAULT_RESPONSE_HOOKS = {"response": ()}
+
+SESSION_DEFAULTS = MappingProxyType(
+    {
+        "headers": DEFAULT_HEADERS,
+        "auth_headers": (),
+        "auth": None,
+        "timeout": 3,
+        "retries": 3,
+        "max_redirects": 16,
+        "backoff_factor": 0.3,
+        "retry_status_code_list": tuple(CLIENT_ERROR_CODES + SERVER_ERROR_CODES),
+        "retry_method_list": (
+            "HEAD",
+            "GET",
+            "PUT",
+            "POST",
+            "PATCH",
+            "DELETE",
+            "OPTIONS",
+            "TRACE"
+        ),
+        "respect_retry_headers": True,
+        "base_url": None,
+        "verify": True,
+        "max_reauth": 3,
+        "request_exception_hook": DEFAULT_EXCEPTION_HOOKS,
+        "response_hooks": DEFAULT_RESPONSE_HOOKS,
+        "safe_arguments": True,
+        "always_relative_url": False
+    }
+)
---
+++ packages/restsession/restsession-1.0.3-py3-none-any.whl/restsession/session_singleton.py
@@ -0,0 +1,29 @@
+"""
+HTTP Session subclass definition for singleton objects. Using this class
+permits an initial session object to be created with all necessary parameters
+such as headers and authentication. Each subsequent invocation of the class
+across modules inside a project will return the same object, reducing the
+number of duplicated parameters being passed throughout functions and
+methods.
+"""
+# pylint: disable=invalid-name
+import logging
+from restsession.session import RestSession
+from restsession.metaclass import Singleton
+
+logger = logging.getLogger(__name__)
+
+
+class RestSessionSingleton(RestSession, metaclass=Singleton):
+    """
+    Singleton class definition. The only method override is for __exit__ to
+    provide a cleanup of the _instances class attribute, effectively removing
+    the singleton's existence when a context manager exits.
+    """
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Cleanup - terminate the request session
+        super().__exit__(exc_type, exc_val, exc_tb)
+
+        # And set the class _instances variable to an empty dict
+        RestSessionSingleton._instances = {}
