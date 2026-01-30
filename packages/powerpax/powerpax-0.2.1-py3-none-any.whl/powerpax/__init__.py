751b4805266d0b4bac9ae49cdfc54d13c9becbe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:42 +0000

    Add package powerpax/0.2.1/powerpax-0.2.1-py3-none-any.whl

---
+++ packages/powerpax/powerpax-0.2.1-py3-none-any.whl/powerpax/__init__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Karl Otness
+# SPDX-License-Identifier: MIT
+
+r"""Useful utilities for JAX.
+
+This package provides a small set of useful utilities for working with
+JAX. For example, functions to limit the memory consumption of
+:func:`jax.vmap` (:func:`ppx.chunked_vmap <powerpax.chunked_vmap>`) or
+to keep the outputs of only a subset of :func:`jax.lax.scan`
+iterations (:func:`ppx.sliced_scan <powerpax.sliced_scan>`).
+"""
+
+__version__ = "0.2.1"
+__all__ = ["chunked_vmap", "sliced_scan", "checkpoint_chunked_scan", "Static"]
+
+from ._vmap import chunked_vmap
+from ._loop import sliced_scan, checkpoint_chunked_scan
+from ._tree import Static
---
+++ packages/powerpax/powerpax-0.2.1-py3-none-any.whl/powerpax/_tree.py
@@ -0,0 +1,36 @@
+# Copyright 2023 Karl Otness
+# SPDX-License-Identifier: MIT
+
+
+import dataclasses
+import typing
+import jax
+
+T = typing.TypeVar("T")
+
+
+@dataclasses.dataclass(frozen=True)
+class Static(typing.Generic[T]):
+    r"""Treat a value as :term:`jax:static` when processing a pytree.
+
+    This class wraps `value` and will instruct JAX to treat it as a
+    static value during pytree processing.
+
+    Parameters
+    ----------
+    value: object
+        The object to wrap and treat as static (must be
+        :term:`python:hashable`).
+
+    Attributes
+    ----------
+    value: object
+        The wrapped, static value.
+    """
+
+    value: T
+
+
+jax.tree_util.register_pytree_node(
+    Static, lambda node: ((), node.value), lambda aux, _: Static(aux)
+)
