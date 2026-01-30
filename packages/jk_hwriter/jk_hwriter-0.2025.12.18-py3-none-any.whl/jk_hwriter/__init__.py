1553a3aeadd0cbc579a5062a1ce29690c36ea2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:57 +0000

    Add package jk_hwriter/0.2025.12.18/jk_hwriter-0.2025.12.18-py3-none-any.whl

---
+++ packages/jk_hwriter/jk_hwriter-0.2025.12.18-py3-none-any.whl/jk_hwriter/__init__.py
@@ -0,0 +1,14 @@
+﻿
+
+
+from .HWriter import HWriter
+from .TextChunk import TextChunk
+from .HWriter2 import HWriter2
+
+
+
+__author__ = "Jürgen Knauth"
+__version__ = "0.2025.12.18"
+__license__ = "Apache2"
+
+
