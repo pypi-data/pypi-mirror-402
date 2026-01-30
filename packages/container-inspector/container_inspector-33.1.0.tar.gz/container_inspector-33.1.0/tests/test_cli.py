2d8ffb4d054c076b1c431f8f6f72112335c754bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:35:39 +0000

    Add package container-inspector/33.1.0/container_inspector-33.1.0.tar.gz

---
+++ packages/container-inspector/container_inspector-33.1.0.tar.gz/container_inspector-33.1.0/tests/test_cli.py
@@ -0,0 +1,94 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/aboutcode-org/container-inspector for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
+import os
+import json
+
+from commoncode.testcase import FileBasedTesting
+
+from container_inspector import cli
+from commoncode import fileutils
+
+from utilities import check_expected
+
+
+def clean_images_data(images):
+    """
+    Clean an a list of Image.to_dict() for testing
+    """
+    for image in images:
+        clean_image_data(image)
+    return images
+
+
+def clean_image_data(image):
+    """
+    Clean `image` data from Image.to_dict() for testing
+    """
+    image["extracted_location"] = ""
+    image["archive_location"] = os.path.basename(image["archive_location"] or "")
+
+    return image
+
+
+class TestContainerInspectorCli(FileBasedTesting):
+    test_data_dir = os.path.join(os.path.dirname(__file__), "data")
+
+    def test_container_inspector_single_layer_from_dir(self):
+        test_dir = self.extract_test_tar("cli/hello-world.tar")
+        expected = self.get_test_loc("cli/hello-world.tar-inventory-from-dir-expected.json")
+        out = cli._container_inspector(image_path=test_dir)
+        result = clean_images_data(json.loads(out))
+        check_expected(result, expected, regen=False)
+
+    def test_container_inspector_single_layer_from_tarball(self):
+        test_dir = self.get_test_loc("cli/hello-world.tar")
+        expected = self.get_test_loc("cli/hello-world.tar-inventory-from-tarball-expected.json")
+        out = cli._container_inspector(image_path=test_dir, _layer_path_segments=1)
+        result = clean_images_data(json.loads(out))
+        check_expected(result, expected, regen=False)
+
+    def test_container_inspector_multiple_layers_from_tarball(self):
+        test_dir = self.get_test_loc("cli/she-image_from_scratch-1.0.tar")
+        expected = self.get_test_loc(
+            "cli/she-image_from_scratch-1.0.tar-inventory-from-tarball-expected.json"
+        )
+        out = cli._container_inspector(image_path=test_dir, _layer_path_segments=1)
+        result = clean_images_data(json.loads(out))
+        check_expected(result, expected, regen=False)
+
+    def test_squash_single_layer(self):
+        test_dir = self.extract_test_tar("cli/hello-world.tar")
+        target_dir = self.get_temp_dir()
+
+        cli._container_inspector_squash(image_path=test_dir, extract_directory=target_dir)
+
+        results = sorted([p.replace(target_dir, "") for p in fileutils.resource_iter(target_dir)])
+        expected = ["/hello"]
+        assert expected == results
+
+    def test_squash_multiple_layers(self):
+        test_dir = self.extract_test_tar("cli/she-image_from_scratch-1.0.tar")
+        target_dir = self.get_temp_dir()
+
+        cli._container_inspector_squash(
+            image_path=test_dir,
+            extract_directory=target_dir,
+        )
+
+        results = sorted([p.replace(target_dir, "") for p in fileutils.resource_iter(target_dir)])
+        expected = [
+            "/additions",
+            "/additions/bar",
+            "/additions/baz",
+            "/additions/baz/this",
+            "/additions/foo",
+            "/additions/hello",
+            "/hello",
+        ]
+        assert expected == results
---
+++ packages/container-inspector/container_inspector-33.1.0.tar.gz/container_inspector-33.1.0/tests/test_image.py
@@ -0,0 +1,160 @@
+#
+# Copyright (c) nexB Inc. and others. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+# See https://github.com/aboutcode-org/container-inspector for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
+
+import os
+
+from commoncode.testcase import FileBasedTesting
+
+from container_inspector.image import Image
+from container_inspector.image import flatten_images_data
+
+from utilities import check_expected
+
+
+class TestImages(FileBasedTesting):
+    test_data_dir = os.path.join(os.path.dirname(__file__), "data")
+
+    def test_Image(self):
+        try:
+            Image()
+            self.fail("Exception not caught")
+        except TypeError as e:
+            assert str(e) == "Image.extracted_location is a required argument"
+
+    def test_Image_with_dir(self):
+        test_dir = self.get_temp_dir()
+        Image(extracted_location=test_dir)
+
+    def test_Image_get_images_from_tarball(self):
+        test_tarball = self.get_test_loc("repos/imagesv11.tar")
+        extract_dir = self.get_temp_dir()
+        expected = os.path.join(
+            self.get_test_loc("repos"),
+            "imagesv11.tar.expected.json",
+        )
+
+        images = Image.get_images_from_tarball(
+            archive_location=test_tarball,
+            extracted_location=extract_dir,
+            verify=False,
+        )
+        result = [i.to_dict(layer_path_segments=2, _test=True) for i in images]
+        check_expected(result, expected, regen=False)
+
+    def test_windows_container_Image_get_images_from_tarball(self):
+        test_tarball = self.get_test_loc("image/windows-mini-image.tar.gz")
+        extract_dir = self.get_temp_dir()
+        image = Image.get_images_from_tarball(
+            archive_location=test_tarball,
+            extracted_location=extract_dir,
+            verify=False,
+        )[0]
+
+        image.extract_layers(extracted_location=extract_dir)
+        image.get_and_set_distro()
+        result = image.to_dict(layer_path_segments=1, _test=True)
+        expected = self.get_test_loc(
+            "image/windows-mini-image.tar.gz.expected.json",
+            must_exist=False,
+        )
+
+        check_expected(result, expected, regen=False)
+
+    def test_Image_get_images_from_dir(self):
+        test_tarball = self.get_test_loc("repos/imagesv11.tar")
+        test_dir = self.extract_test_tar(test_tarball)
+        expected = os.path.join(
+            self.get_test_loc("repos"),
+            "imagesv11.tar.expected.json",
+        )
+        images = Image.get_images_from_dir(
+            extracted_location=test_dir,
+            archive_location=test_tarball,
+        )
+        result = [i.to_dict(layer_path_segments=2, _test=True) for i in images]
+        check_expected(result, expected, regen=False)
+
+    def test_Image_get_images_from_dir_from_hello_world(self):
+        test_arch = self.get_test_loc("repos/hello-world.tar")
+        test_dir = self.extract_test_tar(test_arch)
+        expected = os.path.join(
+            self.get_test_loc("repos"),
+            "hello-world.tar.registry.expected.json",
+        )
+        images = Image.get_images_from_dir(test_dir)
+        result = [i.to_dict(layer_path_segments=2, _test=True) for i in images]
+        check_expected(result, expected, regen=False)
+
+    def test_Image_get_images_from_dir_then_flatten_images_data(self):
+        test_arch = self.get_test_loc("repos/hello-world.tar")
+        test_dir = self.extract_test_tar(test_arch)
+        expected = os.path.join(
+            self.get_test_loc("repos"),
+            "hello-world.tar.flatten.expected.json",
+        )
+        images = list(Image.get_images_from_dir(test_dir))
+        result = list(flatten_images_data(images, layer_path_segments=2, _test=True))
+        check_expected(result, expected, regen=False)
+
+    def test_Image_get_images_from_dir_with_direct_at_root_layerid_dot_tar_tarball(self):
+        test_arch = self.get_test_loc("repos/imagesv11_with_tar_at_root.tar")
+        test_dir = self.extract_test_tar(test_arch)
+        expected = os.path.join(
+            self.get_test_loc("repos"),
+            "imagesv11_with_tar_at_root.tar.registry.expected.json",
+        )
+        images = Image.get_images_from_dir(test_dir, verify=False)
+        result = [i.to_dict(layer_path_segments=2, _test=True) for i in images]
+        check_expected(result, expected, regen=False)
+
+    def test_Image_get_images_from_dir_with_verify(self):
+        test_arch = self.get_test_loc("repos/hello-world.tar")
+        test_dir = self.extract_test_tar(test_arch)
+        Image.get_images_from_dir(test_dir, verify=True)
+
+    def test_Image_get_images_from_dir_with_anotations(self):
+        test_arch = self.get_test_loc("repos/images.tar.gz")
+        test_dir = self.extract_test_tar(test_arch)
+        expected = os.path.join(self.get_test_loc("repos"), "images.tar.gz.expected.json")
+        images = Image.get_images_from_dir(test_dir, verify=False)
+        result = [i.to_dict(layer_path_segments=2, _test=True) for i in images]
+        check_expected(result, expected, regen=False)
+
+    def test_Image_get_images_from_dir_with_verify_fails_if_invalid_checksum(self):
+        test_arch = self.get_test_loc("repos/images.tar.gz")
+        test_dir = self.extract_test_tar(test_arch)
+        try:
+            Image.get_images_from_dir(test_dir, verify=True)
+            self.fail("Exception not raised")
+        except Exception as e:
+            assert str(e).startswith("Layer archive: SHA256:")
+
+    def test_Image_find_format(self):
+        test_arch = self.get_test_loc("image/she-image_from_scratch-1.0.tar")
+        test_dir = self.extract_test_tar(test_arch)
+        assert Image.find_format(test_dir) == "docker"
+
+    def test_Image_find_format_finds_Docker_images_without_repositories(self):
+        test_arch = self.get_test_loc("image/mini-image_from_scratch-2.0.tar")
+        test_dir = self.extract_test_tar(test_arch)
+        assert Image.find_format(test_dir) == "docker"
+
+    def test_Image_to_dict_can_report_image_trimmed_layer_paths_or_not(self):
+        test_image = self.get_test_loc("image/mini-image_from_scratch-2.0.tar")
+        extract_dir = self.get_temp_dir()
+        images = Image.get_images_from_tarball(
+            archive_location=test_image,
+            extracted_location=extract_dir,
+            verify=False,
+        )
+        expected1 = self.get_test_loc(
+            "image/mini-image_from_scratch-2.0.tar-relative-expected.json",
+            must_exist=False,
+        )
+        result = [i.to_dict(layer_path_segments=2, _test=True) for i in images]
+        check_expected(result, expected1, regen=False)
