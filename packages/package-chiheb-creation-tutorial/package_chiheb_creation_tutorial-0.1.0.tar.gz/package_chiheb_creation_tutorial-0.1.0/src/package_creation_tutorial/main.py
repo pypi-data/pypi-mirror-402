edc5576ed01d5beff7bb6fa5c686cc2651947426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:34 +0000

    Add package package-chiheb-creation-tutorial/0.1.0/package_chiheb_creation_tutorial-0.1.0.tar.gz

---
+++ packages/package-chiheb-creation-tutorial/package_chiheb_creation_tutorial-0.1.0.tar.gz/package_chiheb_creation_tutorial-0.1.0/src/package_creation_tutorial/main.py
@@ -0,0 +1,17 @@
+from package_creation_tutorial.string_ops import reverse_string, count_vowels, capitalize_words
+import os
+
+def main():
+    """
+    Main function to demonstrate the string operations.
+    """
+    test_string: str = "hello world"
+    print("Hi! PARIS")
+    print(f"Original string: {test_string}")
+    print(f"Reversed: {reverse_string(test_string)}")
+    print(f"Vowel count: {count_vowels(test_string)}")
+    print(f"Capitalized: {capitalize_words(test_string)}")
+
+
+if __name__ == "__main__":
+    main()
