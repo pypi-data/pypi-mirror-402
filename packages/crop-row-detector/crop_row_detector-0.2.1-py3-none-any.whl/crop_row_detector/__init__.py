87b11c54647419043a8d52628d5ecaa5304ca6ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:16:25 +0000

    Add package crop-row-detector/0.2.1/crop_row_detector-0.2.1-py3-none-any.whl

---
+++ packages/crop-row-detector/crop_row_detector-0.2.1-py3-none-any.whl/crop_row_detector/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+# Current version
+__version__ = "0.2.1"
+
+# import Crop Row Detector objects
+from .crop_row_detector import *
+from .orthomosaic_tiler import *
