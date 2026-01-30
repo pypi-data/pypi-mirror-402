61596bd1a635abe1ca1201cc4560183010137bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:51:21 +0000

    Add package roseau-load-flow-engine/0.19.0/roseau_load_flow_engine-0.19.0-cp314-cp314t-win_amd64.whl

---
+++ packages/roseau-load-flow-engine/roseau_load_flow_engine-0.19.0-cp314-cp314t-win_amd64.whl/roseau/load_flow_engine/__init__.py
@@ -0,0 +1,26 @@
+import importlib.metadata
+
+from roseau.load_flow_engine.__about__ import (
+    __authors__,
+    __copyright__,
+    __credits__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __status__,
+    __url__,
+)
+
+__version__ = importlib.metadata.version("roseau_load_flow_engine")
+
+__all__ = [
+    "__authors__",
+    "__copyright__",
+    "__credits__",
+    "__email__",
+    "__license__",
+    "__maintainer__",
+    "__status__",
+    "__url__",
+    "__version__",
+]
