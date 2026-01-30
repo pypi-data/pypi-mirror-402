e9c81562e22fb7f016d1cfb50ed91cbec04ff711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:24:43 +0000

    Add package huaweicloudsdkdataartsfabricep/3.1.184/huaweicloudsdkdataartsfabricep-3.1.184-py3-none-any.whl

---
+++ packages/huaweicloudsdkdataartsfabricep/huaweicloudsdkdataartsfabricep-3.1.184-py3-none-any.whl/huaweicloudsdkdataartsfabricep/v1/model/list_services_instances_response.py
@@ -0,0 +1,141 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.sdk_response import SdkResponse
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class ListServicesInstancesResponse(SdkResponse):
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'total': 'int',
+        'service_instances': 'list[ServiceInstanceBriefInfo]'
+    }
+
+    attribute_map = {
+        'total': 'total',
+        'service_instances': 'service_instances'
+    }
+
+    def __init__(self, total=None, service_instances=None):
+        r"""ListServicesInstancesResponse
+
+        The model defined in huaweicloud sdk
+
+        :param total: 符合条件的service Istance总数
+        :type total: int
+        :param service_instances: 符合条件的service Instance列表
+        :type service_instances: list[:class:`huaweicloudsdkdataartsfabricep.v1.ServiceInstanceBriefInfo`]
+        """
+        
+        super().__init__()
+
+        self._total = None
+        self._service_instances = None
+        self.discriminator = None
+
+        if total is not None:
+            self.total = total
+        if service_instances is not None:
+            self.service_instances = service_instances
+
+    @property
+    def total(self):
+        r"""Gets the total of this ListServicesInstancesResponse.
+
+        符合条件的service Istance总数
+
+        :return: The total of this ListServicesInstancesResponse.
+        :rtype: int
+        """
+        return self._total
+
+    @total.setter
+    def total(self, total):
+        r"""Sets the total of this ListServicesInstancesResponse.
+
+        符合条件的service Istance总数
+
+        :param total: The total of this ListServicesInstancesResponse.
+        :type total: int
+        """
+        self._total = total
+
+    @property
+    def service_instances(self):
+        r"""Gets the service_instances of this ListServicesInstancesResponse.
+
+        符合条件的service Instance列表
+
+        :return: The service_instances of this ListServicesInstancesResponse.
+        :rtype: list[:class:`huaweicloudsdkdataartsfabricep.v1.ServiceInstanceBriefInfo`]
+        """
+        return self._service_instances
+
+    @service_instances.setter
+    def service_instances(self, service_instances):
+        r"""Sets the service_instances of this ListServicesInstancesResponse.
+
+        符合条件的service Instance列表
+
+        :param service_instances: The service_instances of this ListServicesInstancesResponse.
+        :type service_instances: list[:class:`huaweicloudsdkdataartsfabricep.v1.ServiceInstanceBriefInfo`]
+        """
+        self._service_instances = service_instances
+
+    def to_dict(self):
+        import warnings
+        warnings.warn("ListServicesInstancesResponse.to_dict() is deprecated and no longer maintained, "
+                      "use to_json_object() to get the response content.", DeprecationWarning)
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, ListServicesInstancesResponse):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/huaweicloudsdkdataartsfabricep/huaweicloudsdkdataartsfabricep-3.1.184-py3-none-any.whl/huaweicloudsdkdataartsfabricep/v1/model/update_service_instance_input.py
@@ -0,0 +1,187 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class UpdateServiceInstanceInput:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'source': 'SourceRef',
+        'name': 'str',
+        'description': 'str',
+        'config': 'UpdateServiceInstanceConfig'
+    }
+
+    attribute_map = {
+        'source': 'source',
+        'name': 'name',
+        'description': 'description',
+        'config': 'config'
+    }
+
+    def __init__(self, source=None, name=None, description=None, config=None):
+        r"""UpdateServiceInstanceInput
+
+        The model defined in huaweicloud sdk
+
+        :param source: 
+        :type source: :class:`huaweicloudsdkdataartsfabricep.v1.SourceRef`
+        :param name: 一个Service Instance的名称，只能包含中文、字母、数字、下划线、中划线、点、空格
+        :type name: str
+        :param description: 描述信息
+        :type description: str
+        :param config: 
+        :type config: :class:`huaweicloudsdkdataartsfabricep.v1.UpdateServiceInstanceConfig`
+        """
+        
+        
+
+        self._source = None
+        self._name = None
+        self._description = None
+        self._config = None
+        self.discriminator = None
+
+        if source is not None:
+            self.source = source
+        if name is not None:
+            self.name = name
+        if description is not None:
+            self.description = description
+        if config is not None:
+            self.config = config
+
+    @property
+    def source(self):
+        r"""Gets the source of this UpdateServiceInstanceInput.
+
+        :return: The source of this UpdateServiceInstanceInput.
+        :rtype: :class:`huaweicloudsdkdataartsfabricep.v1.SourceRef`
+        """
+        return self._source
+
+    @source.setter
+    def source(self, source):
+        r"""Sets the source of this UpdateServiceInstanceInput.
+
+        :param source: The source of this UpdateServiceInstanceInput.
+        :type source: :class:`huaweicloudsdkdataartsfabricep.v1.SourceRef`
+        """
+        self._source = source
+
+    @property
+    def name(self):
+        r"""Gets the name of this UpdateServiceInstanceInput.
+
+        一个Service Instance的名称，只能包含中文、字母、数字、下划线、中划线、点、空格
+
+        :return: The name of this UpdateServiceInstanceInput.
+        :rtype: str
+        """
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        r"""Sets the name of this UpdateServiceInstanceInput.
+
+        一个Service Instance的名称，只能包含中文、字母、数字、下划线、中划线、点、空格
+
+        :param name: The name of this UpdateServiceInstanceInput.
+        :type name: str
+        """
+        self._name = name
+
+    @property
+    def description(self):
+        r"""Gets the description of this UpdateServiceInstanceInput.
+
+        描述信息
+
+        :return: The description of this UpdateServiceInstanceInput.
+        :rtype: str
+        """
+        return self._description
+
+    @description.setter
+    def description(self, description):
+        r"""Sets the description of this UpdateServiceInstanceInput.
+
+        描述信息
+
+        :param description: The description of this UpdateServiceInstanceInput.
+        :type description: str
+        """
+        self._description = description
+
+    @property
+    def config(self):
+        r"""Gets the config of this UpdateServiceInstanceInput.
+
+        :return: The config of this UpdateServiceInstanceInput.
+        :rtype: :class:`huaweicloudsdkdataartsfabricep.v1.UpdateServiceInstanceConfig`
+        """
+        return self._config
+
+    @config.setter
+    def config(self, config):
+        r"""Sets the config of this UpdateServiceInstanceInput.
+
+        :param config: The config of this UpdateServiceInstanceInput.
+        :type config: :class:`huaweicloudsdkdataartsfabricep.v1.UpdateServiceInstanceConfig`
+        """
+        self._config = config
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, UpdateServiceInstanceInput):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
