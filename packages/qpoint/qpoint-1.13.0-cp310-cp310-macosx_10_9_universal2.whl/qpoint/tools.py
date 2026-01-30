d53c33f2eed05da1561b1e25a27451954759cf06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:17 +0000

    Add package qpoint/1.13.0/qpoint-1.13.0-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/qpoint/qpoint-1.13.0-cp310-cp310-macosx_10_9_universal2.whl/qpoint/tools.py
@@ -0,0 +1,35 @@
+import numpy as np
+from ._libqpoint import libqp as qp
+
+__all__ = ["refraction"]
+
+
+def refraction(el, temp, press, hum, freq=150.0):
+    """
+    Standalone function for calculating the refraction correction without
+    storing any parameters.  Useful for testing, numpy-vectorized.
+
+    Arguments
+    ---------
+    el : array_like
+        Observer elevation angle, degrees
+    temperature : array_like
+        Ambient temperature, Celcius
+    pressure : array_like
+        Ambient pressure, mbar
+    humidity : array_like
+        Relative humidity, fraction
+    frequency : array_like
+        Observing frequency, GHz
+
+    Returns
+    -------
+    delta : array_like
+        Refraction correction, in degrees
+    """
+
+    fvec = np.vectorize(qp.qp_refraction, [np.double])
+    delta = fvec(el, temp, press, hum, freq)
+    if delta.shape == ():
+        return delta[()]
+    return delta
