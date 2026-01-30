fdcfad6b669619cc41b34de286ad2730eb81ab6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:33:54 +0000

    Add package pennylane-qrack-cpu/0.26.0/pennylane_qrack_cpu-0.26.0.tar.gz

---
+++ packages/pennylane-qrack-cpu/pennylane_qrack_cpu-0.26.0.tar.gz/pennylane_qrack_cpu-0.26.0/setup.py
@@ -0,0 +1,79 @@
+# Copyright 2020 Xanadu Quantum Technologies Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#!/usr/bin/env python3
+import os
+import re
+import sys
+import subprocess
+from setuptools import setup
+from setuptools.command.build_py import build_py
+
+
+class Build(build_py):
+    def run(self):
+        protoc_command = ["make", "build-deps"]
+        if os.name != "nt":
+            if subprocess.call(protoc_command) != 0:
+                sys.exit(-1)
+        super().run()
+
+
+with open("./pennylane_qrack/_version.py") as f:
+    (version,) = re.findall('__version__ = "(.*)"', f.read())
+
+requirements = ["pennylane>=0.39.0", "pyqrack-cpu>=1.30.0", "numpy>=1.16"]
+
+info = {
+    "name": "pennylane-qrack-cpu",
+    "version": version,
+    "maintainer": "vm6502q",
+    "maintainer_email": "stranoj@gmail.com",
+    "url": "http://github.com/vm6502q",
+    "license": "Apache License 2.0",
+    "packages": ["pennylane_qrack"],
+    "cmdclass": {"build_py": Build},
+    "entry_points": {
+        "pennylane.plugins": ["qrack.simulator = pennylane_qrack.qrack_device:QrackDevice", "qrack.ace = pennylane_qrack.qrack_ace_device:QrackAceDevice", "qrack.stabilizer = pennylane_qrack.qrack_stabilizer_device:QrackStabilizerDevice"]
+    },
+    "description": "PennyLane plugin for Qrack.",
+    "long_description": open("README.rst").read(),
+    "long_description_content_type": "text/x-rst",
+    "provides": ["pennylane_qrack"],
+    "install_requires": requirements,
+    "package_data": {"pennylane_qrack": ["QrackDeviceConfig.toml"]},
+    "include_package_data": True,
+}
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: POSIX",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: Microsoft :: Windows",
+    "Programming Language :: Python",
+    # Make sure to specify here the versions of Python supported
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3 :: Only",
+    "Topic :: Scientific/Engineering :: Quantum Computing",
+]
+
+setup(classifiers=classifiers, **info)
---
+++ packages/pennylane-qrack-cpu/pennylane_qrack_cpu-0.26.0.tar.gz/pennylane_qrack_cpu-0.26.0/tests/test_integration.py
@@ -0,0 +1,88 @@
+# Copyright 2018 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests that device integrates with PennyLane"""
+import numpy as np
+import pennylane as qml
+from pennylane_qrack.qrack_device import QrackDevice
+from catalyst import qjit
+
+
+class TestIntegration:
+    """Some basic integration tests."""
+
+    def test_load_device(self):
+        """Test that the Qrack device loads correctly."""
+        dev = QrackDevice(2, shots=int(1e6), isOpenCL=False)
+
+        assert dev.num_wires == 2
+        assert dev.shots == int(1e6)
+        assert dev.short_name == "qrack.simulator"
+        assert "model" in dev.__class__.capabilities()
+
+    def test_realloc(self):
+        """Test that the Qrack device can reallocate after free."""
+        dev = QrackDevice(2, shots=int(1e6), isOpenCL=False)
+
+        @qjit
+        @qml.qnode(dev)
+        def circuit():
+            qml.Hadamard(wires=0)
+            qml.CNOT(wires=[0, 1])
+            qml.Hadamard(wires=0)
+            qml.CNOT(wires=[0, 1])
+            qml.Hadamard(wires=0)
+            return qml.expval(qml.PauliY(0))
+
+        circuit()
+        try:
+            circuit()
+        except:
+            pytest.fail("Reallocation after free failed")
+
+    def test_expectation(self):
+        """Test that expectation of a non-trivial circuit is correct."""
+        dev = QrackDevice(2, shots=int(1e6), isOpenCL=False)
+
+        theta = 0.432
+        phi = 0.123
+
+        @qml.qnode(dev)
+        def circuit():
+            qml.adjoint(qml.RY(theta, wires=[0]))
+            qml.RY(phi, wires=[1])
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.PauliX(wires=0)), qml.expval(qml.PauliX(wires=1))
+
+        res = circuit()
+        expected = np.array([np.sin(-theta) * np.sin(phi), np.sin(phi)])
+        assert np.allclose(res, expected, atol=0.05)
+
+    def test_expectation_qjit(self):
+        """Test that expectation of a non-trivial circuit is correct."""
+        dev = QrackDevice(2, shots=int(1e6), isOpenCL=False)
+
+        theta = 0.432
+        phi = 0.123
+
+        @qjit
+        @qml.qnode(dev)
+        def circuit():
+            qml.adjoint(qml.RY(theta, wires=[0]))
+            qml.RY(phi, wires=[1])
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.PauliX(wires=0)), qml.expval(qml.PauliX(wires=1))
+
+        res = circuit()
+        expected = np.array([np.sin(-theta) * np.sin(phi), np.sin(phi)])
+        assert np.allclose(res, expected, atol=0.05)
