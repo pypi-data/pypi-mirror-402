4f7fc0c512ed028788341790464ef8b046bd2676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:24:49 +0000

    Add package huaweicloudsdkmastudio/3.1.184/huaweicloudsdkmastudio-3.1.184-py3-none-any.whl

---
+++ packages/huaweicloudsdkmastudio/huaweicloudsdkmastudio-3.1.184-py3-none-any.whl/huaweicloudsdkmastudio/v1/model/message_item.py
@@ -0,0 +1,136 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class MessageItem:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'role': 'str',
+        'content': 'str'
+    }
+
+    attribute_map = {
+        'role': 'role',
+        'content': 'content'
+    }
+
+    def __init__(self, role=None, content=None):
+        r"""MessageItem
+
+        The model defined in huaweicloud sdk
+
+        :param role: 角色
+        :type role: str
+        :param content: 模型响应
+        :type content: str
+        """
+        
+        
+
+        self._role = None
+        self._content = None
+        self.discriminator = None
+
+        if role is not None:
+            self.role = role
+        self.content = content
+
+    @property
+    def role(self):
+        r"""Gets the role of this MessageItem.
+
+        角色
+
+        :return: The role of this MessageItem.
+        :rtype: str
+        """
+        return self._role
+
+    @role.setter
+    def role(self, role):
+        r"""Sets the role of this MessageItem.
+
+        角色
+
+        :param role: The role of this MessageItem.
+        :type role: str
+        """
+        self._role = role
+
+    @property
+    def content(self):
+        r"""Gets the content of this MessageItem.
+
+        模型响应
+
+        :return: The content of this MessageItem.
+        :rtype: str
+        """
+        return self._content
+
+    @content.setter
+    def content(self, content):
+        r"""Sets the content of this MessageItem.
+
+        模型响应
+
+        :param content: The content of this MessageItem.
+        :type content: str
+        """
+        self._content = content
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, MessageItem):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/huaweicloudsdkmastudio/huaweicloudsdkmastudio-3.1.184-py3-none-any.whl/huaweicloudsdkmastudio/v1/model/text_choice.py
@@ -0,0 +1,135 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class TextChoice:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'index': 'int',
+        'text': 'str'
+    }
+
+    attribute_map = {
+        'index': 'index',
+        'text': 'text'
+    }
+
+    def __init__(self, index=None, text=None):
+        r"""TextChoice
+
+        The model defined in huaweicloud sdk
+
+        :param index: 回复的索引
+        :type index: int
+        :param text: 模型响应
+        :type text: str
+        """
+        
+        
+
+        self._index = None
+        self._text = None
+        self.discriminator = None
+
+        self.index = index
+        self.text = text
+
+    @property
+    def index(self):
+        r"""Gets the index of this TextChoice.
+
+        回复的索引
+
+        :return: The index of this TextChoice.
+        :rtype: int
+        """
+        return self._index
+
+    @index.setter
+    def index(self, index):
+        r"""Sets the index of this TextChoice.
+
+        回复的索引
+
+        :param index: The index of this TextChoice.
+        :type index: int
+        """
+        self._index = index
+
+    @property
+    def text(self):
+        r"""Gets the text of this TextChoice.
+
+        模型响应
+
+        :return: The text of this TextChoice.
+        :rtype: str
+        """
+        return self._text
+
+    @text.setter
+    def text(self, text):
+        r"""Sets the text of this TextChoice.
+
+        模型响应
+
+        :param text: The text of this TextChoice.
+        :type text: str
+        """
+        self._text = text
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, TextChoice):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
