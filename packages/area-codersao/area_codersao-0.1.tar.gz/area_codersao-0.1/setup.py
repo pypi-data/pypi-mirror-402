3c57cac37db2912761cfd59ff480f77a5b5aeb53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:36 +0000

    Add package area-codersao/0.1/area_codersao-0.1.tar.gz

---
+++ packages/area-codersao/area_codersao-0.1.tar.gz/area_codersao-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="area-codersao",      # must be unique on PyPI
+    version="0.1",
+    packages=find_packages(),
+    description="A simple library to calculate area of shapes",
+    author="Jatin Ghoyal",
+    author_email="codersao@gmail.com",
+)
