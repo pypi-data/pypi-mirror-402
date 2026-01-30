1e4c86670ffc3a36c32d3fbd46a5454477a83cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:09:38 +0000

    Add package otel-bootstrap/0.1.8/otel_bootstrap-0.1.8.tar.gz

---
+++ packages/otel-bootstrap/otel_bootstrap-0.1.8.tar.gz/otel_bootstrap-0.1.8/otel_bootstrap/tracer.py
@@ -0,0 +1,27 @@
+# otel_bootstrap/tracer.py
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+
+
+from .config import OtelSettings
+
+
+def setup_tracer(settings: OtelSettings):
+    provider = TracerProvider(
+        resource=Resource.create({
+            "service.name": settings.service_name,
+            "service.version": settings.service_version,
+        })
+    )
+    
+    exporter = OTLPSpanExporter(
+        endpoint=settings.otel_endpoint,
+        headers={
+            "Authorization": f"Basic {settings.otel_auth_header}",
+        },
+    )
+    provider.add_span_processor(BatchSpanProcessor(exporter))
+    trace.set_tracer_provider(provider)
