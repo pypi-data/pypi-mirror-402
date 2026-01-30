811a3c13065ba9e9cf8079173c141d8afd2d3d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:16:02 +0000

    Add package db-libs/1.3/db_libs-1.3.tar.gz

---
+++ packages/db-libs/db_libs-1.3.tar.gz/db_libs-1.3/db_libs/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf8
+"""
+@author:Administrator
+@file: __init__.py
+@time: 2020/06
+"""
+
+# from torndb_for_python3 import Connection as TorndbConnection
+# import records   # 这也是兼容多种sql数据库的。
+# import dataset  # 方便保存字典
---
+++ packages/db-libs/db_libs-1.3.tar.gz/db_libs-1.3/setup.py
@@ -0,0 +1,69 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+filepath = 'README.md'
+print(filepath)
+
+setup(
+    name='db_libs',  #
+    version="1.3",
+    description=(
+        "'redis', 'mongo', 'elasticsearch', 'mysql', 'sqlachemy', '线程安全的数据库封装，享元模式支持无限实例化调用'"
+    ),
+    keywords=["database", 'redis', 'mongo', 'elasticsearch', 'mysql', 'sqlachemy'],
+    # long_description=open('README.md', 'r',encoding='utf8').read(),
+    long_description_content_type="text/markdown",
+    long_description=open(filepath, 'r', encoding='utf8').read(),
+    # data_files=[filepath],
+    author='bfzs',
+    author_email='ydf0509@sohu.com',
+    maintainer='ydf',
+    maintainer_email='ydf0509@sohu.com',
+    license='BSD License',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms=["all"],
+    url='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=['nb_log',
+                      'decorator_libs',
+                      'redis',
+                      'pymongo',
+                      'elasticsearch',
+                      'threadpool_executor_shrink_able',
+                      'redis2',
+                      'redis3',
+                      'redis5',
+                      'pymysql',
+                      # 'records',
+                      'dbutils==3.1.0',
+                      ]
+)
+"""
+打包上传
+python setup.py sdist upload -r pypi
+
+
+python setup.py sdist & twine upload dist/db_libs-1.2.tar.gz
+
+
+
+python -m pip install db_libs --upgrade -i https://pypi.org/simple   # 及时的方式，不用等待 阿里云 豆瓣 同步
+"""
