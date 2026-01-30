eaf5449b443320ad87037f52b1ae4ee4cfcfc655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:22 +0000

    Add package amp-rsl-rl/1.1.1/amp_rsl_rl-1.1.1-py3-none-any.whl

---
+++ packages/amp-rsl-rl/amp_rsl_rl-1.1.1-py3-none-any.whl/amp_rsl_rl/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2025, Istituto Italiano di Tecnologia
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+"""Main module for the amp_rsl_rl package."""
---
+++ packages/amp-rsl-rl/amp_rsl_rl-1.1.1-py3-none-any.whl/amp_rsl_rl/algorithms/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025, Istituto Italiano di Tecnologia
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Implementation of different RL agents using AMP."""
+
+from .amp_ppo import AMP_PPO
+
+__all__ = ["AMP_PPO"]
