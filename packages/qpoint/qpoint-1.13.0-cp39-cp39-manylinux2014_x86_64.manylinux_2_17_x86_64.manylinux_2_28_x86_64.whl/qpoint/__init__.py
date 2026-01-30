357e26694cfecb666e20dbcec5c24e53b9952d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:29 +0000

    Add package qpoint/1.13.0/qpoint-1.13.0-cp39-cp39-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/qpoint/qpoint-1.13.0-cp39-cp39-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/qpoint/__init__.py
@@ -0,0 +1,13 @@
+"""A lightweight quaternion-based library for efficient telescope pointing."""
+
+from ._version import __version__
+
+
+def version():
+    """Print version string"""
+    return __version__
+
+
+from . import tools
+from .qpoint_class import *
+from .qmap_class import *
