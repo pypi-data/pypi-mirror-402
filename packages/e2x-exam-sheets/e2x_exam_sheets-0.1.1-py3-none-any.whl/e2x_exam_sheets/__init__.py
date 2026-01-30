c3fe5e71e324a40cb6139b0b9f26f83b77cf2a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:53 +0000

    Add package e2x-exam-sheets/0.1.1/e2x_exam_sheets-0.1.1-py3-none-any.whl

---
+++ packages/e2x-exam-sheets/e2x_exam_sheets-0.1.1-py3-none-any.whl/e2x_exam_sheets/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024-present Tim Metzler <tim.metzler@h-brs.de>
+#
+# SPDX-License-Identifier: MIT
+from .examsheetgenerator import ExamSheetGenerator
+
+__all__ = ["ExamSheetGenerator"]
