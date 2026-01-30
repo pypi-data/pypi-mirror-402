63201ccc6017b0c139d95c781213f402e871ec44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:23 +0000

    Add package bmdb/1.3.1/bmdb-1.3.1-py3-none-any.whl

---
+++ packages/bmdb/bmdb-1.3.1-py3-none-any.whl/bmdb/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""BMDB - Minimal schema manager for SQLAlchemy"""
+
+__version__ = "1.3.1"
+__author__ = "Marouan Bouchettoy"
+
+from bmdb.cli import main
+
+__all__ = ["main"]
