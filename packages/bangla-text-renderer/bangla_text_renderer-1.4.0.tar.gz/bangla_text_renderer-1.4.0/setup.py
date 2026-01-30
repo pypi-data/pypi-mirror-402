81636bb2431bc9e0d97c2c08e25b4795ee24e564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:51:02 +0000

    Add package bangla-text-renderer/1.4.0/bangla_text_renderer-1.4.0.tar.gz

---
+++ packages/bangla-text-renderer/bangla_text_renderer-1.4.0.tar.gz/bangla_text_renderer-1.4.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bangla-text-renderer",
+    version="1.4.0",
+    author="Mahfazzalin Shawon Reza",
+    author_email="mahfazzalin1@gmail.com",
+    description="Perfect Bangla/Bengali text rendering on images/video with correct vowel positioning and joint letter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mahfazzalin/bangla-text-renderer",  # GitHub repo
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Text Processing :: Linguistic",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "Pillow>=8.0.0",
+    ],
+    keywords="bangla bengali text rendering image video vowel positioning perfect joint letter unicode typography",
+)
