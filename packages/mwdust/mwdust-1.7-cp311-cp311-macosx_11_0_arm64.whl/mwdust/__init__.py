c7b1884136fec43737d923501a57ef5e8a2c6318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:39 +0000

    Add package mwdust/1.7/mwdust-1.7-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/mwdust/mwdust-1.7-cp311-cp311-macosx_11_0_arm64.whl/mwdust/__init__.py
@@ -0,0 +1,27 @@
+from mwdust.SFD import SFD
+from mwdust.Marshall06 import Marshall06
+from mwdust.Drimmel03 import Drimmel03
+from mwdust.Sale14 import Sale14
+from mwdust.Green15 import Green15
+from mwdust.Green17 import Green17
+from mwdust.Green19 import Green19
+from mwdust.Combined15 import Combined15
+from mwdust.Combined19 import Combined19
+from mwdust.Zucker25 import Zucker25
+from mwdust.Zero import Zero
+
+__version__ = "1.7"
+
+
+def download_all(test=False):
+    SFD.download(test=test)
+    Marshall06.download(test=test)
+    Drimmel03.download(test=test)
+    if not test:
+        Sale14.download(test=test)
+    Green15.download(test=test)
+    Green17.download(test=test)
+    Green19.download(test=test)
+    Combined15.download(test=test)
+    Combined19.download(test=test)
+    Zucker25.download(test=test)
