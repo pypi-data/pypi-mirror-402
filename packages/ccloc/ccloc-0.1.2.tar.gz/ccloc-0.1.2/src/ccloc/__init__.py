39f1151a88c68b7dab48840e54571f96d6bd436a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:24:36 +0000

    Add package ccloc/0.1.2/ccloc-0.1.2.tar.gz

---
+++ packages/ccloc/ccloc-0.1.2.tar.gz/ccloc-0.1.2/src/ccloc/__init__.py
@@ -0,0 +1,6 @@
+"""Count Lines of Code CLI Tool"""
+
+from ccloc.cli import cli
+
+def main() -> None:
+    cli()
---
+++ packages/ccloc/ccloc-0.1.2.tar.gz/ccloc-0.1.2/src/ccloc/__main__.py
@@ -0,0 +1,6 @@
+"""Allow running ccloc as a module: python -m ccloc"""
+
+from ccloc import main
+
+if __name__ == "__main__":
+    main()
