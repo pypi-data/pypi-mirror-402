70b52a890d2341f5822fc7eb42145d4fb245c4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:22:34 +0000

    Add package skyledge-sdk/1.0.17/skyledge_sdk-1.0.17.tar.gz

---
+++ packages/skyledge-sdk/skyledge_sdk-1.0.17.tar.gz/skyledge_sdk-1.0.17/skyledge_sdk/api/__init__.py
@@ -0,0 +1,27 @@
+# flake8: noqa
+
+# import apis into api package
+from skyledge_sdk.api.api_keys_api import ApiKeysApi
+from skyledge_sdk.api.apps_api import AppsApi
+from skyledge_sdk.api.asset_types_api import AssetTypesApi
+from skyledge_sdk.api.assets_api import AssetsApi
+from skyledge_sdk.api.attachments_api import AttachmentsApi
+from skyledge_sdk.api.billing_api import BillingApi
+from skyledge_sdk.api.collaborators_api import CollaboratorsApi
+from skyledge_sdk.api.control_rooms_api import ControlRoomsApi
+from skyledge_sdk.api.cycle_types_api import CycleTypesApi
+from skyledge_sdk.api.cycles_api import CyclesApi
+from skyledge_sdk.api.errors_api import ErrorsApi
+from skyledge_sdk.api.event_types_api import EventTypesApi
+from skyledge_sdk.api.events_api import EventsApi
+from skyledge_sdk.api.form_api import FormApi
+from skyledge_sdk.api.forms_api import FormsApi
+from skyledge_sdk.api.organization_api import OrganizationApi
+from skyledge_sdk.api.place_types_api import PlaceTypesApi
+from skyledge_sdk.api.places_api import PlacesApi
+from skyledge_sdk.api.rule_types_api import RuleTypesApi
+from skyledge_sdk.api.rules_api import RulesApi
+from skyledge_sdk.api.task_types_api import TaskTypesApi
+from skyledge_sdk.api.tasks_api import TasksApi
+from skyledge_sdk.api.users_api import UsersApi
+
