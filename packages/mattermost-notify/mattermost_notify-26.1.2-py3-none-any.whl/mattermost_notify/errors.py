c85668e072322eed5a1acedcde6c1520fb6d76a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:52:42 +0000

    Add package mattermost-notify/26.1.2/mattermost_notify-26.1.2-py3-none-any.whl

---
+++ packages/mattermost-notify/mattermost_notify-26.1.2-py3-none-any.whl/mattermost_notify/errors.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023 Greenbone AG
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+class MattermostNotifyError(Exception):
+    """Base error for all errors originating from mattermost-notify"""
