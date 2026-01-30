3c34cb1ce56a2e69609ef6e83cd4e901ce15f558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:24:47 +0000

    Add package huaweicloudsdkidentitycenterscim/3.1.184/huaweicloudsdkidentitycenterscim-3.1.184-py3-none-any.whl

---
+++ packages/huaweicloudsdkidentitycenterscim/huaweicloudsdkidentitycenterscim-3.1.184-py3-none-any.whl/huaweicloudsdkidentitycenterscim/v1/model/create_user_request.py
@@ -0,0 +1,160 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class CreateUserRequest:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'authorization': 'str',
+        'tenant_id': 'str',
+        'body': 'CreateUserReqBody'
+    }
+
+    attribute_map = {
+        'authorization': 'Authorization',
+        'tenant_id': 'tenant_id',
+        'body': 'body'
+    }
+
+    def __init__(self, authorization=None, tenant_id=None, body=None):
+        r"""CreateUserRequest
+
+        The model defined in huaweicloud sdk
+
+        :param authorization: 承载令牌
+        :type authorization: str
+        :param tenant_id: 租户的全局唯一标识符（ID）
+        :type tenant_id: str
+        :param body: Body of the CreateUserRequest
+        :type body: :class:`huaweicloudsdkidentitycenterscim.v1.CreateUserReqBody`
+        """
+        
+        
+
+        self._authorization = None
+        self._tenant_id = None
+        self._body = None
+        self.discriminator = None
+
+        self.authorization = authorization
+        self.tenant_id = tenant_id
+        if body is not None:
+            self.body = body
+
+    @property
+    def authorization(self):
+        r"""Gets the authorization of this CreateUserRequest.
+
+        承载令牌
+
+        :return: The authorization of this CreateUserRequest.
+        :rtype: str
+        """
+        return self._authorization
+
+    @authorization.setter
+    def authorization(self, authorization):
+        r"""Sets the authorization of this CreateUserRequest.
+
+        承载令牌
+
+        :param authorization: The authorization of this CreateUserRequest.
+        :type authorization: str
+        """
+        self._authorization = authorization
+
+    @property
+    def tenant_id(self):
+        r"""Gets the tenant_id of this CreateUserRequest.
+
+        租户的全局唯一标识符（ID）
+
+        :return: The tenant_id of this CreateUserRequest.
+        :rtype: str
+        """
+        return self._tenant_id
+
+    @tenant_id.setter
+    def tenant_id(self, tenant_id):
+        r"""Sets the tenant_id of this CreateUserRequest.
+
+        租户的全局唯一标识符（ID）
+
+        :param tenant_id: The tenant_id of this CreateUserRequest.
+        :type tenant_id: str
+        """
+        self._tenant_id = tenant_id
+
+    @property
+    def body(self):
+        r"""Gets the body of this CreateUserRequest.
+
+        :return: The body of this CreateUserRequest.
+        :rtype: :class:`huaweicloudsdkidentitycenterscim.v1.CreateUserReqBody`
+        """
+        return self._body
+
+    @body.setter
+    def body(self, body):
+        r"""Sets the body of this CreateUserRequest.
+
+        :param body: The body of this CreateUserRequest.
+        :type body: :class:`huaweicloudsdkidentitycenterscim.v1.CreateUserReqBody`
+        """
+        self._body = body
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, CreateUserRequest):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/huaweicloudsdkidentitycenterscim/huaweicloudsdkidentitycenterscim-3.1.184-py3-none-any.whl/huaweicloudsdkidentitycenterscim/v1/model/filter_dto.py
@@ -0,0 +1,137 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.utils.http_utils import sanitize_for_serialization
+
+
+class FilterDto:
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    sensitive_list = []
+
+    openapi_types = {
+        'supported': 'bool',
+        'max_results': 'int'
+    }
+
+    attribute_map = {
+        'supported': 'supported',
+        'max_results': 'maxResults'
+    }
+
+    def __init__(self, supported=None, max_results=None):
+        r"""FilterDto
+
+        The model defined in huaweicloud sdk
+
+        :param supported: 一个布尔值，表示服务提供商是否支持这种操作
+        :type supported: bool
+        :param max_results: 最大结果数
+        :type max_results: int
+        """
+        
+        
+
+        self._supported = None
+        self._max_results = None
+        self.discriminator = None
+
+        if supported is not None:
+            self.supported = supported
+        if max_results is not None:
+            self.max_results = max_results
+
+    @property
+    def supported(self):
+        r"""Gets the supported of this FilterDto.
+
+        一个布尔值，表示服务提供商是否支持这种操作
+
+        :return: The supported of this FilterDto.
+        :rtype: bool
+        """
+        return self._supported
+
+    @supported.setter
+    def supported(self, supported):
+        r"""Sets the supported of this FilterDto.
+
+        一个布尔值，表示服务提供商是否支持这种操作
+
+        :param supported: The supported of this FilterDto.
+        :type supported: bool
+        """
+        self._supported = supported
+
+    @property
+    def max_results(self):
+        r"""Gets the max_results of this FilterDto.
+
+        最大结果数
+
+        :return: The max_results of this FilterDto.
+        :rtype: int
+        """
+        return self._max_results
+
+    @max_results.setter
+    def max_results(self, max_results):
+        r"""Sets the max_results of this FilterDto.
+
+        最大结果数
+
+        :param max_results: The max_results of this FilterDto.
+        :type max_results: int
+        """
+        self._max_results = max_results
+
+    def to_dict(self):
+        result = {}
+
+        for attr, _ in self.openapi_types.items():
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                if attr in self.sensitive_list:
+                    result[attr] = "****"
+                else:
+                    result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        import simplejson as json
+        return json.dumps(sanitize_for_serialization(self), ensure_ascii=False)
+
+    def __repr__(self):
+        """For `print`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, FilterDto):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
