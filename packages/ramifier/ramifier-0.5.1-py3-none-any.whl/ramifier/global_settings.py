ab157c4b12a5d927ca80c4b61249fd305c743d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:55 +0000

    Add package ramifier/0.5.1/ramifier-0.5.1-py3-none-any.whl

---
+++ packages/ramifier/ramifier-0.5.1-py3-none-any.whl/ramifier/global_settings.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+from .utils import ensure_dir, get_ram_dir
+
+
+class GlobalSettings:
+    def __init__(
+        self,
+        max_backups: int,
+        interval: dict,
+        compression_level: int,
+        compression_threads: int,
+        ram_dir: str,
+    ):
+        self.max_backups = max_backups
+        self.interval = interval
+        self.compression_level = compression_level
+        self.compression_threads = compression_threads
+        if ram_dir is None:
+            self.ram_dir = get_ram_dir()
+        else:
+            ram_dir = Path(os.path.expandvars(ram_dir)).expanduser() / "ramifier"
+            ensure_dir(ram_dir, 0o700)
+            self.ram_dir = ram_dir
---
+++ packages/ramifier/ramifier-0.5.1-py3-none-any.whl/ramifier/state.py
@@ -0,0 +1,103 @@
+import json
+import os
+from collections import deque
+from pathlib import Path
+from threading import Lock
+
+from xdg import BaseDirectory
+
+from .log import log_info
+from .target import Target
+from .utils import ensure_dir
+
+STATE_PATH = Path(BaseDirectory.xdg_state_home) / "ramifier"
+STATE_FILE = STATE_PATH / "state.json"
+STATE_TEMP_FILE = STATE_FILE.with_suffix(".tmp")
+STATE_LOCK = Lock()
+
+STATE = {"targets": {}}
+
+
+def load_state():
+    if STATE_FILE.exists():
+        with STATE_FILE.open("r") as f_in:
+            loaded_state = json.load(f_in)
+
+        STATE.update(loaded_state)
+    else:
+        _save_state()
+
+
+def mark_start(target: Target):
+    STATE["targets"].setdefault(
+        target.name,
+        {
+            "backups": [],
+            "hash_history": deque(),
+            "running": True,
+        },
+    )
+    _save_state()
+
+
+def mark_running(target: Target):
+    STATE["targets"].setdefault(target.name, {})["running"] = True
+    _save_state()
+
+
+def mark_backup(target: Target, backup_file: Path, backup_hash: str):
+    STATE["targets"].setdefault(target.name, {}).setdefault("backups", []).append(
+        {"file": str(backup_file), "hash": backup_hash}
+    )
+    _save_state()
+
+
+def mark_hash(target: Target, hash: str):
+    STATE["targets"].setdefault(target.name, {}).setdefault(
+        "hash_history", deque()
+    ).append(hash)
+    _save_state()
+
+
+def mark_clean_exit(target: Target):
+    STATE["targets"].setdefault(target.name, {})["running"] = False
+    log_info("Exited cleanly", target.name)
+    _save_state()
+
+
+def set_hash_history_len(target: Target, hash_history_len: int):
+    hash_history = get_hash_history(target)
+    STATE["targets"][target.name]["hash_history"] = deque(
+        hash_history, hash_history_len
+    )
+    _save_state()
+
+
+def remove_backup(target: Target, backup: dict):
+    STATE["targets"][target.name]["backups"].remove(backup)
+    _save_state()
+
+
+def get_backups(target: Target) -> list:
+    return STATE["targets"].get(target.name, {}).get("backups", [])
+
+
+def get_hash_history(target: Target) -> deque[str]:
+    return STATE["targets"].get(target.name, {}).get("hash_history", deque())
+
+
+def get_running(target: Target) -> bool:
+    return STATE["targets"].get(target.name, {}).get("running", False)
+
+
+def _save_state():
+    ensure_dir(STATE_PATH)
+    with STATE_LOCK:
+        with STATE_TEMP_FILE.open("w") as f_out:
+            json.dump(
+                STATE,
+                f_out,
+                default=lambda o: list(o) if isinstance(o, deque) else None,
+                indent=4,
+            )
+        os.replace(STATE_TEMP_FILE, STATE_FILE)
