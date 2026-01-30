a2a884c4ab1cb378b34a8a19436f972c9056bac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:36:09 +0000

    Add package udata-search-service/2.3.2/udata_search_service-2.3.2.tar.gz

---
+++ packages/udata-search-service/udata_search_service-2.3.2.tar.gz/udata_search_service-2.3.2/udata_search_service/__init__.py
@@ -0,0 +1,10 @@
+'''
+udata search service
+'''
+
+import os
+
+tag = os.environ.get('CIRCLE_TAG')
+build_num = os.environ.get('CIRCLE_BUILD_NUM')
+
+__version__ = '2.3.2' + (str(build_num) if not tag and build_num else '')
---
+++ packages/udata-search-service/udata_search_service-2.3.2.tar.gz/udata_search_service-2.3.2/udata_search_service/domain/interfaces.py
@@ -0,0 +1,62 @@
+from abc import ABC, abstractmethod
+from typing import Tuple, Optional, List
+from udata_search_service.domain.entities import Dataset, Organization, Reuse, Dataservice
+
+
+class SearchClient(ABC):
+
+    @abstractmethod
+    def init_indices(self) -> None:
+        pass
+
+    @abstractmethod
+    def clean_indices(self) -> None:
+        pass
+
+    @abstractmethod
+    def index_organization(self, to_index: Organization) -> None:
+        pass
+
+    @abstractmethod
+    def index_dataset(self, to_index: Dataset) -> None:
+        pass
+
+    @abstractmethod
+    def index_reuse(self, to_index: Reuse) -> None:
+        pass
+
+    @abstractmethod
+    def index_dataservice(self, to_index: Dataservice) -> None:
+        pass
+
+    @abstractmethod
+    def query_organizations(self, query_text: str, offset: int, page_size: int, filters: dict, sort: Optional[str] = None) -> Tuple[int, List[dict], dict]:
+        pass
+
+    @abstractmethod
+    def query_datasets(self, query_text: str, offset: int, page_size: int, filters: dict, sort: Optional[str] = None) -> Tuple[int, List[dict], dict]:
+        pass
+
+    @abstractmethod
+    def query_reuses(self, query_text: str, offset: int, page_size: int, filters: dict, sort: Optional[str] = None) -> Tuple[int, List[dict], dict]:
+        pass
+
+    @abstractmethod
+    def query_dataservices(self, query_text: str, offset: int, page_size: int, filters: dict, sort: Optional[str] = None) -> Tuple[int, List[dict], dict]:
+        pass
+
+    @abstractmethod
+    def find_one_organization(self, organization_id: str) -> Optional[Organization]:
+        pass
+
+    @abstractmethod
+    def find_one_dataset(self, dataset_id: str) -> Optional[Dataset]:
+        pass
+
+    @abstractmethod
+    def find_one_reuse(self, reuse_id: str) -> Optional[Reuse]:
+        pass
+
+    @abstractmethod
+    def find_one_dataservice(self, dataservice_id: str) -> Optional[Dataservice]:
+        pass
