780eb7bac5d4b5fa79c9fd2e268b615e8dadcdcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:35:39 +0000

    Add package githubtakeout/0.1.20/githubtakeout-0.1.20.tar.gz

---
+++ packages/githubtakeout/githubtakeout-0.1.20.tar.gz/githubtakeout-0.1.20/README.md
@@ -0,0 +1,124 @@
+# githubtakeout
+
+### Backup and archive Git Repos and Gists from GitHub
+
+---
+
+[![Supported Python Versions](https://img.shields.io/pypi/pyversions/githubtakeout)](https://pypi.org/project/githubtakeout)
+
+- Copyright (c) 2015-2026 [Corey Goldberg][github-home]
+- Development: [GitHub][github-repo]
+- Download/Install: [PyPI][pypi-githubtakeout]
+- License: [MIT][mit-license]
+
+----
+
+## About:
+
+`githubtakeout` is a data export tool for backing up and archiving Git
+repositories hosted on GitHub. It clones a user's repos and creates an archive
+of each.
+
+It supports public/private repos and public/secret gists. By default, it
+doesn't save commit history or branches (`.git` directory), or Gist
+repositories (both can be enabled with command line options).
+
+When you run the program, archives of your repos will be saved in a directory
+named `backups` inside your current working directory, unless a different
+location is specified using the `--dir` option.
+
+Archives are saved in compressed zip format (`.zip`) by default. You can also
+save as tarballs (`.tar.gz`) using the `--format=tar` option, or skip archiving
+using the `--format=none` option.
+
+If a repo with history exists from a previous run, it will pull new changes.
+Otherwise, it will clone the repo. Use the `--keep` if you don't want repos
+deleted after an archive is created.
+
+## Requirements:
+
+- Python 3.12+
+- Git 1.7+
+
+## Installation:
+
+#### Install from [PyPI][pypi-githubtakeout]:
+
+```
+pip install githubtakeout
+```
+
+## Authentication:
+
+By default, `githubtakeout` will only retrieve an account's public repos. To
+access private repos and secret gists, you need to authenticate.
+
+First, you must create a [personal access token][github-pat] on Github (either
+a fine-grained or classic personal access token). Once you have a token, you
+can set the `GITHUB_TOKEN` environment variable:
+
+```
+$ export GITHUB_TOKEN=<access token>
+```
+
+You can also set this in an `.env` file in the current directory. It should
+contain:
+
+```
+GITHUB_TOKEN=<access token>
+```
+
+If you prefer to be prompted for your token each time you run the program, use
+the `--token` argument.
+
+## CLI Options:
+
+```
+$ githubtakeout --help
+usage: githubtakeout [-h] [--dir DIR] [--pattern PATTERN] [--skip_pattern PATTERN]
+                     [--format {tar,zip,none}] [--gists] [--history]
+                     [--skip_forks] [--keep] [--list] [--token]
+                     username
+
+positional arguments:
+  username                 github username
+
+options:
+  -h, --help               show this help message and exit
+  --dir DIR                output directory (default: .)
+  --pattern PATTERN        regex matching repo names to include
+  --skip_pattern PATTERN   regex matching repo names to skip
+  --format {tar,zip,none}  archive format (default: zip)
+  --gists                  include gists
+  --history                include commit history and branches (.git directory)
+  --skip_forks             skip repos that are forks
+  --keep                   keep repos after archiving
+  --list                   list repos only
+  --token                  prompt for auth token
+```
+
+## Usage Examples:
+
+#### Install from PyPI with pipx, Run:
+
+```
+pipx install githubtakeout
+githubtakeout <github username>
+```
+
+#### Clone Repo, Create/Activate Virtual Environment, Install from Source, Run:
+
+```
+git clone git@github.com:cgoldberg/githubtakeout.git
+cd ./githubtakeout
+python3 -m venv venv
+source venv/bin/activate
+pip install .
+githubtakeout <github username>
+```
+
+[github-home]: https://github.com/cgoldberg
+[github-repo]: https://github.com/cgoldberg/githubtakeout
+[github-pat]: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+[pypi-githubtakeout]: https://pypi.org/project/githubtakeout
+[mit-license]: https://raw.githubusercontent.com/cgoldberg/githubtakeout/refs/heads/master/LICENSE
---
+++ packages/githubtakeout/githubtakeout-0.1.20.tar.gz/githubtakeout-0.1.20/tests/test_githubtakeout.py
@@ -0,0 +1,267 @@
+# Copyright (c) 2015-2026 Corey Goldberg
+# License: MIT
+
+"""Tests for githubtakeout."""
+
+import os
+import re
+from pathlib import Path
+
+import pytest
+from dotenv import load_dotenv
+
+import githubtakeout
+
+USER = "cgoldberg"
+
+
+@pytest.fixture(autouse=True, scope="session")
+def require_token():
+    load_dotenv()
+    required = "GITHUB_TOKEN"
+    if not os.getenv(required):
+        pytest.skip(
+            reason=f"{required} environment variable or .env file is "
+            "required to avoid rate limiting"
+        )
+
+
+@pytest.mark.skipif(
+    os.getenv("GITHUB_TOKEN"), reason="GITHUB_TOKEN required to avoid rate limiting"
+)
+def test_run_list_1_match(tmp_path, caplog):
+    caplog.set_level("INFO")
+    repo = "githubtakeout"
+    githubtakeout.run(
+        username=USER,
+        base_dir=tmp_path,
+        pattern=repo,
+        skip_pattern=None,
+        archive_format="none",
+        include_gists=False,
+        include_history=False,
+        skip_forks=False,
+        keep=False,
+        list_only=True,
+        prompt_for_token=False,
+    )
+    assert "creating archives" not in caplog.text
+    assert f"found 1 repos for user '{USER}'" in caplog.text
+    assert not re.search(r"found \d* gists", caplog.text)
+    assert f"{USER}/{repo}" in caplog.text
+    assert not Path(tmp_path / "backups" / repo).exists()
+
+
+def test_run_list_0_match(tmp_path, caplog):
+    caplog.set_level("INFO")
+    repo = "this_repo_does_not_exist"
+    githubtakeout.run(
+        username=USER,
+        base_dir=tmp_path,
+        pattern=repo,
+        skip_pattern=None,
+        archive_format="none",
+        include_gists=False,
+        include_history=False,
+        skip_forks=False,
+        keep=False,
+        list_only=True,
+        prompt_for_token=False,
+    )
+    assert "creating archives" not in caplog.text
+    assert "found 0 repos" in caplog.text
+    assert not re.search(r"found \d* gists", caplog.text)
+    assert not Path(tmp_path / "backups" / repo).exists()
+
+
+def test_run_list_0_match_with_gists(tmp_path, caplog):
+    caplog.set_level("INFO")
+    repo = "this_repo_does_not_exist"
+    githubtakeout.run(
+        username=USER,
+        base_dir=tmp_path,
+        pattern=repo,
+        skip_pattern=None,
+        archive_format="none",
+        include_gists=True,
+        include_history=False,
+        skip_forks=False,
+        keep=False,
+        list_only=True,
+        prompt_for_token=False,
+    )
+    assert "creating archives" not in caplog.text
+    assert "found 0 repos" in caplog.text
+    assert re.search(r"found \d* gists", caplog.text)
+    assert not Path(tmp_path / "backups" / repo).exists()
+
+
+def test_run_list_skip_all(tmp_path, caplog):
+    caplog.set_level("INFO")
+    githubtakeout.run(
+        username=USER,
+        base_dir=tmp_path,
+        pattern=".*",
+        skip_pattern=".*",
+        archive_format="none",
+        include_gists=False,
+        include_history=False,
+        skip_forks=False,
+        keep=False,
+        list_only=True,
+        prompt_for_token=False,
+    )
+    assert "creating archives" not in caplog.text
+    assert "found 0 repos" in caplog.text
+
+
+@pytest.mark.parametrize("archive_format", ["zip", "tar"])
+def test_run_archive_1_match(archive_format, tmp_path, caplog):
+    caplog.set_level("INFO")
+    backup_dir = "backups"
+    repo = "githubtakeout"
+    githubtakeout.run(
+        username=USER,
+        base_dir=tmp_path,
+        pattern=repo,
+        skip_pattern=None,
+        archive_format=archive_format,
+        include_gists=False,
+        include_history=False,
+        skip_forks=False,
+        keep=False,
+        list_only=False,
+        prompt_for_token=False,
+    )
+    assert re.search(f"creating archives in: .*{backup_dir}", caplog.text)
+    assert f"found 1 repos for user '{USER}'" in caplog.text
+    assert "gists" not in caplog.text
+    assert re.search(f"cloning repo: {USER}/{repo}.git to: .*{backup_dir}", caplog.text)
+    assert "creating archive:" in caplog.text
+    assert "archive size:" in caplog.text
+    assert "deleting repo" in caplog.text
+    assert f"successfully backed up '{repo}' repo" in caplog.text
+    assert not Path(tmp_path / backup_dir / repo).exists()
+    if archive_format == "tar":
+        extension = "tar.gz"
+    else:
+        extension = archive_format
+    assert Path(tmp_path / backup_dir / f"{repo}.{extension}").exists()
+
+
+def test_run_no_archive_1_match_with_history(tmp_path, caplog):
+    caplog.set_level("INFO")
+    backup_dir = "backups"
+    repo = "githubtakeout"
+    githubtakeout.run(
+        username=USER,
+        base_dir=tmp_path,
+        pattern=repo,
+        skip_pattern=None,
+        archive_format="none",
+        include_gists=False,
+        include_history=True,
+        skip_forks=False,
+        keep=False,
+        list_only=False,
+        prompt_for_token=False,
+    )
+    assert re.search(f"creating archives in: .*{backup_dir}", caplog.text)
+    assert f"found 1 repos for user '{USER}'" in caplog.text
+    assert "gists" not in caplog.text
+    assert re.search(f"cloning repo: {USER}/{repo}.git to: .*{backup_dir}", caplog.text)
+    assert "creating archive:" not in caplog.text
+    assert "archive size:" not in caplog.text
+    assert "deleting repo" not in caplog.text
+    assert f"successfully backed up '{repo}' repo" in caplog.text
+    assert Path(tmp_path / backup_dir / repo).exists()
+    assert Path(tmp_path / backup_dir / repo / ".git").exists()
+    for extension in ("tar.gz", "zip"):
+        assert not Path(tmp_path / backup_dir / f"{repo}.{extension}").exists()
+
+
+def test_run_no_archive_1_match_without_history(tmp_path, caplog):
+    caplog.set_level("INFO")
+    backup_dir = "backups"
+    repo = "githubtakeout"
+    githubtakeout.run(
+        username=USER,
+        base_dir=tmp_path,
+        pattern=repo,
+        skip_pattern=None,
+        archive_format="none",
+        include_gists=False,
+        include_history=False,
+        skip_forks=False,
+        keep=False,  # this doesn't matter when archive_format is "none"
+        list_only=False,
+        prompt_for_token=False,
+    )
+    assert re.search("creating archives in: .*backups", caplog.text)
+    assert f"found 1 repos for user '{USER}'" in caplog.text
+    assert "gists" not in caplog.text
+    assert re.search(f"cloning repo: {USER}/{repo}.git to: .*backups", caplog.text)
+    assert "creating archive:" not in caplog.text
+    assert "archive size:" not in caplog.text
+    assert "deleting repo" not in caplog.text
+    assert f"successfully backed up '{repo}' repo" in caplog.text
+    assert Path(tmp_path / backup_dir / repo).exists()
+    assert not Path(tmp_path / backup_dir / repo / ".git").exists()
+    for extension in ("tar.gz", "zip"):
+        assert not Path(tmp_path / backup_dir / f"{repo}.{extension}").exists()
+
+
+def test_run_archive_1_match_with_history_pull_with_keep(tmp_path, caplog):
+    caplog.set_level("INFO")
+    backup_dir = "backups"
+    repo = "githubtakeout"
+    githubtakeout.run(
+        username=USER,
+        base_dir=tmp_path,
+        pattern=repo,
+        skip_pattern=None,
+        archive_format="none",
+        include_gists=False,
+        include_history=True,
+        skip_forks=False,
+        keep=True,
+        list_only=False,
+        prompt_for_token=False,
+    )
+    assert f"found 1 repos for user '{USER}'" in caplog.text
+    assert "gists" not in caplog.text
+    assert re.search(f"cloning repo: {USER}/{repo}.git to: .*backups", caplog.text)
+    assert "pulling changes" not in caplog.text
+    assert "creating archive:" not in caplog.text
+    assert "archive size:" not in caplog.text
+    assert "deleting repo" not in caplog.text
+    assert f"successfully backed up '{repo}' repo" in caplog.text
+    assert Path(tmp_path / backup_dir / repo).exists()
+    assert Path(tmp_path / backup_dir / repo / ".git").exists()
+    caplog.clear()
+    githubtakeout.run(
+        username=USER,
+        base_dir=tmp_path,
+        pattern=repo,
+        skip_pattern=None,
+        archive_format="zip",
+        include_gists=False,
+        include_history=True,
+        skip_forks=False,
+        keep=True,
+        list_only=False,
+        prompt_for_token=False,
+    )
+    assert f"found 1 repos for user '{USER}'" in caplog.text
+    assert "gists" not in caplog.text
+    assert not re.search(f"cloning repo: {USER}/{repo}.git to: .*backups", caplog.text)
+    assert re.search(
+        f"pulling changes from repo: {USER}/{repo}.git to: .*backups", caplog.text
+    )
+    assert "creating archive:" in caplog.text
+    assert "archive size:" in caplog.text
+    assert "deleting repo" not in caplog.text
+    assert f"successfully backed up '{repo}' repo" in caplog.text
+    assert Path(tmp_path / backup_dir / repo).exists()
+    assert Path(tmp_path / backup_dir / repo / ".git").exists()
+    assert Path(tmp_path / backup_dir / f"{repo}.zip").exists()
