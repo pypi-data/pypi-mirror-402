1a57f6251005d564522eb6a8b2d602c3ffb312f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:44 +0000

    Add package bingart/1.5.0/bingart-1.5.0.tar.gz

---
+++ packages/bingart/bingart-1.5.0.tar.gz/bingart-1.5.0/bingart/__init__.py
@@ -0,0 +1,3 @@
+from .bingart import BingArt, Model, Aspect, AuthCookieError, PromptRejectedError
+
+__all__ = ["BingArt", "Model", "Aspect", "AuthCookieError", "PromptRejectedError"]
---
+++ packages/bingart/bingart-1.5.0.tar.gz/bingart-1.5.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="bingart",
+    version="1.5.0",
+    author="Maehdakvan",
+    author_email="visitanimation@google.com",
+    description="bingart is an unofficial API wrapper for Bing Image & Video Creator.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DedInc/bingart",
+    project_urls={
+        "Bug Tracker": "https://github.com/DedInc/bingart/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["curl_cffi", "rookiepy"],
+    python_requires=">=3.6",
+)
