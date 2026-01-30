7571de3f6cd1b7bf2dc2148dd43b760a197c411a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:17:23 +0000

    Add package pyflyby/1.10.5/pyflyby-1.10.5-cp314-cp314-macosx_11_0_arm64.whl

---
+++ packages/pyflyby/pyflyby-1.10.5-cp314-cp314-macosx_11_0_arm64.whl/pyflyby/__main__.py
@@ -0,0 +1,9 @@
+# pyflyby/__main__.py
+# Copyright (C) 2014, 2015 Karl Chen.
+# License: MIT http://opensource.org/licenses/MIT
+
+
+
+if __name__ == "__main__":
+    from pyflyby._py import py_main
+    py_main()
