2959c9630df004e30f06bbd43c2e9e30d79d2ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:21 +0000

    Add package ommx/2.3.4/ommx-2.3.4-cp310-abi3-macosx_11_0_arm64.whl

---
+++ packages/ommx/ommx-2.3.4-cp310-abi3-macosx_11_0_arm64.whl/ommx/adapter.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+from typing import Any
+from ommx.v1 import Instance, Solution, SampleSet
+
+
+SolverInput = Any
+SolverOutput = Any
+SamplerInput = Any
+SamplerOutput = Any
+
+
+class SolverAdapter(ABC):
+    """
+    An abstract interface for OMMX Solver Adapters, defining how solvers should be used with OMMX.
+
+    See the `implementation guide <https://jij-inc.github.io/ommx/en/user_guide/solver_adapter.html>`_ for more details.
+    """
+
+    @abstractmethod
+    def __init__(self, ommx_instance: Instance):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def solve(cls, ommx_instance: Instance) -> Solution:
+        pass
+
+    @property
+    @abstractmethod
+    def solver_input(self) -> SolverInput:
+        pass
+
+    @abstractmethod
+    def decode(self, data: SolverOutput) -> Solution:
+        pass
+
+
+class SamplerAdapter(SolverAdapter):
+    """
+    An abstract interface for OMMX Sampler Adapters, defining how samplers should be used with OMMX.
+
+    See the `implementation guide <https://jij-inc.github.io/ommx/en/user_guide/solver_adapter.html>`_ for more details.
+    """
+
+    @classmethod
+    @abstractmethod
+    def sample(cls, ommx_instance: Instance) -> SampleSet:
+        pass
+
+    @property
+    @abstractmethod
+    def sampler_input(self) -> SamplerInput:
+        pass
+
+    @abstractmethod
+    def decode_to_sampleset(self, data: SamplerOutput) -> SampleSet:
+        pass
+
+
+class InfeasibleDetected(Exception):
+    """
+    Raised when the problem is proven to be infeasible.
+
+    This corresponds to ``Optimality.OPTIMALITY_INFEASIBLE`` and indicates that
+    the mathematical model itself has no feasible solution.
+    Should not be used when infeasibility cannot be proven (e.g., heuristic solvers).
+    """
+
+    pass
+
+
+class UnboundedDetected(Exception):
+    """
+    Raised when the problem is proven to be unbounded.
+
+    This corresponds to ``Optimality.OPTIMALITY_UNBOUNDED`` and indicates that
+    the mathematical model itself is unbounded.
+    Should not be used when unboundedness cannot be proven (e.g., heuristic solvers).
+    """
+
+    pass
+
+
+class NoSolutionReturned(Exception):
+    """
+    Raised when no solution was returned.
+
+    This indicates that the solver did not return any solution (whether feasible
+    or not) (e.g., due to time limits).
+    This does not prove that the mathematical model itself is infeasible.
+    """
+
+    pass
