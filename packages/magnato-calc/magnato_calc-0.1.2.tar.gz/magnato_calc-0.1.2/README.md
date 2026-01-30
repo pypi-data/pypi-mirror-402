c38a42ff2ad3cefdc7939f53aba387058b225dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:36:23 +0000

    Add package magnato-calc/0.1.2/magnato_calc-0.1.2.tar.gz

---
+++ packages/magnato-calc/magnato_calc-0.1.2.tar.gz/magnato_calc-0.1.2/README.md
@@ -0,0 +1,7 @@
+# Mangnato
+
+A simple Calculator package for python have multi options
+
+## Installation
+
+pip install magnato-calc
---
+++ packages/magnato-calc/magnato_calc-0.1.2.tar.gz/magnato_calc-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="magnato_calc",
+    version="0.1.2",
+    author= "NeerajKr",
+    author_email= "magnato1997@gmail.com",
+    description="a simple calculator have multi options",
+    long_description=open("README.md","r", encoding ="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages = find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "magnato = magnato.calculator:main",],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
