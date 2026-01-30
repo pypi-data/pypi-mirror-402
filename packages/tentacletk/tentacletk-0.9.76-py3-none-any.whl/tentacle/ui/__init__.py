947b02144704a40710a2c1a0d2566af58a6044a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:51:36 +0000

    Add package tentacletk/0.9.76/tentacletk-0.9.76-py3-none-any.whl

---
+++ packages/tentacletk/tentacletk-0.9.76-py3-none-any.whl/tentacle/ui/__init__.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python
+# coding=utf-8
+import inspect
+import pythontk as ptk
+
+
+# module = inspect.getmodule(inspect.currentframe())  # this module.
+# path = ptk.get_object_path(module)  # this modules directory.
+
+# --------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
