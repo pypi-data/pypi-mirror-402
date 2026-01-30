46f2e37d01b8709eb8de3e42dc3f0eec6636a73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:09:28 +0000

    Add package apic0nnect0r/1.0.0/apic0nnect0r-1.0.0-py3-none-any.whl

---
+++ packages/apic0nnect0r/apic0nnect0r-1.0.0-py3-none-any.whl/apic0nnect0r/__init__.py
@@ -0,0 +1,9 @@
+from .code_data import SOURCE_CODE
+
+def show():
+    """Выводит код приложения"""
+    print(SOURCE_CODE)
+
+# Если кто-то запустит файл напрямую: python -m apic0nnect0r
+if __name__ == "__main__":
+    show()
