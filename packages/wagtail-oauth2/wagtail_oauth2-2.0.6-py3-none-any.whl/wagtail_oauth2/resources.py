5ccb1b974deab8dd5fb81a335ac49e5054f515e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:44 +0000

    Add package wagtail-oauth2/2.0.6/wagtail_oauth2-2.0.6-py3-none-any.whl

---
+++ packages/wagtail-oauth2/wagtail_oauth2-2.0.6-py3-none-any.whl/wagtail_oauth2/resources.py
@@ -0,0 +1,104 @@
+"""
+Manage OAuth2.0 Resources.
+"""
+
+import logging
+from urllib.parse import urlencode
+
+import requests
+from requests.exceptions import ConnectionError, HTTPError, Timeout
+
+from .settings import get_setting
+
+log = logging.getLogger(__name__)
+
+
+class Token:
+    """Retrieve tokens from the OAuth2.0 Resource server."""
+
+    @classmethod
+    def by_authcode(cls, auth_code):
+        "Retrieve Tokens from an authorization code."
+        try:
+            token_url = get_setting("TOKEN_URL")
+            log.info(f"Fetching token on {token_url}/token")
+            response = requests.post(
+                token_url,
+                data={
+                    "client_id": get_setting("CLIENT_ID"),
+                    "client_secret": get_setting("CLIENT_SECRET"),
+                    "grant_type": "authorization_code",
+                    "code": auth_code,
+                },
+                verify=get_setting("VERIFY_CERTIFICATE", True),
+                timeout=get_setting("TIMEOUT", 30),
+            )
+            response.raise_for_status()
+            return response.json()
+
+        except (ConnectionError, TimeoutError, Timeout) as exc:
+            log.error(f"OAuth2 server is down: {exc.__class__.__name__}")
+        except HTTPError as exc:
+            if exc.response.status_code >= 500:
+                log.error(
+                    f"OAuth2 server is not working properly? got {exc.response.status_code}"
+                )
+            else:
+                log.error(f"Cannot retrieve token: {exc.response.text}")
+        except Exception:
+            log.exception(
+                "Unexpected exception while retrieving token using authorization code"
+            )
+        return {}
+
+    @classmethod
+    def get_authenticated_url(cls, login_url, state):
+        "Get the authorization url with its parameter."
+        data = {
+            "client_id": get_setting("CLIENT_ID"),
+            "redirect_uri": login_url,
+            "response_type": "code",
+            "state": state,
+        }
+        return "{}?{}".format(get_setting("AUTH_URL"), urlencode(data, doseq=True))
+
+    @classmethod
+    def by_refresh_token(cls, refresh_token):
+        "Retrieve Tokens from a refresh tokend."
+        try:
+            token_url = get_setting("TOKEN_URL")
+            log.info(f"Refreshing token on {token_url}/token")
+            response = requests.post(
+                token_url,
+                data={
+                    "client_id": get_setting("CLIENT_ID"),
+                    "client_secret": get_setting("CLIENT_SECRET"),
+                    "grant_type": "refresh_token",
+                    "refresh_token": refresh_token,
+                },
+                verify=get_setting("VERIFY_CERTIFICATE", True),
+                timeout=get_setting("TIMEOUT", 30),
+            )
+            response.raise_for_status()
+            tokens = response.json()
+            return tokens
+        except (ConnectionError, TimeoutError, Timeout) as exc:
+            log.error(f"OAuth2 server is down: {exc.__class__.__name__}")
+        except HTTPError as exc:
+            if 400 <= exc.response.status_code < 500:
+                log.warning(
+                    "OAuth2 server does not refresh the token (%s: %s), "
+                    "force disconnect",
+                    exc.response.status_code,
+                    exc.response.text,
+                )
+            elif 500 <= exc.response.status_code < 600:
+                log.error(
+                    "OAuth2 server is not working properly? got %s",
+                    exc.response.status_code,
+                )
+        except Exception:
+            log.exception(
+                "Unexpected exception while retrieving token using refresh token"
+            )
+        return {}
---
+++ packages/wagtail-oauth2/wagtail_oauth2-2.0.6-py3-none-any.whl/wagtail_oauth2/settings.py
@@ -0,0 +1,10 @@
+"""Load settings for the wagtail-oauth2 app."""
+
+from django.conf import settings
+
+global_prefix = "OAUTH2_"
+
+
+def get_setting(name, default=None):
+    """Get the settings without the prefix."""
+    return getattr(settings, global_prefix + name, default)
