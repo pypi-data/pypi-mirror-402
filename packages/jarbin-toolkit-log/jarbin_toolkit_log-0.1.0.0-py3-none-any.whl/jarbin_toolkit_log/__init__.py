74ccf9b4b195d7ac75a75ec55356d82664057438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:16:49 +0000

    Add package jarbin-toolkit-log/0.1.0.0/jarbin_toolkit_log-0.1.0.0-py3-none-any.whl

---
+++ packages/jarbin-toolkit-log/jarbin_toolkit_log-0.1.0.0-py3-none-any.whl/jarbin_toolkit_log/__init__.py
@@ -0,0 +1,20 @@
+#############################
+###                       ###
+###     Jarbin-ToolKit    ###
+###          log          ###
+###  ----__init__.py----  ###
+###                       ###
+###=======================###
+### by JARJARBIN's STUDIO ###
+#############################
+
+
+from jarbin_toolkit_log import Log
+
+__all__ : list[str] = [
+    'Log'
+]
+
+
+__author__ : str = 'Nathan Jarjarbin'
+__email__ : str = 'nathan.amaraggi@epitech.eu'
