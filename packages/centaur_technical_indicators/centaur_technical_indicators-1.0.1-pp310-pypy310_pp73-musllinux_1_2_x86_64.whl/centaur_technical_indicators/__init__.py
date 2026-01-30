a29fea32c05a4aa7dc4890662c4df0b8b72715f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:09:46 +0000

    Add package centaur_technical_indicators/1.0.1/centaur_technical_indicators-1.0.1-pp310-pypy310_pp73-musllinux_1_2_x86_64.whl

---
+++ packages/centaur_technical_indicators/centaur_technical_indicators-1.0.1-pp310-pypy310_pp73-musllinux_1_2_x86_64.whl/centaur_technical_indicators/__init__.py
@@ -0,0 +1,5 @@
+from .centaur_technical_indicators import *
+
+__doc__ = centaur_technical_indicators.__doc__
+if hasattr(centaur_technical_indicators, "__all__"):
+    __all__ = centaur_technical_indicators.__all__
