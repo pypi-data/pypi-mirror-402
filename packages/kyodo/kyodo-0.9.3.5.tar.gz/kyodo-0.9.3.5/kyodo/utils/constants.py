8e01e01ef456b166c45ea6dea6ce8a1bb45ff181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:22 +0000

    Add package kyodo/0.9.3.5/kyodo-0.9.3.5.tar.gz

---
+++ packages/kyodo/kyodo-0.9.3.5.tar.gz/kyodo-0.9.3.5/kyodo/utils/constants.py
@@ -0,0 +1,10 @@
+
+
+api_url: str = "https://api.kyodo.app/v1"
+ws_api: str = "wss://ws.kyodo.app"
+
+app_version: str = "4.135.671"
+app_id: str = f"ios app.kyodo.android/{app_version}"
+
+AGORA_APP_ID: str = "2237f1a75eab41179f53546d82b82152"
+AGORA_APP_KEY: str = ""
