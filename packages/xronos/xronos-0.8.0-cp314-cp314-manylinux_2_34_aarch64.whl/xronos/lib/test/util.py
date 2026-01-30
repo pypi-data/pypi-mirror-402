09f6958fbc5e0d6e620fc9b2b4560cc937d2cd2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:51:14 +0000

    Add package xronos/0.8.0/xronos-0.8.0-cp314-cp314-manylinux_2_34_aarch64.whl

---
+++ packages/xronos/xronos-0.8.0-cp314-cp314-manylinux_2_34_aarch64.whl/xronos/lib/test/util.py
@@ -0,0 +1,51 @@
+# SPDX-FileCopyrightText: Copyright (c) Xronos Inc.
+# SPDX-License-Identifier: BSD-3-Clause
+
+import datetime
+from typing import Callable, Generic, TypeVar
+
+import xronos
+
+T = TypeVar("T")
+
+
+class AssertList(xronos.Reactor, Generic[T]):
+    """A reactor that asserts its input values against an expected list.
+
+    It also terminates the execution when all the expected values have been received.
+    """
+
+    input_ = xronos.InputPortDeclaration[T]()
+
+    def __init__(self, expected: list[T], debug: bool = False) -> None:
+        super().__init__()
+        self.expected = expected
+        self.expected_iter = iter(expected)
+        self.count = 0
+        self.debug = debug
+
+    @xronos.reaction
+    def on_input(self, interface: xronos.ReactionInterface) -> Callable[[], None]:
+        input_trigger = interface.add_trigger(self.input_)
+        shutdown_effect = interface.add_effect(self.shutdown)
+
+        def handler() -> None:
+            if self.debug:
+                print(f"{datetime.datetime.now()} - Received: {input_trigger.get()}")
+            assert input_trigger.get() == next(self.expected_iter)
+            self.count += 1
+
+            # When all the expected values have been received, request a shutdown.
+            if self.count == len(self.expected):
+                shutdown_effect.trigger_shutdown()
+
+        return handler
+
+    @xronos.reaction
+    def on_shutdown(self, interface: xronos.ReactionInterface) -> Callable[[], None]:
+        interface.add_trigger(self.shutdown)
+
+        def handler() -> None:
+            assert self.count == len(self.expected)
+
+        return handler
