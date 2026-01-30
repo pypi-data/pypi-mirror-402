aa3a554f78b902bcee8104f306b713f6bdb562ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:52:21 +0000

    Add package mslk-rocm/0.0.0/mslk_rocm-0.0.0.tar.gz

---
+++ packages/mslk-rocm/mslk_rocm-0.0.0.tar.gz/mslk_rocm-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mslk-rocm", # Make sure this name is unique on PyPI
+    version="0.0.0",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    # Add other required metadata like author, description, etc.
+)
