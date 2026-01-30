e4ffa53f4fa6e9b45c18a38e181a66d0c80802f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:17:24 +0000

    Add package area-Irfankhan/0.1.1/area_irfankhan-0.1.1-py3-none-any.whl

---
+++ packages/area-Irfankhan/area_irfankhan-0.1.1-py3-none-any.whl/area_irfankhan/library.py
@@ -0,0 +1,11 @@
+def square(side):
+    return side * side
+
+def rectangle(length, breadth):
+    return length * breadth
+
+def triangle(base, height):
+    return (base * height) / 2
+
+def circle(radius):
+    return 3.14 * radius * radius
