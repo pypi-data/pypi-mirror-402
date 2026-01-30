d9ef496a2fa33e25a06c240024110c6ff0320c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:42 +0000

    Add package area-lib-limendra/1.0.0/area_lib_limendra-1.0.0-py3-none-any.whl

---
+++ packages/area-lib-limendra/area_lib_limendra-1.0.0-py3-none-any.whl/area/area.py
@@ -0,0 +1,13 @@
+import math
+
+def area_square(side):
+    return side*side
+
+def area_circle(radius):
+    return math.pi*radius*radius
+
+def area_rectangle(length,breadth):
+    return length * breadth
+
+def area_triangle(base,height):
+    return 0.5*base*height
