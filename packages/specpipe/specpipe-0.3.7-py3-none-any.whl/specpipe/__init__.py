6cfd5f52404efa945f29e0823c5bea6a67090118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:46 +0000

    Add package specpipe/0.3.7/specpipe-0.3.7-py3-none-any.whl

---
+++ packages/specpipe/specpipe-0.3.7-py3-none-any.whl/specpipe/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+SpecPipe (DEPRECATED)
+
+This package has been renamed to 'swectral'.
+It is no longer maintained.
+"""
+
+# Package meta
+__version__ = "0.3.7"
+__author__ = "Siwei Luo"
+__license__ = "MIT"
+
+import warnings
+
+warnings.warn(
+    "SpecPipe has been renamed to 'swectral'. Please install and use 'swectral' instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
