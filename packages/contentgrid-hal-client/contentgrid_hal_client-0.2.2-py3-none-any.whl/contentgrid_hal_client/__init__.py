354fdc58fb5b21f4eda136dad630ab367657106a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:16:11 +0000

    Add package contentgrid-hal-client/0.2.2/contentgrid_hal_client-0.2.2-py3-none-any.whl

---
+++ packages/contentgrid-hal-client/contentgrid_hal_client-0.2.2-py3-none-any.whl/contentgrid_hal_client/__init__.py
@@ -0,0 +1,3 @@
+from .hal import HALLink, CurieRegistry, Curie, HALResponse, InteractiveHALResponse, HALFormsClient # noqa: F401
+from .exceptions import NotFound, Unauthorized, BadRequest, IncorrectAttributeType, NonExistantAttribute, MissingRequiredAttribute, MissingHALTemplate, Forbidden # noqa: F401
+from .hal_forms import HALFormsTemplate, HALFormsProperty # noqa: F401
