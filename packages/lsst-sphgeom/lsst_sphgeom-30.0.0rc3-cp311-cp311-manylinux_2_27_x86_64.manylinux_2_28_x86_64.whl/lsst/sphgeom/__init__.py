5b56e160244ea0da72eed6b6de2f6726d088fb39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:09:40 +0000

    Add package lsst-sphgeom/30.0.0rc3/lsst_sphgeom-30.0.0rc3-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/lsst-sphgeom/lsst_sphgeom-30.0.0rc3-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/lsst/sphgeom/__init__.py
@@ -0,0 +1,40 @@
+# This file is part of sphgeom.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This software is dual licensed under the GNU General Public License and also
+# under a 3-clause BSD license. Recipients may choose which of these licenses
+# to use; please see the files gpl-3.0.txt and/or bsd_license.txt,
+# respectively.  If you choose the GPL option then the following text applies
+# (but note that there is still no warranty even if you opt for BSD instead):
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""lsst.sphgeom
+"""
+
+from ._continue_class import *
+from ._healpixPixelization import *
+from ._sphgeom import *
+from ._sphgeom import Pixelization
+from ._yaml import *
+from .pixelization_abc import *
+from .version import *
+
+PixelizationABC.register(Pixelization)
