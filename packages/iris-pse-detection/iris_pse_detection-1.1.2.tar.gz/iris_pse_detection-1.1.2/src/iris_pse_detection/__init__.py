33981ab5e32da4671ee340db5bd6d60eb6860f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:22:09 +0000

    Add package iris-pse-detection/1.1.2/iris_pse_detection-1.1.2.tar.gz

---
+++ packages/iris-pse-detection/iris_pse_detection-1.1.2.tar.gz/iris_pse_detection-1.1.2/src/iris_pse_detection/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2026 iris_pse_detection contributors
+# SPDX-License-Identifier: MIT
+#
+# Based on IRIS by Electronic Arts Inc.
+# https://github.com/electronicarts/IRIS
+
+"""
+IRIS-PSE-Detection: Photosensitive epilepsy risk detection for video content.
+
+A Python port of EA's IRIS library for detecting:
+- Luminance flashes
+- Red saturation flashes
+- Spatial patterns
+
+that could potentially cause photosensitive epileptic risks.
+"""
+
+from iris_pse_detection.configuration import Configuration
+from iris_pse_detection.frame_data import FrameData
+from iris_pse_detection.result import AnalysisResult, FlashResult, PatternResult, Result
+from iris_pse_detection.video_analyser import VideoAnalyser
+
+__version__ = "1.1.2"
+__all__ = [
+    "VideoAnalyser",
+    "Configuration",
+    "FrameData",
+    "AnalysisResult",
+    "FlashResult",
+    "PatternResult",
+    "Result",
+]
