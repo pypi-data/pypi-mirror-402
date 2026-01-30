40e8a9a919991ddff80b29a613347a79d1021bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:35:58 +0000

    Add package glowprom/1.2.1/glowprom-1.2.1.tar.gz

---
+++ packages/glowprom/glowprom-1.2.1.tar.gz/glowprom-1.2.1/glowprom/arguments.py
@@ -0,0 +1,106 @@
+# glowprom
+# Copyright (C) 2020 Andrew Wilkinson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+import os
+
+from .exceptions import InvalidArguments
+
+DEFAULT_MQTT = "glowmqtt.energyhive.com"
+
+parser = argparse.ArgumentParser(
+    description="Listens to meter reports from Glow (glowmarkt.com) MQTT and"
+    + " exposes them as prometheus metrics"
+)
+parser.add_argument(
+    "--mqtt",
+    type=str,
+    nargs="?",
+    default=DEFAULT_MQTT,
+    help="the mqtt server to connect to. leave unset for the "
+    + "Glow cloud MQTT. (can also be set with $GLOWPROM_MQTT)",
+)
+parser.add_argument(
+    "--port",
+    type=int,
+    nargs="?",
+    default=1883,
+    help="the mqtt port to connect to. (can also be set with " + "$GLOWPROM_MQTT_PORT)",
+)
+parser.add_argument(
+    "--user",
+    type=str,
+    nargs="?",
+    help="the user name to use (can also be set with " + "$GLOWPROM_USER)",
+)
+parser.add_argument(
+    "--passwd",
+    type=str,
+    nargs="?",
+    help="the password to use (can also be set with " + "$GLOWPROM_PASSWD)",
+)
+parser.add_argument(
+    "--topic",
+    type=str,
+    nargs="?",
+    help="the topic to listen on for cloud MQTT"
+    + " (can also be set with $GLOWPROM_TOPIC)",
+)
+parser.add_argument(
+    "--bind",
+    type=str,
+    nargs="?",
+    default="0.0.0.0:9100",
+    help="the ip address and port to bind to",
+)
+
+
+def get_arguments(args):
+    args = parser.parse_args(args)
+    if "GLOWPROM_MQTT" in os.environ:
+        args.mqtt = os.environ["GLOWPROM_MQTT"]
+    if "GLOWPROM_MQTT_PORT" in os.environ:
+        args.port = os.environ["GLOWPROM_MQTT_PORT"]
+    if "GLOWPROM_USER" in os.environ:
+        args.user = os.environ["GLOWPROM_USER"]
+    if "GLOWPROM_PASSWD" in os.environ:
+        args.passwd = os.environ["GLOWPROM_PASSWD"]
+    if "GLOWPROM_TOPIC" in os.environ:
+        args.topic = os.environ["GLOWPROM_TOPIC"]
+
+    args.cloud = args.mqtt == DEFAULT_MQTT
+
+    if args.cloud and args.user is None:
+        raise InvalidArguments(
+            "No username supplied. Either use --user " + "or set $GLOWPROM_USER"
+        )
+    if args.cloud and args.passwd is None:
+        raise InvalidArguments(
+            "No password supplied. Either use --passwd " + "or set $GLOWPROM_PASSWD"
+        )
+    if args.cloud and args.topic is None:
+        raise InvalidArguments(
+            "No topic supplied. Either use --topic " + "or set $GLOWPROM_TOPIC"
+        )
+    if not args.cloud and args.topic is not None:
+        raise InvalidArguments("A topic should only be supplied when using cloud MQTT.")
+
+    if ":" not in args.bind:
+        args.bind = (args.bind, 9100)
+    else:
+        args.bind = (args.bind.split(":")[0], int(args.bind.split(":")[1]))
+
+    return args
---
+++ packages/glowprom/glowprom-1.2.1.tar.gz/glowprom-1.2.1/glowprom/mqtt.py
@@ -0,0 +1,69 @@
+# glowprom
+# Copyright (C) 2020 Andrew Wilkinson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import functools
+import time
+import traceback
+from typing import List
+import sys
+
+import paho.mqtt.client as mqtt
+
+
+def on_connect(
+    topics: List[str], client: mqtt.Client, userdata, flags, reason_code, properties
+):
+    if reason_code != 0:
+        sys.stderr.write(f"Failed to connect to mqtt: {reason_code}")
+    else:
+        print("Successfully connected to mqtt broker.")
+        for topic in topics:
+            client.subscribe(topic)
+
+
+def safe_on_message(on_message):
+    def safe_on_message(client, userdata, msg):
+        try:
+            return on_message(client, userdata, msg)
+        except Exception as e:
+            sys.stderr.write(f"Exception processing message: {msg}\n")
+
+            traceback.print_exc()
+
+    return safe_on_message
+
+
+def connect(args, on_message, retry=True):
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.on_connect = functools.partial(
+        on_connect, [args.topic] if args.cloud else ["glow/+/+", "glow/+/+/+"]
+    )
+    client.on_message = safe_on_message(on_message)
+
+    client.username_pw_set(args.user, password=args.passwd)
+
+    while True:
+        try:
+            client.connect(args.mqtt, args.port, 60)
+        except Exception as e:  # pragma: no cover
+            if not retry:
+                raise
+            print(e)
+            time.sleep(10)
+        else:
+            break
+
+    client.loop_forever()
