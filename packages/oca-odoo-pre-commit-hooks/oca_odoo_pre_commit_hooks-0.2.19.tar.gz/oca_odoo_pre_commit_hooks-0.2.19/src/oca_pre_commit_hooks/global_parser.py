a96be4d57861f176f0b87bb0b268be1430b450ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:51:06 +0000

    Add package oca-odoo-pre-commit-hooks/0.2.19/oca_odoo_pre_commit_hooks-0.2.19.tar.gz

---
+++ packages/oca-odoo-pre-commit-hooks/oca_odoo_pre_commit_hooks-0.2.19.tar.gz/oca_odoo_pre_commit_hooks-0.2.19/src/oca_pre_commit_hooks/global_parser.py
@@ -0,0 +1,99 @@
+import argparse
+import configparser
+from os import environ, getcwd
+from os.path import isfile, join
+
+from oca_pre_commit_hooks.utils import top_path
+
+CONFIG_NAME = ".oca_hooks.cfg"
+MSG_CTRL = "MESSAGES_CONTROL"
+
+ENABLE_ENV_VAR = "OCA_HOOKS_ENABLE"
+DISABLE_ENV_VAR = "OCA_HOOKS_DISABLE"
+
+
+def parse_csv(comma_sep_str):
+    return set(map(str.strip, comma_sep_str.split(",")))
+
+
+class GlobalParser(argparse.ArgumentParser):
+    def __init__(self):
+        super().__init__()
+        self.add_argument(
+            "--no-verbose",
+            action="store_true",
+            default=False,
+            help="If enabled so disable verbose mode.",
+        )
+        self.add_argument(
+            "--no-exit",
+            action="store_true",
+            default=False,
+            help="If enabled so it will not call exit.",
+        )
+        self.add_argument(
+            "--disable",
+            "-d",
+            type=parse_csv,
+            default=self._default_env_csv(DISABLE_ENV_VAR),
+            help="Disable the checker with the given 'check-name', separated by commas.",
+        )
+        self.add_argument(
+            "--enable",
+            "-e",
+            type=parse_csv,
+            default=self._default_env_csv(ENABLE_ENV_VAR),
+            help=(
+                "Enable the checker with the given 'check-name', separated by commas. "
+                "Default: All checks are enabled by default"
+            ),
+        )
+        self.add_argument(
+            "--config",
+            "-c",
+            type=str,
+            help=f"Path to a configuration file (default: {CONFIG_NAME})",
+        )
+        self.add_argument(
+            "--list-msgs", default=False, action="store_true", help="List all currently enabled messages."
+        )
+        self.add_argument(
+            "--fix",
+            action="store_true",
+            dest="autofix",
+            help="Automatically fix files when possible",
+        )
+
+    @staticmethod
+    def _default_env_csv(env_var):
+        if environ.get(env_var, False):
+            return parse_csv(environ[env_var])
+
+        return set()
+
+    def parse_args(self, args=None, namespace=None):
+        res = super().parse_args(args)
+
+        if not res.config:
+            if isfile(join(getcwd(), CONFIG_NAME)):
+                res.config = join(getcwd(), CONFIG_NAME)
+            elif isfile(join(top_path(getcwd()), CONFIG_NAME)):
+                # TODO: Add unittest creating a new git repo
+                res.config = join(top_path(getcwd()), CONFIG_NAME)  # pragma: no cover
+
+        if res.config:
+            conf = configparser.ConfigParser()
+            with open(res.config, encoding="UTF-8") as f_cfg:
+                conf.read_file(f_cfg)
+
+            if conf.has_section(MSG_CTRL):
+                message_conf = conf[MSG_CTRL]
+                # --arguments takes precedence over config file
+                if not res.enable and message_conf.get("enable"):
+                    res.enable = parse_csv(message_conf.get("enable"))
+                if not res.disable and message_conf.get("disable"):
+                    res.disable = parse_csv(message_conf.get("disable"))
+
+        # Not expected/used by any other program component as of now.
+        delattr(res, "config")
+        return res
---
+++ packages/oca-odoo-pre-commit-hooks/oca_odoo_pre_commit_hooks-0.2.19.tar.gz/oca_odoo_pre_commit_hooks-0.2.19/tests/test_checks_po.py
@@ -0,0 +1,138 @@
+import glob
+import os
+import re
+import subprocess
+import sys
+from shutil import copyfile
+from tempfile import TemporaryDirectory
+
+import pytest
+
+import oca_pre_commit_hooks
+from . import common
+
+RE_CHECK_DOCSTRING = r"\* Check (?P<check>[\w|\-]+)"
+RE_CHECK_OUTPUT = r"\- \[(?P<check>[\w|-]+)\]"
+
+ALL_CHECK_CLASS = [
+    oca_pre_commit_hooks.checks_odoo_module_po.ChecksOdooModulePO,
+]
+
+EXPECTED_ERRORS = {
+    "po-duplicate-message-definition": 3,
+    "po-python-parse-format": 4,
+    "po-python-parse-printf": 2,
+    "po-requires-module": 1,
+    "po-syntax-error": 2,
+    "po-pretty-format": 6,
+    "po-duplicate-model-definition": 7,
+}
+
+
+class TestChecksPO(common.ChecksCommon):
+    @classmethod
+    def setup_class(cls):
+        super().setup_class()
+        cls.test_repo_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "test_repo")
+        po_glob_pattern = os.path.join(cls.test_repo_path, "**", "*.po")
+        pot_glob_pattern = f"{po_glob_pattern}t"
+        cls.file_paths = glob.glob(po_glob_pattern, recursive=True) + glob.glob(pot_glob_pattern, recursive=True)
+        cls.compatible_with_directories = False  # *.po files are not compatible with directoris only run with files
+
+    def setup_method(self, method):
+        super().setup_method(method)
+        self.expected_errors = EXPECTED_ERRORS.copy()
+        self.checks_run = oca_pre_commit_hooks.checks_odoo_module_po.run
+        self.checks_cli_main = oca_pre_commit_hooks.cli_po.main
+
+    @pytest.mark.parametrize("check2disable", EXPECTED_ERRORS)
+    def test_checks_disable_one_by_one_with_random_cli_env_conf(self, check2disable):
+        self._test_checks_disable_one_by_one_with_random_cli_env_conf(check2disable)
+
+    @pytest.mark.parametrize("check2enable", EXPECTED_ERRORS)
+    def test_checks_enable_one_by_one_with_random_cli_env_conf(self, check2enable):
+        self._test_checks_enable_one_by_one_with_random_cli_env_conf(check2enable)
+
+    def test_non_exists_path(self):
+        all_check_errors = self.checks_run(["/tmp/no_exists"], no_exit=True, no_verbose=False)
+        real_errors = self.get_count_code_errors(all_check_errors)
+        common.assertDictEqual(self, real_errors, {"po-syntax-error": 1})
+
+    def test_pretty_format_po(self):
+        ugly_po = os.path.join(self.test_repo_path, "eleven_module", "i18n", "ugly.po")
+        pretty_po = os.path.join(self.test_repo_path, "eleven_module", "i18n", "pretty.po")
+
+        errors = self.checks_run([pretty_po], enable={"po-pretty-format"}, no_exit=True, no_verbose=False)
+        assert not errors
+
+        errors = self.checks_run([ugly_po], enable={"po-pretty-format"}, no_exit=True, no_verbose=False)
+        real_errors = self.get_count_code_errors(errors)
+        assert "po-pretty-format" in real_errors
+
+    def test_pretty_format_po_autofix(self):
+        ugly_po = os.path.join(self.test_repo_path, "eleven_module", "i18n", "ugly.po")
+        autofix_po = os.path.join(self.test_repo_path, "eleven_module", "i18n", "autofixed_ugly.po")
+
+        with TemporaryDirectory() as tmpdir:
+            # Compatible with top_path method that only works with .git folders
+            subprocess.check_output(["git", "init", tmpdir])
+            ugly_po_cp = os.path.join(tmpdir, "ugly.po")
+            copyfile(ugly_po, ugly_po_cp)
+
+            self.checks_run([ugly_po_cp], enable={"po-pretty-format"}, no_exit=True, no_verbose=False, autofix=False)
+            with open(ugly_po_cp, encoding="utf-8") as ugly_fd, open(autofix_po, encoding="utf-8") as pretty_fd:
+                assert ugly_fd.read() != pretty_fd.read()
+
+            self.checks_run([ugly_po_cp], enable={"po-pretty-format"}, no_exit=True, no_verbose=False, autofix=True)
+            with open(ugly_po_cp, encoding="utf-8") as ugly_fd, open(autofix_po, encoding="utf-8") as pretty_fd:
+                assert ugly_fd.read() == pretty_fd.read()
+
+    @pytest.mark.skipif(not os.getenv("BUILD_README"), reason="BUILD_README environment variable not enabled")
+    def test_build_docstring(self):
+        checks_found, checks_docstring = oca_pre_commit_hooks.utils.get_checks_docstring(
+            [oca_pre_commit_hooks.checks_odoo_module_po.ChecksOdooModulePO]
+        )
+
+        readme_path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "README.md")
+        with open(readme_path, encoding="UTF-8") as f_readme:
+            readme_content = f_readme.read()
+
+        checks_docstring = f"# Checks PO\n{checks_docstring}"
+        new_readme = self.re_replace(
+            "[//]: # (start-checks-po)", "[//]: # (end-checks-po)", checks_docstring, readme_content
+        )
+
+        # Find a better way to get the --help string
+        help_content = subprocess.check_output(["oca-checks-po", "--help"], stderr=subprocess.STDOUT).decode(
+            sys.stdout.encoding
+        )
+        help_content = f"# Help PO\n```bash\n{help_content}\n```"
+        # remove extra spaces
+        help_content = re.sub(r"\n(      )+", " ", help_content)
+        help_content = re.sub(r"( )+", " ", help_content)
+        new_readme = self.re_replace("[//]: # (start-help-po)", "[//]: # (end-help-po)", help_content, new_readme)
+
+        all_check_errors = self.checks_run(sorted(self.file_paths), no_exit=True, no_verbose=False)
+        all_check_errors_by_code = self.get_grouped_errors(all_check_errors)
+
+        version = oca_pre_commit_hooks.__version__
+        check_example_content = ""
+        for code in sorted(all_check_errors_by_code):
+            check_example_content += f"\n\n * {code}\n"
+            for check_error in all_check_errors_by_code[code]:
+                msg = f"{check_error.position.filepath}"
+                if check_error.position.line:
+                    msg += f"#L{check_error.position.line}"
+                if check_error.message:
+                    msg += f" {check_error.message}"
+                check_example_content += f"\n    - https://github.com/OCA/odoo-pre-commit-hooks/blob/v{version}/{msg}"
+        check_example_content = f"# Examples PO\n{check_example_content}"
+        new_readme = self.re_replace(
+            "[//]: # (start-example-po)", "[//]: # (end-example-po)", check_example_content, new_readme
+        )
+        with open(readme_path, "w", encoding="UTF-8") as f_readme:
+            f_readme.write(new_readme)
+        assert (
+            readme_content == new_readme
+        ), "The README was updated! Don't panic only failing for CI purposes. Run the same test again."
+        assert not set(self.expected_errors) - checks_found, "Missing docstring of checks tested"
