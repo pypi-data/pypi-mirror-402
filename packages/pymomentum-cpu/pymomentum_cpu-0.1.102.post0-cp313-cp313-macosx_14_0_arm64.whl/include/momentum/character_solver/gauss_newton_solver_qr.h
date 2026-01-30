85a421e22cfda567981130a6177acc15a8931270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:36:47 +0000

    Add package pymomentum-cpu/0.1.102.post0/pymomentum_cpu-0.1.102.post0-cp313-cp313-macosx_14_0_arm64.whl

---
+++ packages/pymomentum-cpu/pymomentum_cpu-0.1.102.post0-cp313-cp313-macosx_14_0_arm64.whl/include/momentum/character_solver/gauss_newton_solver_qr.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <momentum/character/skeleton_state.h>
+#include <momentum/character_solver/fwd.h>
+#include <momentum/character_solver/skeleton_solver_function.h>
+#include <momentum/math/online_householder_qr.h>
+#include <momentum/solver/gauss_newton_solver.h>
+
+#include <Eigen/Core>
+
+namespace momentum {
+
+/// Gauss-Newton solver with QR decomposition specific options
+struct GaussNewtonSolverQROptions : GaussNewtonSolverBaseOptions {
+  GaussNewtonSolverQROptions() = default;
+
+  /* implicit */ GaussNewtonSolverQROptions(const SolverOptions& baseOptions)
+      : GaussNewtonSolverBaseOptions(baseOptions) {}
+};
+
+template <typename T>
+class GaussNewtonSolverQRT : public SolverT<T> {
+ public:
+  GaussNewtonSolverQRT(const SolverOptions& options, SkeletonSolverFunctionT<T>* solver);
+  ~GaussNewtonSolverQRT() override;
+
+  [[nodiscard]] std::string_view getName() const override;
+
+  void setOptions(const SolverOptions& options) final;
+
+ protected:
+  void doIteration() final;
+  void initializeSolver() final;
+
+ private:
+  std::unique_ptr<SkeletonStateT<T>> skeletonState_;
+  std::unique_ptr<MeshStateT<T>> meshState_;
+
+  ResizeableMatrix<T> jacobian_;
+  ResizeableMatrix<T> residual_;
+
+  OnlineHouseholderQR<T> qrSolver_;
+
+  float regularization_;
+  bool doLineSearch_;
+};
+
+} // namespace momentum
