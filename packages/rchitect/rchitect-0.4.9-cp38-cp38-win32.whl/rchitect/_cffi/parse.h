93f7a9368ede4c6603afc476882bef5ae733823a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:11 +0000

    Add package rchitect/0.4.9/rchitect-0.4.9-cp38-cp38-win32.whl

---
+++ packages/rchitect/rchitect-0.4.9-cp38-cp38-win32.whl/rchitect/_cffi/parse.h
@@ -0,0 +1,12 @@
+#ifndef PARSE_H__
+#define PARSE_H__
+
+#include "R.h"
+
+// begin cdef
+
+SEXP R_ParseVector(SEXP, int, ParseStatus *, SEXP);
+
+// end cdef
+
+#endif /* end of include guard: PARSE_H__ */
