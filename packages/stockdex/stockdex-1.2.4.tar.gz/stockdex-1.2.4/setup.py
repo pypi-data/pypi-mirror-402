3f19f34d17d7f861c8c92454824e304522fbf680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:35 +0000

    Add package stockdex/1.2.4/stockdex-1.2.4.tar.gz

---
+++ packages/stockdex/stockdex-1.2.4.tar.gz/stockdex-1.2.4/setup.py
@@ -0,0 +1,22 @@
+from __version__ import VERSION
+from setuptools import find_packages, setup
+
+setup(
+    name="stockdex",
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=open("requirements.txt").read().splitlines(),
+    python_requires=">=3.8",
+    author="Amir Nazary",
+    author_email="ah.nazary.aghchemazary@gmail.com",
+    description="A package to get stock data from Yahoo Finance",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ahnazary/stockdex",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={"stockdex": ["chromedriver_linux64/*"]},
+)
