445969c485248f8b10bdf4c2a62576a9abe1000a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:22 +0000

    Add package cleantam/0.1.0/cleantam-0.1.0.tar.gz

---
+++ packages/cleantam/cleantam-0.1.0.tar.gz/cleantam-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cleantam",
+    version="0.1.0",
+    author="Federico Gabriel Gutierrez",
+    author_email="gutierrezfedericog@gmail.com",
+    description="Una extensión de Pandas para limpiar datos con formato latinoamericano",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fedco-gtz/cleantam",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "pandas",
+    ],
+)
