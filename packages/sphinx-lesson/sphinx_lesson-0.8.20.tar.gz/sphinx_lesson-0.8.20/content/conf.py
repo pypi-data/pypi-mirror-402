60f1ab886795aa223c504c2442bbbc8a4748c3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:33:50 +0000

    Add package sphinx-lesson/0.8.20/sphinx_lesson-0.8.20.tar.gz

---
+++ packages/sphinx-lesson/sphinx_lesson-0.8.20.tar.gz/sphinx_lesson-0.8.20/content/conf.py
@@ -0,0 +1,112 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Sphinx-lesson'
+copyright = '2020-2024, CodeRefinery'
+author = 'CodeRefinery'
+
+# roundabout way to get version.  "import sphinx_lesson" would be easier, but
+# then it becomes harder to have the same github action for this repo and
+# the lessons themselves.
+version_ns = { }
+exec(open('../sphinx_lesson/_version.py').read(), version_ns)
+version = version_ns['__version__']
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+import sys
+sys.path.append('.')
+extensions = [
+    # githubpages just adds a .nojekyll file
+    'sphinx.ext.githubpages',
+    # myst_parser is not needed, because myst_nb replaces and conflicts with it
+    # (provides all functionality and more).  But, myst_parser has fewer
+    # dependencies so could be used instead.
+    #'myst_parser',
+    'sphinx_lesson',
+    #'myst_nb',  # now done as part of sphinx_lesson
+    'sphinx_rtd_theme_ext_color_contrast',
+]
+
+# Sphinx-lesson config:
+sphinx_lesson_transform_html_img = False
+
+# Settings for myst_nb:
+# https://myst-nb.readthedocs.io/en/latest/computation/execute.html#notebook-execution-modes
+#nb_execution_mode = "off"
+#nb_execution_mode = "auto"   # *only* execute if at least one output is missing.
+#nb_execution_mode = "force"
+nb_execution_mode = "cache"
+
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
+myst_enable_extensions = [
+    "colon_fence",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['README*', '_build', 'Thumbs.db', '.DS_Store',
+                    'jupyter_execute', '*venv*']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+#html_static_path = ['_static']
+
+# favicon location
+html_favicon = 'favicon.ico'
+
+# Github link in the theme
+html_context = {
+    'display_github': True,
+    'github_user': 'coderefinery',
+    'github_repo': 'sphinx-lesson',
+    'github_version': 'master',
+    'conf_py_path': '/content/',
+    }
+
+# Intersphinx mapping.  For example, with this you can use
+# :py:mod:`multiprocessing` to link straight to the Python docs of that module.
+# List all available references:
+#   python -msphinx.ext.intersphinx https://docs.python.org/3/objects.inv
+extensions.append('sphinx.ext.intersphinx')
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'sphinx': ('https://www.sphinx-doc.org/', None),
+#    #'numpy': ('https://numpy.org/doc/stable/', None),
+#    #'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
+#    #'pandas': ('https://pandas.pydata.org/docs/', None),
+#    #'matplotlib': ('https://matplotlib.org/', None),
+#    'seaborn': ('https://seaborn.pydata.org/', None),
+}
---
+++ packages/sphinx-lesson/sphinx_lesson-0.8.20.tar.gz/sphinx_lesson-0.8.20/sphinx_lesson/exerciselist.py
@@ -0,0 +1,197 @@
+import re
+
+from docutils import nodes
+from docutils.parsers.rst import Directive
+import sphinx.util
+
+# Currently only sphinx-lesson admonitions can be included. Hint to fix:
+# nodes.admonition to nodes.Admonition and then you have to examine the Python
+# object class name.
+DEFAULT_EXERCISELIST_CLASSES = {
+    'exercise',
+    'solution',
+    'challenge',   # backwards compatibility
+    'exerciselist-include',
+    }
+
+DEFAULT_EXERCISELIST_CLASSES_EXCLUDE = {
+    'exerciselist-exclude',
+    }
+
+class exerciselist(nodes.General, nodes.Element):
+    """Node for exercise list
+
+    Gets replaced with contents in the second pass.
+    """
+    include_classes = frozenset()
+    exclude_classes = frozenset()
+    def __init__(self, *args,
+                 include_classes=DEFAULT_EXERCISELIST_CLASSES,
+                 exclude_classes=DEFAULT_EXERCISELIST_CLASSES_EXCLUDE):
+        super().__init__(*args)
+        self.include_classes = set(include_classes)
+        self.exclude_classes = set(exclude_classes)
+
+class ExerciselistDirective(Directive):
+    """Run when a directive is parsed, returns the node (which is handled later)
+    """
+    option_spec = {'include': str, 'exclude': str}
+
+    def run(self):
+        kwargs = { }
+        if 'include' in self.options:
+            kwargs['include_classes'] = re.split('[ ,]+', self.options['include'])
+        if 'exclude' in self.options:
+            kwargs['exclude_classes'] = re.split('[ ,]+', self.options['exclude'])
+        el = exerciselist('', **kwargs)
+        return [el]
+
+
+def is_exercise_node(node,
+                     include_classes=DEFAULT_EXERCISELIST_CLASSES,
+                     exclude_classes=DEFAULT_EXERCISELIST_CLASSES_EXCLUDE):
+    """Should a single node be included in the exercise list?"""
+    # If not an admonition, never include
+    if not isinstance(node, nodes.admonition):
+        return False
+    # If wrong classes, exclude
+    if not hasattr(node, 'attributes'):
+        return False
+    classes = node.attributes.get('classes', ())
+    if not include_classes.intersection(classes):
+        return False
+    if exclude_classes.intersection(classes):
+        return False
+    # If parent is included, we don't need to include us.
+    # TODO: higher level parents
+    if (hasattr(node, 'parent')
+        and is_exercise_node(
+              node.parent,
+              include_classes=include_classes,
+              exclude_classes=exclude_classes,
+       )):
+        #import pdb ; pdb.set_trace()
+        return False
+    return True
+
+def find_exerciselist_nodes(app, env):
+    """Find all nodes for the exercise list, in order.
+
+    Go through all documents (in toctree order) and make a list of all
+    docnames.  Then, from those docnames, find all admonitions that
+    match certain classes.  Store this for the next round.
+    """
+
+    env = app.builder.env
+
+    # Find all docnames in toctree order.
+    docnames = [ ]
+    def process_docname(docname):
+        """Process this doc and children"""
+        if docname in docnames: # already visited
+            return
+        docnames.append(docname)
+        for docname2 in env.toctree_includes.get(docname, []):  # children
+            process_docname(docname2)
+    # Sphinx 4.0 renamed master_doc > root_doc
+    root_doc = app.config.root_doc if hasattr(app.config, 'root_doc') else app.config.master_doc
+    if root_doc not in env.toctree_includes:
+        logger = sphinx.util.logging.getLogger(__name__)
+        logger.error(f'sphinx_lesson.exerciselist could not find root doc {root_doc}')
+        return
+    process_docname(root_doc)
+
+    # The list of all the exercises will be stored here.
+    if not hasattr(env, 'sphinx_lesson_all_admonitions'):
+        env.sphinx_lesson_all_admonitions = [ ]
+    all_admonitions = env.sphinx_lesson_all_admonitions
+
+    # Now go through and collect all admonitions from these docnames, in order.
+    for docname in docnames:
+        doctree = env.get_doctree(docname)
+        for node in doctree.traverse(nodes.admonition):
+            all_admonitions.append(node)
+
+
+def process_exerciselist_nodes(app, doctree, fromdocname):
+    """Find 'exerciselist' directives and replace with the exercise list.
+    """
+    env = app.builder.env
+    # List of exercises
+    if not hasattr(env, 'sphinx_lesson_all_admonitions'):
+        env.sphinx_lesson_all_admonitions = [ ]
+    all_admonitions = env.sphinx_lesson_all_admonitions
+
+    for exerciselist_node in doctree.traverse(exerciselist):
+        content = []  # This new content in place of 'exerciselist'
+        last_docname = None
+        for exercise_node in all_admonitions:
+            # Skipp all admonitions which don't match our criteria
+            if not is_exercise_node(exercise_node, include_classes=exerciselist_node.include_classes, exclude_classes=exerciselist_node.exclude_classes):
+                continue
+            # Set title of the document.  We need to make a new section with a
+            # 'title' node for this.
+            if exercise_node.target_docname != last_docname:
+                # find the page title
+                last_docname = exercise_node.target_docname
+                doctree = env.get_doctree(exercise_node.target_docname)
+                page_title = next(iter(doctree.traverse(nodes.title)))
+                # make section with the stuff
+                section = nodes.section()
+                content.append(section)
+                section += page_title
+                slug = page_title.rawsource.replace(' ', '-').lower()
+                slug = re.sub(r'[^\w\d_-]', '', slug)
+                section['ids'].append(slug)
+
+            filename = env.doc2path(exercise_node.target_docname, base=None)
+            par = nodes.paragraph()
+
+            # Create a reference
+            newnode = nodes.reference(f'In {filename}:', f'In {filename}:')
+            newnode['refdocname'] = exercise_node.target_docname
+            newnode['refuri'] = app.builder.get_relative_uri(
+                fromdocname, exercise_node.target_docname)
+            newnode['refuri'] += '#' + exercise_node.target_id
+            par += newnode
+            section.append(par)
+            section.append(exercise_node)
+
+        exerciselist_node.replace_self(content)
+
+
+
+from sphinx.transforms import SphinxTransform
+# Find the transform priority of MystReferenceResolver and be less than it.
+# But don't fail if use this extension without myst_parser
+try:
+    from myst_parser.myst_refs import MystReferenceResolver
+    # This is 8 in 2022 (thus the default value below)
+    el_transform_priority = MystReferenceResolver.default_priority - 1
+except ImportError:
+    el_transform_priority = 8
+
+class ExerciseListTransform(SphinxTransform):
+    """Expand exerciselist nodes into the list of the exercises.
+
+    This has to run before references are resolved
+    """
+    # myst_parser.myst_refs.MystReferenceResolver is priority 9
+    default_priority = el_transform_priority
+    def apply(self):
+        doctree = self.document
+        #import IPython ; IPython.embed()
+        process_exerciselist_nodes(self.app, doctree, self.env.docname)
+
+
+
+def setup(app):
+    app.add_node(exerciselist)
+    app.add_directive('exerciselist', ExerciselistDirective)
+    app.connect('env-check-consistency', find_exerciselist_nodes)
+    app.add_post_transform(ExerciseListTransform)
+    return {
+        'version': '0.2',
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
