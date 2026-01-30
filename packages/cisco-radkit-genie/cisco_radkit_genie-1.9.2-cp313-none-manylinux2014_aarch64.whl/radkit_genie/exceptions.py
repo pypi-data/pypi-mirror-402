c45b108a74ff9f2afe4060dc523fed5379ddce28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:36:43 +0000

    Add package cisco-radkit-genie/1.9.2/cisco_radkit_genie-1.9.2-cp313-none-manylinux2014_aarch64.whl

---
+++ packages/cisco-radkit-genie/cisco_radkit_genie-1.9.2-cp313-none-manylinux2014_aarch64.whl/radkit_genie/exceptions.py
@@ -0,0 +1,11 @@
+# This file is part of RADKit / Lazy Maestro <radkit@cisco.com>
+# Copyright (c) 2018-2025 by Cisco Systems, Inc.
+# All rights reserved.
+
+
+class RADKitGenieException(Exception):
+    pass
+
+
+class RADKitGenieMissingOS(RADKitGenieException):
+    pass
